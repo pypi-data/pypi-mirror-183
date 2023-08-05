aa4c7d55f71ae5a6bc560d5ebfe8170acdc60927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:36 +0000

    Add package iToppy/0.1.1/iToppy-0.1.1-py3-none-any.whl

---
+++ packages/iToppy/iToppy-0.1.1-py3-none-any.whl/iToppy/__version__.py
@@ -0,0 +1,20 @@
+"""Module information."""
+import os
+
+# The title and description of the package
+__title__ = "iToppy"
+__description__ = """
+    Python package for the iTop CMDB/Service Management Rest API
+"""
+
+# The version and build number
+# Without specifying a unique number, you cannot overwrite packages in the PyPi repo
+# __version__ = os.getenv("RELEASE_NAME", "0.1.0" + os.getenv("GITHUB_RUN_ID", ""))
+__version__ = "0.1.1"
+# Author and license information
+__author__ = "Lars Scheibling"
+__author_email__ = "lars@scheibling.se"
+__license__ = "GnuPG 3.0"
+
+# URL to the project
+__url__ = f"https://github.com/scheiblingcopip3/{__title__}"
