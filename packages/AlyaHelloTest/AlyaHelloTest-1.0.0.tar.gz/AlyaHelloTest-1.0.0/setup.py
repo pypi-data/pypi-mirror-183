69c9f75e544118d36a0bcc627047bb6d8c3de07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:30 +0000

    Add package AlyaHelloTest/1.0.0/AlyaHelloTest-1.0.0.tar.gz

---
+++ packages/AlyaHelloTest/AlyaHelloTest-1.0.0.tar.gz/AlyaHelloTest-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages 
+
+long_description = "My Hello Test Pack"
+
+setup(
+	name="AlyaHelloTest",
+	version="1.0.0",
+	description="A sample pkg",
+	long_description= long_description,
+	long_description_content_typr="text/markdown",
+	author="Alya Developer",
+	author_email="alya.alhammadi@beamtrail.com",
+	classifiers=[
+		"Development Status :: 1 - Planning",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Build Tools", 
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3",
+		"Operating System :: Microsoft :: Windows",
+		],
+)
+
+
