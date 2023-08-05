1cedeca631586c556be613bcc67f8f57b325f898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:40 +0000

    Add package spinne/0.0.9/spinne-0.0.9.tar.gz

---
+++ packages/spinne/spinne-0.0.9.tar.gz/spinne-0.0.9/src/spinne/__main__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import spinne
+
+import main
+import index
+import css # TODO: add css support
+
+def main(name):
+    print("Creating app...")
+    if not os.path.exists(name):
+        os.mkdir(name)
+    with open(f"{name}/main.py", "w") as f:
+        f.write(main.main)
+        print("created main file...")
+    with open(f"{name}/index.py", "w") as f:
+        f.write(index.index)
+        print("created index...")
+
+try:
+    if sys.argv[1] == "--new" or "-n":
+        main(sys.argv[2])
+except IndexError:
+    print("Usage: spinne --new <name>")
