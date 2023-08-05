80de9904de25114a96772cb2b4ded9fe002159c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:32 +0000

    Add package weipyweb/0.0.2/weipyweb-0.0.2.tar.gz

---
+++ packages/weipyweb/weipyweb-0.0.2.tar.gz/weipyweb-0.0.2/weipyweb/__version__.py
@@ -0,0 +1,8 @@
+__package__ = 'weipyweb'
+__description__ = 'Beautify the original weipyweb'
+__url__ = 'https://github.com/acloudtwei'
+__version__ = "0.0.2"
+__version_info__ = (1, 5, 2, 0)
+__author__ = 'Acloudtwei'
+__author_email__ = '1559295172@qq.com'
+__license__ = 'MIT'
