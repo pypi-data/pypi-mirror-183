c180b7cce3eaea409958bf5b77c236cd14927b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:26 +0000

    Add package conjector/1.3.2/conjector-1.3.2.tar.gz

---
+++ packages/conjector/conjector-1.3.2.tar.gz/conjector-1.3.2/setup.py
@@ -0,0 +1,31 @@
+import io
+import re
+import setuptools
+
+with io.open("app_properties/__init__.py", encoding="utf-8") as f:
+    regex_result = re.search(r"__version__ = \"(.+)\"", f.read())
+    version = regex_result.group(1) if regex_result else "0.1.0"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="conjector",
+    version=version,
+    author="Yaroslav Kikvadze",
+    author_email="yaroslav.k@simporter.com",
+    description="Config injector for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yar-kik/conjector",
+    license="MIT",
+    packages=["app_properties"],
+    install_requires=["PyYAML>=6.0"],
+    python_requires=">=3.8",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
