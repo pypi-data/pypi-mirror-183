e4a12f56b99cc3e0b7ceb25277848fd9bcd45989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:14 +0000

    Add package spinne/0.0.3/spinne-0.0.3-py3-none-any.whl

---
+++ packages/spinne/spinne-0.0.3-py3-none-any.whl/spinne/__init__.py
@@ -0,0 +1,3 @@
+from .app import *
+from .views import *
+from __main__ import *
---
+++ packages/spinne/spinne-0.0.3-py3-none-any.whl/spinne/__main__.py
@@ -0,0 +1,15 @@
+import os
+
+def main(name):
+    print("Creating app...")
+    if not os.path.exists(name):
+        os.mkdir(name)
+    with open(f"{name}/main.py", "w") as f:
+        f.write(open("templates/main.py", "r").read())
+        print("created main file...")
+    with open(f"{name}/index.py", "w") as f:
+        f.write(open("templates/index.py", "r").read())
+        print("created index...")
+    with open(f"{name}/index.css", "w") as f:
+        f.write(open("templates/index.css", "r").read())
+        print("created index.css...")
