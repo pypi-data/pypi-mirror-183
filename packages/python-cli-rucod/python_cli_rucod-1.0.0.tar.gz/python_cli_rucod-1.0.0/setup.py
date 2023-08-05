41951762de6474af99ce35b2ca7a1d4cbabcabfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:42 +0000

    Add package python-cli-rucod/1.0.0/python_cli_rucod-1.0.0.tar.gz

---
+++ packages/python-cli-rucod/python_cli_rucod-1.0.0.tar.gz/python_cli_rucod-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='python_cli_rucod',
+    version='1.0.0',
+    packages=['python_cli_rucod'],
+    author='RuCodinger',
+    author_email='rucodinger@gmail.com',
+    description='Package for create cli on Python',
+    zip_safe=False
+)
