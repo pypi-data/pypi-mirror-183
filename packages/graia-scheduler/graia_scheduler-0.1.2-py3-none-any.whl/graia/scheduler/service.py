69c2744d09ead0113cef14890378db53a4a84ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:48 +0000

    Add package graia-scheduler/0.1.2/graia_scheduler-0.1.2-py3-none-any.whl

---
+++ packages/graia-scheduler/graia_scheduler-0.1.2-py3-none-any.whl/graia/scheduler/service.py
@@ -0,0 +1,37 @@
+from typing import Literal, Set
+from launart import Launchable, Launart
+from . import GraiaScheduler
+import asyncio
+
+
+class SchedulerService(Launchable):
+    """GraiaScheduler 的 Launart 服务
+
+    Args:
+        scheduler (GraiaScheduler): 任务计划器
+    """
+
+    id = "scheduler.service"
+
+    def __init__(self, scheduler: GraiaScheduler) -> None:
+        self.scheduler = scheduler
+
+    @property
+    def required(self):
+        return set()
+
+    @property
+    def stages(self) -> Set[Literal["preparing", "blocking", "cleanup"]]:
+        return {"preparing", "cleanup"}
+
+    async def launch(self, manager: Launart):
+        async with self.stage("preparing"):
+            pass  # Wait for preparation to complete, then we run tasks
+
+        tsk = asyncio.create_task(self.scheduler.run())
+
+        async with self.stage("cleanup"):
+            # Stop all schedule tasks
+            self.scheduler.stop()
+            await self.scheduler.join()
+            await tsk
