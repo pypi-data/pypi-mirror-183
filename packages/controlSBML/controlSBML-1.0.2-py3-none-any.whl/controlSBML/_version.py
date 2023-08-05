b9d81e29ba2cd3300aadb339918ba28766d25cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:37 +0000

    Add package controlSBML/1.0.2/controlSBML-1.0.2-py3-none-any.whl

---
+++ packages/controlSBML/controlSBML-1.0.2-py3-none-any.whl/controlSBML/_version.py
@@ -0,0 +1,14 @@
+"""Finds the version in pyproject.toml"""
+
+import tomli
+import os
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+DIR = os.path.dirname(DIR)
+DIR = os.path.dirname(DIR)
+PATH = os.path.join(DIR, "pyproject.toml")
+
+with open(PATH, "rb") as f:
+    data = tomli.load(f)
+
+__version__ = data["project"]["version"]
