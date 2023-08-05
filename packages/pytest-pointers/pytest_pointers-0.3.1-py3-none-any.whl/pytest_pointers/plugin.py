b64adb87746f5342e479c4f44ece357a096eeb1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:32 +0000

    Add package pytest-pointers/0.3.1/pytest_pointers-0.3.1-py3-none-any.whl

---
+++ packages/pytest-pointers/pytest_pointers-0.3.1-py3-none-any.whl/pytest_pointers/plugin.py
@@ -0,0 +1,170 @@
+from pathlib import Path
+from typing import Union
+
+import pytest
+from rich.console import Console
+
+from pytest_pointers.utils import FuncFinder, make_report, MIN_NUM_POINTERS, FuncResult
+
+CACHE_TARGETS = "pointers/targets"
+CACHE_ALL_FUNC = "pointers/funcs"
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup("pointers")
+    group.addoption(
+        "--pointers-report",
+        action="store_true",
+        dest="pointers_report",
+        default=False,
+        help="Show report in console",
+    )
+    group.addoption(
+        "--pointers-func-min-pass",
+        action="store",
+        dest="pointers_func_min_pass",
+        default=MIN_NUM_POINTERS,
+        type=int,
+        help="Minimum number of pointer marks for a unit to pass.",
+    )
+    group.addoption(
+        "--pointers-fail-under",
+        action="store",
+        dest="pointers_fail_under",
+        default=0.,
+        type=float,
+        help="Minimum percentage of units to pass (exit 0), if greater than exit 1.",
+    )
+    group.addoption(
+        "--pointers-collect",
+        dest="pointers_collect",
+        default='src',
+        help="Gather targets and tests for them",
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "pointer(element): Define a tested element.")
+
+
+def pytest_sessionstart(session: pytest.Session):
+    if session.config.option.pointers_collect or session.config.option.pointers_report:
+        session.config.cache.set(CACHE_TARGETS, {})
+
+
+@pytest.fixture(scope="function", autouse=True)
+def _pointer_marker(request):
+    if (
+        not request.config.option.pointers_collect
+        and not request.config.option.pointers_report
+    ):
+        return
+
+    pointers = request.config.cache.get(CACHE_TARGETS, {})
+    marker = request.node.get_closest_marker("pointer")
+    if marker:
+        target_full_name = None
+        if "target" in marker.kwargs:
+            # support for regular pointers
+            # @pytest.mark.pointer(target=Some.for_test)
+            target = marker.kwargs.get("target", None)
+            target_full_name = f"{target.__module__}.{target.__qualname__}"
+        elif len(marker.args) == 2:
+            # support for property pointers
+            # @pytest.mark.pointer(Some, 'for_test')
+            target_parent, target_member = marker.args
+            target_full_name = f"{target_parent.__module__}.{target_parent.__qualname__}.{target_member}"
+
+        if target_full_name not in pointers:
+            pointers[target_full_name] = []
+
+        pointers[target_full_name].append(request.node.nodeid)
+        request.config.cache.set(CACHE_TARGETS, pointers)
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtestloop(session):
+
+    # do the report here so we can give the exit code, in pytest_sessionfinish
+    # you cannot alter the exit code
+
+    # run the inner hook
+    yield
+
+    # after the runtestloop is finished we can generate the report etc.
+
+    pointers = session.config.cache.get(CACHE_TARGETS, {})
+
+    start_dir = Path(session.startdir) # noqa
+
+    # the collect option can also tell where to start within the project,
+    # otherwise it will collect a lot of wrong paths in virtualenvs etc.
+    source_dir = start_dir / session.config.option.pointers_collect
+
+    # collect all the functions by scanning the source code
+    funcs = FuncFinder(source_dir)
+
+    # collect the pass/fails for all the units
+
+    func_results = []
+    for func in funcs:
+        test_count = len(pointers.get(func, []))
+
+        is_pass = False
+
+        if test_count >= session.config.option.pointers_func_min_pass:
+            is_pass = True
+        else:
+            is_pass = False
+
+        func_results.append(
+            FuncResult(
+                name=func,
+                num_pointers=test_count,
+                is_pass=is_pass,
+            )
+        )
+
+    console = Console()
+    console.print("")
+    console.print("")
+    console.print("----------------------")
+    console.print("Pointers unit coverage")
+    console.print("========================================")
+
+    if session.config.option.pointers_report:
+
+        report = make_report(func_results)
+
+        # console.print("")
+        console.print(report)
+
+    # test whether the whole thing passed
+    PERCENT_PASS = session.config.option.pointers_fail_under
+
+    num_funcs = len(func_results)
+    total_passes = sum([
+        1 if res.is_pass else 0
+        for res
+        in func_results
+    ])
+
+    if total_passes == num_funcs:
+        percent_passes = 100.0
+    elif total_passes > 0:
+        percent_passes = (total_passes / num_funcs) * 100
+    else:
+        percent_passes = 0.
+
+    if percent_passes < PERCENT_PASS:
+        session.testsfailed = 1
+        console.print(
+            f"[bold red]Pointers unit coverage failed. Target was {PERCENT_PASS}, achieved {percent_passes}.[/bold red]"
+        )
+
+    console.print("END Pointers unit coverage")
+    console.print("========================================")
+
+# def pytest_sessionfinish(
+#     session: pytest.Session,
+#     exitstatus: Union[int, pytest.ExitCode]
+# ):
---
+++ packages/pytest-pointers/pytest_pointers-0.3.1-py3-none-any.whl/pytest_pointers/utils.py
@@ -0,0 +1,108 @@
+import sys
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Set
+from textwrap import dedent
+
+import libcst as cst
+from libcst.metadata import QualifiedNameProvider, ParentNodeProvider
+
+from rich.padding import Padding
+
+MIN_NUM_POINTERS = 2
+
+
+class MethodQualNamesCollector(cst.CSTVisitor):
+    METADATA_DEPENDENCIES = (QualifiedNameProvider, ParentNodeProvider)
+
+    def __init__(self):
+        self.found = []
+        super().__init__()
+
+    def visit_FunctionDef(self, node: cst.FunctionDef):
+        header = getattr(node.body, "header", None)
+        excluded = (
+            header is not None
+            and header.comment
+            and header.comment.value.find("notest:") > -1
+        )
+
+        if not excluded:
+            # TODO: Find better way to remove locals
+            qual_names = self.get_metadata(QualifiedNameProvider, node)
+            for qn in qual_names:
+                from_local = qn.name.find("<locals>") > -1
+                if not from_local:
+                    self.found.append(qn.name)
+
+
+@dataclass
+class FuncFinder:
+    start_dir: Path
+
+    @classmethod
+    def get_methods_qual_names(cls, node: cst.Module):
+        collector = MethodQualNamesCollector()
+        cst.MetadataWrapper(node).visit(collector)
+        for method_name in collector.found:
+            yield method_name
+
+    def get_py_files(self) -> Set[Path]:
+        py_files = [p for p in self.start_dir.glob("**/*.py")]
+        py_files = set(py_files)
+
+        for pattern in [".venv/**/*.py", "venv/**/*.py", "tests/**/*.py"]:
+            py_files = py_files - set(self.start_dir.rglob(pattern))
+
+        return py_files
+
+    def __iter__(self):
+        source_paths = [Path(p) for p in sys.path]
+
+        py_files = self.get_py_files()
+
+        for p in py_files:
+            with open(p, "r") as f:
+                tree = cst.parse_module(f.read())
+
+            source = min(set(p.parents) & set(source_paths))
+            abs_import = p.parts[len(source.parts) : -1] + (p.stem,)
+
+            for fun_name in self.get_methods_qual_names(tree):
+                yield ".".join(abs_import + (fun_name,))
+
+@dataclass
+class FuncResult:
+    name: str
+    num_pointers: int
+    is_pass: bool
+
+def make_report(func_results: list[FuncResult]):
+
+        def report_line(func_result: FuncResult):
+
+            if func_result.is_pass:
+                color = "green"
+            elif func_result.num_pointers > 0:
+                color = "blue"
+            else:
+                color = "red"
+
+            test_count_str = f"{func_result.num_pointers: <2}"
+            return f"[{color}]{test_count_str:·<5}[/{color}] {func_result.name}"
+
+        report_lines = "\n".join([
+            report_line(func_result)
+            for func_result
+            in func_results
+        ])
+
+        report = dedent(
+            f"""
+        [bold]List of functions in project and the number of tests for them[/bold]
+
+        \n{report_lines}
+        """
+        )
+
+        return Padding(report, (2, 4), expand=False)
