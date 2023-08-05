5c94aab0c98f815dc9afcc623ad98ab50e0acdd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:38 +0000

    Add package torch-hd/4.1.1/torch-hd-4.1.1.tar.gz

---
+++ packages/torch-hd/torch-hd-4.1.1.tar.gz/torch-hd-4.1.1/torchhd/datasets/__init__.py
@@ -0,0 +1,30 @@
+from torchhd.datasets.beijing_air_quality import BeijingAirQuality
+from torchhd.datasets.isolet import ISOLET
+from torchhd.datasets.european_languages import EuropeanLanguages
+from torchhd.datasets.ucihar import UCIHAR
+from torchhd.datasets.airfoil_self_noise import AirfoilSelfNoise
+from torchhd.datasets.emg_hand_gestures import EMGHandGestures
+from torchhd.datasets.pamap import PAMAP
+from torchhd.datasets.ccpp import CyclePowerPlant
+from torchhd.datasets.dataset import CollectionDataset
+from torchhd.datasets.dataset import DatasetFourFold
+from torchhd.datasets.dataset import DatasetTrainTest
+from torchhd.datasets.abalone import Abalone
+from torchhd.datasets.adult import Adult
+
+
+__all__ = [
+    "BeijingAirQuality",
+    "ISOLET",
+    "EuropeanLanguages",
+    "UCIHAR",
+    "AirfoilSelfNoise",
+    "EMGHandGestures",
+    "PAMAP",
+    "CyclePowerPlant",
+    "CollectionDataset",
+    "DatasetFourFold",
+    "DatasetTrainTest",
+    "Abalone",
+    "Adult",
+]
