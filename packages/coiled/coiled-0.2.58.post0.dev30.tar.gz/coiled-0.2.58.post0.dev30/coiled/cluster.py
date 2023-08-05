c22c2dea69193f02ee1900b55403c917d53551ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:14 +0000

    Add package coiled/0.2.58.post0.dev30/coiled-0.2.58.post0.dev30.tar.gz

---
+++ packages/coiled/coiled-0.2.58.post0.dev30.tar.gz/coiled-0.2.58.post0.dev30/coiled/cluster.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import enum
+import logging
+from typing import Generic
+
+import distributed.deploy
+from distributed.deploy.adaptive import Adaptive
+
+from .core import IsAsynchronous
+from .utils import COILED_LOGGER_NAME
+
+logger = logging.getLogger(COILED_LOGGER_NAME)
+
+
+@enum.unique
+class CredentialsPreferred(enum.Enum):
+    LOCAL = "local"
+    # USER = 'user'
+    ACCOUNT = "account"  # doesn't work, should be fixed or deprecated/removed someday
+    NONE = None
+
+
+class CoiledAdaptive(Adaptive):
+    async def scale_up(self, n):
+        logger.info(f"Adaptive scaling up to {n} workers.")
+        await self.cluster.scale_up(n)
+
+    async def scale_down(self, workers):
+        if not workers:
+            return
+        logger.info(f"Adaptive is removing {len(workers)} workers: {workers}.")
+        await self.cluster.scale_down(workers)
+
+
+class Cluster(distributed.deploy.Cluster, Generic[IsAsynchronous]):
+    """This class serves no purpose and should be deleted."""
+
+    pass
