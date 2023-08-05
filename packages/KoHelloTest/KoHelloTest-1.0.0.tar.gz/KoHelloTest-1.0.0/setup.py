6bf224e4a47a9c466789bd556c6842af49c4bbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:25 +0000

    Add package KoHelloTest/1.0.0/KoHelloTest-1.0.0.tar.gz

---
+++ packages/KoHelloTest/KoHelloTest-1.0.0.tar.gz/KoHelloTest-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+long_description = "My Hello Test Pack"
+
+setup(
+	name="KoHelloTest",
+	version="1.0.0",
+	description="A sample pkg",
+	long_description= long_description,
+	long_description_content_type= "text/markdown",
+	author= "Khaled Developer",
+	author_email="k_1_99_4@hotmail.com",
+	classifiers=[
+		"Development Status :: 1 - Planning",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Build Tools",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3",
+		"Operating System :: Microsoft :: Windows",
+		],
+)
