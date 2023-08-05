7ea0d885733a715a8b07b09ba967bd129ea72005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:34 +0000

    Add package rlbot-smh/1.0.13/rlbot_smh-1.0.13-py3-none-any.whl

---
+++ packages/rlbot-smh/rlbot_smh-1.0.13-py3-none-any.whl/rlbot_smh/match_handler.py
@@ -0,0 +1,200 @@
+import json
+import multiprocessing as mp
+import sys
+from pathlib import Path
+from threading import Thread
+from traceback import print_exc
+from typing import List
+
+from rlbot.matchconfig.match_config import Team
+from rlbot.setup_manager import RocketLeagueLauncherPreference, SetupManager
+
+from .showroom_util import (fetch_game_tick_packet, set_game_state,
+                            spawn_car_for_viewing)
+from .start_match_util import create_match_config, start_match_helper
+from .story_mode_util import add_match_result, run_challenge
+
+
+def start_match(params: List[str], sm: SetupManager, out: mp.Queue):
+    bot_list = json.loads(params[1])
+    match_settings = json.loads(params[2])
+
+    preferred_launcher = params[3]
+    use_login_tricks = bool(params[4])
+    if params[5] != "":
+        rocket_league_exe_path = Path(params[5])
+    else:
+        rocket_league_exe_path = None
+
+    start_match_helper(sm, bot_list, match_settings, RocketLeagueLauncherPreference(preferred_launcher, use_login_tricks, rocket_league_exe_path), out)
+
+
+def stop_match(sm: SetupManager):
+    if sm.has_started:
+        sm.shut_down(kill_all_pids=True)
+
+
+def fetch_gtp(sm: SetupManager):
+    print(f"-|-*|GTP {json.dumps(fetch_game_tick_packet(sm))}|*-|-", flush=True)
+
+
+def set_state(params: List[str], sm: SetupManager):
+    state = json.loads(params[1])
+    set_game_state(sm, state)
+
+
+def spawn_view_car(params: List[str], sm: SetupManager):
+    config = json.loads(params[1])
+    team = int(params[2])
+    showcase_type = params[3]
+    map_name = params[4]
+
+    preferred_launcher = params[5]
+    use_login_tricks = bool(params[6])
+    if params[7] != "":
+        rocket_league_exe_path = Path(params[7])
+    else:
+        rocket_league_exe_path = None
+
+    spawn_car_for_viewing(sm, config, team, showcase_type, map_name, RocketLeagueLauncherPreference(preferred_launcher, use_login_tricks, rocket_league_exe_path))
+
+
+def launch_challenge(params: List[str], sm: SetupManager, out: mp.Queue):
+    challenge_id = params[1]
+    city_color = json.loads(params[2])
+    team_color = json.loads(params[3])
+    upgrades = json.loads(params[4])
+    bot_list = json.loads(params[5])
+    match_settings = json.loads(params[6])
+    challenge = json.loads(params[7])
+    save_state = json.loads(params[8])
+
+    preferred_launcher = params[9]
+    use_login_tricks = bool(params[10])
+    if params[11] != "":
+        rocket_league_exe_path = Path(params[11])
+    else:
+        rocket_league_exe_path = None
+
+    match_config = create_match_config(bot_list, match_settings)
+
+    for config in match_config.player_configs:
+        if config.bot:
+            # set the team colors
+            if config.team == Team.BLUE:
+                config.loadout_config.custom_color_id = team_color
+            elif city_color is not None:
+                config.loadout_config.team_color_id = city_color
+
+    completed, results = run_challenge(sm, match_config, challenge, upgrades, RocketLeagueLauncherPreference(preferred_launcher, use_login_tricks, rocket_league_exe_path), out)
+
+    save_state = add_match_result(save_state, challenge_id, completed, results)
+
+    print(f"-|-*|STORY_RESULT {json.dumps(save_state)}|*-|-", flush=True)
+
+
+def match_handler(q: mp.Queue, out: mp.Queue):
+    sm = SetupManager()
+    online = True
+
+    while online:
+        command = q.get()
+        print(f"Received command: {command}")
+        params = command.split(" | ")
+        if len(params) == 0:
+            continue
+
+        try:
+            if params[0] == "start_match":
+                Thread(target=start_match, args=(params, sm, out)).start()
+            elif params[0] == "kill_bots":
+                stop_match(sm)
+                out.put("done")
+            elif params[0] == "shut_down":
+                print("Got shut down signal")
+                online = False
+                out.put("shut_down")
+            elif params[0] == "fetch_gtp":
+                Thread(target=fetch_gtp, args=(sm,)).start()
+            elif params[0] == "set_state":
+                Thread(target=set_state, args=(params, sm)).start()
+                out.put("done")
+            elif params[0] == "spawn_car_for_viewing":
+                Thread(target=spawn_view_car, args=(params, sm)).start()
+                out.put("done")
+            elif params[0] == "launch_challenge":
+                Thread(target=launch_challenge, args=(params, sm, out)).start()
+        except Exception:
+            print_exc()
+    stop_match(sm)
+
+
+def listen(is_raw_json=True):
+    if not is_raw_json:
+        from base64 import standard_b64decode
+        from gzip import decompress
+
+    stdin_queue = mp.Queue()
+    out_queue = mp.Queue()
+    match_handler_thread = mp.Process(target=match_handler, args=(stdin_queue, out_queue))
+    match_handler_thread.start()
+
+    online = True
+    while online:
+        try:
+            line = sys.stdin.readline()
+
+            if not is_raw_json:
+                # if the command is not raw json, then it must be decoded
+                line = decompress(standard_b64decode(line)).decode("utf-8")
+
+            stdin_queue.put(str(line))
+            if out_queue.get() == "shut_down":
+                online = False
+        except BaseException:
+            stdin_queue.put("shut_down | ")
+            online = False
+
+    print("Closing...")
+    match_handler_thread.join(timeout=60)
+    if match_handler_thread.is_alive():
+        print("Match handler thread is still alive after 60 seconds, killing it")
+        match_handler_thread.terminate()
+
+    exit()
+
+def from_file(file_path: str, is_raw_json: bool=True):
+    from time import sleep
+
+    if not is_raw_json:
+        from base64 import standard_b64decode
+        from gzip import decompress
+
+    # do listen but instead of reading from sys.stdin, read from a file as set by the first argument
+    # when CTRL+C is pressed, treat it as if "shut_down | " was read from stdin
+
+    stdin_queue = mp.Queue()
+    out_queue = mp.Queue()
+    match_handler_thread = mp.Process(target=match_handler, args=(stdin_queue, out_queue))
+    match_handler_thread.start()
+
+    with open(file_path + ".txt", "r") as f:
+        command = f.readline()
+
+    if not is_raw_json:
+        command = decompress(standard_b64decode(command)).decode("utf-8")
+
+    stdin_queue.put(str(command))
+
+    # wait for an error (e.x. KeyboardInterrupt or CTRL+C)
+    try:
+        while True:
+            sleep(1)
+    except BaseException:
+        stdin_queue.put("shut_down | ")
+
+    print("Closing...")
+    match_handler_thread.join(timeout=60)
+    if match_handler_thread.is_alive():
+        print("Match handler thread is still alive after 60 seconds, killing it")
+        match_handler_thread.terminate()
---
+++ packages/rlbot-smh/rlbot_smh-1.0.13-py3-none-any.whl/rlbot_smh/start_match_util.py
@@ -0,0 +1,154 @@
+from time import sleep
+from traceback import print_exc
+from typing import List, Optional
+import multiprocessing as mp
+
+from rlbot.matchconfig.match_config import (MatchConfig, MutatorConfig,
+                                            PlayerConfig, ScriptConfig)
+from rlbot.matchconfig.psyonix_config import set_random_psyonix_bot_preset
+from rlbot.parsing.agent_config_parser import load_bot_appearance
+from rlbot.parsing.bot_config_bundle import get_bot_config_bundle
+from rlbot.parsing.incrementing_integer import IncrementingInteger
+from rlbot.setup_manager import RocketLeagueLauncherPreference, SetupManager
+from rlbot.utils import logging_utils
+
+from .custom_map_util import identify_map_directory, prepare_custom_map
+
+logger = logging_utils.get_logger("match_handler")
+
+
+def create_player_config(bot: dict, human_index_tracker: IncrementingInteger):
+    player_config = PlayerConfig()
+    player_config.bot = bot['runnable_type'] in ('rlbot', 'psyonix')
+    player_config.rlbot_controlled = bot['runnable_type'] in ('rlbot', 'party_member_bot')
+    player_config.bot_skill = bot['skill']
+    player_config.human_index = 0 if player_config.bot else human_index_tracker.increment()
+    player_config.name = bot['name']
+    player_config.team = int(bot['team'])
+
+    if 'path' in bot and bot['path']:
+        bot_path = bot['path']
+        player_config.config_path = bot_path
+        config = get_bot_config_bundle(bot_path)
+        loadout = load_bot_appearance(config.get_looks_config(), player_config.team)
+        player_config.loadout_config = loadout
+    elif player_config.bot and not player_config.rlbot_controlled:
+        set_random_psyonix_bot_preset(player_config)
+
+    return player_config
+
+
+def create_script_config(script):
+    return ScriptConfig(script['path'])
+
+
+def setup_match(
+    setup_manager: SetupManager, match_config: MatchConfig, launcher_pref: RocketLeagueLauncherPreference, out: Optional[mp.Queue] = None
+):
+    """Starts the match and bots. Also detects and handles custom maps"""
+
+    def do_setup():
+        setup_manager.early_start_seconds = 5
+        setup_manager.connect_to_game(launcher_preference=launcher_pref)
+
+        # Loading the setup manager's game interface just as a quick fix because story mode uses it. Ideally story mode
+        # should now make its own game interface to use.
+        setup_manager.game_interface.load_interface(wants_ball_predictions=False, wants_quick_chat=False, wants_game_messages=False)
+        setup_manager.load_match_config(match_config)
+        setup_manager.launch_early_start_bot_processes()
+        setup_manager.start_match()
+        setup_manager.launch_bot_processes()
+
+        if out is not None:
+            out.put("done")
+
+        logger.info("Waiting to recieve metadata from all bots...")
+
+        times_waited = 0
+        # wait for all metadata, or for 10 seconds
+        while not setup_manager.has_received_metadata_from_all_bots() and times_waited < 40:
+            if times_waited != 0:
+                expected_metadata = sum(1 for player in setup_manager.match_config.player_configs if player.rlbot_controlled)
+                needed_metadata = expected_metadata - setup_manager.num_metadata_received
+                logger.info(f"Waiting for metadata from {needed_metadata} bot{'s' if needed_metadata > 1 else ''}...")
+                sleep(0.25)
+            times_waited += 1
+            setup_manager.try_recieve_agent_metadata()
+
+        if not setup_manager.has_received_metadata_from_all_bots():
+            expected_metadata = sum(1 for player in setup_manager.match_config.player_configs if player.rlbot_controlled)
+            logger.warning(f"Did not receive metadata from all bots. Expected {expected_metadata} but only got {setup_manager.num_metadata_received}")
+
+    map_file = match_config.game_map
+    if map_file.endswith('.upk') or map_file.endswith('.udk'):
+        rl_directory = identify_map_directory(launcher_pref)
+
+        if not rl_directory:
+            raise Exception("Couldn't find path to Rocket League maps folder")
+
+        with prepare_custom_map(map_file, rl_directory) as (map_file, metadata):
+            match_config.game_map = map_file
+            if "config_path" in metadata:
+                config_path = metadata["config_path"]
+                match_config.script_configs.append(
+                    create_script_config({'path': config_path}))
+                logger.info(f"Will load custom script for map {config_path}")
+            do_setup()
+    else:
+        do_setup()
+
+
+def create_match_config(bot_list: List[dict], match_settings: dict) -> MatchConfig:
+    match_config = MatchConfig()
+    match_config.game_mode = match_settings['game_mode']
+    match_config.game_map = match_settings['map']
+    match_config.skip_replays = match_settings['skip_replays']
+    match_config.instant_start = match_settings['instant_start']
+    match_config.enable_lockstep = match_settings['enable_lockstep']
+    match_config.enable_rendering = match_settings['enable_rendering']
+    match_config.enable_state_setting = match_settings['enable_state_setting']
+    match_config.auto_save_replay = match_settings['auto_save_replay']
+    match_config.existing_match_behavior = match_settings['match_behavior']
+    match_config.mutators = MutatorConfig()
+
+    mutators = match_settings['mutators']
+    match_config.mutators.match_length = mutators['match_length']
+    match_config.mutators.max_score = mutators['max_score']
+    match_config.mutators.overtime = mutators['overtime']
+    match_config.mutators.series_length = mutators['series_length']
+    match_config.mutators.game_speed = mutators['game_speed']
+    match_config.mutators.ball_max_speed = mutators['ball_max_speed']
+    match_config.mutators.ball_type = mutators['ball_type']
+    match_config.mutators.ball_weight = mutators['ball_weight']
+    match_config.mutators.ball_size = mutators['ball_size']
+    match_config.mutators.ball_bounciness = mutators['ball_bounciness']
+    match_config.mutators.boost_amount = mutators['boost_amount']
+    match_config.mutators.rumble = mutators['rumble']
+    match_config.mutators.boost_strength = mutators['boost_strength']
+    match_config.mutators.gravity = mutators['gravity']
+    match_config.mutators.demolish = mutators['demolish']
+    match_config.mutators.respawn_time = mutators['respawn_time']
+
+    human_index_tracker = IncrementingInteger(0)
+    match_config.player_configs = [create_player_config(bot, human_index_tracker) for bot in bot_list]
+    match_config.script_configs = [create_script_config(script) for script in match_settings['scripts']]
+
+    return match_config
+
+
+def start_match_wrapper(sm: SetupManager, match_config: MatchConfig, launcher_prefs: RocketLeagueLauncherPreference, out: Optional[mp.Queue] = None):
+    logger.info(f"Launcher preferences: {launcher_prefs}")
+
+    # these fancy prints will not get printed to the console
+    # the Rust port of the RLBotGUI will capture it and fire a tauri event
+
+    try:
+        setup_match(sm, match_config, launcher_prefs, out)
+        print("-|-*|MATCH STARTED|*-|-", flush=True)
+    except Exception:
+        print_exc()
+        print("-|-*|MATCH START FAILED|*-|-", flush=True)
+
+
+def start_match_helper(sm: SetupManager, bot_list: List[dict], match_settings: dict, launcher_prefs: RocketLeagueLauncherPreference, out: Optional[mp.Queue] = None):
+    start_match_wrapper(sm, create_match_config(bot_list, match_settings), launcher_prefs, out)
