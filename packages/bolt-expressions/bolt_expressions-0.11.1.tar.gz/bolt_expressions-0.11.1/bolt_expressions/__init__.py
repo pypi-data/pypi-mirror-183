830acd455fdbbd544063a811520d7ab9833054ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:54 +0000

    Add package bolt-expressions/0.11.1/bolt_expressions-0.11.1.tar.gz

---
+++ packages/bolt-expressions/bolt_expressions-0.11.1.tar.gz/bolt_expressions-0.11.1/bolt_expressions/__init__.py
@@ -0,0 +1,21 @@
+__version__ = "0.11.1"
+
+from .api import Data as _Data
+from .api import Expression as _Expression
+from .api import Scoreboard as _Scoreboard
+from .plugin import beet_default
+
+__all__ = ["beet_default", "Data", "Expression", "Scoreboard"]
+
+
+def __monkey_patch():
+    from nbtlib import Compound, List
+
+    def __patched_hash(self):
+        return hash(str(self))
+
+    Compound.__hash__ = __patched_hash
+    List.__hash__ = __patched_hash
+
+
+__monkey_patch()
