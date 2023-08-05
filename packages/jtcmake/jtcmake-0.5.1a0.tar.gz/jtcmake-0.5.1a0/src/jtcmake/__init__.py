f88adee2d2429a3b29211e4f94dc0d0fa3f188d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:12 +0000

    Add package jtcmake/0.5.1a0/jtcmake-0.5.1a0.tar.gz

---
+++ packages/jtcmake/jtcmake-0.5.1a0.tar.gz/jtcmake-0.5.1a0/src/jtcmake/__init__.py
@@ -0,0 +1,43 @@
+from .group_tree.groups import (
+    UntypedGroup,
+    StaticGroupBase,
+    GroupsGroup,
+    RulesGroup,
+)
+from .group_tree.core import make
+from .group_tree.atom import Atom, Mem, Memstr, Memnone
+from .group_tree.rule import Rule, SELF
+from .group_tree.file import File, VFile, IFile
+from .group_tree.tools.graphviz import print_graphviz
+from .group_tree.tools.mermaid import print_mermaid
+from .group_tree.tools.misc import (
+    print_method,
+    print_dirtree,
+    stringify_dirtree,
+)
+from .core.make import MakeSummary
+
+VERSION = "0.5.1a0"  # Automatically set by hatch
+
+__all__ = [
+    "SELF",
+    "UntypedGroup",
+    "StaticGroupBase",
+    "RulesGroup",
+    "GroupsGroup",
+    "make",
+    "Atom",
+    "Mem",
+    "Memstr",
+    "Memnone",
+    "File",
+    "VFile",
+    "IFile",
+    "print_graphviz",
+    "print_mermaid",
+    "print_method",
+    "MakeSummary",
+    "Rule",
+    "print_dirtree",
+    "stringify_dirtree",
+]
