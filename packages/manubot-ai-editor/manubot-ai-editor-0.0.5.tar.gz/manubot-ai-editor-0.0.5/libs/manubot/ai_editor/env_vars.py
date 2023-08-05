71fd664a2bbc9cac6b5e6e4c07030b4abcb67789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:30 +0000

    Add package manubot-ai-editor/0.0.5/manubot-ai-editor-0.0.5.tar.gz

---
+++ packages/manubot-ai-editor/manubot-ai-editor-0.0.5.tar.gz/manubot-ai-editor-0.0.5/libs/manubot/ai_editor/env_vars.py
@@ -0,0 +1,8 @@
+"""
+This file contains environment variables names used by manubot-ai-editor
+package.
+"""
+
+OPENAI_API_KEY = "OPENAI_API_KEY"
+LANGUAGE_MODEL = "AI_EDITOR_LANGUAGE_MODEL"
+MAX_TOKENS_PER_REQUEST = "AI_EDITOR_MAX_TOKENS_PER_REQUEST"
