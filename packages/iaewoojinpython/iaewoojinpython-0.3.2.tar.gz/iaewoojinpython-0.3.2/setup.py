62747590478767b803ba45be39e129ac6adb3f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:02 +0000

    Add package iaewoojinpython/0.3.2/iaewoojinpython-0.3.2.tar.gz

---
+++ packages/iaewoojinpython/iaewoojinpython-0.3.2.tar.gz/iaewoojinpython-0.3.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="iaewoojinpython", # Replace with your own username
+    py_modules=['iaewoojinmysql'],
+    version="0.3.2",
+    author="Woojin Cho",
+    author_email="wooju_1@iae.re.kr",
+    description="pymysql Simple used",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IAEWoojinCho",
+    #packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
