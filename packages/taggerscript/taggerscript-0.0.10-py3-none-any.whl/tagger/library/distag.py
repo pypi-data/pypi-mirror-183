de093988ccb69dd1194c5089f69a15adc47481c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package taggerscript/0.0.10/taggerscript-0.0.10-py3-none-any.whl

---
+++ packages/taggerscript/taggerscript-0.0.10-py3-none-any.whl/tagger/library/distag.py
@@ -0,0 +1,109 @@
+import discord
+from discord.ext import commands
+
+from tagger.visitor.obj_builtin import *
+
+class UserBase(TagBase):
+    NAME = 'User'
+
+class User(TagInstance):
+    
+    def __init__(self, user: discord.User | discord.Member) -> None:
+        super().__init__()
+        self.user = user
+        self.set_type(user_)
+        
+        self.field['name'] = TagString(user.name)
+        self.field['id'] = TagInteger(user.id)
+        self.field['avatar'] = TagString(user.display_avatar.url)
+        self.field['bot'] = TagInteger(int(user.bot))
+
+class MessageBase(TagBase):
+    NAME = 'Message'
+
+class Message(TagInstance):
+    
+    def __init__(self, msg: discord.Message) -> None:
+        super().__init__()
+        self.msg = msg
+        self.set_type(message)
+        
+        self.field['author'] = User(self.msg.author)
+        self.field['id'] = TagInteger(self.msg.id)
+        self.field['content'] = TagString(self.msg.content)
+
+class ContextTagBase(TagBase):
+    NAME = 'MessageTag'
+
+class ContextTag(TagInstance):
+    
+    def __init__(self, bot: commands.Bot, message: discord.Message, ctx: commands.Context) -> None:
+        super().__init__()
+        self.msg = message
+        self.bot = bot
+        self.ctx = ctx
+        self.set_type(messagetag)
+        
+        self.field['user'] = User(self.ctx.message.author)
+        self.field['message'] = Message(message)
+        
+        add_Amethod(self, 'set', _ctx_set)
+        add_Amethod(self, 'add', _ctx_add)
+        add_Amethod(self, 'wait_for_react', _ctx_waitfor_reaction)
+        add_Amethod(self, 'wait_for_reply', _ctx_waitfor_reply)
+
+messagetag = ContextTagBase()
+user_ = UserBase()
+message = MessageBase()
+
+async def get_this_msg(env: Enviroment) -> ContextTag:
+    return typing.cast(ContextTag, env.get('this'))
+
+async def _ctx_set(args: TagArg, kwargs: Kargs, env: Enviroment) -> TagInstance:
+    this = await get_this_msg(env)
+    
+    try:
+        await this.msg.edit(content=(' '.join([arg.__repr__() for arg in args] or ['``````'])))
+    except Exception as e:
+        return make_error('DisTagError', e.args[0].__str__())
+    
+    return tag_null
+
+async def _ctx_add(args: TagArg, kwargs: Kargs, env: Enviroment):
+    this = await get_this_msg(env)
+    
+    try:
+        await this.msg.edit(content=(this.msg.content + (' '.join([arg.__repr__() for arg in args] or ['']))))
+    except Exception as e:
+        return make_error('DisTagError', e.args[0].__str__())
+    
+    return tag_null
+
+async def _ctx_waitfor_reaction(args: TagArg, kwargs: Kargs, env: Enviroment):
+    this = await get_this_msg(env)
+    
+    success, er = expect('MessageTag.wait_for_react', [TagBaseString], {}, args, kwargs)
+    if not success:
+        return er
+    
+    t = tstring(args[0])
+    
+    r: discord.Reaction | None = this.bot.wait_for('reaction_add', check=lambda r: r.emoji.name == t, timeout=100)
+    
+    if r is None:
+        return tag_null
+    
+    v: list[TagInstance] = []
+    async for u in r.users():
+        v.append(User(u))
+    return TagArray(v)
+
+async def _ctx_waitfor_reply(args: TagArg, kwargs: Kargs, env: Enviroment):
+    this = await get_this_msg(env)
+    
+    r: discord.Message = this.bot.wait_for('message', check=lambda m: this.bot in m.mentions, timeout=100)
+    
+    if r is None:
+        return tag_null
+    
+    return Message(r)
---
+++ packages/taggerscript/taggerscript-0.0.10-py3-none-any.whl/tagger/parser/parser.py
@@ -0,0 +1,139 @@
+from lark import Lark, Transformer, Token, Tree
+from lark.exceptions import UnexpectedCharacters, UnexpectedEOF, UnexpectedToken
+from tagger.core.node import *
+from tagger.core.error import Error
+
+tparser = Lark(open('tagger/parser/tagger.lark'))
+
+def tparse(text: str):
+    try:
+        return tparser.parse(text)
+    except UnexpectedCharacters as e:
+        return Error('SyntaxError', 'Unexpected character', e.line, e.column)
+    except UnexpectedToken as e:
+        return Error('SyntaxError', 'Invalid Syntax', e.line, e.column)
+    except UnexpectedEOF:
+        l = text.splitlines()
+        return Error('SyntaxError', 'Unexpted End of line', len(l), len(l[-1]))
+
+Terminal = tuple[Token]
+BinOp = tuple[Node, Token, Node]
+
+class TagTransformer(Transformer):
+    
+    def start(self, t):
+        return t
+
+    def integer(self, t: Terminal):
+        (v,) = t
+        return NLiteral(v.line, v.column, int(v)) # type: ignore
+    
+    def float_(self, t: Terminal):
+        (v,) = t
+        return NLiteral(v.line, v.column, float(v)) # type: ignore
+    
+    def string(self, t: Terminal):
+        (v,) = t
+        a = str(v[1:-1])
+        _a = ''
+        _p = ''
+        for c in a:
+            if _p == '\\':
+                if c == 'n':
+                    _a += '\n'
+                elif c == 't':
+                    _a += '\t'
+                elif c == 'r':
+                    _a += '\r'
+                else:
+                    _a += c
+                _p = c
+                continue
+            _p = c
+            if c != '\\':
+                _a += c
+        return NLiteral(v.line, v.column, _a) # type: ignore
+    
+    def array(self, t):
+        b, v = t[0], t[1:]
+        return NArray(b.line, b.column, v or []) # type: ignore
+    
+    def arith(self, t: BinOp):
+        l, o, r = t
+        return NArithmetic(o.line, o.column, l, o, r) # type: ignore
+    
+    def unary(self, t: tuple[Token, Node]):
+        o, v = t
+        return NUnary(o.line, o.column, o, v) # type: ignore
+    
+    def cmp(self, t: BinOp):
+        l, o, r = t
+        return NCmp(o.line, o.column, l, o, r) # type: ignore
+    
+    def tagdo(self, t: list[Node]):
+        d, a = t[0], t[1:]
+        return NDo(d.line, d.column, a) # type: ignore
+    
+    def tagvar(self, t: tuple[Token, Node]):
+        n, v = t
+        return NSetVar(n.line, n.column, str(n), v) # type: ignore
+    
+    def getvar(self, t: Terminal):
+        (n,) = t
+        return NGetVar(n.line, n.column, str(n)) # type: ignore
+    
+    def dot(self, t: BinOp):
+        l, o, r = t
+        return NDot(o.line, o.column, l, r) # type: ignore
+    
+    def keyarg(self, t: tuple[Token, Node]):
+        return (str(t[0]), t[1])
+    
+    def call(self, t: tuple[Node, list[Node | tuple[str, Node]]]):
+        caller, arg = t
+        r = NCall(caller.l, caller.c, caller, [], {}) # type: ignore
+        for a in arg.children: # type: ignore
+            if isinstance(a, Node):
+                r.args.append(a)
+            else:
+                r.argk[a[0]] = a[1]
+        return r
+    
+    def fieldset(self, t: tuple[list[Token], Node]):
+        n, v = t[0].children, t[1] # type: ignore
+        return NFieldSet(n[0].line, n[0].column, n[0], n[1:], v) # type: ignore
+    
+    # syntantic sugar
+    def indexing(self, t: tuple[Node, Token, NLiteral]):
+        n, o, i = t
+        return NCall(o.line, o.column, NDot(o.line, o.column, n, 'index'), [i], {}) # type: ignore
+    
+    def tagelse(self, t: tuple[Token, Node]):
+        s, b = t
+        if s is None:
+            return None
+        return NElse(s.line, s.column, b)
+    
+    def tagelif(self, t: tuple[Token, Node, Node]):
+        s, c, b = t
+        if s is None:
+            return None
+        return NElif(s.line, s.column, c, b)
+    
+    def tagif(self, t: list[Token | Tree | Node]): # type: ignore
+        stmt, cond, body = t[:3]
+        elif_ = t[3:-1]
+        else_ = t[-1]
+        
+        if None in elif_:
+            elif_ = []
+        
+        return NIfElse(stmt.line, stmt.column, cond, body, elif_, else_) # type: ignore
+    
+    def tagwhile(self, t: tuple[Token, Node, Node]):
+        s, c, b = t
+        return NWhile(s.line, s.column, c, b)
+    
+    def tagfor(self, t: tuple[Token, Token, Node, Node]):
+        s, n, v, b = t
+        return NFor(s.line, s.column, str(n), v, b)
