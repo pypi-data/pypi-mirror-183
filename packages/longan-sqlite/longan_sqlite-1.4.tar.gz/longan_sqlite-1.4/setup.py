e6002a618a706886ce74b57a4eadc909b99d741e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:19 +0000

    Add package longan-sqlite/1.4/longan_sqlite-1.4.tar.gz

---
+++ packages/longan-sqlite/longan_sqlite-1.4.tar.gz/longan_sqlite-1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='longan_sqlite',
+    version='1.4',
+    author='TakeTornado',
+    author_email='cmaye@foxmail.com',
+    license='MIT License',
+    url='https://github.com/myemperor/longan-sqlite3',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+)
