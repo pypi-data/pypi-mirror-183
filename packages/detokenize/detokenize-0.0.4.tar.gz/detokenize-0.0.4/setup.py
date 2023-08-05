f7441bae1ec6427760983f629ae6c15db3262588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:21 +0000

    Add package detokenize/0.0.4/detokenize-0.0.4.tar.gz

---
+++ packages/detokenize/detokenize-0.0.4.tar.gz/detokenize-0.0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='detokenize',
+      version='0.0.4',
+      description='Detokenize stream of tokens',
+      packages=['detokenize'],
+      author_email='littinrajan@gmail.com',
+      zip_safe=False)
