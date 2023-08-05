b2ea225df5b8b4d41f094b16b5e1946eeedfdb11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:51 +0000

    Add package poethepoet/0.17.0/poethepoet-0.17.0.tar.gz

---
+++ packages/poethepoet/poethepoet-0.17.0.tar.gz/poethepoet-0.17.0/poethepoet/exceptions.py
@@ -0,0 +1,24 @@
+class PoeException(RuntimeError):
+    def __init__(self, msg, *args):
+        self.msg = msg
+        self.cause = args[0].args[0] if args else None
+        self.args = (msg, *args)
+
+
+class CyclicDependencyError(PoeException):
+    pass
+
+
+class ScriptParseError(PoeException):
+    pass
+
+
+class ExecutionError(RuntimeError):
+    def __init__(self, msg, *args):
+        self.msg = msg
+        self.cause = args[0].args[0] if args else None
+        self.args = (msg, *args)
+
+
+class PoePluginException(PoeException):
+    pass
---
+++ packages/poethepoet/poethepoet-0.17.0.tar.gz/poethepoet-0.17.0/poethepoet/task/__init__.py
@@ -0,0 +1,7 @@
+from .base import PoeTask
+from .cmd import CmdTask
+from .ref import RefTask
+from .script import ScriptTask
+from .sequence import SequenceTask
+from .shell import ShellTask
+from .switch import SwitchTask
