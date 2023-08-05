568bc8744967bc67a47f41d6662f1605384dd327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:31 +0000

    Add package robonomics-interface/1.4.0/robonomics_interface-1.4.0-py3-none-any.whl

---
+++ packages/robonomics-interface/robonomics_interface-1.4.0-py3-none-any.whl/robonomicsinterface/constants.py
@@ -0,0 +1,12 @@
+REMOTE_WS = "wss://kusama.rpc.robonomics.network"
+TYPE_REGISTRY = {
+    "types": {
+        "Record": "Vec<u8>",
+        "<T as frame_system::Config>::AccountId": "AccountId",
+        "RingBufferItem": {"type": "struct", "type_mapping": [["timestamp", "Compact<u64>"], ["payload", "Vec<u8>"]]},
+        "RingBufferIndex": {"type": "struct", "type_mapping": [["start", "Compact<u64>"], ["end", "Compact<u64>"]]},
+    }
+}
+
+W3GW = "https://crustwebsites.net"
+W3PS = "https://pin.crustcode.com"
