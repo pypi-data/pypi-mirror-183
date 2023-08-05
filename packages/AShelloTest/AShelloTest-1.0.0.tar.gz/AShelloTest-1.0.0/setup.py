134024b0c82f631e4294d3f92a7e0f9affabbc18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:26 +0000

    Add package AShelloTest/1.0.0/AShelloTest-1.0.0.tar.gz

---
+++ packages/AShelloTest/AShelloTest-1.0.0.tar.gz/AShelloTest-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+long_description = "My Hello Test Pack"
+
+setup(
+	name = "AShelloTest",
+	version = "1.0.0",
+	description = "A sample pkg",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	author = "Abdulla Alhebsi",
+	author_email = "abcde@gmail.com",
+	classifiers = [
+		"Development Status :: 1 - Planning",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Build Tools",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3",
+		"Operating System :: Microsoft :: Windows",
+		 ],
+)
