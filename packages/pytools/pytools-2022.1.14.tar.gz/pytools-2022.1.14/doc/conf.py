7aee7bd32999ad639ce17a99ee0b656be4667d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:03 +0000

    Add package pytools/2022.1.14/pytools-2022.1.14.tar.gz

---
+++ packages/pytools/pytools-2022.1.14.tar.gz/pytools-2022.1.14/doc/conf.py
@@ -0,0 +1,43 @@
+from urllib.request import urlopen
+
+_conf_url = \
+        "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
+with urlopen(_conf_url) as _inf:
+    exec(compile(_inf.read(), _conf_url, "exec"), globals())
+
+copyright = "2009-21, Andreas Kloeckner"
+author = "Andreas Kloeckner"
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+ver_dic = {}
+exec(compile(open("../pytools/version.py").read(), "../pytools/version.py", "exec"),
+        ver_dic)
+version = ".".join(str(x) for x in ver_dic["VERSION"])
+release = ver_dic["VERSION_TEXT"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This patterns also effect to html_static_path and html_extra_path
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+intersphinx_mapping = {
+    "https://docs.python.org/3": None,
+    "https://numpy.org/doc/stable": None,
+    "https://documen.tician.de/pymbolic/": None,
+    "https://documen.tician.de/loopy/": None,
+    "https://docs.pytest.org/en/stable/": None,
+    }
+
+nitpick_ignore_regex = [
+        ["py:class", r"typing_extensions\.(.+)"],
+        ]
+
+nitpicky = True
+
+autodoc_type_aliases = {"GraphT": "pytools.graph.GraphT",
+                        "NodeT": "pytools.graph.NodeT",
+                        }
