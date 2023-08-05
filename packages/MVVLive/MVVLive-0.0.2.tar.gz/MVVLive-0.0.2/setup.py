708e89734ecdd59c86d5b834c552a9ac2869a640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:21 +0000

    Add package MVVLive/0.0.2/MVVLive-0.0.2.tar.gz

---
+++ packages/MVVLive/MVVLive-0.0.2.tar.gz/MVVLive-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MVVLive",
+    packages=find_packages(include=["MVVLive"]),
+    version="0.0.2",
+    description="Live MVV data for Munich public transport",
+    author="Dario Schmid",
+    author_email="darioschmid99@outlook.com",
+    license="?",
+    install_requires=['soupsieve==2.3.2.post1', 'beautifulsoup4==4.11.1', 'requests'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+    url='https://github.com/darioschmid/PyMVVLive',
+)
