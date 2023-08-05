c96eb54a531b33ecc4e649215214dddb846b96f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:01 +0000

    Add package amora/0.1.21/amora-0.1.21-py3-none-any.whl

---
+++ packages/amora/amora-0.1.21-py3-none-any.whl/amora/dash/components/model_data_owner.py
@@ -0,0 +1,18 @@
+import dash_core_components as dcc
+from dash import html
+from dash.development.base_component import Component
+
+from amora.models import Model
+
+
+def layout(model: Model) -> Component:
+    owner = model.owner()
+    if owner:
+        content = [
+            "Owner: ",
+            dcc.Link(model.owner(), href=f"/owners/{owner}"),
+        ]
+    else:
+        content = ["Owner: Unowned"]
+
+    return html.H6(children=content, id="model-data-owner")
