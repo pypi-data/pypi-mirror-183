e625380e66250e3b9d12665a5eb502688708ac68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:26 +0000

    Add package mllp-https-gui/0.0.20/mllp_https_gui-0.0.20-py2.py3-none-any.whl

---
+++ packages/mllp-https-gui/mllp_https_gui-0.0.20-py2.py3-none-any.whl/mllp_https_gui/loading_window.py
@@ -0,0 +1,79 @@
+### Added feature: HTTPS to MLLP
+### By Tiago Rodrigues
+### Sectra Iberia, Dec 2022
+import subprocess
+import threading
+import os
+from time import sleep
+
+import PySimpleGUI as sg
+
+
+class LoadingWindow:
+    def __init__(self):
+        sg.theme('SystemDefault1')
+        # Layout
+
+        layout_header = [
+            [sg.Image(os.path.abspath('../lib/site-packages/mllp_https_gui/doc/logo.png'))],
+            [sg.Text('Tiago Rodrigues (Tiago.Rodrigues@sectra.com)\nSECTRA - CO Iberia', justification='c')],
+        ]
+
+        layout_body = [
+            [sg.Text('Loading...')],
+        ]
+
+        layout_footer = [
+            [sg.Image(os.path.abspath('../lib/site-packages/mllp_https_gui/doc/sectra.png'))],
+        ]
+
+        layout = [
+            [sg.Column(layout_header,
+                       k='layout_header',
+                       expand_x=True,
+                       element_justification='c',
+                       size=(500, 250))],
+            [sg.Column(layout_body,
+                       k='layout_body',
+                       element_justification='c')],
+            [sg.VSeparator(pad=(0, 20))],
+            [sg.Stretch(), sg.Column(layout_footer,
+                                     k='layout_footer',
+                                     element_justification='right',
+                                     vertical_alignment='b',
+                                     size=(200, 100))]
+        ]
+
+        # Window
+        self.window = sg.Window(
+            'Choose Program',
+            element_justification='c',
+            icon=os.path.abspath('../lib/site-packages/mllp_https_gui/doc/icon.ico'),
+        ).layout(layout)
+
+        # Get values from window
+        # self.button, self.values = self.window.Read()
+
+    def open(self):
+        # Open loading window
+        button, values = self.window.Read(timeout=0)
+
+        x = threading.Thread(target=self.background_tasks(), daemon=True)
+        x.start()
+
+    def background_tasks(self):
+        # Make sure that Pip is installed:
+        try:
+            # os.system('py -m pip --version')
+            subprocess.run(['py -m pip install --upgrade pip'], check=True)
+
+            # Install mllp-https
+            subprocess.run(['py -m pip install mllp-https --upgrade'], check=True)
+        except subprocess.CalledProcessError as e:
+            print('Error: ', e)
+        else:
+            print('Could not upgrade pip and mllp-https modules.')
+
+
+        sleep(1)
+        self.window.close()
