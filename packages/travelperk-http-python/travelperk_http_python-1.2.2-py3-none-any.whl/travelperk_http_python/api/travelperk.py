44fcfdd417177c7194b966d6458c7e60cb553ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:31 +0000

    Add package travelperk-http-python/1.2.2/travelperk_http_python-1.2.2-py3-none-any.whl

---
+++ packages/travelperk-http-python/travelperk_http_python-1.2.2-py3-none-any.whl/travelperk_http_python/api/travelperk.py
@@ -0,0 +1,77 @@
+from typing import List
+from .expenses_api import ExpensesAPI
+from .scim_api import SCIMAPI
+from .webhooks_api import WebhooksAPI
+from .travelsafe_api import TravelSafeAPI
+from .greenperk_api import GreenPerkAPI
+from .users_api import UsersAPI
+from .trips_api import TripsAPI
+from .cost_centers_api import CostCentersAPI
+
+# from oauth.client.client import Client
+from travelperk_http_python.client.client import Client
+
+
+class TravelPerk:
+    BASE_URL = "https://api.travelperk.com/"
+    SANDBOX_BASE_URL = "https://api.sandbox-travelperk.com/"
+
+    def __init__(self, client: Client, is_sandbox: bool):
+        self.client = client
+        self._expenses = ExpensesAPI(self)
+        self._scim = SCIMAPI(self)
+        self._webhooks = WebhooksAPI(self)
+        self._travelsafe = TravelSafeAPI(self)
+        self._greenperk = GreenPerkAPI(self)
+        self._users = UsersAPI(self)
+        self._trips = TripsAPI(self)
+        self._cost_centers = CostCentersAPI(self)
+        self.base_url = self.SANDBOX_BASE_URL if is_sandbox else self.BASE_URL
+
+    def get_auth_uri(self, target_link_uri: str) -> str:
+        # return self.client.get_auth_uri(target_link_uri)
+        return target_link_uri
+
+    def get(self, url: str) -> dict:
+        return self.client.get(self.base_url + url)
+
+    def post(self, url: str, params: List[str]) -> str:
+        return self.client.post(self.base_url + url, params)
+
+    def patch(self, url: str, params: dict) -> dict:
+        return self.client.patch(self.base_url + url, params)
+
+    def put(self, url: str, params: List[str]) -> str:
+        return self.client.put(self.base_url + url, params)
+
+    def delete(self, url: str) -> str:
+        return self.client.delete(self.base_url + url)
+
+    def set_authorization_code(self, authorization_code: str) -> "TravelPerk":
+        # self.client.set_authorization_code(authorization_code)
+        # return self
+        return authorization_code
+
+    def expenses(self) -> ExpensesAPI:
+        return self._expenses
+
+    def scim(self) -> SCIMAPI:
+        return self._scim
+
+    def webhooks(self) -> WebhooksAPI:
+        return self._webhooks
+
+    def travelsafe(self) -> TravelSafeAPI:
+        return self._travelsafe
+
+    def greenperk(self) -> GreenPerkAPI:
+        return self._greenperk
+
+    def users(self) -> UsersAPI:
+        return self._users
+
+    def trips(self) -> TripsAPI:
+        return self._trips
+
+    def cost_centers(self) -> CostCentersAPI:
+        return self._cost_centers
---
+++ packages/travelperk-http-python/travelperk_http_python-1.2.2-py3-none-any.whl/travelperk_http_python/cost_centers/bulk_update_cost_center_request.py
@@ -0,0 +1,32 @@
+from typing import List, TYPE_CHECKING
+from .bulk_update_cost_center_input_params import BulkUpdateCostCenterInputParams
+from travelperk_python_api_types.cost_centers.cost_centers.bulk_update_response import (
+    BulkUpdateResponse,
+)
+from dataclass_map_and_log.mapper import DataclassMapper
+
+if TYPE_CHECKING:
+    from api.travelperk import TravelPerk
+
+
+class BulkUpdateCostCenterRequest:
+    def __init__(self, travelperk: "TravelPerk"):
+        self.params = BulkUpdateCostCenterInputParams()
+        self.travelperk = travelperk
+
+    def save(self) -> BulkUpdateResponse:
+        return DataclassMapper.map(
+            BulkUpdateResponse,
+            self.travelperk.patch(
+                "/".join(["cost_centers", "bulk_update"]),
+                self.params.to_dict(),
+            ),
+        )
+
+    def set_ids(self, ids: List[int]) -> "BulkUpdateCostCenterRequest":
+        self.params.set_ids(ids)
+        return self
+
+    def set_archive(self, archive: bool) -> "BulkUpdateCostCenterRequest":
+        self.params.set_archive(archive)
+        return self
