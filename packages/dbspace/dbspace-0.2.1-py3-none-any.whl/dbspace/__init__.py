88afa935616fca208af348251355105b33fadd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:35 +0000

    Add package dbspace/0.2.1/dbspace-0.2.1-py3-none-any.whl

---
+++ packages/dbspace/dbspace-0.2.1-py3-none-any.whl/dbspace/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 13:44:38 2018
+
+@author: virati
+Main DBSpace module for SCCwm-DBS project
+This is the primary file with the methods needed for the rest of the libraries. Used to be called "DBS_Osc" or "DBSOsc".
+
+Copyright (C) 2018 Vineet Ravi Tiruvadi
+"""
+
+print("Using DBSpace LATEST")
