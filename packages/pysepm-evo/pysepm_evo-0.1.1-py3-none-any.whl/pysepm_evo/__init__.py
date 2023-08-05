5b60058c7b7f2e6b8070e448f0fd0c8f0f9273d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:04 +0000

    Add package pysepm-evo/0.1.1/pysepm_evo-0.1.1-py3-none-any.whl

---
+++ packages/pysepm-evo/pysepm_evo-0.1.1-py3-none-any.whl/pysepm_evo/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.1.1'
+
+
+from .qualityMeasures import fwSNRseg,SNRseg,llr,wss,composite,cepstrum_distance
+from .intelligibilityMeasures import stoi,csii,ncm
+from .reverberationMeasures import srr_seg,bsd,srmr
+
+
