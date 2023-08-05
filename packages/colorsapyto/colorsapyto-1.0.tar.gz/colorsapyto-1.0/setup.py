0b074e5088cba1ce662fdeee0c982708423b3fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:59 +0000

    Add package colorsapyto/1.0/colorsapyto-1.0.tar.gz

---
+++ packages/colorsapyto/colorsapyto-1.0.tar.gz/colorsapyto-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='colorsapyto',
+      version='1.0',
+      description='test',
+      packages=['colorsapyto'],
+      author_email='nicolastecasselecrane@gmail.com',
+      zip_safe=False)
