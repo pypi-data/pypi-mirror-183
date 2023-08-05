2f8e5d6c130d5805112f5271d8dd202f9e8803c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package django-coreplus/0.1.5/django_coreplus-0.1.5-py3-none-any.whl

---
+++ packages/django-coreplus/django_coreplus-0.1.5-py3-none-any.whl/coreplus/media/helpers.py
@@ -0,0 +1,26 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework.exceptions import ValidationError
+
+
+def validate_file_type(file, exts) -> bool:
+    """
+    Function to validate file based on the extension.
+    Input the file and a list of extenstions,
+    return True if the file type is in the list of extensions.
+    """
+    for ext in exts:
+        if file.name.lower().endswith(ext):
+            return True
+    return False
+
+
+def validate_file_type_api(file, exts):
+    """
+    Function to validate file based on the extension.
+    Input the file and a list of extenstions,
+    raise Validation Error if the file type is not in the list of extensions.
+    """
+    if validate_file_type(file, exts):
+        pass
+    else:
+        raise ValidationError(_("The file type inputted is not supported"))
