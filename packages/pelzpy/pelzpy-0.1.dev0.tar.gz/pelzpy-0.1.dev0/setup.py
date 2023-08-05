75574bd896876b079be276cfa8b720252234c6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:30 +0000

    Add package pelzpy/0.1.dev0/pelzpy-0.1.dev0.tar.gz

---
+++ packages/pelzpy/pelzpy-0.1.dev0.tar.gz/pelzpy-0.1.dev0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pelzpy',
+    version='0.1dev',
+    packages=['pelzpy', ],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
