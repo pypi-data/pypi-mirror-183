8e5d4e0913024952cc52caa4cae871f14a1898c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:26 +0000

    Add package ShHelloTest/1.0.0/ShHelloTest-1.0.0.tar.gz

---
+++ packages/ShHelloTest/ShHelloTest-1.0.0.tar.gz/ShHelloTest-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+long_description = "My Hello Test Pack"
+setup(
+	name="ShHelloTest",
+	version="1.0.0",
+	description="A sample pkg",
+	long_description= long_description,
+	author="Shaima DevOps",
+	author_email="shaima.alshouq@outlook.com",
+	classifiers=[
+		"Development Status :: 1 - Planning",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Build Tools", 
+		"Operating System :: Microsoft :: Windows",
+		],
+)
