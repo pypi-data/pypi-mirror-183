af5627998c7ed356ffa83b85dc389a394a9892ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:56 +0000

    Add package bruhcolor/0.0.33/bruhcolor-0.0.33.tar.gz

---
+++ packages/bruhcolor/bruhcolor-0.0.33.tar.gz/bruhcolor-0.0.33/bruhcolor/__init__.py
@@ -0,0 +1,13 @@
+from bruhcolor import ATTRIBUTES, COLORS, HIGHLIGHTS, RESET, bruhcolored, bruhcolorwrapper, colors, color_codes
+from collections.abc import Sequence
+
+__all__ = [
+    "ATTRIBUTES",
+    "COLORS",
+    "HIGHLIGHTS",
+    "RESET",
+    "bruhcolored",
+    "bruhcolorwrapper",
+    "colors",
+    "color_codes"
+]
