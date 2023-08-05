fd8c16f84f3177d83d227b287cb84860daee3edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:35 +0000

    Add package iaewoojinpython/0.2.2/iaewoojinpython-0.2.2.tar.gz

---
+++ packages/iaewoojinpython/iaewoojinpython-0.2.2.tar.gz/iaewoojinpython-0.2.2/iaewoojinmysql.py
@@ -0,0 +1,35 @@
+import pymysql
+import pandas as pd
+
+
+def iae_db_connect(*argv):
+    conn = pymysql.connect(host=argv[0], user=argv[1], password=argv[2], db=argv[3], charset='utf8',
+                           cursorclass=pymysql.cursors.DictCursor)
+    cur = conn.cursor()
+    if argv[3] == 'fems':
+        if len(argv) == 7:
+            sql = 'select * from ' + argv[4] + '\' where time >= \'' + argv[5] + '\' and time <= \'' + argv[6] + '\';'
+            cur.execute(sql)
+            rows = cur.fetchall()
+            conn.close()
+            return pd.DataFrame(rows)
+        else:
+            sql = 'select * from ' + argv[4] + ';'
+            cur.execute(sql)
+            rows = cur.fetchall()
+            conn.close()
+            return pd.DataFrame(rows)
+    elif argv[3] == 'vup':
+        if len(argv) == 7:
+            sql = 'select * from ' + argv[4] + ' where collected_dt >= \'' + argv[5] + '\' and collected_dt <= \'' + \
+                  argv[6] + '\';'
+            cur.execute(sql)
+            rows = cur.fetchall()
+            conn.close()
+            return pd.DataFrame(rows)
+        else:
+            sql = 'select * from ' + argv[4] + ';'
+            cur.execute(sql)
+            rows = cur.fetchall()
+            conn.close()
+            return pd.DataFrame(rows)
