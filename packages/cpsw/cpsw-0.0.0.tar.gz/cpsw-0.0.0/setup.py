f6c6625aba28f04d18c834ab339c58f0721fb025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:17 +0000

    Add package cpsw/0.0.0/cpsw-0.0.0.tar.gz

---
+++ packages/cpsw/cpsw-0.0.0.tar.gz/cpsw-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import *
+
+
+setup(
+    name="cpsw",
+    version="0.0.0",
+    description="a basic universal coding project setup.",
+    author="Villias",
+    packages=find_packages(),
+    install_requires="click"
+)
