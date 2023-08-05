9b8a9a4742bc528173dd55a805c9ed6269c08c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:09 +0000

    Add package geostatista/0.1.5/geostatista-0.1.5.tar.gz

---
+++ packages/geostatista/geostatista-0.1.5.tar.gz/geostatista-0.1.5/tests/test_distributions.py
@@ -0,0 +1,10 @@
+from typing import List
+
+import numpy as np
+
+# from matplotlib.figure import Figure
+# from statista.distributions import GEV, ConfidenceInterval, Gumbel, PlottingPosition
+
+
+def test_trial():
+    print("")
