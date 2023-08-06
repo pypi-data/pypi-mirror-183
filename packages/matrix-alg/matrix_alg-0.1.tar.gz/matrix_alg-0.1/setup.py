10561a08c17c3a0b2df5dddaf202ebf882fe1c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:37 +0000

    Add package matrix-alg/0.1/matrix_alg-0.1.tar.gz

---
+++ packages/matrix-alg/matrix_alg-0.1.tar.gz/matrix_alg-0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools 
+setuptools.setup(
+name='matrix_alg', 
+version='0.1', 
+author="Aditya Panayal",
+author_email="adpanayal@gmail.com", 
+description="Basic matrix algebra", 
+packages=setuptools.find_packages(), 
+classifiers=[ "Programming Language :: Python :: 3", 
+"License :: OSI Approved :: MIT License", 
+"Operating System :: OS Independent", ], )
