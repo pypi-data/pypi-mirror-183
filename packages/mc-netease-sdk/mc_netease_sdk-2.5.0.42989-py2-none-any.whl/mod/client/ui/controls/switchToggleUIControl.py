39bd7f8a83e705c69abc36dff5daf5530b4962af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:26:41 +0000

    Add package mc-netease-sdk/2.5.0.42989/mc_netease_sdk-2.5.0.42989-py2-none-any.whl

---
+++ packages/mc-netease-sdk/mc_netease_sdk-2.5.0.42989-py2-none-any.whl/mod/client/ui/controls/switchToggleUIControl.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from mod.client.ui.controls.baseUIControl import BaseUIControl
+
+class SwitchToggleUIControl(BaseUIControl):
+    def SetToggleState(self, is_on, toggle_path='/this_toggle'):
+        # type: (bool, str) -> None
+        """
+        设置Toggle开关控件的值
+        """
+        pass
+
