3c900427eb96fb8c197a35958d8a5b1f45a34b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:09 +0000

    Add package backhoe/0.0.1/backhoe-0.0.1.tar.gz

---
+++ packages/backhoe/backhoe-0.0.1.tar.gz/backhoe-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='backhoe',
+    version='0.0.1',
+    license='NA',
+    author="",
+    author_email='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='stub',
+    install_requires=[],
+)
