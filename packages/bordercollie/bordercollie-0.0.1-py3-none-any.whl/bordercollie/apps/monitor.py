4906fcbe2db9e8b7f138f1ea71c706d6db860b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:13 +0000

    Add package bordercollie/0.0.1/bordercollie-0.0.1-py3-none-any.whl

---
+++ packages/bordercollie/bordercollie-0.0.1-py3-none-any.whl/bordercollie/apps/monitor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from typing import Callable, Dict, List, Optional, Set, Tuple, Union
+
+import codefast as cf
+import fire
+
+from bugfinder.utils import post_to_lark
+import pandas as pd
+
+def nsqtopic(webhook: str, max_depth: int = 100):
+    """nsq topic monitor"""
+    js = cf.net.get('http://localhost:4151/stats?format=json').json()
+    messages = []
+
+    for j in js['topics']:
+        channel_list = j['channels']
+        for chan in channel_list:
+            depth = int(chan['depth'])
+            name = chan['channel_name']
+            if depth > max_depth:
+                messages.append('NSQ channel {} 当前队列长度为 {}，请留意系统状态。'.format(name, depth))
+    if messages:
+        cur_time = pd.Timestamp.now().strftime('%Y-%m-%d %H:%M:%S')
+        messages = [cur_time] + messages
+        msg = '\n'.join(messages)
+        post_to_lark(webhook, msg)
