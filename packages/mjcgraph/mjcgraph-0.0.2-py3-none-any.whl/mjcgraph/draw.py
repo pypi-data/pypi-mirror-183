722272f97cbdc016288f4d51877f27e15ec72b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:06 +0000

    Add package mjcgraph/0.0.2/mjcgraph-0.0.2-py3-none-any.whl

---
+++ packages/mjcgraph/mjcgraph-0.0.2-py3-none-any.whl/mjcgraph/draw.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+
+import sys
+import graphviz
+
+class Draw:
+    def toPNG(self, Graph, path):
+        g = graphviz.Graph()
+        g.engine = 'neato'
+        g.attr('node', label='', color='black', shape='circle', style='filled', width='0.1')
+        g.attr('edge', color='grey', penwidth='0.75')
+
+        for v in range(Graph.V):
+            g.node(str(v))
+
+        pset = set()
+        if len(path) >= 2:
+            for i in range(len(path) - 1):
+                pset.add((path[i], path[i+1]))
+                pset.add((path[i+1], path[i]))
+
+        seen = set()
+        for v, e in enumerate(Graph.G):
+            for w in e:
+                if not (w, v) in seen:
+                    if (w,v) in pset:
+                        g.edge(str(v), str(w), color='black', penwidth='2.5')
+                    else:
+                        g.edge(str(v), str(w))
+                    seen.add((v,w))
+
+
+        g.view()
