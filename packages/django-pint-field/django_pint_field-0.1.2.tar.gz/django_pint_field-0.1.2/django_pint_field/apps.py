1bdc876a4de32575ae03c3418ddbf41a73a3a400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:10 +0000

    Add package django-pint-field/0.1.2/django_pint_field-0.1.2.tar.gz

---
+++ packages/django-pint-field/django_pint_field-0.1.2.tar.gz/django_pint_field-0.1.2/django_pint_field/apps.py
@@ -0,0 +1,66 @@
+import logging
+from decimal import Decimal, getcontext
+from django.apps import AppConfig
+from django.conf import settings
+from django.core.management import call_command
+from django.db.models import Lookup
+from django.utils.translation import gettext_lazy as _
+from io import StringIO
+from psycopg2.extensions import register_adapter
+from . import (
+    get_IntegerPintDBField,
+    get_DecimalPintDBField,
+    get_BigIntegerPintDBField,
+    integer_pint_field_adapter,
+    big_integer_pint_field_adapter,
+    decimal_pint_field_adapter,
+)
+
+
+logger = logging.getLogger("watervize.entities_flags")
+
+
+def check_migrations_complete():
+    """Runs the showmigrations command, and returns false if any have not been run yet"""
+    out = StringIO()
+    call_command("showmigrations", format="plan", stdout=out)
+    out.seek(0)
+    for line in out.readlines():
+        status, name = line.rsplit(" ", 1)
+        logger.debug(f"Migration '{name}' status: {status}")
+        if "[X]" not in status:
+            logger.warning(f"Migration '{name}' incomplete")
+            return False
+    return True
+
+
+def set_decimal_precision():
+    """
+    If `DJANGO_PINT_FIELD_DECIMAL_PRECISION` is set to an int value greater than 0, the project's
+      decimalprecision will be set to that value. Otherwise, it is left as default.
+    """
+    decimal_precision = getattr(settings, "DJANGO_PINT_FIELD_DECIMAL_PRECISION", 0)
+
+    if isinstance(decimal_precision, int) and decimal_precision > 0:
+        getcontext().prec = decimal_precision
+
+
+class DjangoPintFieldAppConfig(AppConfig):
+    name = "django_pint_field"
+
+    def ready(self):
+        set_decimal_precision()
+
+        if check_migrations_complete():
+            from .lookups import get_pint_field_lookups
+
+            # e.g.: x, y = IntegerPintDBField(comparator=1, magnitude=1, units="xyz")
+            IntegerPintDBField = get_IntegerPintDBField()
+            BigIntegerPintDBField = get_BigIntegerPintDBField()
+            DecimalPintDBField = get_DecimalPintDBField()
+
+            register_adapter(IntegerPintDBField, integer_pint_field_adapter)
+            register_adapter(BigIntegerPintDBField, big_integer_pint_field_adapter)
+            register_adapter(DecimalPintDBField, decimal_pint_field_adapter)
+
+            get_pint_field_lookups()
