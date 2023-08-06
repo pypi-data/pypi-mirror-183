b8c55c221de9e548ce974e94d8e99ba80e4c628d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:52 +0000

    Add package transformers-rlfh/0.0.1.dev0/transformers-rlfh-0.0.1.dev0.tar.gz

---
+++ packages/transformers-rlfh/transformers-rlfh-0.0.1.dev0.tar.gz/transformers-rlfh-0.0.1.dev0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="transformers-rlfh",
+    version="0.0.1.dev.0",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    description="RLFH with transformers",
+    long_description="RLFH with transformers",
+    install_requires=[
+        "transformers",
+        "torch",
+        "numpy",
+        "wandb",
+        "optuna",
+        "datasets",
+        "tqdm",
+    ]
+)
