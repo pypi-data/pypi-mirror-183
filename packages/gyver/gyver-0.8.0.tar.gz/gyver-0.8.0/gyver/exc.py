202f7f16ecdf5a7bd3eab0824e09bfeefee18263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:14 +0000

    Add package gyver/0.8.0/gyver-0.8.0.tar.gz

---
+++ packages/gyver/gyver-0.8.0.tar.gz/gyver-0.8.0/gyver/exc.py
@@ -0,0 +1,22 @@
+class GyverError(Exception):
+    """Base exception for all gyver exceptions"""
+
+
+class InvalidCast(GyverError):
+    """Exception raised for config when
+    cast callable raises an error"""
+
+
+class MissingName(GyverError, KeyError):
+    """Exception raised for config when
+    name is not found in given environ"""
+
+
+class InvalidField(GyverError, KeyError):
+    """Exception raised when field lookup
+    found nothing on mapping"""
+
+
+class CacheMiss(GyverError, KeyError):
+    """Exception raised when cache lookup
+    returns None or raises error"""
