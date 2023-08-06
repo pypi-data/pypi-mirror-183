77147356cbb68ee9248897112c5ffa8dac3208ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:22:32 +0000

    Add package xwind/1.3.3/xwind-1.3.3-cp39-cp39-win_amd64.whl

---
+++ packages/xwind/xwind-1.3.3-cp39-cp39-win_amd64.whl/xwind/gis/epsg_finder.py
@@ -0,0 +1,51 @@
+import math
+from collections import namedtuple
+
+
+def cgcs2000_6deg_zone(longitude):
+    return math.ceil(longitude / 6)
+
+
+def cgcs2000_6deg_cm(longitude):
+    return cgcs2000_6deg_zone(longitude) * 6
+
+
+def cgcs2000_3deg_zone(longitude):
+    return math.ceil((longitude - 1.5) / 3)
+
+
+def cgcs2000_3deg_cm(longitude):
+    return cgcs2000_3deg_zone(longitude) * 3
+
+
+def utm_zone(longitude):
+    return math.floor((180 + longitude) / 6) + 1
+
+
+def utm_cm(longitude):
+    return -180 + utm_zone(longitude) * 6 - 3
+
+
+utm_epsg = namedtuple('utm_epsg', ['north', 'south'])
+
+
+def get_wgs84utm_epsg(longitude):
+    code = utm_zone(longitude)
+    north_code = 32600 + code
+    south_code = 32700 + code
+    return utm_epsg(north_code, south_code)
+
+
+cgcs2000_epsg = namedtuple('epsg', ['epsg_zone', 'epsg_cm'])
+
+
+def get_cgcs2000_3deg_epsg(longitude):
+    epsg_zone = 4513 + cgcs2000_3deg_zone(longitude) - 25
+    epsg_cm = epsg_zone + 21
+    return cgcs2000_epsg(epsg_zone, epsg_cm)
+
+
+def get_cgcs2000_6deg_epsg(longitude):
+    epsg_zone = 4491 + cgcs2000_6deg_zone(longitude) - 13
+    epsg_cm = epsg_zone + 11
+    return cgcs2000_epsg(epsg_zone, epsg_cm)
