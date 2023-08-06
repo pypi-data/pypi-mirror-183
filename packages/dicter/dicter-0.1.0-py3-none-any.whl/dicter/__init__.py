422fcba99311e2c604fb5b6ba37bfe0af3740d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:18 +0000

    Add package dicter/0.1.0/dicter-0.1.0-py3-none-any.whl

---
+++ packages/dicter/dicter-0.1.0-py3-none-any.whl/dicter/__init__.py
@@ -0,0 +1,38 @@
+from dicter.dicter import (
+    set_nested,
+    get_nested,
+    path,
+    flatten,
+    depth,
+    compare,
+    save,
+    load,
+    clean_filename,
+    set_logger,
+    )
+
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.0'
+
+# module level doc-string
+__doc__ = """
+dicter
+=====================================================================
+
+Description
+-----------
+dicter is for...
+
+Example
+-------
+>>> import dicter as dicter
+>>> model = dicter.fit_transform(X)
+>>> fig,ax = dicter.plot(model)
+
+References
+----------
+https://github.com/erdogant/dicter
+
+"""
