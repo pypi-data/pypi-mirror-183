0b2570567db6483a4e84e6cd2353df088d998843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:07 +0000

    Add package nonebot-plugin-ayaka-scan-cmd/0.0.3/nonebot_plugin_ayaka_scan_cmd-0.0.3.tar.gz

---
+++ packages/nonebot-plugin-ayaka-scan-cmd/nonebot_plugin_ayaka_scan_cmd-0.0.3.tar.gz/nonebot_plugin_ayaka_scan_cmd-0.0.3/nonebot_plugin_ayaka_scan_cmd.py
@@ -0,0 +1,89 @@
+'''扫描当前所有matcher，总结命令'''
+from itertools import chain
+from nonebot.matcher import matchers, Matcher
+from nonebot.rule import CommandRule, RegexRule, EndswithRule, KeywordsRule, FullmatchRule, StartswithRule, ShellCommandRule, ToMeRule
+from ayaka import AyakaBox
+
+box = AyakaBox("命令探查")
+
+
+def get_info(m: Matcher):
+    handle_names = [h.call.__name__ for h in m.handlers]
+    handle_locs = list(set(h.call.__module__ for h in m.handlers))
+    handle_docs = [h.call.__doc__ for h in m.handlers if h.call.__doc__]
+    if not handle_docs:
+        handle_docs = ["无"]
+    checker_names = []
+    for checker in m.rule.checkers:
+        call = checker.call
+        if isinstance(call, CommandRule):
+            info = "[cmds] "
+            info += "/".join(
+                repr(c)
+                for cmds in call.cmds for c in cmds
+            )
+        elif isinstance(call, ShellCommandRule):
+            info = "[shellcmd] "
+            info += "/".join(
+                repr(c)
+                for cmds in call.cmds for c in cmds
+            )
+        elif isinstance(call, KeywordsRule):
+            info = "[keywords] "
+            info += "/".join(repr(c) for c in call.keywords)
+        elif isinstance(call, RegexRule):
+            info = "[regex] "
+            info += repr(call.regex)
+        elif isinstance(call, StartswithRule):
+            info = "[startwith] "
+            info += repr(call.msg)
+        elif isinstance(call, EndswithRule):
+            info = "[endwith] "
+            info += repr(call.msg)
+        elif isinstance(call, FullmatchRule):
+            info = "[fullmatch] "
+            info += repr(call.msg)
+        elif isinstance(call, ToMeRule):
+            info = "[tome] @bot"
+        else:
+            info = "[other] 未知规则"
+        checker_names.append(info)
+    checker_names.sort()
+    info = f"[模块名称] {m.module_name}"
+    info += "\n[回调位置] " + "/".join(handle_locs)
+    info += "\n[回调名称] " + "/".join(handle_names)
+    info += "\n[可用命令]\n  " + "\n  ".join(checker_names)
+    info += "\n[回调注释] " + "\n".join(handle_docs)
+    return info
+
+
+@box.on_cmd(cmds=["scan-all"])
+async def scan_all():
+    ms = list(chain(*matchers.values()))
+    items = list(set(get_info(m) for m in ms))
+    items.sort()
+    await box.send_many(items)
+
+
+@box.on_cmd(cmds=["scan-list"])
+async def scan_list():
+    ms = list(chain(*matchers.values()))
+    names = set()
+    for m in ms:
+        names.add(m.module_name)
+        for h in m.handlers:
+            names.add(h.call.__module__)
+    names = list(names)
+    names.sort()
+    await box.send("\n".join(names))
+
+
+@box.on_cmd(cmds=["scan-search"])
+async def scan_search():
+    ms = list(chain(*matchers.values()))
+    items = [get_info(m) for m in ms]
+    items = list(set(get_info(m) for m in ms))
+    key = str(box.arg)
+    items = [item for item in items if key in item]
+    items.sort()
+    await box.send_many(items)
---
+++ packages/nonebot-plugin-ayaka-scan-cmd/nonebot_plugin_ayaka_scan_cmd-0.0.3.tar.gz/nonebot_plugin_ayaka_scan_cmd-0.0.3/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '.'}
+
+modules = \
+['nonebot_plugin_ayaka_scan_cmd']
+install_requires = \
+['nonebot-plugin-ayaka>=1.0.0b0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-ayaka-scan-cmd',
+    'version': '0.0.3',
+    'description': '扫描当前所有matcher，总结命令',
+    'long_description': '<div align="center">\n\n# 命令探查 0.0.3\n\n</div>\n\n缓解下载了新插件却不会使用的焦虑\n\n使用`scan-all`指令，让bot展示一份简陋甚至错误百出的帮助菜单\n\n使用`scan-search <name>`指令，让bot展示`<name>`相关的内容\n\n使用`scan-list`指令，让bot展示所有模块名\n\n## 效果示例\n\n以安装了`nonebot_plugin_game_collection`为例\n\n`scan-search 赛马`\n\n```\nAyaka Bot(123) 说：\n[模块名称] nonebot_plugin_game_collection\n[回调位置] nonebot_plugin_game_collection\n[回调名称] _\n[可用命令]\n  [cmds] \'创建赛马\'/\'赛马创建\'\n[回调注释] 无\n\nAyaka Bot(123) 说：\n[模块名称] nonebot_plugin_game_collection\n[回调位置] nonebot_plugin_game_collection\n[回调名称] _\n[可用命令]\n  [cmds] \'加入赛马\'/\'赛马加入\'\n[回调注释] 无\n\nAyaka Bot(123) 说：\n[模块名称] nonebot_plugin_game_collection\n[回调位置] nonebot_plugin_game_collection\n[回调名称] _\n[可用命令]\n  [cmds] \'清空赛马\'/\'赛马清空\'\n[回调注释] 无\n\nAyaka Bot(123) 说：\n[模块名称] nonebot_plugin_game_collection\n[回调位置] nonebot_plugin_game_collection\n[回调名称] _\n[可用命令]\n  [cmds] \'赛马事件重载\'\n[回调注释] 无\n\nAyaka Bot(123) 说：\n[模块名称] nonebot_plugin_game_collection\n[回调位置] nonebot_plugin_game_collection\n[回调名称] _\n[可用命令]\n  [cmds] \'赛马开始\'/\'开始赛马\'\n[回调注释] 无\n\nAyaka Bot(123) 说：\n[模块名称] nonebot_plugin_game_collection\n[回调位置] nonebot_plugin_game_collection\n[回调名称] _\n[可用命令]\n  [cmds] \'赛马暂停\'/\'暂停赛马\'\n[回调注释] 无\n\nAyaka Bot(123) 说：\n[模块名称] nonebot_plugin_game_collection\n[回调位置] nonebot_plugin_game_collection\n[回调名称] _\n[可用命令]\n  [cmds] \'赛马重置\'/\'重置赛马\'\n[回调注释] 无\n```\n\n以安装了`nonebot_plugin_ayaka_games`为例\n\n`scan-search suspect`\n\n```\nAyaka Bot(123) 说：\n[模块名称] ayaka.box\n[回调位置] ayaka_games.plugins.who_is_suspect\n[回调名称] box_entrance\n[可用命令]\n  [cmds] \'谁是卧底\'\n  [other] 未知指令\n[回调注释] 打开应用\n\nAyaka Bot(123) 说：\n[模块名称] ayaka.box\n[回调位置] ayaka_games.plugins.who_is_suspect\n[回调名称] exit_play\n[可用命令]\n  [cmds] \'exit\'/\'退出\'\n  [other] 未知指令\n[回调注释] 无\n\nAyaka Bot(123) 说：\n[模块名称] ayaka.box\n[回调位置] ayaka_games.plugins.who_is_suspect\n[回调名称] exit_room\n[可用命令]\n  [cmds] \'exit\'/\'退出\'\n  [other] 未知指令\n[回调注释] 关闭游戏\n\nAyaka Bot(123) 说：\n[模块名称] ayaka.box\n[回调位置] ayaka_games.plugins.who_is_suspect\n[回调名称] join\n[可用命令]\n  [cmds] \'join\'/\'加入\'\n  [other] 未知指令\n[回调注释] 加入房间\n\nAyaka Bot(123) 说：\n[模块名称] ayaka.box\n[回调位置] ayaka_games.plugins.who_is_suspect\n[回调名称] leave\n[可用命令]\n  [cmds] \'离开\'/\'leave\'\n  [other] 未知指令\n[回调注释] 离开房间\n\nAyaka Bot(123) 说：\n[模块名称] ayaka.box\n[回调位置] ayaka_games.plugins.who_is_suspect\n[回调名称] play_info\n[可用命令]\n  [cmds] \'信息\'/\'info\'\n  [other] 未知指令\n[回调注释] 展示投票情况\n\nAyaka Bot(123) 说：\n[模块名称] ayaka.box\n[回调位置] ayaka_games.plugins.who_is_suspect\n[回调名称] room_info\n[可用命令]\n  [cmds] \'信息\'/\'info\'\n  [other] 未知指令\n[回调注释] 展示房间内成员列表\n\nAyaka Bot(123) 说：\n[模块名称] ayaka.box\n[回调位置] ayaka_games.plugins.who_is_suspect\n[回调名称] start\n[可用命令]\n  [cmds] \'start\'/\'begin\'/\'开始\'\n  [other] 未知指令\n[回调注释] 开始游戏\n\nAyaka Bot(123) 说：\n[模块名称] ayaka.box\n[回调位置] ayaka_games.plugins.who_is_suspect\n[回调名称] vote\n[可用命令]\n  [cmds] \'vote\'/\'投票\'\n  [other] 未知指令\n[回调注释] 请at你要投票的对象，一旦投票无法更改\n```\n\n\n## 实现原理\n\n遍历`nonebot.matcher.matchers`对象，分析所有`Matcher`\n',
+    'author': 'Su',
+    'author_email': 'wxlxy316@163.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/bridgeL/nonebot-plugin-ayaka-scan-cmd',
+    'package_dir': package_dir,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
