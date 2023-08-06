63336832661d3b70a1434f0e240302fe56a59a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:10 +0000

    Add package dj-beatcloud/2.4.0b1/dj_beatcloud-2.4.0b1-py3-none-any.whl

---
+++ packages/dj-beatcloud/dj_beatcloud-2.4.0b1-py3-none-any.whl/djtools/rekordbox/__init__.py
@@ -0,0 +1,20 @@
+"""The "rekordbox" package contains modules for operating on exported Rekordbox
+databases i.e. rekordbox.xml files.
+
+Included in this package:
+    * playlist_builder.py: Automatically create a desired playlist structure
+        based on the tags present in an XML.
+    * randomize_tracks.py: Set tags of tracks in playlists sequentially
+        (after shuffling) to randomize.
+"""
+
+from djtools.rekordbox.playlist_builder import rekordbox_playlists
+from djtools.rekordbox.randomize_tracks import randomize_tracks
+
+
+REKORDBOX_OPERATIONS = {
+    "RANDOMIZE_TRACKS": randomize_tracks,
+    "REKORDBOX_PLAYLISTS": rekordbox_playlists
+}
+
+__all__ = "randomize_tracks", "REKORDBOX_OPERATIONS", "rekordbox_playlists"
