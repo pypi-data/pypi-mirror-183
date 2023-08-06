d75a919ee8048da6591ba9375669b45146a67396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:50 +0000

    Add package stoobly-agent/0.18.0/stoobly_agent-0.18.0-py3-none-any.whl

---
+++ packages/stoobly-agent/stoobly_agent-0.18.0-py3-none-any.whl/stoobly_agent/app/proxy/utils/publish_change_service.py
@@ -0,0 +1,23 @@
+from stoobly_agent.app.settings import Settings
+from stoobly_agent.lib.api.agent_api import AgentApi
+from stoobly_agent.lib.api.keys import ProjectKey
+from stoobly_agent.lib.logger import Logger
+
+# Announce that a new request has been created
+def publish_change(status: str):
+  settings = Settings.instance()
+
+  # If ui is not active, return
+  if not settings.ui.active:
+    return False
+
+  ui_url = settings.ui.url
+
+  if not ui_url:
+    Logger.instance().warn('Settings.ui.url not configured')
+    return False
+  else:
+    api: AgentApi = AgentApi(ui_url)
+    project_key = ProjectKey(settings.proxy.intercept.project_key)
+    api.update_status(status, project_key.id)
+    return True
---
+++ packages/stoobly-agent/stoobly_agent-0.18.0-py3-none-any.whl/stoobly_agent/lib/api/agent_api.py
@@ -0,0 +1,19 @@
+import requests
+
+from typing import Union
+
+class AgentApi:
+    STATUSES_ENDPOINT = '/api/v1/admin/statuses'
+
+    def __init__(self, service_url):
+        self.service_url = service_url
+
+    @property
+    def default_headers(self):
+        return {
+            'X-Do-Proxy': '1',
+        }
+
+    def update_status(self, status_id: str, project_id: Union[int, str]):
+        url = f"{self.service_url}{self.STATUSES_ENDPOINT}/{status_id}"
+        return requests.put(url, data=str(project_id), headers=self.default_headers)
