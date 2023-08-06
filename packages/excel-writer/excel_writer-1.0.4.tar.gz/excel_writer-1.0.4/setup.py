2f6252d2a56b8c2dfe1bc3229988f812f15d0c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:22:15 +0000

    Add package excel-writer/1.0.4/excel_writer-1.0.4.tar.gz

---
+++ packages/excel-writer/excel_writer-1.0.4.tar.gz/excel_writer-1.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="excel_writer",
+    version="1.0.4",
+    license='MIT',
+    description="Simple library to write excel files from Python Dictionary or Pandas DataFrame.",
+    author="EncoreSky Technologies",
+    author_email='coontact@encoresky.com',
+    url='https://github.com/encoresky/excel-writer',
+    packages=[
+        "excel_writer"
+    ],
+    include_package_data=True,
+    install_requires=[
+        "XlsxWriter==3.0.3",
+        "pandas==1.5.2",
+        "openpyxl==3.0.10",
+        "Jinja2==3.1.2"
+    ],
+    dependency_links=[
+
+    ],
+    entry_points={
+
+    }
+)
