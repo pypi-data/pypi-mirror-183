6428a786af1504b9ae3247865a06aed7c474b4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:41 +0000

    Add package manim-ml/0.0.12/manim_ml-0.0.12.tar.gz

---
+++ packages/manim-ml/manim_ml-0.0.12.tar.gz/manim_ml-0.0.12/manim_ml/neural_network/layers/__init__.py
@@ -0,0 +1,37 @@
+from manim_ml.neural_network.layers.convolutional_3d_to_feed_forward import Convolutional3DToFeedForward
+from manim_ml.neural_network.layers.image_to_convolutional3d import ImageToConvolutional3DLayer
+from .convolutional3d_to_convolutional3d import Convolutional3DToConvolutional3D
+from .convolutional2d_to_convolutional2d import Convolutional2DToConvolutional2D
+from .convolutional3d import Convolutional3DLayer
+from .convolutional2d import Convolutional2DLayer
+from .feed_forward_to_vector import FeedForwardToVector
+from .paired_query_to_feed_forward import PairedQueryToFeedForward
+from .embedding_to_feed_forward import EmbeddingToFeedForward
+from .embedding import EmbeddingLayer
+from .feed_forward_to_embedding import FeedForwardToEmbedding
+from .feed_forward_to_feed_forward import FeedForwardToFeedForward
+from .feed_forward_to_image import FeedForwardToImage
+from .feed_forward import FeedForwardLayer
+from .image_to_feed_forward import ImageToFeedForward
+from .image import ImageLayer
+from .parent_layers import ConnectiveLayer, NeuralNetworkLayer
+from .triplet import TripletLayer
+from .triplet_to_feed_forward import TripletToFeedForward
+from .paired_query import PairedQueryLayer
+from .paired_query_to_feed_forward import PairedQueryToFeedForward
+
+connective_layers_list = (
+    EmbeddingToFeedForward,
+    FeedForwardToEmbedding,
+    FeedForwardToFeedForward,
+    FeedForwardToImage,
+    ImageToFeedForward,
+    PairedQueryToFeedForward,
+    TripletToFeedForward,
+    PairedQueryToFeedForward,
+    FeedForwardToVector,
+    Convolutional3DToConvolutional3D,
+    Convolutional2DToConvolutional2D,
+    ImageToConvolutional3DLayer,
+    Convolutional3DToFeedForward
+)
