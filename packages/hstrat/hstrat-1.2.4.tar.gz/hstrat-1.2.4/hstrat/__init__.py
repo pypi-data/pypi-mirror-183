a584e91f4505383334ee8fad1021ff7afe126972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:36 +0000

    Add package hstrat/1.2.4/hstrat-1.2.4.tar.gz

---
+++ packages/hstrat/hstrat-1.2.4.tar.gz/hstrat-1.2.4/hstrat/__init__.py
@@ -0,0 +1,25 @@
+"""Top-level package for hstrat."""
+
+__author__ = """Matthew Andres Moreno"""
+__email__ = "m.more500@gmail.com"
+__version__ = "1.2.4"
+
+from . import (
+    _auxiliary_lib,
+    genome_instrumentation,
+    juxtaposition,
+    phylogenetic_inference,
+    serialization,
+    stratum_retention_strategy,
+    stratum_retention_viz,
+)
+
+__all__ = [
+    "_auxiliary_lib",
+    "genome_instrumentation",
+    "juxtaposition",
+    "phylogenetic_inference",
+    "serialization",
+    "stratum_retention_strategy",
+    "stratum_retention_viz",
+]
