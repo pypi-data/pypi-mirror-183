aba95cd3e33b7a6287b505c18d9800719daf1c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:10 +0000

    Add package django-codemirror6/1.0.1/django-codemirror6-1.0.1.tar.gz

---
+++ packages/django-codemirror6/django-codemirror6-1.0.1.tar.gz/django-codemirror6-1.0.1/src/django_codemirror6/__init__.py
@@ -0,0 +1,8 @@
+"""Django CodeMirror 6
+"""
+
+__title__ = 'django-codemirror6'
+__copyright__ = '2022-2023, Grant Jenks'
+__author__ = 'Grant Jenks'
+__version__ = '1.0.1'
+__license__ = 'Apache 2.0'
