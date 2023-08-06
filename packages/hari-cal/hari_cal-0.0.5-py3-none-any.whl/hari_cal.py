bfd6ff1e3138bb074fccd81a42a346ad8cc34d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:09 +0000

    Add package hari-cal/0.0.5/hari_cal-0.0.5-py3-none-any.whl

---
+++ packages/hari-cal/hari_cal-0.0.5-py3-none-any.whl/hari_cal.py
@@ -0,0 +1,47 @@
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a,b):
+    return a * b
+def div(a,b):
+    return a / b
+def cal():
+   
+
+
+   
+    while True:
+        
+        a = float(input("enter a value: "))
+        b = float(input("enter b value: "))
+        try:
+            optios = """
+
+            1. add
+            2. sub
+            3. mul
+            4. div
+            5. stop
+        
+            """
+            print(optios)
+            user = int(input("enter ur num :"))
+            if user == 1:
+                data = add(a,b)
+                print(data)
+            elif user == 2:
+                data = sub(a,b)
+                print(data)
+            elif user == 3:
+                data = mul(a,b)
+                print(data)
+            elif user == 4:
+                data = div(a,b)
+                print(data)
+            elif user == 5:
+                break
+        except Exception as e:
+            raise e
+
+# cal()
