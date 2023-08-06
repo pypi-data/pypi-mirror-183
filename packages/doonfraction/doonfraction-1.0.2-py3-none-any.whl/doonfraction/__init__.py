6c27f1e02f4505645bd8f02baf0f4fb8f959fd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:28 +0000

    Add package doonfraction/1.0.2/doonfraction-1.0.2-py3-none-any.whl

---
+++ packages/doonfraction/doonfraction-1.0.2-py3-none-any.whl/doonfraction/__init__.py
@@ -0,0 +1,6 @@
+
+__version__ = "1.0.0"  # current version
+
+# improved the functionality of division operator, test files and mixed fraction functionality
+
+__version__ = "1.0.2"  # updated version
