b1e13b56b886c9858d1b234e37d0967f96746600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:20 +0000

    Add package BERTSimilarWords/0.0.4/BERTSimilarWords-0.0.4.tar.gz

---
+++ packages/BERTSimilarWords/BERTSimilarWords-0.0.4.tar.gz/BERTSimilarWords-0.0.4/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='BERTSimilarWords',
+    version='0.0.4',
+    description="Find Similar Words using BERT",
+    keywords="BERT NLP similar words",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    author="Pahalavan R D",
+    author_email='rdpahalavan24@gmail.com',
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.7.0',
+    url='https://github.com/rdpahalavan/BERTSimilarWords',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements
+)
