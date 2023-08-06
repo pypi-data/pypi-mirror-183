e383cc706a71f0e3a74b3df59e06c1a017bf13a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:17 +0000

    Add package pyfindfiles/1.5.2/pyfindfiles-1.5.2-py3-none-any.whl

---
+++ packages/pyfindfiles/pyfindfiles-1.5.2-py3-none-any.whl/pyfindfiles/vid.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+import typing as T
+from pathlib import Path
+import logging
+import sys
+import subprocess
+import shutil
+
+
+def findvid(path: Path, ext: list[str]) -> T.Iterator[Path]:
+    """
+    recursive file search in Pure Python.
+    about 10 times slower than Linux find, but platform-independent.
+    """
+
+    path = Path(path).expanduser()
+
+    for e in ext:
+        for file in path.glob("**/*" + e):
+            yield file
+
+
+def findvid_gnu(path: Path, exts: list[str]) -> T.Iterator[str]:
+    """
+    recursive file search using GNU find
+    """
+    path = Path(path).expanduser()
+    if isinstance(exts, str):
+        exts = [exts]
+
+    find = shutil.which("find")
+    if not find:
+        raise FileNotFoundError('could not find "find"')
+
+    cmd = [find, str(path), "-type", "f"]
+
+    if sys.platform != "darwin":
+        cmd += ["-regextype", "posix-egrep"]
+
+    cmd += ["-iregex", r".*(" + r"|".join(exts) + r")$"]
+
+    logging.debug(" ".join(cmd))
+
+    stdout = subprocess.check_output(cmd, universal_newlines=True).strip()
+
+    for file in stdout.split("\n"):
+        yield file
