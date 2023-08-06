58dd37153e5bb4661866aa81013dfd6d77a7c2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:34 +0000

    Add package PyOnsetT/1.0.4/PyOnsetT-1.0.4-py3-none-any.whl

---
+++ packages/PyOnsetT/PyOnsetT-1.0.4-py3-none-any.whl/PyOnsetT/main.py
@@ -0,0 +1,7 @@
+def main(): 
+    print('PyOnsetT main.py')
+    print('PyOnsetT is a Python package for automatic onset temperature extractor.')
+    print('Please visit https://pypi.org/project/PyOnsetT/ for more information.')
+
+if __name__ == '__main__':
+    main()
