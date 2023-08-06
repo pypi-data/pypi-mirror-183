3682bc184e451dc75986e0ba84dd8209f56dfa8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:43 +0000

    Add package bloxflip-crash/1.0.2/bloxflip_crash-1.0.2.tar.gz

---
+++ packages/bloxflip-crash/bloxflip_crash-1.0.2.tar.gz/bloxflip_crash-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+# Setting up
+setup(
+    version = "1.0.2",
+    name="bloxflip_crash", 
+    author="",
+    author_email="",
+    packages=find_packages(),
+)
