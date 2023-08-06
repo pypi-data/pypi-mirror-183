483fb0177978f7a7e1328172ff242904ce33e951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:40 +0000

    Add package dahlia/2.2.1/dahlia-2.2.1-py3-none-any.whl

---
+++ packages/dahlia/dahlia-2.2.1-py3-none-any.whl/dahlia/__init__.py
@@ -0,0 +1,5 @@
+from .dahlia import Dahlia, Depth
+from .legacy import dahlia, dprint
+from .utils import clean, clean_ansi
+
+__all__ = ("clean", "clean_ansi", "Dahlia", "Depth", "dahlia", "dprint")
