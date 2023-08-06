a61fe3ca3cd517f89c8caa5944e49a4307b64006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:16 +0000

    Add package KhbHello/1.0.7/KhbHello-1.0.7.tar.gz

---
+++ packages/KhbHello/KhbHello-1.0.7.tar.gz/KhbHello-1.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+long_description = "My Hello Test Pack"
+
+setup(
+	name="KhbHello",
+	version="1.0.7",
+	description="A sample pkg",
+	long_description= long_description,
+	long_description_content_type="text/markdown",
+	author="Khobeib Developer",
+	author_email="khobeib.moahmmed@gmail.com",
+    packages=['KhbHello'],
+	classifiers=[
+		"Development Status :: 1 - Planning",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Build Tools",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3",
+		"Operating System :: Microsoft :: Windows",
+		],
+)
