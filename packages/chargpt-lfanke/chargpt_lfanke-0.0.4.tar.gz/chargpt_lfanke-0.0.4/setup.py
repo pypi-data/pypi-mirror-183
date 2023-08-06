1f90bcd3fc3d252980c911b339e8b4fe0151b933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:08 +0000

    Add package chargpt-lfanke/0.0.4/chargpt_lfanke-0.0.4.tar.gz

---
+++ packages/chargpt-lfanke/chargpt_lfanke-0.0.4.tar.gz/chargpt_lfanke-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+      name='chargpt_lfanke',
+      version='0.0.4',
+      description='from charget_lfanke import Session',
+      author='Lfan_ke',
+      author_email='chengkelfan@qq.com',
+      requires= ['requests','openai'], # 定义依赖哪些模块
+      packages=['chargpt_lfanke'],
+      license="apache 3.0"
+      )
+
