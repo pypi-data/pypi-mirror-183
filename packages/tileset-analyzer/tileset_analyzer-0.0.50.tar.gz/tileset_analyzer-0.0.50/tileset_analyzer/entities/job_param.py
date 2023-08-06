8df5e2c4c059a0f56416df3c6fe40dfbb754f4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:14 +0000

    Add package tileset-analyzer/0.0.50/tileset_analyzer-0.0.50.tar.gz

---
+++ packages/tileset-analyzer/tileset_analyzer-0.0.50.tar.gz/tileset_analyzer-0.0.50/tileset_analyzer/entities/job_param.py
@@ -0,0 +1,65 @@
+from enum import Enum
+from typing import List
+import os
+
+
+class JobAction(str, Enum):
+    PROCESS = 'process'
+    SERVE = 'serve'
+
+
+class CompressionType(str, Enum):
+    GZIP = 'gzip'
+
+
+class TileScheme(str, Enum):
+    TMS = 'TMS'
+    XYZ = 'XYZ'
+
+
+class TileSourceType(str, Enum):
+    MBTiles = 'MBTILES'
+    FOLDER = 'FOLDER'
+    PMTiles = 'PMTiles'
+    COMTILES = 'COMTiles'
+
+
+class JobParam:
+    def __init__(self,
+                 source: str = None,
+                 scheme: TileScheme = None,
+                 temp_folder: str = None,
+                 actions: List[JobAction] = None,
+                 compressed: bool = False,
+                 compression_type: CompressionType = CompressionType.GZIP,
+                 verbose: str = False):
+        self.source = source
+        self.scheme = scheme
+        self.temp_folder = temp_folder
+        self.actions = actions
+        self.compressed = compressed
+        self.compression_type = compression_type
+        self.verbose = verbose
+        self._source_type: TileSourceType = None
+
+    def get_source_type(self) -> TileSourceType:
+        if self._source_type is not None:
+            return self._source_type
+
+        source_type = None
+        if os.path.isdir(self.source):
+            source_type = TileSourceType.FOLDER
+        elif os.path.isfile(self.source) and self.source.endswith('.pmtiles'):
+            source_type = TileSourceType.PMTiles
+        elif os.path.isfile(self.source) and self.source.endswith('.comtiles'):
+            source_type = TileSourceType.COMTILES
+        elif os.path.isfile(self.source) or self.source.endswith('.mbtiles'):
+            source_type = TileSourceType.MBTiles
+
+        if source_type is None:
+            raise AssertionError("Tileset Type is not valid.")
+
+        return source_type
+
+
+
