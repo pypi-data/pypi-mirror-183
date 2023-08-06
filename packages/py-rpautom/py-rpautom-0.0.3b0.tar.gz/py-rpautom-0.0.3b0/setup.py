9b7aac0d183a8dec1069f99b3d0fcdbe4cb31534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:31 +0000

    Add package py-rpautom/0.0.3b0/py-rpautom-0.0.3b0.tar.gz

---
+++ packages/py-rpautom/py-rpautom-0.0.3b0.tar.gz/py-rpautom-0.0.3b0/setup.py
@@ -0,0 +1,50 @@
+import os
+import setuptools
+
+
+base_dir = os.path.dirname(__file__)
+
+with open(os.path.join(base_dir, "README.md")) as f:
+    long_description = f.read()
+
+long_description = long_description.replace(
+    ' A documentação pode ser conferida [aqui](https://pypi.org/project/py-rpautom/).',
+    ''
+)
+
+setuptools.setup(
+    name="py-rpautom",
+    version="0.0.3b",
+    author="aranseiki",
+    author_email="techall@hotmail.com.br",
+    description="Conjunto de utilitários para automação de processos.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aranseiki/py-rpautom/",
+    packages=setuptools.find_packages(),
+    project_urls={
+        "Py-RPAutom": "https://github.com/aranseiki/py-rpautom/",
+    },
+    classifiers=[
+        "Framework :: Robot Framework :: Library",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        'openpyxl',
+        'psutil',
+        'pywinauto',
+        'pywin32',
+        'python-docx',
+        'PyMuPDF',
+        'PyPDF2',
+        'Pytesseract',
+        'requests',
+        'selenium',
+        'urllib3',
+    ]
+)
