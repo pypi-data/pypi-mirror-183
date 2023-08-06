df05cbbb0823a4c456254af4c893396721f8d52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:03 +0000

    Add package SansiLibrary/0.0.4/SansiLibrary-0.0.4-py3-none-any.whl

---
+++ packages/SansiLibrary/SansiLibrary-0.0.4-py3-none-any.whl/SansiLibrary/version.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: yongfanmao
+# @email: maoyongfan@163.com
+# @Date:   2022-11-28 16:09:55
+# @Last Modified by:   yongfanmao
+# @Last Modified time: 2022-12-30 17:44:25
+VERSION = '0.0.4'
