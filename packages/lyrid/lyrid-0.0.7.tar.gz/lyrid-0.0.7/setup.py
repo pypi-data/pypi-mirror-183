813ff2dd75eb6ea28c62c2624d9dc09b2519bc69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:34 +0000

    Add package lyrid/0.0.7/lyrid-0.0.7.tar.gz

---
+++ packages/lyrid/lyrid-0.0.7.tar.gz/lyrid-0.0.7/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+NAME = 'lyrid'
+VERSION = '0.0.7'
+URL = 'https://github.com/SSripilaipong/lyrid'
+LICENSE = 'MIT'
+AUTHOR = 'SSripilaipong'
+EMAIL = 'SHSnail@mail.com'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    zip_safe=False,
+    package_data={"lyrid": ["py.typed"]},
+    packages=[p for p in setuptools.find_packages() if p.startswith('lyrid.') or p == 'lyrid'],
+    url=URL,
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=None,
+    long_description=None,
+    python_requires='>=3.8',
+    install_requires=requirements,
+    classifiers=[],
+    include_package_data=True,
+)
