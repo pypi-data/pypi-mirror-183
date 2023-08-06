9faf7291b59161c473840aa5b6b9686eb2237544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:09 +0000

    Add package thingsvision/2.2.17/thingsvision-2.2.17.tar.gz

---
+++ packages/thingsvision/thingsvision-2.2.17.tar.gz/thingsvision-2.2.17/thingsvision/custom_models/custom.py
@@ -0,0 +1,13 @@
+import abc
+
+
+class Custom(metaclass=abc.ABCMeta):
+    def __init__(self, device) -> None:
+        self.device = device
+
+    @abc.abstractmethod
+    def create_model(self):
+        """Create the model"""
+
+    def get_backend(self):
+        return self.backend
