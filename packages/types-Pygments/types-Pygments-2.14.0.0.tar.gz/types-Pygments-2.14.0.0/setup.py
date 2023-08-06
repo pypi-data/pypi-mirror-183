31282ecf9c30a2becb7b7e1ebb1abfddcd06cc03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:55 +0000

    Add package types-Pygments/2.14.0.0/types-Pygments-2.14.0.0.tar.gz

---
+++ packages/types-Pygments/types-Pygments-2.14.0.0.tar.gz/types-Pygments-2.14.0.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+name = "types-Pygments"
+description = "Typing stubs for Pygments"
+long_description = '''
+## Typing stubs for Pygments
+
+This is a PEP 561 type stub package for the `Pygments` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `Pygments`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/Pygments. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `ca28b1f24dd20f77522e5b7f20f2e289bdef6733`.
+'''.lstrip()
+
+setup(name=name,
+      version="2.14.0.0",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/Pygments.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=['types-docutils', 'types-setuptools'],
+      packages=['pygments-stubs'],
+      package_data={'pygments-stubs': ['__init__.pyi', 'cmdline.pyi', 'console.pyi', 'filter.pyi', 'filters/__init__.pyi', 'formatter.pyi', 'formatters/__init__.pyi', 'formatters/_mapping.pyi', 'formatters/bbcode.pyi', 'formatters/html.pyi', 'formatters/img.pyi', 'formatters/irc.pyi', 'formatters/latex.pyi', 'formatters/other.pyi', 'formatters/pangomarkup.pyi', 'formatters/rtf.pyi', 'formatters/svg.pyi', 'formatters/terminal.pyi', 'formatters/terminal256.pyi', 'lexer.pyi', 'lexers/__init__.pyi', 'modeline.pyi', 'plugin.pyi', 'regexopt.pyi', 'scanner.pyi', 'sphinxext.pyi', 'style.pyi', 'styles/__init__.pyi', 'token.pyi', 'unistring.pyi', 'util.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
