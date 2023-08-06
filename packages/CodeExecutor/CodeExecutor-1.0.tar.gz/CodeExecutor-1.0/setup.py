d9f61a1962c87bb766410a19910c0aa2332455cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:30 +0000

    Add package CodeExecutor/1.0/CodeExecutor-1.0.tar.gz

---
+++ packages/CodeExecutor/CodeExecutor-1.0.tar.gz/CodeExecutor-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='CodeExecutor',
+      version='1.0',
+      description='Running code in a sandbox environment',
+      packages=['CodeExecutor'],
+      author_email='neuralteam@mail.ru',
+      zip_safe=False)
