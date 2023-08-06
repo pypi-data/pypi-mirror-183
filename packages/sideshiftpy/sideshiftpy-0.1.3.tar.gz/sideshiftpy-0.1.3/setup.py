e14fc3a7e141c4c9be32c8871f9c5a39ce20ac9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:32 +0000

    Add package sideshiftpy/0.1.3/sideshiftpy-0.1.3.tar.gz

---
+++ packages/sideshiftpy/sideshiftpy-0.1.3.tar.gz/sideshiftpy-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sideshiftpy']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'sideshiftpy',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': '',
+    'author': 'niglet',
+    'author_email': '-@x.x',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
