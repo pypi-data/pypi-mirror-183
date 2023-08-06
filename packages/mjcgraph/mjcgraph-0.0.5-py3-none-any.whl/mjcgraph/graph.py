30128a1b14cecae0dfa1c446ce4b890a0cbca6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:10 +0000

    Add package mjcgraph/0.0.5/mjcgraph-0.0.5-py3-none-any.whl

---
+++ packages/mjcgraph/mjcgraph-0.0.5-py3-none-any.whl/mjcgraph/graph.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3
+
+import sys
+
+class Graph():
+    def __init__(self, infile):
+        with open(infile) as f:
+            self.V = int(f.readline())
+            self.E = int(f.readline())
+            self.G = [[] for i in range(self.V)]
+            for i in range(self.E):
+                From, To = f.readline().split()
+                self.add_edge(int(From), int(To))
+        f.close()
+
+
+    def add_edge(self, v, w):
+        assert v < self.V
+        assert w < self.V
+        self.G[v].append(w)
+        self.G[w].append(v)
+        pass
+
+
+    def adj(self, v):
+        assert v < self.V
+        return self.G[v]
+
+
+    def to_string(self):
+        s = f'G: {self.V} vertices, {self.E} edges'
+        return s
+
+
+if __name__ == '__main__':
+    infile = sys.argv[1] if len(sys.argv) > 1 else ''
+    g = Graph(infile)
+    print(g.to_string())
