6a02f14c2923448ac762b237e0a2584766682270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:33 +0000

    Add package prueba1-gha/0.1.1.dev1672522467/prueba1_gha-0.1.1.dev1672522467-py3-none-any.whl

---
+++ packages/prueba1-gha/prueba1_gha-0.1.1.dev1672522467-py3-none-any.whl/prueba1_gha/main.py
@@ -0,0 +1,12 @@
+def print_hi(name):
+    # assert name == "bad_name", f"name should be More!!"  # PASS
+    # x = 10
+    # print(eval("x * 10"))  # PASS
+    # username = "user"           # FAIL
+    # password = "admin123456"    # FAIL
+    # print(username, password)
+    print(f'Hi, {name}')
+
+
+if __name__ == "__main__":
+    print_hi("Martin+mjmorep+2022+to+2023")
