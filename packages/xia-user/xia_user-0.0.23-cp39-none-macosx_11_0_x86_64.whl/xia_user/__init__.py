def71ef0f7e1c546af3f9c5607eb493933dcc26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:15 +0000

    Add package xia-user/0.0.23/xia_user-0.0.23-cp39-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-user/xia_user-0.0.23-cp39-none-macosx_11_0_x86_64.whl/xia_user/__init__.py
@@ -0,0 +1,11 @@
+from xia_user.user import User, UserRoles, ApiInfo, ApiKey
+from xia_user.role_matrix import RoleMatrix, RoleContent
+
+
+__all__ = [
+    "User", "UserRoles", "ApiInfo", "ApiKey",
+    "RoleMatrix", "RoleContent",
+]
+
+
+__version__ = "0.0.23"
