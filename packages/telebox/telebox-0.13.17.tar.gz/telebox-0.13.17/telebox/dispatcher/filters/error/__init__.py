6c6e55010ddf9b01e647c8fd6e94f7e64ab52a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:18 +0000

    Add package telebox/0.13.17/telebox-0.13.17.tar.gz

---
+++ packages/telebox/telebox-0.13.17.tar.gz/telebox-0.13.17/telebox/dispatcher/filters/error/__init__.py
@@ -0,0 +1,12 @@
+from .none import ErrorNoneFilter
+from .type import ErrorTypeFilter
+from .text import ErrorTextFilter
+from .reg_exp_text import ErrorRegExpTextFilter
+
+
+__all__ = [
+    "ErrorNoneFilter",
+    "ErrorTypeFilter",
+    "ErrorTextFilter",
+    "ErrorRegExpTextFilter"
+]
