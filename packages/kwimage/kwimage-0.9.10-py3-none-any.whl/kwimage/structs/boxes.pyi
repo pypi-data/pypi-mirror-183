64636d7c29935b24eadc46bd04238742aafceb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:45 +0000

    Add package kwimage/0.9.10/kwimage-0.9.10-py3-none-any.whl

---
+++ packages/kwimage/kwimage-0.9.10-py3-none-any.whl/kwimage/structs/boxes.pyi
@@ -0,0 +1,345 @@
+from numpy import ndarray
+from typing import Union
+from typing import List
+import shapely
+from typing import Any
+from typing import Tuple
+import kwimage
+from typing import Callable
+from numpy.typing import ArrayLike
+import numpy as np
+from numbers import Number
+from typing import Optional
+import matplotlib
+from torch import Tensor
+from numpy.random import RandomState
+from typing import Sequence
+import torch
+import skimage
+import torch
+import ubelt as ub
+from _typeshed import Incomplete
+from collections.abc import Generator
+from typing import Any
+
+
+class NeedsWarpCorners(AssertionError):
+    ...
+
+
+class BoxFormat:
+    cannonical: Incomplete
+    XYWH: Incomplete
+    CXYWH: Incomplete
+    LTRB: Incomplete
+    TLBR: Incomplete
+    XXYY: Incomplete
+    aliases: Incomplete
+    blocklist: Incomplete
+
+
+def box_ious(ltrb1: ndarray,
+             ltrb2: ndarray,
+             bias: int = 0,
+             impl: Union[str, None] = None):
+    ...
+
+
+class _BoxConversionMixins:
+    convert_funcs: Incomplete
+
+    def toformat(self, format: str, copy: bool = True) -> Boxes:
+        ...
+
+    def to_xxyy(self, copy: bool = ...):
+        ...
+
+    to_extent: Incomplete
+
+    def to_xywh(self, copy: bool = ...):
+        ...
+
+    def to_cxywh(self, copy: bool = ...):
+        ...
+
+    def to_ltrb(self, copy: bool = ...):
+        ...
+
+    def to_tlbr(self, **kwargs):
+        ...
+
+    def to_imgaug(self, shape: tuple):
+        ...
+
+    def to_shapely(self) -> List[shapely.geometry.Polygon]:
+        ...
+
+    def to_shapley(self) -> List[shapely.geometry.Polygon]:
+        ...
+
+    @classmethod
+    def from_shapely(cls, geom: shapely.geometry.Polygon) -> Boxes:
+        ...
+
+    @classmethod
+    def coerce(Boxes, data, **kwargs) -> Boxes:
+        ...
+
+    @classmethod
+    def from_imgaug(Boxes, bboi: Any) -> Boxes:
+        ...
+
+    @classmethod
+    def from_slice(Boxes,
+                   slices,
+                   shape: Tuple[int, int] = None,
+                   clip: bool = True,
+                   endpoint: bool = True,
+                   wrap: bool = False):
+        ...
+
+    def to_slices(self, endpoint: bool = True) -> List[Tuple[slice, slice]]:
+        ...
+
+    def to_coco(self, style: str = ...) -> Generator[List[float], None, None]:
+        ...
+
+    def to_polygons(self) -> kwimage.PolygonList:
+        ...
+
+
+class _BoxPropertyMixins:
+
+    @property
+    def xy_center(self):
+        ...
+
+    @property
+    def components(self):
+        ...
+
+    @property
+    def dtype(self):
+        ...
+
+    @property
+    def shape(self):
+        ...
+
+    @property
+    def tl_x(self):
+        ...
+
+    @property
+    def tl_y(self):
+        ...
+
+    @property
+    def br_x(self):
+        ...
+
+    @property
+    def br_y(self):
+        ...
+
+    @property
+    def width(self):
+        ...
+
+    @property
+    def height(self):
+        ...
+
+    @property
+    def aspect_ratio(self):
+        ...
+
+    @property
+    def area(self):
+        ...
+
+    @property
+    def center(self) -> Tuple[ndarray, ndarray]:
+        ...
+
+
+class _BoxTransformMixins:
+
+    def warp(self,
+             transform: Union[ArrayLike, Callable, kwimage.Affine,
+                              skimage.transform._geometric.GeometricTransform,
+                              Any],
+             input_dims: Tuple = None,
+             output_dims: Tuple = None,
+             inplace: bool = False) -> Boxes:
+        ...
+
+    def corners(self) -> np.ndarray:
+        ...
+
+    def scale(self,
+              factor: Union[float, Tuple[float, float]],
+              about: Union[str, ArrayLike] = 'origin',
+              output_dims: Tuple = None,
+              inplace: bool = False) -> Boxes:
+        ...
+
+    def translate(self,
+                  amount,
+                  output_dims: Tuple = None,
+                  inplace: bool = ...) -> Boxes:
+        ...
+
+    def clip(self,
+             x_min: int,
+             y_min: int,
+             x_max: int,
+             y_max: int,
+             inplace: bool = False) -> Boxes:
+        ...
+
+    def resize(self,
+               width: Union[Number, ndarray, None] = None,
+               height: Union[Number, ndarray, None] = None,
+               inplace: bool = False) -> Boxes:
+        ...
+
+    def pad(self,
+            x_left: Union[int, float],
+            y_top: Union[int, float],
+            x_right: Union[int, float],
+            y_bot: Union[int, float],
+            inplace: bool = ...) -> Boxes:
+        ...
+
+    def transpose(self):
+        ...
+
+
+class _BoxDrawMixins:
+
+    def draw(self,
+             color: Union[str, Any, List[Any]] = 'blue',
+             alpha: Union[float, List[float], None] = None,
+             labels: Union[List[str], None] = None,
+             centers: bool = False,
+             fill: bool = ...,
+             lw: float = 2,
+             ax: Optional[matplotlib.axes.Axes] = None,
+             setlim: bool = False,
+             **kwargs):
+        ...
+
+    def draw_on(self,
+                image: ndarray = None,
+                color: Union[str, Any, List[Any]] = 'blue',
+                alpha: float = None,
+                labels: List[str] = None,
+                copy: bool = False,
+                thickness: int = 2,
+                label_loc: str = 'top_left') -> ndarray:
+        ...
+
+
+class Boxes(_BoxConversionMixins, _BoxPropertyMixins, _BoxTransformMixins,
+            _BoxDrawMixins, ub.NiceRepr):
+    data: Union[ndarray, Tensor, Boxes]
+    format: str
+
+    def __init__(self,
+                 data: Union[ndarray, Tensor, Boxes],
+                 format: str = None,
+                 check: bool = True) -> None:
+        ...
+
+    def __getitem__(self, index):
+        ...
+
+    def __eq__(self, other):
+        ...
+
+    def __len__(self) -> int:
+        ...
+
+    def __nice__(self) -> str:
+        ...
+
+    @classmethod
+    def random(Boxes,
+               num: int = 1,
+               scale: Union[float, Tuple[float, float]] = 1.0,
+               format: str = ...,
+               anchors: ndarray = None,
+               anchor_std: float = ...,
+               tensor: bool = False,
+               rng: Union[None, int, RandomState] = None) -> Boxes:
+        ...
+
+    def copy(self) -> Boxes:
+        ...
+
+    @classmethod
+    def concatenate(cls, boxes: Sequence[Boxes], axis: int = 0) -> Boxes:
+        ...
+
+    def compress(self,
+                 flags: ArrayLike,
+                 axis: int = 0,
+                 inplace: bool = False) -> Boxes:
+        ...
+
+    def take(self, idxs, axis: int = 0, inplace: bool = False) -> Boxes:
+        ...
+
+    def is_tensor(self) -> bool:
+        ...
+
+    def is_numpy(self) -> bool:
+        ...
+
+    @property
+    def device(self):
+        ...
+
+    def astype(self, dtype) -> Boxes:
+        ...
+
+    def round(self, inplace: bool = False) -> Boxes:
+        ...
+
+    def quantize(self, inplace: bool = False, dtype: type = ...) -> Boxes:
+        ...
+
+    def numpy(self) -> Boxes:
+        ...
+
+    def tensor(self, device: Union[int, None, torch.device] = ...) -> Boxes:
+        ...
+
+    def ious(self,
+             other: Boxes,
+             bias: int = 0,
+             impl: str = 'auto',
+             mode: str = None) -> ndarray:
+        ...
+
+    def iooas(self, other: Boxes, bias: int = 0) -> ndarray:
+        ...
+
+    def isect_area(self, other: Boxes, bias: int = 0) -> ndarray:
+        ...
+
+    def intersection(self, other: Boxes) -> Boxes:
+        ...
+
+    def union_hull(self, other: Boxes) -> Boxes:
+        ...
+
+    def bounding_box(self) -> Boxes:
+        ...
+
+    def contains(self, other: kwimage.Points) -> ArrayLike:
+        ...
+
+    def view(self, *shape: Tuple[int, ...]) -> Boxes:
+        ...
---
+++ packages/kwimage/kwimage-0.9.10-py3-none-any.whl/kwimage/structs/polygon.pyi
@@ -0,0 +1,377 @@
+from typing import Union
+from typing import Any
+from typing import Tuple
+from typing import Callable
+from skimage.transform._geometric import GeometricTransform
+from numpy.typing import ArrayLike
+import kwimage
+from typing import List
+from typing import Iterable
+from numbers import Number
+from numpy import ndarray
+import shapely
+from typing import Dict
+import numpy as np
+import matplotlib
+import ubelt as ub
+from _typeshed import Incomplete
+from kwimage.structs import _generic
+from typing import Any
+
+
+class _ShapelyMixin:
+
+    def oriented_bounding_box(self):
+        ...
+
+    def buffer(self, *args, **kwargs):
+        ...
+
+    def simplify(self, tolerance, preserve_topology: bool = ...):
+        ...
+
+    @property
+    def __geo_interface__(self):
+        ...
+
+    def union(self, other):
+        ...
+
+    def intersection(self, other):
+        ...
+
+    def difference(self, other):
+        ...
+
+    def symmetric_difference(self, other):
+        ...
+
+    def iooa(self, other):
+        ...
+
+    def iou(self, other):
+        ...
+
+    @property
+    def area(self) -> float:
+        ...
+
+    @property
+    def convex_hull(self):
+        ...
+
+    def is_invalid(self, explain: bool = False) -> bool | str:
+        ...
+
+    def fix(self):
+        ...
+
+
+class _PolyArrayBackend:
+
+    def is_numpy(self):
+        ...
+
+    def is_tensor(self):
+        ...
+
+    def tensor(self, device=...):
+        ...
+
+    def numpy(self):
+        ...
+
+
+class _PolyWarpMixin:
+
+    def to_imgaug(self, shape):
+        ...
+
+    def warp(self,
+             transform: Union[GeometricTransform, ArrayLike, Any, Callable],
+             input_dims: Tuple = None,
+             output_dims: Tuple = None,
+             inplace: bool = False):
+        ...
+
+    def scale(self,
+              factor: Union[float, Tuple[float, float]],
+              about: Union[Tuple, None] = None,
+              output_dims: Tuple = None,
+              inplace: bool = False):
+        ...
+
+    def translate(self,
+                  offset,
+                  output_dims: Tuple = None,
+                  inplace: bool = False):
+        ...
+
+    def rotate(self,
+               theta: float,
+               about: Union[Tuple, None, str] = None,
+               output_dims: Tuple = None,
+               inplace: bool = ...):
+        ...
+
+    def round(self, decimals: int = 0, inplace: bool = False) -> Polygon:
+        ...
+
+    def astype(self, dtype, inplace: bool = False) -> Polygon:
+        ...
+
+    def swap_axes(self, inplace: bool = False) -> Polygon:
+        ...
+
+
+class Polygon(_generic.Spatial, _PolyArrayBackend, _PolyWarpMixin,
+              _ShapelyMixin, ub.NiceRepr):
+    __datakeys__: Incomplete
+    __metakeys__: Incomplete
+    data: Incomplete
+    meta: Incomplete
+
+    def __init__(self,
+                 data: Incomplete | None = ...,
+                 meta: Incomplete | None = ...,
+                 datakeys: Incomplete | None = ...,
+                 metakeys: Incomplete | None = ...,
+                 **kwargs) -> None:
+        ...
+
+    @property
+    def exterior(self) -> kwimage.Coords:
+        ...
+
+    @property
+    def interiors(self) -> List[kwimage.Coords]:
+        ...
+
+    def __nice__(self) -> str:
+        ...
+
+    @classmethod
+    def circle(cls,
+               xy: Iterable[Number],
+               r: Union[Number, Tuple[Number, Number]],
+               resolution: int = 64) -> Polygon:
+        ...
+
+    @classmethod
+    def random(cls,
+               n: int = 6,
+               n_holes: int = 0,
+               convex: bool = True,
+               tight: bool = False,
+               rng: Incomplete | None = ...) -> Polygon:
+        ...
+
+    def to_mask(self,
+                dims: Tuple = None,
+                pixels_are: str = 'points') -> kwimage.Mask:
+        ...
+
+    def to_relative_mask(self, return_offset: bool = ...) -> kwimage.Mask:
+        ...
+
+    @classmethod
+    def coerce(Polygon, data: object) -> kwimage.Polygon:
+        ...
+
+    @classmethod
+    def from_shapely(
+            Polygon,
+            geom: shapely.geometry.polygon.Polygon) -> kwimage.Polygon:
+        ...
+
+    @classmethod
+    def from_wkt(Polygon, data: str) -> kwimage.Polygon:
+        ...
+
+    @classmethod
+    def from_geojson(Polygon, data_geojson: dict) -> Polygon:
+        ...
+
+    def to_shapely(self,
+                   fix: bool = False) -> shapely.geometry.polygon.Polygon:
+        ...
+
+    def to_geojson(self) -> Dict[str, object]:
+        ...
+
+    def to_wkt(self) -> str:
+        ...
+
+    @classmethod
+    def from_coco(cls,
+                  data: Union[List[Number], Dict],
+                  dims: Union[None, Tuple[int, ...]] = None) -> Polygon:
+        ...
+
+    def to_coco(self, style: str = 'orig') -> List | Dict:
+        ...
+
+    def to_multi_polygon(self) -> MultiPolygon:
+        ...
+
+    def to_boxes(self) -> kwimage.Boxes:
+        ...
+
+    @property
+    def centroid(self) -> Tuple[Number, Number]:
+        ...
+
+    def bounding_box(self) -> kwimage.Boxes:
+        ...
+
+    def bounding_box_polygon(self) -> kwimage.Polygon:
+        ...
+
+    def copy(self) -> Polygon:
+        ...
+
+    def clip(self, x_min, y_min, x_max, y_max, inplace: bool = ...) -> Polygon:
+        ...
+
+    def fill(self,
+             image: ndarray,
+             value: Union[int, Tuple[int]] = 1,
+             pixels_are: str = 'points') -> ndarray:
+        ...
+
+    def draw_on(self,
+                image: ndarray,
+                color: Union[str, tuple] = 'blue',
+                fill: bool = True,
+                border: bool = False,
+                alpha: float = 1.0,
+                edgecolor: Union[str, tuple] = None,
+                facecolor: Union[str, tuple] = None,
+                copy: bool = False) -> np.ndarray:
+        ...
+
+    def draw(self,
+             color: Union[str, Tuple] = 'blue',
+             ax: Incomplete | None = ...,
+             alpha: float = 1.0,
+             radius: int = ...,
+             setlim: bool = False,
+             border: bool = None,
+             linewidth: bool = None,
+             edgecolor: Union[None, Any] = None,
+             facecolor: Union[None, Any] = None,
+             fill: bool = True,
+             vertex: float = False,
+             vertexcolor: Any = None) -> matplotlib.patches.PathPatch | None:
+        ...
+
+    def interpolate(self, other, alpha):
+        ...
+
+    def morph(self, other: kwimage.Polygon,
+              alpha: Union[float, List[float]]) -> Polygon | List[Polygon]:
+        ...
+
+
+class MultiPolygon(_generic.ObjectList, _ShapelyMixin):
+
+    @classmethod
+    def random(self,
+               n: int = ...,
+               n_holes: int = ...,
+               rng: Incomplete | None = ...,
+               tight: bool = ...) -> MultiPolygon:
+        ...
+
+    def fill(self,
+             image: ndarray,
+             value: Union[int, Tuple[int, ...]] = 1,
+             pixels_are: str = ...) -> ndarray:
+        ...
+
+    def to_multi_polygon(self) -> MultiPolygon:
+        ...
+
+    def to_boxes(self) -> kwimage.Boxes:
+        ...
+
+    def bounding_box(self) -> kwimage.Boxes:
+        ...
+
+    def to_mask(self,
+                dims: Incomplete | None = ...,
+                pixels_are: str = ...) -> kwimage.Mask:
+        ...
+
+    def to_relative_mask(self, return_offset: bool = ...) -> kwimage.Mask:
+        ...
+
+    @classmethod
+    def coerce(cls,
+               data,
+               dims: Incomplete | None = ...) -> None | MultiPolygon:
+        ...
+
+    def to_shapely(self, fix: bool = False) -> shapely.geometry.MultiPolygon:
+        ...
+
+    @classmethod
+    def from_shapely(
+        MultiPolygon, geom: Union[shapely.geometry.MultiPolygon,
+                                  shapely.geometry.Polygon]
+    ) -> MultiPolygon:
+        ...
+
+    @classmethod
+    def from_geojson(MultiPolygon, data_geojson: Dict) -> MultiPolygon:
+        ...
+
+    def to_geojson(self) -> Dict:
+        ...
+
+    @classmethod
+    def from_coco(cls,
+                  data: List[Union[List[Number], Dict]],
+                  dims: Union[None, Tuple[int, ...]] = None) -> MultiPolygon:
+        ...
+
+    def to_coco(self, style: str = 'orig'):
+        ...
+
+    def swap_axes(self, inplace: bool = False) -> MultiPolygon:
+        ...
+
+    def draw_on(self, image, **kwargs):
+        ...
+
+
+class PolygonList(_generic.ObjectList):
+
+    def to_mask_list(self,
+                     dims: Incomplete | None = ...,
+                     pixels_are: str = ...) -> kwimage.MaskList:
+        ...
+
+    def to_polygon_list(self) -> PolygonList:
+        ...
+
+    def to_segmentation_list(self) -> kwimage.SegmentationList:
+        ...
+
+    def swap_axes(self, inplace: bool = ...) -> PolygonList:
+        ...
+
+    def to_geojson(self, as_collection: bool = False) -> List[Dict] | Dict:
+        ...
+
+    def fill(self,
+             image: ndarray,
+             value: Union[int, Tuple[int, ...]] = 1,
+             pixels_are: str = ...) -> ndarray:
+        ...
+
+    def draw_on(self, *args, **kw):
+        ...
+
+    def unary_union(self):
+        ...
