8f590b04d61ad3c315f0802c583139eb076bece3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:11 +0000

    Add package torchlikelihoods/0.0.5/torchlikelihoods-0.0.5.tar.gz

---
+++ packages/torchlikelihoods/torchlikelihoods-0.0.5.tar.gz/torchlikelihoods-0.0.5/torchlikelihoods/scalers/identity.py
@@ -0,0 +1,24 @@
+from .base import BaseScaler
+
+
+class IdentityScaler(BaseScaler):
+    def __init__(self):
+        return
+
+    def _fit_params(self, x, dims=None):
+        return {}
+
+    def aggregate_param(self, name, param):
+        return param
+
+    def fit_manual(self):
+        return
+
+    def fit_with_loader(self, loader):
+        return
+
+    def _transform(self, x, inplace):
+        return x
+
+    def _inverse_transform(self, x_norm, inplace):
+        return x_norm
