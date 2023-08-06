29f8ec0c8db794af403546f927ba9d70f5d2dd46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:21:56 +0000

    Add package youdotcom/0.4.4/youdotcom-0.4.4.tar.gz

---
+++ packages/youdotcom/youdotcom-0.4.4.tar.gz/youdotcom-0.4.4/youdotcom/__init__.py
@@ -0,0 +1,17 @@
+# type: ignore[attr-defined]
+"""unofficial api wrapper for you.com and all of its apps"""
+
+import sys
+from importlib import metadata as importlib_metadata
+from .init import Init
+from .apps import Apps
+from .youchat import Chat
+from .code import Code
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+__all__ = ['YouDotCom']
