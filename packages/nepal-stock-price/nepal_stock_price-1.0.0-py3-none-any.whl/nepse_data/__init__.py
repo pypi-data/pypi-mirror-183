9da502d58f3c466d33ed94c150c99315e3f88d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:14 +0000

    Add package nepal-stock-price/1.0.0/nepal_stock_price-1.0.0-py3-none-any.whl

---
+++ packages/nepal-stock-price/nepal_stock_price-1.0.0-py3-none-any.whl/nepse_data/__init__.py
@@ -0,0 +1,10 @@
+from .src import NepseData
+
+# Version of the nepse_data package
+__version__ = "1.0.0"
+
+
+__name__ = "nepal-stock-price"
+__description__ = "python package to get the stock price of companies that are listed on Nepse."
+
+__author__ = "Keshav Khanal"
