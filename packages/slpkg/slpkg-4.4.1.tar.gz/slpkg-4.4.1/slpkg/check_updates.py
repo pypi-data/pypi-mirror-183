ebeff1d044b813fd95de02525d97c928aaddcedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:13 +0000

    Add package slpkg/4.4.1/slpkg-4.4.1.tar.gz

---
+++ packages/slpkg/slpkg-4.4.1.tar.gz/slpkg-4.4.1/slpkg/check_updates.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import urllib3
+
+from slpkg.configs import Configs
+
+
+class CheckUpdates:
+    """ Check for changes in the ChangeLog file. """
+
+    def __init__(self):
+        self.configs = Configs
+
+    def check(self) -> bool:
+        """ Checks the ChangeLogs and returns True or False. """
+        print(end='\rChecking for news in the Changelog.txt file... ')
+        local_date = 0
+        local_chg_txt = (f'{self.configs.sbo_repo_path}/'
+                         f'{self.configs.sbo_chglog_txt}')
+
+        http = urllib3.PoolManager()
+        repo = http.request(
+            'GET', f'{self.configs.sbo_repo_url}/{self.configs.sbo_chglog_txt}')
+
+        if os.path.isfile(local_chg_txt):
+            local_date = int(os.stat(local_chg_txt).st_size)
+
+        repo_date = int(repo.headers['Content-Length'])
+
+        return repo_date != local_date
+
+    def updates(self):
+        if self.check():
+            print('\n\nThere are new updates available!\n')
+        else:
+            print('\n\nNo updated packages since the last check.\n')
---
+++ packages/slpkg/slpkg-4.4.1.tar.gz/slpkg-4.4.1/slpkg/configs.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import tomli
+import platform
+
+from dataclasses import dataclass
+
+
+class LoadConfigs:
+    @staticmethod
+    def file(path: str, file: str) -> dict:
+        try:
+            """ Load the configs from the file. """
+            config_file: str = f'{path}/{file}.toml'
+            if os.path.isfile(config_file):
+                with open(config_file, 'rb') as conf:
+                    return tomli.load(conf)
+        except (KeyError, tomli.TOMLDecodeError) as error:
+            raise SystemExit(f"\nError: {error}: in the configuration file "
+                  "'/etc/slpkg/slpkg.toml'\n")
+
+
+@dataclass
+class Configs:
+    """ Default configurations. """
+
+    # Programme name
+    prog_name: str = 'slpkg'
+
+    # OS architecture by default
+    os_arch: str = platform.machine()
+
+    # All necessary paths
+    tmp_path: str = '/tmp'
+    tmp_slpkg: str = f'{tmp_path}/{prog_name}'
+    build_path: str = f'/tmp/{prog_name}/build'
+    download_only: str = f'{tmp_slpkg}/'
+    lib_path: str = f'/var/lib/{prog_name}'
+    etc_path: str = f'/etc/{prog_name}'
+    db_path: str = f'/var/lib/{prog_name}/database'
+    sbo_repo_path: str = f'/var/lib/{prog_name}/repository'
+    log_packages: str = '/var/log/packages'
+
+    # Database name
+    database: str = f'database.{prog_name}'
+
+    # SBo repository configs
+    sbo_repo_url: str = 'http://slackbuilds.org/slackbuilds/15.0'
+    sbo_txt: str = 'SLACKBUILDS.TXT'
+    sbo_chglog_txt: str = 'ChangeLog.txt'
+    sbo_tar_suffix: str = '.tar.gz'
+    sbo_repo_tag: str = '_SBo'
+
+    # Slackware commands
+    installpkg: str = 'upgradepkg --install-new'
+    reinstall: str = 'upgradepkg --reinstall'
+    removepkg: str = 'removepkg'
+
+    # Cli menu colors configs
+    colors: str = False
+
+    # Wget options
+    wget_options = '-c -N'
+
+    # Dialog utility
+    dialog: str = True
+
+    load = LoadConfigs()
+    configs = load.file(etc_path, prog_name)
+    config = configs['configs']
+
+    if config:
+        # OS architecture by default
+        os_arch: str = config['os_arch']
+
+        # All necessary paths
+        tmp_slpkg: str = config['tmp_slpkg']
+        build_path: str = config['build_path']
+        download_only: str = config['download_only']
+        sbo_repo_path: str = config['sbo_repo_path']
+
+        # Database name
+        database: str = config['database']
+
+        # SBo repository details
+        sbo_repo_url: str = config['sbo_repo_url']
+        sbo_txt: str = config['sbo_txt']
+        sbo_chglog_txt: str = config['sbo_chglog_txt']
+        sbo_tar_suffix: str = config['sbo_tar_suffix']
+        sbo_repo_tag: str = config['sbo_repo_tag']
+
+        # Slackware commands
+        installpkg: str = config['installpkg']
+        reinstall: str = config['reinstall']
+        removepkg: str = config['removepkg']
+
+        # Cli menu colors configs
+        colors: str = config['colors']
+
+        # Wget options
+        wget_options: str = config['wget_options']
+
+        # Dialog utility
+        dialog: str = config['dialog']
+
+        # Creating the paths if not exists
+        paths = [tmp_slpkg,
+                 build_path,
+                 download_only,
+                 sbo_repo_path,
+                 lib_path,
+                 etc_path,
+                 db_path]
+
+        for path in paths:
+            if not os.path.isdir(path):
+                os.makedirs(path)
+
+    @classmethod
+    def colour(cls):
+        color = {
+            'bold': '',
+            'red': '',
+            'green': '',
+            'yellow': '',
+            'cyan': '',
+            'blue': '',
+            'grey': '',
+            'endc': ''
+        }
+
+        if cls.colors:
+            color = {
+                'bold': '\033[1m',
+                'red': '\x1b[91m',
+                'green': '\x1b[32m',
+                'yellow': '\x1b[93m',
+                'cyan': '\x1b[96m',
+                'blue': '\x1b[94m',
+                'grey': '\x1b[38;5;247m',
+                'endc': '\x1b[0m'
+            }
+
+        return color
