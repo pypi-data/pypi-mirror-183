80ea66603360feea3922125e37b3faba904bc3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:43 +0000

    Add package pycocotoolse/2.0.14/pycocotoolse-2.0.14.tar.gz

---
+++ packages/pycocotoolse/pycocotoolse-2.0.14.tar.gz/pycocotoolse-2.0.14/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, Extension
+# import numpy as np
+
+# To compile and install locally run "python setup.py build_ext --inplace"
+# To install library to Python site-packages run "python setup.py build_ext install"
+
+# ext_modules = [
+#     Extension(
+#         "pycocotoolse._mask",
+#         sources=["common/maskApi.c", "pycocotoolse/_mask.pyx"],
+#         include_dirs=[np.get_include(), "common"],
+#         extra_compile_args=["-Wno-cpp", "-Wno-unused-function", "-std=c99"],
+#     )
+# ]
+
+setup(
+    name="pycocotoolse",
+    packages=["pycocotoolse"],
+    package_dir={"pycocotoolse": "pycocotoolse"},
+    install_requires=["numpy", "setuptools>=18.0", "cython>=0.27.3", "matplotlib>=2.1.0"],
+    version="2.0.14",
+    # ext_modules=ext_modules,
+)
