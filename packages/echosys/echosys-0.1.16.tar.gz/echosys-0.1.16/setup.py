a1562bc083bd923ecd42f7e7e779cbdb49bd90f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:33 +0000

    Add package echosys/0.1.16/echosys-0.1.16.tar.gz

---
+++ packages/echosys/echosys-0.1.16.tar.gz/echosys-0.1.16/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("ECHO/README.org", "r") as ldf:
+    long_description = ldf.read()
+
+print(find_packages())
+
+setup(
+    name='echosys',
+    version='0.1.16',
+    description='A python library for modeling and solving epistemic multi agents planning problems',
+    long_description = long_description,
+    long_description_content_type = '',
+    url='https://github.com/DavideSolda/echosys',
+    author='Davide Solda\'',
+    author_email='davide.solda@tuwien.ac.at',
+    license='GNU General Public License v3.0',
+    packages=find_packages(),
+    install_requires=['clingo'],
+    classifiers=[]
+    )
