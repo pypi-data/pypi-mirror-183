02b99a5d8aa90a7626371c45d7462c5b08924f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:08 +0000

    Add package pycolorgenerator/1.0/pycolorgenerator-1.0.tar.gz

---
+++ packages/pycolorgenerator/pycolorgenerator-1.0.tar.gz/pycolorgenerator-1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().split('\n')
+
+setuptools.setup(
+	name="pycolorgenerator",
+	version="1.0",
+	author="Alexey-Chebotarev",
+	description="A package for useful python templates.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/Chebotarev-Alexey/pycolorgenerator",
+	packages=setuptools.find_packages(),
+	install_requires=requirements,
+	classifiers=[
+        "Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
