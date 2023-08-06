0d322afa3ae281c87af50cc74d75e137d56cc0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:16 +0000

    Add package modulomuysimple/0.0.2/modulomuysimple-0.0.2.tar.gz

---
+++ packages/modulomuysimple/modulomuysimple-0.0.2.tar.gz/modulomuysimple-0.0.2/setup.py
@@ -0,0 +1,33 @@
+"""
+TUTORIAL --> https://www.freecodecamp.org/espanol/news/como-construir-tu-primer-paquete-de-python/
+"""
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'Mi primer paquete de Python'
+LONG_DESCRIPTION = 'Mi primer paquete de Python con una descripción ligeramente más larga'
+
+# Configurando
+setup(
+       # el nombre debe coincidir con el nombre de la carpeta 	  
+       #'modulomuysimple'
+        name="modulomuysimple", 
+        version=VERSION,
+        author="Eduardo de Tena",
+        author_email="<tena15.eng@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # añade cualquier paquete adicional que debe ser
+        #instalado junto con tu paquete. Ej: 'caer'
+        
+        keywords=['python', 'primer paquete'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
