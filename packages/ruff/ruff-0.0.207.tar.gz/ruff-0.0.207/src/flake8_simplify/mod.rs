2eaef4ff553332b69cd9da50847231011fea5a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:32 +0000

    Add package ruff/0.0.207/ruff-0.0.207.tar.gz

---
+++ packages/ruff/ruff-0.0.207.tar.gz/ruff-0.0.207/src/flake8_simplify/mod.rs
@@ -0,0 +1,28 @@
+pub mod plugins;
+
+#[cfg(test)]
+mod tests {
+    use std::convert::AsRef;
+    use std::path::Path;
+
+    use anyhow::Result;
+    use test_case::test_case;
+
+    use crate::checks::CheckCode;
+    use crate::linter::test_path;
+    use crate::settings;
+
+    #[test_case(CheckCode::SIM118, Path::new("SIM118.py"); "SIM118")]
+    #[test_case(CheckCode::SIM300, Path::new("SIM300.py"); "SIM300")]
+    fn checks(check_code: CheckCode, path: &Path) -> Result<()> {
+        let snapshot = format!("{}_{}", check_code.as_ref(), path.to_string_lossy());
+        let checks = test_path(
+            Path::new("./resources/test/fixtures/flake8_simplify")
+                .join(path)
+                .as_path(),
+            &settings::Settings::for_rule(check_code),
+        )?;
+        insta::assert_yaml_snapshot!(snapshot, checks);
+        Ok(())
+    }
+}
