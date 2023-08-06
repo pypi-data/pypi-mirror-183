d2f5f9e6f776885441c73f6383425d9329962486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:25:08 +0000

    Add package atk-training-nitesh-psq/0.1.0/atk_training_nitesh_psq-0.1.0-py3-none-any.whl

---
+++ packages/atk-training-nitesh-psq/atk_training_nitesh_psq-0.1.0-py3-none-any.whl/atk_training_nitesh_psq/__init__.py
@@ -0,0 +1,37 @@
+import os,sys,logging
+from abc import ABC,abstractmethod
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+#log_dir = "logs"
+#log_filepath = os.path.join(log_dir, "running_logs.log")
+#os.makedirs(log_dir, exist_ok=True)
+#logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+#log_dir = "logs"
+#log_filepath = os.path.join(log_dir, "running_logs.log")
+#os.makedirs(log_dir, exist_ok=True)
+# logging.FileHandler(log_filepath),
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format=logging_str,
+    handlers=[
+       
+        logging.StreamHandler(sys.stdout),
+    ])
+
+logger = logging.getLogger("PSQ")
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format=logging_str,
+    handlers=[
+        
+        logging.StreamHandler(sys.stdout),
+    ])
+
+logger = logging.getLogger("PSQ")
+
+
+class process:
+    @abstractmethod
+    def init_process(self,*args,**kwargs):
+        pass
