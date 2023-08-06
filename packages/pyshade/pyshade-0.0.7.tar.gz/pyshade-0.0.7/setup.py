c36ab26c8fbdf229d01186cc09f04f31b6b19f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:26 +0000

    Add package pyshade/0.0.7/pyshade-0.0.7.tar.gz

---
+++ packages/pyshade/pyshade-0.0.7.tar.gz/pyshade-0.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.7'
+DESCRIPTION = 'a python module that will allow you to make a colorful text with dynamics colors and display your text slowly'
+
+# Setting up
+setup(
+    name="pyshade",
+    version=VERSION,
+    author="Platipus (PLATIPUS#5696)",
+    author_email="<plati@platipuss.xyz>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['pyshade','python', 'colors', 'beautiful_colors', 'shade', 'animation', 'display', 'dynamics_colors', 'command', 'terminal', 'prompt'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
