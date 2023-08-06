771a53dd27555b5675176331128ddcdcc282f0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:31 +0000

    Add package gpforecaster/0.3.24/gpforecaster-0.3.24-py3-none-any.whl

---
+++ packages/gpforecaster/gpforecaster-0.3.24-py3-none-any.whl/gpforecaster/tests/test_sparse_gps.py
@@ -0,0 +1,36 @@
+import unittest
+
+import tsaugmentation as tsag
+
+from gpforecaster.model.gpf import GPF
+from gpforecaster.visualization.plot_predictions import plot_predictions_vs_original
+
+
+class TestModel(unittest.TestCase):
+    def setUp(self):
+        self.dataset_name = "prison"
+        self.data = tsag.preprocessing.PreprocessDatasets(self.dataset_name).apply_preprocess()
+        self.n = self.data["predict"]["n"]
+        self.s = self.data["train"]["s"]
+        self.gpf = GPF(
+            self.dataset_name,
+            self.data,
+            log_dir="..",
+            gp_type="sparse",
+            inducing_points_perc=0.75,
+        )
+
+    def test_sparse_gp(self):
+        model, like = self.gpf.train(epochs=200, patience=4, track_mem=True)
+        samples = self.gpf.predict(model, like)
+        plot_predictions_vs_original(
+            dataset=self.dataset_name,
+            prediction_samples=samples,
+            origin_data=self.gpf.original_data,
+            inducing_points=self.gpf.inducing_points,
+            n_series_to_plot=8,
+            gp_type=self.gpf.gp_type
+        )
+        self.gpf.plot_losses(5)
+        self.gpf.metrics(samples)
+        self.assertLess(self.gpf.losses[-1], 2)
