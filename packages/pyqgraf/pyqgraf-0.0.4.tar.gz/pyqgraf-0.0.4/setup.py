2bc63dcc4c9788bc6cafbb4174cb62227ff503de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:12 +0000

    Add package pyqgraf/0.0.4/pyqgraf-0.0.4.tar.gz

---
+++ packages/pyqgraf/pyqgraf-0.0.4.tar.gz/pyqgraf-0.0.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyqgraf']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests', 'scikit-build', 'smpl_io']
+
+setup_kwargs = {
+    'name': 'pyqgraf',
+    'version': '0.0.4',
+    'description': 'PyQgraf is a Python wrapper for Qgraf, a Feynman diagram generator.',
+    'long_description': 'pyqgraf',
+    'author': 'Alexander Puck Neuwirth',
+    'author_email': 'alexander@neuwirth-informatik.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/APN-Pucky/pyqgraf',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
