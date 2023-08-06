17f912d6a4e8b833c6b91941237b3083759b4d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:55 +0000

    Add package earth-osm/0.0.7/earth_osm-0.0.7.tar.gz

---
+++ packages/earth-osm/earth_osm-0.0.7.tar.gz/earth_osm-0.0.7/setup.py
@@ -0,0 +1,53 @@
+"""Python setup.py for earth_osm package"""
+import io
+import os
+from setuptools import find_packages, setup
+
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("earth_osm", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_reqs(name):
+    return [line for line in read(name).split('\n') if line and not line.strip().startswith('#')]
+
+
+setup(
+    name="earth_osm",
+    version=read("earth_osm", "VERSION"),
+    description="Python tool to extract large-amounts of OpenStreetMap data",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="pypsa-meets-earth",
+    url="https://github.com/pypsa-meets-earth/earth-osm/",
+    packages=find_packages(exclude=["docs", "tests"]),
+    include_package_data=True,
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": ["earth_osm = earth_osm.__main__:main"]
+    },
+    install_requires=read_reqs("requirements.txt"),
+    extras_require={
+    'test': read_reqs('requirements-test.txt'),
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+    ],
+)
