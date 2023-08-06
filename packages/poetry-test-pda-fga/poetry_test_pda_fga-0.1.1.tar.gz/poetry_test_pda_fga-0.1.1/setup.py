3f4374fa5ffc1eb224302b734995c3aace0372b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:50 +0000

    Add package poetry-test-pda-fga/0.1.1/poetry_test_pda_fga-0.1.1.tar.gz

---
+++ packages/poetry-test-pda-fga/poetry_test_pda_fga-0.1.1.tar.gz/poetry_test_pda_fga-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_test_pda_fga']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['poetry_test_pda_fga-cli = poetry_test_pda_fga.cli:cli']}
+
+setup_kwargs = {
+    'name': 'poetry-test-pda-fga',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': 'Bruno-Felix',
+    'author_email': 'balvesfelix@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
