5eed3e34a34751be2be63ef574e1f571a08432de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:56 +0000

    Add package cfnv1a/0.1.0/cfnv1a-0.1.0.tar.gz

---
+++ packages/cfnv1a/cfnv1a-0.1.0.tar.gz/cfnv1a-0.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import Extension, setup
+
+
+
+setup(
+    name="cfnv1a",
+    version="0.1.0",
+    ext_modules=[Extension("cfnv1a", ["cfnv1a.c"])],
+    description="fnv1a c implementation for python",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    author="Dmitry Orlov",
+    author_email="me@mosquito.su",
+    url="https://github.com/mosquito/cfnv1a/",
+    packages=["."],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development",
+    ],
+    python_requires=">=3.7.*, <4",
+)
