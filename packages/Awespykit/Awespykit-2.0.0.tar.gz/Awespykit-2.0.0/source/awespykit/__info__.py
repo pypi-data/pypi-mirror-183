981f446802ea931b89b11c5cf986b96f8e356d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:52 +0000

    Add package Awespykit/2.0.0/Awespykit-2.0.0.tar.gz

---
+++ packages/Awespykit/Awespykit-2.0.0.tar.gz/Awespykit-2.0.0/source/awespykit/__info__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+__all__ = ["NAME", "VERSION", "AUTHOR", "URL"]
+
+NAME = "Awespykit"
+VERSION = "2.0.0"
+AUTHOR = "hrpzcf / hrp"
+URL = "https://github.com/hrpzcf/AwesomePyKit"
---
+++ packages/Awespykit/Awespykit-2.0.0.tar.gz/Awespykit-2.0.0/source/awespykit/ui/__init__.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+from .about_window import Ui_about_window
+from .addenv_dialog import Ui_input_dialog
+from .environ_chosen import Ui_environ_chosen
+from .generic_output import Ui_generic_output
+from .imports_check import Ui_imports_check
+from .index_manager import Ui_index_manager
+from .main_entrance import Ui_main_entrance
+from .package_download import Ui_package_download
+from .package_install import Ui_package_install
+from .package_manager import Ui_package_manager
+from .pyinstaller_tool import Ui_pyinstaller_tool
+from .query_panel import Ui_query_panel
+from .show_download import Ui_show_download
+
+__all__ = [
+    "Ui_about_window",
+    "Ui_environ_chosen",
+    "Ui_generic_output",
+    "Ui_imports_check",
+    "Ui_index_manager",
+    "Ui_input_dialog",
+    "Ui_main_entrance",
+    "Ui_package_download",
+    "Ui_package_install",
+    "Ui_package_manager",
+    "Ui_pyinstaller_tool",
+    "Ui_query_panel",
+    "Ui_show_download",
+]
