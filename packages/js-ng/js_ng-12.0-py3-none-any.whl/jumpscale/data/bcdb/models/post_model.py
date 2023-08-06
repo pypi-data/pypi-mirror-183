518e0b1c0a0f65c2b6145a8478247f4d3fc295fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:29 +0000

    Add package js-ng/12.0/js_ng-12.0-py3-none-any.whl

---
+++ packages/js-ng/js_ng-12.0-py3-none-any.whl/jumpscale/data/bcdb/models/post_model.py
@@ -0,0 +1,10 @@
+from .base import ModelBase
+
+
+class QuoteModel(ModelBase):
+    _schema = """
+    @url = quote
+    author** = "Anonymous" (S)
+    quote*** = "" (S)
+    """
+    _name = "quote"
