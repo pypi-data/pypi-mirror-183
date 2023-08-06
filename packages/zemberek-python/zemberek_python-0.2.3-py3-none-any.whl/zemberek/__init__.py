7eed5a75f0712076b716c9ca15906a18e8ab9e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:06 +0000

    Add package zemberek-python/0.2.3/zemberek_python-0.2.3-py3-none-any.whl

---
+++ packages/zemberek-python/zemberek_python-0.2.3-py3-none-any.whl/zemberek/__init__.py
@@ -0,0 +1,18 @@
+from .morphology import TurkishMorphology
+from .normalization import TurkishSentenceNormalizer, TurkishSpellChecker
+from .tokenization import TurkishSentenceExtractor, TurkishTokenizer
+
+import logging
+import sys
+
+__version__ = '0.2.3'
+
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s\nMsg: %(message)s\n')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
