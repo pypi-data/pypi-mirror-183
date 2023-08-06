853f8ae5ab0675084207dbfed8bc190be54d9be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:02 +0000

    Add package prueba1-gha/0.2.0/prueba1_gha-0.2.0.tar.gz

---
+++ packages/prueba1-gha/prueba1_gha-0.2.0.tar.gz/prueba1_gha-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['prueba1_gha']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'prueba1-gha',
+    'version': '0.2.0',
+    'description': 'Prueba Github actions',
+    'long_description': '## Readme\n\n\n\n\n[![PyPI](https://img.shields.io/pypi/v/prueba1-gha?logo=pypi&logoColor=white&style=for-the-badge)](https://pypi.org/project/prueba1-gha/)\n\n',
+    'author': 'Martin More',
+    'author_email': 'martinmore@mail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://example.com/',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
