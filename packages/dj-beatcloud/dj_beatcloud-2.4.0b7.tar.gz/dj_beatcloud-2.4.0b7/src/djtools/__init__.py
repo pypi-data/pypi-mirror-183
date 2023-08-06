809e350dbf412d71b6f6bc7edc1ef9c173f88094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:39 +0000

    Add package dj-beatcloud/2.4.0b7/dj_beatcloud-2.4.0b7.tar.gz

---
+++ packages/dj-beatcloud/dj_beatcloud-2.4.0b7.tar.gz/dj_beatcloud-2.4.0b7/src/djtools/__init__.py
@@ -0,0 +1,40 @@
+from djtools.rekordbox import (
+    randomize_tracks, REKORDBOX_OPERATIONS, rekordbox_playlists
+)
+from djtools.spotify import (
+    playlist_from_upload,
+    SPOTIFY_OPERATIONS,
+    update_auto_playlists,
+)
+from djtools.sync import (
+    download_music, download_xml, SYNC_OPERATIONS, upload_music, upload_xml
+)
+from djtools.utils import (
+    build_config,
+    compare_tracks,
+    copy_playlists_tracks,
+    UTILS_OPERATIONS,
+    upload_log,
+    youtube_dl,
+)
+
+
+__all__ = (
+    "build_config",
+    "compare_tracks",
+    "copy_playlists_tracks",
+    "download_music",
+    "download_xml",
+    "playlist_from_upload",
+    "randomize_tracks",
+    "REKORDBOX_OPERATIONS",
+    "rekordbox_playlists",
+    "SPOTIFY_OPERATIONS",
+    "SYNC_OPERATIONS",
+    "update_auto_playlists",
+    "upload_log",
+    "upload_music",
+    "upload_xml",
+    "UTILS_OPERATIONS",
+    "youtube_dl",
+)
