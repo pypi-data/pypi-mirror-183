a48d8e089c598c54223a64a9e7516c03c607dc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:08 +0000

    Add package TaskManagerHandler/0.0.1/TaskManagerHandler-0.0.1.tar.gz

---
+++ packages/TaskManagerHandler/TaskManagerHandler-0.0.1.tar.gz/TaskManagerHandler-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name='TaskManagerHandler',
+    version='0.0.01',
+    packages=find_packages(),
+    author="Shivam Sharma",
+    author_email="shivamsharma1913@gmail.com",
+    description="This package will take care of interacting with speech interactions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
+
+install_requires = ['setuptools', 'wheel']
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
