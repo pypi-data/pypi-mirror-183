6eff1e400cb5fcc374cb8ecd2094822d829bdad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:48 +0000

    Add package insoundz-api/0.1.20rc18/insoundz_api-0.1.20rc18.tar.gz

---
+++ packages/insoundz-api/insoundz_api-0.1.20rc18.tar.gz/insoundz_api-0.1.20rc18/setup.py
@@ -0,0 +1,47 @@
+import subprocess
+import os
+from setuptools import setup
+from packaging import version
+
+
+insoundz_api_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+# verify version format
+assert isinstance(version.parse(insoundz_api_version), version.Version)
+
+assert os.path.isfile("src/version.py")
+with open("src/VERSION", "w", encoding="utf-8") as fh:
+    fh.write("%s\n" % insoundz_api_version)
+
+setup(
+    name='insoundz_api',
+    version=insoundz_api_version,
+    description="Insoundz API client implementation \
+        to produce audio enhancement.",
+    license='MIT',
+    python_requires=">=3.7",
+    package_dir={"insoundz_api": "src"},
+    packages=['insoundz_api'],
+    package_data={"insoundz_api": ["VERSION"]},
+    url='https://github.com/InSoundz/insoundz-api',
+    install_requires=[
+        'requests',
+        'tqdm',
+        'halo',
+        'validators',
+    ],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+ )
