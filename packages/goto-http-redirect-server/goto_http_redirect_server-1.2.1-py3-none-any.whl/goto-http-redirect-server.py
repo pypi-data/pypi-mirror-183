681a1a37a6de245a3a757b07e65b753be5de2e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:17 +0000

    Add package goto-http-redirect-server/1.2.1/goto_http_redirect_server-1.2.1-py3-none-any.whl

---
+++ packages/goto-http-redirect-server/goto_http_redirect_server-1.2.1-py3-none-any.whl/goto-http-redirect-server.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+from goto_http_redirect_server import goto_http_redirect_server
+
+
+if __name__ == '__main__':
+    goto_http_redirect_server.main()
