36dbaf0f48365c31c74368f0029662599c873c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:51 +0000

    Add package monitescript/0.5.0/monitescript-0.5.0.tar.gz

---
+++ packages/monitescript/monitescript-0.5.0.tar.gz/monitescript-0.5.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['monitescript']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ideas>=0.0.37,<0.0.38',
+ 'tomlkit>=0.10.0,<0.11.0',
+ 'typer[all]>=0.4.0,<0.5.0',
+ 'typing-extensions>=4.2.0,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['monitescript = monitescript.monitescript:main']}
+
+setup_kwargs = {
+    'name': 'monitescript',
+    'version': '0.5.0',
+    'description': 'MoniteScript is a tool for creating scripts for stuff.',
+    'long_description': '',
+    'author': 'Stanislav Zmiev',
+    'author_email': 'szmiev2000@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/Ovsyanka83/monitescript',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
