7b16a0dea33ae8ce4b7f63bba5f5e99f4ace45cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:04 +0000

    Add package preliz/0.1.1/preliz-0.1.1-py2.py3-none-any.whl

---
+++ packages/preliz/preliz-0.1.1-py2.py3-none-any.whl/preliz/distributions/discrete.py
@@ -0,0 +1,379 @@
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=attribute-defined-outside-init
+"""
+Discrete probability distributions.
+"""
+from copy import copy
+import logging
+from math import ceil, floor
+
+import numpy as np
+from scipy import stats
+
+
+from .distributions import Discrete
+from ..utils.optimization import optimize_ml
+
+_log = logging.getLogger("preliz")
+
+eps = np.finfo(float).eps
+
+
+class Binomial(Discrete):
+    R"""
+    Binomial distribution.
+
+    The discrete probability distribution of the number of successes
+    in a sequence of n independent yes/no experiments, each of which
+    yields success with probability p.
+
+    The pmf of this distribution is
+
+    .. math:: f(x \mid n, p) = \binom{n}{x} p^x (1-p)^{n-x}
+
+    .. plot::
+        :context: close-figs
+
+        import arviz as az
+        from preliz import Binomial
+        az.style.use('arviz-white')
+        ns = [5, 10, 10]
+        ps = [0.5, 0.5, 0.7]
+        for n, p in zip(ns, ps):
+            Binomial(n, p).plot_pdf()
+
+    ========  ==========================================
+    Support   :math:`x \in \{0, 1, \ldots, n\}`
+    Mean      :math:`n p`
+    Variance  :math:`n p (1 - p)`
+    ========  ==========================================
+
+    Parameters
+    ----------
+    n : int
+        Number of Bernoulli trials (n >= 0).
+    p : float
+        Probability of success in each trial (0 < p < 1).
+    """
+
+    def __init__(self, n=None, p=None):
+        super().__init__()
+        self.dist = copy(stats.binom)
+        self.support = (0, np.inf)
+        self._parametrization(n, p)
+
+    def _parametrization(self, n=None, p=None):
+        self.n = n
+        self.p = p
+        self.params = (self.n, self.p)
+        self.param_names = ("n", "p")
+        self.params_support = ((eps, np.inf), (eps, 1 - eps))
+        if (n and p) is not None:
+            self._update(n, p)
+
+    def _get_frozen(self):
+        frozen = None
+        if any(self.params):
+            frozen = self.dist(self.n, self.p)
+        return frozen
+
+    def _update(self, n, p):
+        self.n = int(n)
+        self.p = float(p)
+        self.params = (self.n, self.p)
+        self.support = (0, self.n)
+        self._update_rv_frozen()
+
+    def _fit_moments(self, mean, sigma):
+        # crude approximation for n and p
+        n = mean + sigma * 2
+        p = mean / n
+        self._update(n, p)
+
+    def _fit_mle(self, sample):
+        # see https://doi.org/10.1016/j.jspi.2004.02.019 for details
+        x_bar = np.mean(sample)
+        x_std = np.std(sample)
+        x_max = np.max(sample)
+        n = ceil(x_max ** (1.5) * x_std / (x_bar**0.5 * (x_max - x_bar) ** 0.5))
+        p = x_bar / n
+        self._update(n, p)
+
+
+class DiscreteUniform(Discrete):
+    R"""
+    Discrete Uniform distribution.
+
+    The pmf of this distribution is
+
+    .. math:: f(x \mid lower, upper) = \frac{1}{upper-lower+1}
+
+    .. plot::
+        :context: close-figs
+
+        import arviz as az
+        from preliz import DiscreteUniform
+        az.style.use('arviz-white')
+        ls = [1, -2]
+        us = [6, 2]
+        for l, u in zip(ls, us):
+            ax = DiscreteUniform(l, u).plot_pdf()
+            ax.set_ylim(0, 0.25)
+
+    ========  ===============================================
+    Support   :math:`x \in {lower, lower + 1, \ldots, upper}`
+    Mean      :math:`\dfrac{lower + upper}{2}`
+    Variance  :math:`\dfrac{(upper - lower + 1)^2 - 1}{12}`
+    ========  ===============================================
+
+    Parameters
+    ----------
+    lower: int
+        Lower limit.
+    upper: int
+        Upper limit (upper > lower).
+    """
+
+    def __init__(self, lower=None, upper=None):
+        super().__init__()
+        self.dist = copy(stats.randint)
+        self._parametrization(lower, upper)
+
+    def _parametrization(self, lower=None, upper=None):
+        self.lower = lower
+        self.upper = upper
+        self.params = (self.lower, self.upper)
+        self.param_names = ("lower", "upper")
+        self.params_support = ((-np.inf, np.inf), (-np.inf, np.inf))
+        if lower is None:
+            self.lower = -np.inf
+        if upper is None:
+            self.upper = np.inf
+        self.support = (self.lower, self.upper)
+        self.dist.a = self.lower
+        self.dist.b = self.upper
+        if (lower and upper) is not None:
+            self._update(lower, upper)
+        else:
+            self.lower = lower
+            self.upper = upper
+
+    def _get_frozen(self):
+        frozen = None
+        if any(self.params):
+            frozen = self.dist(self.lower, self.upper + 1)
+        return frozen
+
+    def _update(self, lower, upper):
+        self.lower = floor(lower)
+        self.upper = ceil(upper)
+        self.params = (self.lower, self.upper)
+        self.support = (self.lower, self.upper)
+        self._update_rv_frozen()
+
+    def _fit_moments(self, mean, sigma):
+        spr = (12 * sigma**2 + 1) ** 0.5
+        lower = 0.5 * (2 * mean - spr + 1)
+        upper = 0.5 * (2 * mean + spr - 1)
+        self._update(lower, upper)
+
+    def _fit_mle(self, sample):
+        lower = np.min(sample)
+        upper = np.max(sample)
+        self._update(lower, upper)
+
+
+class NegativeBinomial(Discrete):
+    R"""
+    Negative binomial distribution.
+
+    The negative binomial distribution describes a Poisson random variable
+    whose rate parameter is gamma distributed.
+    Its pmf, parametrized by the parameters alpha and mu of the gamma distribution, is
+
+    .. math::
+
+       f(x \mid \mu, \alpha) =
+           \binom{x + \alpha - 1}{x}
+           (\alpha/(\mu+\alpha))^\alpha (\mu/(\mu+\alpha))^x
+
+
+    .. plot::
+        :context: close-figs
+
+        import arviz as az
+        from preliz import NegativeBinomial
+        az.style.use('arviz-white')
+        mus = [1, 2, 8]
+        alphas = [0.9, 2, 4]
+        for mu, alpha in zip(mus, alphas):
+            NegativeBinomial(mu, alpha).plot_pdf(support=(0, 20))
+
+    ========  ==========================
+    Support   :math:`x \in \mathbb{N}_0`
+    Mean      :math:`\mu`
+    Variance  :math:`\frac{\mu (\alpha + \mu)}{\alpha}`
+    ========  ==========================
+
+    The negative binomial distribution can be parametrized either in terms of mu and alpha,
+    or in terms of n and p. The link between the parametrizations is given by
+
+    .. math::
+
+        p &= \frac{\alpha}{\mu + \alpha} \\
+        n &= \alpha
+
+    If it is parametrized in terms of n and p, the negative binomial describes the probability
+    to have x failures before the n-th success, given the probability p of success in each trial.
+    Its pmf is
+
+    .. math::
+
+        f(x \mid n, p) =
+           \binom{x + n - 1}{x}
+           (p)^n (1 - p)^x
+
+    Parameters
+    ----------
+    alpha : float
+        Gamma distribution shape parameter (alpha > 0).
+    mu : float
+        Gamma distribution mean (mu > 0).
+    p : float
+        Probability of success in each trial (0 < p < 1).
+    n : float
+        Number of target success trials (n > 0)
+    """
+
+    def __init__(self, mu=None, alpha=None, p=None, n=None):
+        super().__init__()
+        self.dist = copy(stats.nbinom)
+        self.support = (0, np.inf)
+        self._parametrization(mu, alpha, p, n)
+
+    def _parametrization(self, mu=None, alpha=None, p=None, n=None):
+        if (mu or alpha) is not None and (p or n) is not None:
+            raise ValueError("Incompatible parametrization. Either use mu and alpha, or p and n.")
+
+        self.param_names = ("mu", "alpha")
+        self.params_support = ((eps, np.inf), (eps, np.inf))
+
+        if (p or n) is not None:
+            self.p = p
+            self.n = n
+            self.param_names = ("p", "n")
+            if (p and n) is not None:
+                mu, alpha = self._from_p_n(p, n)
+
+        self.mu = mu
+        self.alpha = alpha
+        if (mu and alpha) is not None:
+            self._update(mu, alpha)
+
+    def _from_p_n(self, p, n):
+        alpha = n
+        mu = n * (1 / p - 1)
+        return mu, alpha
+
+    def _to_p_n(self, mu, alpha):
+        p = alpha / (mu + alpha)
+        n = alpha
+        return p, n
+
+    def _get_frozen(self):
+        frozen = None
+        if any(self.params):
+            frozen = self.dist(self.n, self.p)
+        return frozen
+
+    def _update(self, mu, alpha):
+        self.mu = float(mu)
+        self.alpha = float(alpha)
+        self.p, self.n = self._to_p_n(self.mu, self.alpha)
+
+        if self.param_names[0] == "mu":
+            self.params = (self.mu, self.alpha)
+        elif self.param_names[0] == "p":
+            self.params = (self.p, self.n)
+
+        self._update_rv_frozen()
+
+    def _fit_moments(self, mean, sigma):
+        mu = mean
+        alpha = mean**2 / (sigma**2 - mean)
+        self._update(mu, alpha)
+
+    def _fit_mle(self, sample):
+        optimize_ml(self, sample)
+
+
+class Poisson(Discrete):
+    R"""
+    Poisson log-likelihood.
+
+    Often used to model the number of events occurring in a fixed period
+    of time when the times at which events occur are independent.
+    The pmf of this distribution is
+
+    .. math:: f(x \mid \mu) = \frac{e^{-\mu}\mu^x}{x!}
+
+    .. plot::
+        :context: close-figs
+
+        import arviz as az
+        from preliz import Poisson
+        az.style.use('arviz-white')
+        for mu in [0.5, 3, 8]:
+            Poisson(mu).plot_pdf()
+
+    ========  ==========================
+    Support   :math:`x \in \mathbb{N}_0`
+    Mean      :math:`\mu`
+    Variance  :math:`\mu`
+    ========  ==========================
+
+    Parameters
+    ----------
+    mu: float
+        Expected number of occurrences during the given interval
+        (mu >= 0).
+
+    Notes
+    -----
+    The Poisson distribution can be derived as a limiting case of the
+    binomial distribution.
+    """
+
+    def __init__(self, mu=None):
+        super().__init__()
+        self.mu = mu
+        self.dist = copy(stats.poisson)
+        self.support = (0, np.inf)
+        self._parametrization(mu)
+
+    def _parametrization(self, mu=None):
+        self.mu = mu
+        self.params = (self.mu,)
+        self.param_names = ("mu",)
+        self.params_support = ((eps, np.inf),)
+        if (mu) is not None:
+            self._update(mu)
+
+    def _get_frozen(self):
+        frozen = None
+        if any(self.params):
+            frozen = self.dist(self.mu)
+        return frozen
+
+    def _update(self, mu):
+        self.mu = float(mu)
+        self.params = (self.mu,)
+        self._update_rv_frozen()
+
+    def _fit_moments(self, mean, sigma=None):  # pylint: disable=unused-argument
+        self._update(mean)
+
+    def _fit_mle(self, sample):
+        mu = np.mean(sample)
+        self._update(mu)
---
+++ packages/preliz/preliz-0.1.1-py2.py3-none-any.whl/preliz/utils/plot_utils.py
@@ -0,0 +1,197 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import _pylab_helpers
+from scipy.stats._distn_infrastructure import rv_continuous_frozen, rv_discrete_frozen
+from scipy.interpolate import interp1d, PchipInterpolator
+
+
+def plot_pointinterval(distribution, quantiles=None, rotated=False, ax=None):
+    """
+    By default plot the quantiles [0.05, 0.25, 0.5, 0.75, 0.95]
+    The median as dot and the two interquantiles ranges (0.05-0.95) and (0.25-0.75) as lines.
+
+    Parameters
+    ----------
+    distribution : preliz distribution or array
+    quantiles : list
+        The number of elements should be 5, 3, 1 or 0 (in this last case nothing will be plotted).
+        defaults to [0.05, 0.25, 0.5, 0.75, 0.95].
+    rotated : bool
+        Whether to do the plot along the x-axis (default) or on the y-axis
+    ax : matplotlib axis
+    """
+    if quantiles is None:
+        quantiles = [0.05, 0.25, 0.5, 0.75, 0.95]
+
+    if isinstance(distribution, (rv_continuous_frozen, rv_discrete_frozen)):
+        q_s = distribution.ppf(quantiles).tolist()
+    else:
+        q_s = np.quantile(distribution, quantiles).tolist()
+
+    q_s_size = len(q_s)
+    if not q_s_size in (5, 3, 1, 0):
+        raise ValueError("quantiles should have 5, 3, 1 or 0 elements")
+
+    if rotated:
+        if q_s_size == 5:
+            ax.plot([0, 0], (q_s.pop(0), q_s.pop(-1)), "k", solid_capstyle="butt", lw=1.5)
+        if q_s_size > 2:
+            ax.plot([0, 0], (q_s.pop(0), q_s.pop(-1)), "k", solid_capstyle="butt", lw=4)
+        if q_s_size > 0:
+            ax.plot(0, q_s[0], "wo", mec="k")
+    else:
+        if q_s_size == 5:
+            ax.plot((q_s.pop(0), q_s.pop(-1)), [0, 0], "k", solid_capstyle="butt", lw=1.5)
+        if q_s_size > 2:
+            ax.plot((q_s.pop(0), q_s.pop(-1)), [0, 0], "k", solid_capstyle="butt", lw=4)
+
+        if q_s_size > 0:
+            ax.plot(q_s[0], 0, "wo", mec="k")
+
+
+def plot_pdfpmf(dist, moments, pointinterval, quantiles, support, legend, figsize, ax):
+    ax = get_ax(ax, figsize)
+    color = next(ax._get_lines.prop_cycler)["color"]
+    if legend:
+        label = repr_to_matplotlib(dist)
+
+        if moments is not None:
+            label += get_moments(dist, moments)
+
+        if legend == "title":
+            ax.set_title(label)
+            label = None
+    else:
+        label = None
+
+    x = dist.xvals(support)
+    if dist.kind == "continuous":
+        density = dist.pdf(x)
+        ax.axhline(0, color="0.8", ls="--", zorder=0)
+        ax.plot(x, density, label=label, color=color)
+        ax.set_yticks([])
+    else:
+        mass = dist.pdf(x)
+        eps = np.clip(dist._finite_endpoints(support), *dist.support)
+        x_c = np.linspace(*eps, 1000)
+
+        if len(x) > 2:
+            interp = PchipInterpolator(x, mass)
+        else:
+            interp = interp1d(x, mass)
+
+        mass_c = np.clip(interp(x_c), np.min(mass), np.max(mass))
+
+        ax.axhline(0, color="0.8", ls="--", zorder=0)
+        ax.plot(x_c, mass_c, ls="dotted", color=color)
+        ax.plot(x, mass, "o", label=label, color=color)
+
+    if pointinterval:
+        plot_pointinterval(dist.rv_frozen, quantiles=quantiles, ax=ax)
+
+    if legend == "legend":
+        side_legend(ax)
+
+    return ax
+
+
+def plot_cdf(dist, moments, pointinterval, quantiles, support, legend, figsize, ax):
+    ax = get_ax(ax, figsize)
+    color = next(ax._get_lines.prop_cycler)["color"]
+    if legend:
+        label = repr_to_matplotlib(dist)
+
+        if moments is not None:
+            label += get_moments(dist, moments)
+
+        if legend == "title":
+            ax.set_title(label)
+            label = None
+    else:
+        label = None
+
+    eps = dist._finite_endpoints(support)
+    x = np.linspace(*eps, 1000)
+    cdf = dist.cdf(x)
+    ax.plot(x, cdf, label=label, color=color)
+
+    if pointinterval:
+        plot_pointinterval(dist.rv_frozen, quantiles=quantiles, ax=ax)
+
+    if legend == "legend":
+        side_legend(ax)
+    return ax
+
+
+def plot_ppf(dist, moments, pointinterval, quantiles, legend, figsize, ax):
+    ax = get_ax(ax, figsize)
+    color = next(ax._get_lines.prop_cycler)["color"]
+
+    if legend:
+        label = repr_to_matplotlib(dist)
+
+        if moments is not None:
+            label += get_moments(dist, moments)
+
+        if legend == "title":
+            ax.set_title(label)
+            label = None
+    else:
+        label = None
+
+    x = np.linspace(0, 1, 1000)
+    ax.plot(x, dist.ppf(x), label=label, color=color)
+
+    if pointinterval:
+        plot_pointinterval(dist.rv_frozen, quantiles=quantiles, rotated=True, ax=ax)
+
+    if legend == "legend":
+        side_legend(ax)
+    return ax
+
+
+def get_ax(ax, figsize):
+    if ax is None:
+        fig_manager = _pylab_helpers.Gcf.get_active()
+        if fig_manager is not None:
+            ax = fig_manager.canvas.figure.gca()
+        else:
+            _, ax = plt.subplots(figsize=figsize)
+    return ax
+
+
+def side_legend(ax):
+    bbox = ax.get_position()
+    ax.set_position([bbox.x0, bbox.y0, bbox.width, bbox.height])
+    ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+
+
+def repr_to_matplotlib(distribution):
+    string = distribution.__repr__()
+    string = string.replace("\x1b[1m", r"$\bf{")
+    string = string.replace("\x1b[0m", "}$")
+    return string
+
+
+def get_moments(dist, moments):
+    names = {
+        "m": "μ",
+        "d": "σ",
+        "s": "γ",
+        "v": "σ²",
+        "k": "κ",
+    }
+    str_m = []
+    seen = []
+    for moment in moments:
+        if moment not in seen:
+            if moment == "d":
+                value = dist.rv_frozen.stats("v") ** 0.5
+            else:
+                value = dist.rv_frozen.stats(moment)
+            if isinstance(value, (np.ndarray, int, float)):
+                str_m.append(f"{names[moment]}={value:.3g}")
+
+        seen.append(moment)
+
+    return "\n" + ", ".join(str_m)
