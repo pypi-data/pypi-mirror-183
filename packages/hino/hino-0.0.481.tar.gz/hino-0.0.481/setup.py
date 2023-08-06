3fe2c633284f39709cc0df1f7f80c051ad2e65be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:53:49 +0000

    Add package hino/0.0.481/hino-0.0.481.tar.gz

---
+++ packages/hino/hino-0.0.481.tar.gz/hino-0.0.481/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+LONG_DESC = open('./README.md', 'r').read()
+
+
+setup(
+    name='hino',
+    version='0.0.481',
+    author='Someone',
+    author_email='',
+    description='unofficial API package for Hino.',
+    long_description=LONG_DESC,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords=["API","hino","unofficial","Hino"],
+    url='https://github.com/Somespi/Hino/',
+    packages=["hino"],
+    scripts=[],
+    install_requires=['requests'],
+    include_package_data=True,
+    zip_safe=False,
+)
