14713f6285efe189e56c7f798f681a198c0998da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:11 +0000

    Add package QRNG-QL-OT/0.0.1/QRNG_QL_OT-0.0.1.tar.gz

---
+++ packages/QRNG-QL-OT/QRNG_QL_OT-0.0.1.tar.gz/QRNG_QL_OT-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='qrngotp',
+version='0.1',
+description='this is Quantum Random Number package',
+url='https://github.com/zuburz/quantlase_OTP',
+author='auth',
+author_email='hammadzahid2254@gmail.com',
+license='MIT',
+packages=['qrngotp'],
+zip_safe=False)
