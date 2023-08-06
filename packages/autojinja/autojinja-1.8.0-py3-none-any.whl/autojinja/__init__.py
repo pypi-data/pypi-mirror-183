d0241a9a024e505b01412009e2431d7adc652acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:56:38 +0000

    Add package autojinja/1.8.0/autojinja-1.8.0-py3-none-any.whl

---
+++ packages/autojinja/autojinja-1.8.0-py3-none-any.whl/autojinja/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "1.8.0"
+
+from . import defaults
+from . import exceptions
+from . import main
+from .parser import ParserSettings
+from . import path
+from . import templates
+from .templates import CogTemplate
+from .templates import JinjaTemplate
+from .templates import RawTemplate
+from . import utils
+
+import os
