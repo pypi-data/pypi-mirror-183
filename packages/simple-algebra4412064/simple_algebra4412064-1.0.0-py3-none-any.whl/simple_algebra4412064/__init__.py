627d93af79deb46b80f6c67de8e5e3472d137ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:09 +0000

    Add package simple-algebra4412064/1.0.0/simple_algebra4412064-1.0.0-py3-none-any.whl

---
+++ packages/simple-algebra4412064/simple_algebra4412064-1.0.0-py3-none-any.whl/simple_algebra4412064/__init__.py
@@ -0,0 +1,15 @@
+def add(a: float, b: float) -> float:
+    """Add two numbers and return the result """
+    return a + b
+
+def subtract(a: float, b: float) -> float:
+    """Subtract two numbers and return the result """
+    return a - b
+
+def multiply(a: float, b: float) -> float:
+    """Multiply two numbers and return the result """
+    return a * b
+
+def divide(a: float, b: float) -> float:
+    """Divide two numbers and return the result """
+    return a / b
