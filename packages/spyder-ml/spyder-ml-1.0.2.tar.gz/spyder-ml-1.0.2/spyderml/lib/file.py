e98cfc7697023c4275caf87da30c0fd3319a9d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:28 +0000

    Add package spyder-ml/1.0.2/spyder-ml-1.0.2.tar.gz

---
+++ packages/spyder-ml/spyder-ml-1.0.2.tar.gz/spyder-ml-1.0.2/spyderml/lib/file.py
@@ -0,0 +1,25 @@
+from spyderml.lib.detectos import detect
+
+
+def open_file(filepath):
+    try:
+        if '~/' in filepath:
+            filepath = filepath.replace('~/',detect())
+        with open(filepath, 'r') as file:
+            lines = file.read()
+            lines = lines.strip()
+        lines = lines.split('\n')
+        return lines
+    except FileNotFoundError:
+        print(f"File NotFound({filepath})")
+        exit()
+
+
+def save_output(filename, text):
+    if '~' in filename:
+        filename = filename.split('~/')
+        path = detect()
+        filename = path+filename[1]
+    with open(filename, 'a+') as file:
+        file.write(str(text))
+        file.write('\n' )
