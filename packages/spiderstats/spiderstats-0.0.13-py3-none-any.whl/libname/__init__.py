de71eeadc42202ae5d4558c6ba1f31e1a70576f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:53:49 +0000

    Add package spiderstats/0.0.13/spiderstats-0.0.13-py3-none-any.whl

---
+++ packages/spiderstats/spiderstats-0.0.13-py3-none-any.whl/libname/__init__.py
@@ -0,0 +1,6 @@
+import pandas as pandas
+import numpy as numpy
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import missingno as msn
+from eda import spider_stats
---
+++ packages/spiderstats/spiderstats-0.0.13-py3-none-any.whl/libname/eda.py
@@ -0,0 +1,3 @@
+def spider_stats(df):
+    df.describe()
+    print('pandasfucntion')
