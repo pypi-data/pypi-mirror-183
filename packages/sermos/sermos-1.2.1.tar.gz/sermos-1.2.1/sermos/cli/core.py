c85db2b5db20c804289ffe7b81281caee28996fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:35 +0000

    Add package sermos/1.2.1/sermos-1.2.1.tar.gz

---
+++ packages/sermos/sermos-1.2.1.tar.gz/sermos-1.2.1/sermos/cli/core.py
@@ -0,0 +1,46 @@
+""" Primary CLI group entrypoint
+"""
+import logging
+import click
+from sermos.logging_config import setup_logging
+
+setup_logging(default_level='INFO', establish_logging_config=False)
+logger = logging.getLogger(__name__)
+
+# Not all CLI tools will be functional / available depending on which extras
+# are installed. For example, the config server won't work if the `workers`
+# extra isn't available, which installs celery and networkx.
+collection = []
+
+warning_msg = "{} CLI tools are not available. This is most "\
+               "likely due to a missing import. Verify you have the correct "\
+               "Sermos extras installed."
+try:
+    from sermos.cli.deploy import deployment
+    collection.append(deployment)
+except ImportError as e:
+    logger.warning(warning_msg.format("Deployment"))
+    logger.warning(f"{e}")
+
+try:
+    from sermos.cli.config_server import config_server
+    collection.append(config_server)
+except ImportError as e:
+    logger.warning(warning_msg.format("Configuration Server"))
+    logger.warning(f"{e}")
+
+try:
+    from sermos.cli.proxy import sermos_proxy
+    collection.append(sermos_proxy)
+except ImportError as e:
+    logger.warning(warning_msg.format("Proxy Service"))
+    logger.warning(f"{e}")
+
+try:
+    from sermos.cli.version import get_version
+    collection.append(get_version)
+except ImportError as e:
+    logger.warning(warning_msg.format("Version"))
+    logger.warning(f"{e}")
+
+sermos = click.CommandCollection(sources=collection)
