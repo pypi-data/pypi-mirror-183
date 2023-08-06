e8121f67f60eb2d3adf4339cd7c246b7ccb6acea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:29 +0000

    Add package infoml/0.1.0/infoml-0.1.0.tar.gz

---
+++ packages/infoml/infoml-0.1.0.tar.gz/infoml-0.1.0/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['infoml',
+ 'infoml.binf',
+ 'infoml.bmes.ahmet',
+ 'infoml.bmes.ahmet.binf',
+ 'infoml.bmes.ahmet.oldfiles',
+ 'infoml.bmes.ahmet.qsb',
+ 'infoml.hcitools.hcitools']
+
+package_data = \
+{'': ['*'],
+ 'infoml': ['hcitools/*',
+            'hcitools/.vscode/*',
+            'hcitools/docs/*',
+            'hcitools/docs/assets/*',
+            'hcitools/docs/examples/*',
+            'hcitools/docs/hcitools/*',
+            'hcitools/scripts/*'],
+ 'infoml.bmes.ahmet': ['@bmes/*', 'cbe/*', 'ta/*', 'temp/*'],
+ 'infoml.bmes.ahmet.qsb': ['copy of some presentations .DELETE LATER/*',
+                           'keggdemo/*',
+                           'temp/*'],
+ 'infoml.hcitools.hcitools': ['datasets/*']}
+
+setup_kwargs = {
+    'name': 'infoml',
+    'version': '0.1.0',
+    'description': 'Python package for bioinformatics analysis and machine learning.',
+    'long_description': '# infoml\n\nPython package for bioinformatics analysis and machine learning.\n\n## Installation\n\n```bash\n$ pip install infoml\n```\n\n## Usage\n\n- TODO\n\n## Contributing\n\nInterested in contributing? Check out the contributing guidelines. Please note that\nthis project is released with a Code of Conduct. By contributing to this project, you\nagree to abide by its terms.\n\n## License\n\n`infoml` was created by Tony Kabilan Okeke. It is licensed under the terms of the MIT \nlicense.\n\n## Credits\n\n`infoml` was created with [`cookiecutter`](https://cookiecutter.readthedocs.io/en/latest/) \nand the `py-pkgs-cookiecutter` [template](https://github.com/py-pkgs/py-pkgs-cookiecutter).\n\n',
+    'author': 'Tony Kabilan Okeke',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
