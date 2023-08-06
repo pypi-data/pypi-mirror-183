0f4d338784cba2e8fcb495f47ce0a7fb89e090d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:00 +0000

    Add package nester-laga/1.3.0/nester_laga-1.3.0.tar.gz

---
+++ packages/nester-laga/nester_laga-1.3.0.tar.gz/nester_laga-1.3.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    
+    name          = 'nester_laga',
+    version       = '1.3.0',
+    py_modules    = ['nester_laga'],
+    author        = 'lagabreu',
+    author_email  =  'lagabreu@yahoo.com.br',
+    url           =  'http://lagabreu.org',
+    description   = 'Um impressor simples de listas aninhadas.'
+
+  
+    )
