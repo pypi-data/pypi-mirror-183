84f9fb06ca88c471047e1eb7098ae7a89d4fee31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:15 +0000

    Add package ruff/0.0.202/ruff-0.0.202.tar.gz

---
+++ packages/ruff/ruff-0.0.202.tar.gz/ruff-0.0.202/src/mccabe/mod.rs
@@ -0,0 +1,31 @@
+pub mod checks;
+pub mod settings;
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use anyhow::Result;
+    use test_case::test_case;
+
+    use crate::checks::CheckCode;
+    use crate::linter::test_path;
+    use crate::{mccabe, Settings};
+
+    #[test_case(0)]
+    #[test_case(3)]
+    #[test_case(10)]
+    fn max_complexity_zero(max_complexity: usize) -> Result<()> {
+        let snapshot = format!("max_complexity_{max_complexity}");
+        let mut checks = test_path(
+            Path::new("./resources/test/fixtures/mccabe/C901.py"),
+            &Settings {
+                mccabe: mccabe::settings::Settings { max_complexity },
+                ..Settings::for_rules(vec![CheckCode::C901])
+            },
+        )?;
+        checks.sort_by_key(|check| check.location);
+        insta::assert_yaml_snapshot!(snapshot, checks);
+        Ok(())
+    }
+}
