cc594c8181b49df0c88590221eb0450dce7eebb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:16:42 +0000

    Add package smb3-eh-manip/1.2.0/smb3_eh_manip-1.2.0-py3-none-any.whl

---
+++ packages/smb3-eh-manip/smb3_eh_manip-1.2.0-py3-none-any.whl/smb3_eh_manip/util/events.py
@@ -0,0 +1,31 @@
+"""
+Helper class for eventing. Currently unused.
+"""
+from dataclasses import asdict, dataclass
+import logging
+
+from pydispatch import dispatcher
+
+
+@dataclass
+class LagFramesObserved:
+    current_frame: float
+    observed_lag_frames: int
+    observed_load_frames: int
+
+
+@dataclass
+class AddActionFrame:
+    action_frame: int
+    window: int
+
+
+def listen(event_type, callback, **kwargs):
+    # Listen to all events with event_type
+    dispatcher.connect(callback, signal=event_type, **kwargs)
+
+
+def emit(sender, event, **kwargs):
+    # Emit an event with the given event_type
+    logging.debug(f"Emitting {type(event).__name__} event: {asdict(event)}")
+    dispatcher.send(type(event), sender, event=event, **kwargs)
