e265fcd9f02caa45c4255e5094b6207721e524c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:31 +0000

    Add package cripto-show/1.0.0/cripto_show-1.0.0.tar.gz

---
+++ packages/cripto-show/cripto_show-1.0.0.tar.gz/cripto_show-1.0.0/README.md
@@ -0,0 +1,4 @@
+# Cripto_show
+Usefull library for web scrapping cripto currencys value and plotting with metrics on streamlit.
+## Running mode
+Use the command streamlit run streamlit_file.py and enjoy the web
---
+++ packages/cripto-show/cripto_show-1.0.0.tar.gz/cripto_show-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cripto_show',
+    version='1.0.0',
+    description='Usefull library for web scrapping cripto currencys value and plotting with metrics on streamlit.',
+    author='Enrique and Javier',
+    author_email='pepeelhackeos@gmail.com',
+    url='https://github.com/JavCaracuel/Cripto_show'
+)
