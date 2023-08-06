4630cd71bc82d9ad1946940f6d389b95e5b6bdd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:25:18 +0000

    Add package pymince/2.1.0/pymince-2.1.0-py3-none-any.whl

---
+++ packages/pymince/pymince-2.1.0-py3-none-any.whl/pymince/functional.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import functools
+
+
+class classproperty:
+    """
+    Decorator that converts a method with a single cls argument into a property
+    that can be accessed directly from the class.
+
+    Examples:
+        from pymince.functional import classproperty
+
+        class MyClass:
+            __foo = "var"
+
+            @classproperty
+            def foo(cls):
+                return cls.__foo
+    """
+
+    def __init__(self, method=None):
+        self.fget = method
+
+    def __get__(self, instance, cls=None):
+        return self.fget(cls)
+
+
+def pipe(*fns):
+    """
+    Simple pipe function implementation using function composition.
+
+    :param fns: Functions to pipe.
+    :rtype: Callable[[Any], Any]
+
+    Examples:
+        from pymince.functional import pipe
+
+        addtwo = lambda n: n + 2
+        double = lambda n: n * 2
+        square = lambda n: n * n
+
+        fn = pipe(addtwo, double, square)
+        fn(1) # 36
+    """
+
+    def wrap(arg):
+        return functools.reduce(lambda v, fn: fn(v), fns, arg)
+
+    return wrap
