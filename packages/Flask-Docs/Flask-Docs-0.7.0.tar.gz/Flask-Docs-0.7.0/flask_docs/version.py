0f29f43aa9aae612a81bd59c2f388d8cf63e8c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:29 +0000

    Add package Flask-Docs/0.7.0/Flask-Docs-0.7.0.tar.gz

---
+++ packages/Flask-Docs/Flask-Docs-0.7.0.tar.gz/Flask-Docs-0.7.0/flask_docs/version.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+Program:
+    Flask-Docs Version
+History:
+    Created on 2021/06/06
+    Last modified on 2022/12/28
+Author:
+    kwkw
+"""
+
+__version__ = "0.7.0"
