adff0afc4994587d93a189cdbeb58799ee02879f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:29 +0000

    Add package bountyful/0.0.1/bountyful-0.0.1-py3-none-any.whl

---
+++ packages/bountyful/bountyful-0.0.1-py3-none-any.whl/bountyful/crowd.py
@@ -0,0 +1,7 @@
+class TuningCrowd:
+    def __init__(self, n=30, mode="RLHF"):
+        self.n = n
+        self.mode = mode
+
+    def feed(r):
+        raise NotImplementedError("Please contact Bountyful team for beta access.")
---
+++ packages/bountyful/bountyful-0.0.1-py3-none-any.whl/bountyful/optim.py
@@ -0,0 +1,4 @@
+class RLHF:
+    def __init__(crowd):
+        self.crowd = crowd
+        raise NotImplementedError("Please contact Bountyful team for beta access.")
