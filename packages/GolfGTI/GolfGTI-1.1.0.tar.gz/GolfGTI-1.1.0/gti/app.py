82aa269c4b927f5046ae4f2c0c5159561e23c9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:10 +0000

    Add package GolfGTI/1.1.0/GolfGTI-1.1.0.tar.gz

---
+++ packages/GolfGTI/GolfGTI-1.1.0.tar.gz/GolfGTI-1.1.0/gti/app.py
@@ -0,0 +1,44 @@
+import os
+import click
+from tkinter import *
+
+@click.command()
+def show_gti():
+    # Create the window
+    root = Tk()
+    root.config(bg="#454545")
+    root.overrideredirect(True)
+    root.wm_attributes("-topmost", True)
+    root.wm_attributes("-transparentcolor", "white")
+    path = os.path.dirname(os.path.realpath(__file__))
+
+    # Load the image
+    photo = PhotoImage(file= path + "/gti.png")
+    width, height = photo.width(), photo.height()
+
+    # Place image on canvas for transparency
+    canvas = Canvas(root, bg="white", width=width,
+                    height=height, highlightthickness=0)
+    canvas.pack()
+    canvas.create_image(0, 0, image=photo, anchor=NW)
+
+    screen_width = root.winfo_screenwidth() * 2
+    screen_height = root.winfo_screenheight()
+    x = 0 - width
+    y = screen_height - height
+
+    # Move window from left to right
+    shouldRun = True
+    try:
+        while shouldRun:
+            root.geometry(f"{width}x{height}+{x}+{y}")
+            root.update()
+            x += 1
+            if x > screen_width:
+                shouldRun = False
+    except KeyboardInterrupt:
+        pass
+    root.destroy()
+
+if __name__ == '__main__':
+    show_gti()
---
+++ packages/GolfGTI/GolfGTI-1.1.0.tar.gz/GolfGTI-1.1.0/setup.py
@@ -0,0 +1,32 @@
+#python setup.py check
+
+#python setup.py sdist
+
+#python setup.py bdist_wheel
+
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='GolfGTI',
+    version='1.1.0',
+    description='A simple script that shows a Golf GTI when you accidentally type gti instead of git.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Wouter van der Velde',
+    author_email='woutervandervelde2000@hotmail.com',
+    url='https://github.com/WoutervdVelde',
+    license='MIT',
+    python_requires='>=3.1',
+    requires=['click', 'tkinter'],
+    packages=['gti'],
+    entry_points = {
+        'console_scripts': ['gti=gti.app:show_gti'],
+    },
+    include_package_data=True,
+)
