a0be00c6a819ec66a7f61878db36f020bb7ee91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:07 +0000

    Add package sphinxcontrib-confluencebuilder/2.0.0/sphinxcontrib-confluencebuilder-2.0.0.tar.gz

---
+++ packages/sphinxcontrib-confluencebuilder/sphinxcontrib-confluencebuilder-2.0.0.tar.gz/sphinxcontrib-confluencebuilder-2.0.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:copyright: Copyright 2016-2022 Sphinx Confluence Builder Contributors (AUTHORS)
+:license: BSD-2-Clause (LICENSE)
+"""
+
+import setuptools
+
+if __name__ == '__main__':
+    setuptools.setup()
---
+++ packages/sphinxcontrib-confluencebuilder/sphinxcontrib-confluencebuilder-2.0.0.tar.gz/sphinxcontrib-confluencebuilder-2.0.0/sphinxcontrib/confluencebuilder/writer.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+:copyright: Copyright 2016-2022 Sphinx Confluence Builder Contributors (AUTHORS)
+:license: BSD-2-Clause (LICENSE)
+"""
+
+from docutils import writers
+
+
+class ConfluenceWriter(writers.Writer):
+    supported = ('text',)
+    settings_spec = ('No options here.', '', ())
+    settings_defaults = {}
+
+    def __init__(self, builder):
+        writers.Writer.__init__(self)
+        self.builder = builder
+        self.output = None
+
+    def translate(self):
+        visitor = self.builder.create_translator(self.document, self.builder)
+        self.document.walkabout(visitor)
+        if hasattr(visitor, 'body_final'):
+            self.output = visitor.body_final
