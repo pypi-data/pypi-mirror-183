3c6c5cb900ece2ce4b8106c48b527aa5e63b8148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:56 +0000

    Add package earnapp/0.1.9/earnapp-0.1.9.tar.gz

---
+++ packages/earnapp/earnapp-0.1.9.tar.gz/earnapp-0.1.9/setup.py
@@ -0,0 +1,34 @@
+"""
+EarnApp.py - A Python library to interact with the EarnApp API
+Copyright (C) 2022  Woodie
+
+This file is part of EarnApp.py.
+
+EarnApp.py is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+EarnApp.py is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with EarnApp.py. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from setuptools import find_packages, setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='earnapp',
+    packages=find_packages(),
+    version='0.1.9',
+    description='A python library to interact with the EarnApp API',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Woodie',
+    author_email="woodie@woodie.cf",
+    url='https://github.com/Woodie-07/earnapp.py',
+    install_requires=['requests'],
+)
