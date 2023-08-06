8e3e532218a025e3f9f4944634e17b1d10af96fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:18 +0000

    Add package pyrenoweb/1.0.6/pyrenoweb-1.0.6.tar.gz

---
+++ packages/pyrenoweb/pyrenoweb-1.0.6.tar.gz/pyrenoweb-1.0.6/pyrenoweb/const.py
@@ -0,0 +1,34 @@
+BASE_URL = "https://servicesgh.renoweb.dk/v1_13"
+DAWA_URL = "https://dawa.aws.dk/kommuner"
+
+DEFAULT_TIMEOUT = 10
+NO_WASTE_SCHEDULE_TIMESTAMP = 1767135600
+
+WASTE_LIST = [
+    {"type": "Restaffald-Madaffald", "icon": "mdi:trash-can"},
+    {"type": "Dagrenovation", "icon": "mdi:trash-can"},
+    {"type": "Metal-Glas", "icon": "mdi:glass-fragile"},
+    {"type": "PAPPI", "icon": "mdi:recycle"},
+    {"type": "Farligt affald", "icon": "mdi:biohazard"},
+    {"type": "Tekstiler", "icon": "mdi:hanger"},
+    {"type": "Jern", "icon": "mdi:bucket"},
+    {"type": "Papir", "icon": "mdi:file"},
+    {"type": "Pap", "icon": "mdi:note"},
+    {"type": "Plast Metal", "icon": "mdi:trash-can-outline"},
+    {"type": "Storskrald", "icon": "mdi:table-furniture"},
+    {"type": "Haveaffald", "icon": "mdi:leaf"},
+]
+
+class color:
+    """Defines colors used for Terminal Output."""
+
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
