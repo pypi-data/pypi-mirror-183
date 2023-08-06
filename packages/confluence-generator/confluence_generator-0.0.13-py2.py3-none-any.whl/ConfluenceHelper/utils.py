0afac45590c1956c49be25efe72d61b6d8c29d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:23 +0000

    Add package confluence-generator/0.0.13/confluence_generator-0.0.13-py2.py3-none-any.whl

---
+++ packages/confluence-generator/confluence_generator-0.0.13-py2.py3-none-any.whl/ConfluenceHelper/utils.py
@@ -0,0 +1,19 @@
+class ColourUtils:
+    def __init__(self):
+        self._init_colours()
+
+    def _init_colours(self):
+        self.blue = "#0052CC"
+        self.dark_blue = "#172B4D"
+        self.light_blue = "#00B8D9"
+        self.white = "#FFFFFF"
+        self.grey = "#C1C7D0"
+        self.red = "#FF5630"
+        self.yellow = "#FFAB00"
+        self.green = "#36B37E"
+        self.purple = "#6554C0"
+        self.black = "#000000"
+        self.light_grey = "#EBECF0"
+
+
+COLOUR = ColourUtils()
