9c573d7cb5db857f8a39458c75cf4bbb6f0dd3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:24 +0000

    Add package kinneyotp/0.0.7/kinneyotp-0.0.7.tar.gz

---
+++ packages/kinneyotp/kinneyotp-0.0.7.tar.gz/kinneyotp-0.0.7/test/test_key.py
@@ -0,0 +1,12 @@
+from kinneyotp.key import Key
+
+def test_key_with_defaults_and_seed_set():
+    k = Key(seed=10)
+    assert k.generated == "sbnpsagopiuzfbqpkchx"
+    assert len(k.generated) == 20
+
+
+def test_key_with_defaults_and_seed_set_override_length():
+    k = Key(seed=10, length=10)
+    assert k.generated == "sbnpsagopi"
+    assert len(k.generated) == 10
