d621127f45d3cf6773d3a8098b6151205b12a7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:13 +0000

    Add package damv1time7/0.1.7/damv1time7-0.1.7-py3-none-any.whl

---
+++ packages/damv1time7/damv1time7-0.1.7-py3-none-any.whl/damv1time7/mylogger.py
@@ -0,0 +1,28 @@
+import damv1airtableprojectk8salert as airtblk8salert
+
+from enum import Enum
+from .mytime7 import currentTime7 as cT7
+
+class const_thread(Enum):
+    number_1 = 1
+    number_2 = 2
+    number_3 = 3
+
+aCls = airtblk8salert.sandbox()
+
+def logger(_time=cT7(), *args, **kwargs):   
+    
+    print(_time,' '.join(args))
+
+    try:
+        threadNumber = const_thread.number_1.value
+        if '_argThreadNumber' in kwargs:
+            threadNumber = kwargs.get("_argThreadNumber") 
+            if "'int'" in str(type(threadNumber)):
+                idAirtable = None
+                if '_argIdAirtable' in kwargs:
+                    idAirtable = kwargs.get("_argIdAirtable") 
+                    if str(idAirtable).strip()!='':
+                        aCls.pyairtable_updateDateTime_CurrentNumberLastOfLog(threadNumber,idAirtable)
+    except ValueError:
+        pass
