7965e21b66265c7f951ff8bb1d05c0eaff0d9def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:24 +0000

    Add package description-generator/0.1.2/description_generator-0.1.2.tar.gz

---
+++ packages/description-generator/description_generator-0.1.2.tar.gz/description_generator-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+long_description = open("README.md", "r").read()
+setup(
+    name="description_generator",
+    version="0.1.2",
+    py_modules=["description_generator"],
+    entry_points={
+        "console_scripts": [
+            "description_generator=description_generator:main"
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="Generate a personal description for a GitHub user's profile.",
+    author="Wadjih Bencheikh",
+    author_email="jw_bencheikh@esi.dz",
+    install_requires=[
+        "openai",
+        "requests"
+    ],
+)
