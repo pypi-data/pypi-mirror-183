5074fe35d50590903cbf59798f15e2946b4b1cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:09 +0000

    Add package sto-plugin/0.0.1/sto-plugin-0.0.1.tar.gz

---
+++ packages/sto-plugin/sto-plugin-0.0.1.tar.gz/sto-plugin-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sto-plugin',
+    version='0.0.1',
+    license='NA',
+    author="",
+    author_email='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='stub',
+    install_requires=[],
+)
