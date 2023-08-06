18712988e01ee6437cf942f1f6ed52e60520cb63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:59 +0000

    Add package heart-heart/0.0.1/heart-heart-0.0.1.tar.gz

---
+++ packages/heart-heart/heart-heart-0.0.1.tar.gz/heart-heart-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import *
+
+setup(name='heart-heart',
+      version='0.0.1',
+      description='This is a Python algorithm for generating text summaries.',
+      author='LiuZhenming',
+      author_email='2124619132@qq.com',
+      requires= ['nltk','jieba'],
+      packages=find_packages(),
+      license="gun general public 3.0"
+      )
