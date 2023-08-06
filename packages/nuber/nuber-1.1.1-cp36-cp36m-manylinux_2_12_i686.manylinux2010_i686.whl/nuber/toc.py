e08b2c04b5f97c069aee2f117942c61a9ed46bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:04 +0000

    Add package nuber/1.1.1/nuber-1.1.1-cp36-cp36m-manylinux_2_12_i686.manylinux2010_i686.whl

---
+++ packages/nuber/nuber-1.1.1-cp36-cp36m-manylinux_2_12_i686.manylinux2010_i686.whl/nuber/toc.py
@@ -0,0 +1,13 @@
+import curses
+from .listview import ListView
+
+class Toc(ListView):
+    def __init__(self, stdscr: curses.window, toc: list[tuple[str, int]], keybinds: dict | None = None) -> None:
+        keys = {ord("t"): "close_view"}
+        if keybinds is not None:
+            keys.update(keybinds)
+        super().__init__(stdscr, toc, keybinds=keys)
+        self.title = "Table of Contents"
+
+    def determine_selected_row(self, chapter_idx) -> int:
+        return next((idx for idx, val in enumerate(map(lambda c: c[1] > chapter_idx, self.data)) if val), len(self.data)) - 1
