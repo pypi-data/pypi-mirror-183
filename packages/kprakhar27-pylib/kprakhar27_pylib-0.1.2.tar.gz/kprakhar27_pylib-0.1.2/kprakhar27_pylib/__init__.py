66404ab9c6d3f87ec376fc537176fd69ebd716e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:27:24 +0000

    Add package kprakhar27-pylib/0.1.2/kprakhar27_pylib-0.1.2.tar.gz

---
+++ packages/kprakhar27-pylib/kprakhar27_pylib-0.1.2.tar.gz/kprakhar27_pylib-0.1.2/kprakhar27_pylib/__init__.py
@@ -0,0 +1,5 @@
+# Warning: If you import a lot of classes into this file, 
+# the order of the declarations can be important! 
+# Especially when your classes need other parent classes to run.
+
+from kprakhar27_pylib.multiplication import Multiplication
