4680c4d6b985a6e8688187055a10fee966e2153e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:11 +0000

    Add package netbox-data-disk/0.0.0.0.2/netbox_data_disk-0.0.0.0.2-py3-none-any.whl

---
+++ packages/netbox-data-disk/netbox_data_disk-0.0.0.0.2-py3-none-any.whl/netbox_data_disk/template_content.py
@@ -0,0 +1,53 @@
+from django.contrib.contenttypes.models import ContentType
+from extras.plugins import PluginTemplateExtension
+
+from .models import AccessList
+
+__all__ = (
+    "AccessLists",
+    "DeviceAccessLists",
+    "VirtualChassisAccessLists",
+    "VMAccessLists",
+)
+
+
+class AccessLists(PluginTemplateExtension):
+    def right_page(self):
+        obj = self.context["object"]
+
+        access_lists = None
+        ctype = ContentType.objects.get_for_model(obj)
+        if ctype.model in ["device", "virtualchassis", "virtualmachine"]:
+            access_lists = AccessList.objects.filter(
+                assigned_object_id=obj.pk,
+                assigned_object_type=ctype,
+            )
+
+        return self.render(
+            "inc/assigned_host/access_lists.html",
+            extra_context={
+                "access_lists": access_lists,
+                "type": ctype.model
+                if ctype.model == "device"
+                else ctype.name.replace(" ", "_"),
+            },
+        )
+
+
+class DeviceAccessLists(AccessLists):
+    model = "dcim.device"
+
+
+class VirtualChassisAccessLists(AccessLists):
+    model = "dcim.virtualchassis"
+
+
+class VMAccessLists(AccessLists):
+    model = "virtualization.virtualmachine"
+
+
+template_extensions = [
+    DeviceAccessLists,
+    VirtualChassisAccessLists,
+    VMAccessLists,
+]
