6658544865b46698e97ec55f3e52ec2257c8191e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:56 +0000

    Add package manot/0.1.5/manot-0.1.5.tar.gz

---
+++ packages/manot/manot-0.1.5.tar.gz/manot-0.1.5/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='manot',
+    version='0.1.5',
+    description='the manot package',
+    author='manot',
+    author_email='engineering@manot.ai',
+    url='https://www.manot.ai',
+    packages=['manot'],
+    package_dir={'manot': 'src/manot'},
+)
