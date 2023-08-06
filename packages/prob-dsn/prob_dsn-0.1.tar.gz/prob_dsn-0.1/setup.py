de62562bb7cb4a3b3a9147257ec21303d9c4f93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:52 +0000

    Add package prob-dsn/0.1/prob_dsn-0.1.tar.gz

---
+++ packages/prob-dsn/prob_dsn-0.1.tar.gz/prob_dsn-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_dsn',
+      version='0.1',
+      description='Probability Distributions',
+      packages=['prob_dsn'],
+      author = 'Seyifunmi Owoeye',
+      author_email = 'seyifunmiowoeye@gmail.com',
+      zip_safe=False)
