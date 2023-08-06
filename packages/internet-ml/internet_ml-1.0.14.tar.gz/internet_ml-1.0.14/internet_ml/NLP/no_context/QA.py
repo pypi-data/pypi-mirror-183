0a6ecf8dd913a947d5e97ca21f36aa8b7e2ce3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:42 +0000

    Add package internet-ml/1.0.14/internet_ml-1.0.14.tar.gz

---
+++ packages/internet-ml/internet_ml-1.0.14.tar.gz/internet_ml-1.0.14/internet_ml/NLP/no_context/QA.py
@@ -0,0 +1,44 @@
+from typing import Any, List, Tuple
+
+import logging
+import os
+import sys
+from pathlib import Path
+
+import dotenv
+from transformers import pipeline
+
+dotenv.load_dotenv()
+
+logging.basicConfig(
+    filename="QA.log",
+    filemode="w",
+    level=logging.INFO,
+    format="%(name)s - %(levelname)s - %(message)s",
+)
+
+sys.path.append(str(Path(__file__).parent.parent.parent) + "/tools/NLP/data")
+sys.path.append(str(Path(__file__).parent.parent.parent) + "/utils")
+import config
+import internet
+
+
+def answer(
+    query: str, GOOGLE_SEARCH_API_KEY: str, GOOGLE_SEARCH_ENGINE_ID: str
+) -> tuple[Any, list[str]]:
+    QA_MODEL: Any = pipeline("question-answering")
+    results: tuple[list[str], list[str]] = internet.Google(
+        query, GOOGLE_SEARCH_API_KEY, GOOGLE_SEARCH_ENGINE_ID
+    ).google()
+    answer: tuple[Any, list[str]] = (
+        QA_MODEL(question=query, context=str(results[0])),
+        results[1],
+    )
+    if config.CONF_DEBUG:
+        logging.info(f"Answer: {answer}")
+    return answer
+
+
+print(os.environ)
+print(answer("Who is Elon Musk?", os.environ.get("INTERNET_ML_GOOGLE_API"), os.environ.get("INTERNET_ML_GOOGLE_SEARCH_ENGINE_ID")))
+# def custom_answer
