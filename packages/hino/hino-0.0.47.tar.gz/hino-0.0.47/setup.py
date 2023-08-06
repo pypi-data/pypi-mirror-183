c8173fcd4a591a042822b68e2cfe6478251acd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:41 +0000

    Add package hino/0.0.47/hino-0.0.47.tar.gz

---
+++ packages/hino/hino-0.0.47.tar.gz/hino-0.0.47/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+LONG_DESC = open('./README.md', 'r').read()
+
+
+setup(
+    name='hino',
+    version='0.0.47',
+    author='Someone',
+    author_email='',
+    description='unofficial API package for Hino.',
+    long_description=LONG_DESC,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords=["API","hino","unofficial","Hino"],
+    url='https://github.com/Somespi/Hino/',
+    packages=find_packages(),
+    scripts=[],
+    install_requires=['requests'],
+    include_package_data=True,
+    zip_safe=False,
+)
