20932bc26328b97d1bad63dc023a46e34a1b97d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:07 +0000

    Add package netbox-data-disk/0.0.0.0.13/netbox_data_disk-0.0.0.0.13-py3-none-any.whl

---
+++ packages/netbox-data-disk/netbox_data_disk-0.0.0.0.13-py3-none-any.whl/netbox_data_disk/urls.py
@@ -0,0 +1,17 @@
+"""
+Map Views to URLs.
+"""
+
+from django.urls import path
+
+from netbox_data_disk import views
+
+urlpatterns = (
+    # Access Lists
+    path("data-disk/", views.DataDiskListView.as_view(), name="datadisk_list"),
+    path("data-disk/add/", views.DataDiskEditView.as_view(), name="datadisk_add"),
+    # path('access-lists/edit/', views.AccessListBulkEditView.as_view(), name='accesslist_bulk_edit'),
+    path('data-disk/<int:pk>/', views.DataDiskListView.as_view(), name='datadisk'),
+    path('data-disk/<int:pk>/edit/', views.DataDiskListView.as_view(), name='datadisk_edit'),
+    path('data-disk/<int:pk>/delete/', views.DataDiskListView.as_view(), name='datadisk_delete'),
+)
