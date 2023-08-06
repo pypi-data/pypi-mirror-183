91e7b0012af8b5ac4f0e9747ea0bbc6b38c0be46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:55:36 +0000

    Add package ingl/0.0.1/ingl-0.0.1.tar.gz

---
+++ packages/ingl/ingl-0.0.1.tar.gz/ingl-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'ingl',
+    version = '0.0.1',
+    description = "A command line interface for interacting with Ingl-DAO Program Instructions",
+    author = 'Ingl',
+    author_email = 'admin@ingl.io',
+    url = 'https://www.ingl.io',
+    packages = find_packages(),
+    install_requires = [
+        'asyncclick',
+        'solana',
+        'borsh_construct',
+        'base58',
+        'rich',
+        'ledgerblue',
+        ],
+    entry_points = '''
+    [console_scripts]
+    ingl=ingl_cli:entry
+    '''
+)
