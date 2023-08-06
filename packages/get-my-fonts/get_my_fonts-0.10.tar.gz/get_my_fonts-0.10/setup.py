9420750b33ff637c697c7747f19a9b5be4e17c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:21 +0000

    Add package get-my-fonts/0.10/get_my_fonts-0.10.tar.gz

---
+++ packages/get-my-fonts/get_my_fonts-0.10.tar.gz/get_my_fonts-0.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.10'
+DESCRIPTION = " Lists all installed fonts"
+
+# Setting up
+setup(
+    name="get_my_fonts",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/get_my_fonts',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=['fontTools', 'numpy', 'pandas'],
+    keywords=['fonts', 'ttf', 'windows'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.9', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Text Editors :: Text Processing', 'Topic :: Text Processing :: General', 'Topic :: Text Processing :: Indexing', 'Topic :: Text Processing :: Filters', 'Topic :: Utilities'],
+    install_requires=['fontTools', 'numpy', 'pandas'],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
