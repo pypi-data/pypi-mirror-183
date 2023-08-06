6ebf2454d3e80f5d6affa4ecfe4582728f43ad64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:02 +0000

    Add package betwatch/0.4.17/betwatch-0.4.17-py3-none-any.whl

---
+++ packages/betwatch/betwatch-0.4.17-py3-none-any.whl/betwatch/types/__init__.py
@@ -0,0 +1,13 @@
+from .bookmakers import Bookmaker
+from .filters import RaceProjection
+from .race import Race, Runner, Meeting, RaceLink
+from .markets import (
+    MarketPriceType,
+    Price,
+    Fluc,
+    BetfairTick,
+    BetfairSide,
+    BookmakerMarket,
+    BetfairMarket,
+    BetfairTick,
+)
