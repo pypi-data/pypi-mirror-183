7d85de93b50bb2d8012d4db64eef6482334b0392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:24 +0000

    Add package matmul/0.0.1/matmul-0.0.1.tar.gz

---
+++ packages/matmul/matmul-0.0.1.tar.gz/matmul-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='matmul',
+      version='0.0.1',
+      description='Matrix Multiplication for 2 matrices',
+      author='Dhruv Gupta',
+      packages=['matmul'],
+      zip_safe=False)
