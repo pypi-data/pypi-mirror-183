3095a68b4c6b2a370ccdf381d8f08dfabd81a6a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:23:53 +0000

    Add package tapioca-arbache/2.2.0/tapioca_arbache-2.2.0.tar.gz

---
+++ packages/tapioca-arbache/tapioca_arbache-2.2.0.tar.gz/tapioca_arbache-2.2.0/tapioca_arbache/__init__.py
@@ -0,0 +1,4 @@
+from tapioca_arbache.tapioca_arbache import (
+    PerfilClient, OauthClient, LicencaClient, JogoSelfClient, RelatorioClient,
+    JogoClient, EquipeClient, PlayMidiaClient, InterfaceClient, CrmMidiaClient
+)
