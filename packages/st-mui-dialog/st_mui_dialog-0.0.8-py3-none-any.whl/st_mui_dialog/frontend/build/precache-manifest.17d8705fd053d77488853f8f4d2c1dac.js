01e0b652f8888179de464a20c2ba692699afc7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:34 +0000

    Add package st-mui-dialog/0.0.8/st_mui_dialog-0.0.8-py3-none-any.whl

---
+++ packages/st-mui-dialog/st_mui_dialog-0.0.8-py3-none-any.whl/st_mui_dialog/frontend/build/precache-manifest.17d8705fd053d77488853f8f4d2c1dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79cdbddab8585c57b69bebd8acd89a3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23416afd1954fc425261",
+    "url": "./static/js/2.0fce2338.chunk.js"
+  },
+  {
+    "revision": "2c41efe49968baecc5f58fc671d668d2",
+    "url": "./static/js/2.0fce2338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57778137c98a68d0dfee",
+    "url": "./static/js/main.8bd25ac0.chunk.js"
+  },
+  {
+    "revision": "4eae9cfdeaa4b417ff84",
+    "url": "./static/js/runtime-main.7050f38a.js"
+  }
+]);
