3cdfd76393fe9a8b7140edf927daa733bb45817b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:40 +0000

    Add package smpl-doc/1.0.5/smpl_doc-1.0.5.tar.gz

---
+++ packages/smpl-doc/smpl_doc-1.0.5.tar.gz/smpl_doc-1.0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['smpl_doc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['deprecation']
+
+setup_kwargs = {
+    'name': 'smpl-doc',
+    'version': '1.0.5',
+    'description': 'SiMPLe plotting and fitting',
+    'long_description': 'smpl_io\n',
+    'author': 'Alexander Puck Neuwirth',
+    'author_email': 'alexander@neuwirth-informatik.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/APN-Pucky/smpl_io',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
