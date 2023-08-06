5235da7d334241c09e0e51b390f113507d2ab642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:30 +0000

    Add package riweather/0.1.0/riweather-0.1.0-py3-none-any.whl

---
+++ packages/riweather/riweather-0.1.0-py3-none-any.whl/riweather/__init__.py
@@ -0,0 +1,26 @@
+"""riweather.
+
+Grab publicly available weather data.
+"""
+__version__ = "0.1.0"
+
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+_dbpath = os.path.join(os.path.expanduser("~"), ".riweather")
+try:
+    os.mkdir(_dbpath)
+except FileExistsError:
+    pass
+metadata_engine = create_engine(f"sqlite+pysqlite:///{_dbpath}/metadata.db")
+
+from riweather.db import Base
+
+Base.metadata.create_all(metadata_engine)
+MetadataSession = sessionmaker(metadata_engine)
+
+from riweather.connection import NOAAFTPConnection
+from riweather.stations import rank_stations, select_station, Station, zcta_to_lat_lon
+from riweather.viz import plot_stations
