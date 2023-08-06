d400ee7caf6d9036724594d9544aa1d68d6f2b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:31 +0000

    Add package postgres-database-utils/1.1.0/postgres_database_utils-1.1.0-py3-none-any.whl

---
+++ packages/postgres-database-utils/postgres_database_utils-1.1.0-py3-none-any.whl/postgres_database_utils/__init__.py
@@ -0,0 +1,7 @@
+from postgres_database_utils.main import create_connection, query_database, PostgresCredentials
+
+__all__ = [
+    create_connection,
+    query_database,
+    PostgresCredentials,
+]
