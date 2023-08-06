4c41b301e9a3bf611ad31ca933ba9ef09a4d325b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:31 +0000

    Add package dsmanager/1.0.12/dsmanager-1.0.12.tar.gz

---
+++ packages/dsmanager/dsmanager-1.0.12.tar.gz/dsmanager-1.0.12/dsmanager/datamanager/datasources/sfsource.py
@@ -0,0 +1,121 @@
+"""@Author: Rayane AMROUCHE
+
+Local Sources Handling
+"""
+
+import os
+
+from typing import Any
+
+import simple_salesforce  # type: ignore # pylint: disable=import-error
+
+import pandas as pd  # type: ignore
+
+from dsmanager.datamanager.datasources.datasource import DataSource
+
+
+class SFSource(DataSource):
+    """Inherited Data Source Class for sql sources
+    """
+
+    @staticmethod
+    def read_source(
+            username_env_name: str,
+            password_env_name: str,
+            token_env_name: str,
+            domain_env_name: str,
+            table_name: str,
+            **kwargs: Any) -> Any:
+        """Salesforce source reader
+
+        Args:
+            username_env_name (str): Name of the username env variable
+            password_env_name (str): Name of the password env variable
+            token_env_name (str): Name of the token env variable
+            domain_env_name (str): Name of the domain env variable
+            table_name (str): Name of the table in the dataset
+
+        Returns:
+            Any: Data from source
+        """
+        engine = simple_salesforce.Salesforce(
+            username=os.environ.get(username_env_name, ""),
+            password=os.environ.get(password_env_name, ""),
+            security_token=os.environ.get(token_env_name, ""),
+            domain=os.environ.get(domain_env_name, ""),
+            **kwargs
+        )
+        if table_name:
+            column_list = (
+                pd.DataFrame(
+                    getattr(engine, table_name)
+                    .describe()
+                    ["fields"]
+                )["name"]
+            ).to_list()
+
+            columns = ", ".join(column_list)
+            query = f"""SELECT {columns} FROM {table_name}"""
+
+            data = engine.query(query)["records"]
+            data = pd.DataFrame.from_dict(
+                data,
+                orient='columns'
+            ).drop("attributes", axis=1)
+        else:
+            data = engine
+
+        return data
+
+    def read(self, source_info: dict, **kwargs: Any) -> Any:
+        """Handle source and returns the source data
+
+        Args:
+            source_info (dict): Source metadatas
+
+        Returns:
+            Any: Source datas
+        """
+        self.load_source(source_info, **kwargs)
+        args = source_info["args"] if "args" in source_info else {}
+
+        data = self.read_source(
+            source_info["username_env_name"],
+            source_info["password_env_name"],
+            source_info["token_env_name"],
+            source_info["domain_env_name"],
+            source_info["table_name"],
+            **args
+        )
+
+        self.logger.info(
+            "Read data from salesforce",
+        )
+        return data
+
+    def read_db(self, source_info: dict, **kwargs: Any) -> Any:
+        """Read source and returns a source engine
+
+        Args:
+            source_info (dict): Source metadatas
+
+        Returns:
+            Any: Source engine
+        """
+        self.load_source(source_info, **kwargs)
+        args = source_info["args"] if "args" in source_info else {}
+
+        engine = self.read_source(
+            source_info["username_env_name"],
+            source_info["password_env_name"],
+            source_info["token_env_name"],
+            source_info["domain_env_name"],
+            "",
+            **args
+        )
+
+        self.logger.info(
+            "Read data from salesforce",
+        )
+
+        return engine
---
+++ packages/dsmanager/dsmanager-1.0.12.tar.gz/dsmanager-1.0.12/setup.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dsmanager',
+ 'dsmanager.controller',
+ 'dsmanager.datamanager',
+ 'dsmanager.datamanager.datasources',
+ 'dsmanager.datamanager.utils',
+ 'dsmanager.model',
+ 'dsmanager.view']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dash>=2.7.1,<3.0.0',
+ 'explainerdashboard>=0.4.0,<0.5.0',
+ 'llvmlite>=0.39.1,<0.40.0',
+ 'numba>=0.56.4,<0.57.0',
+ 'numexpr>=2.8.4,<3.0.0',
+ 'numpy>=1.23.3,<2.0.0',
+ 'openpyxl>=3.0.10,<4.0.0',
+ 'pandas>=1.5.0,<2.0.0',
+ 'pickle-mixin>=1.0.2,<2.0.0',
+ 'python-dotenv>=0.21.0,<0.22.0',
+ 'requests>=2.28.1,<3.0.0',
+ 'scikit-learn>=1.2.0,<2.0.0',
+ 'setuptools>=65.6.3,<66.0.0',
+ 'sqlalchemy>=1.4.45,<2.0.0',
+ 'sweetviz>=2.1.4,<3.0.0',
+ 'tqdm>=4.64.1,<5.0.0']
+
+extras_require = \
+{'kaggle': ['kaggle>=1.5.12,<2.0.0'],
+ 'mysql': ['mysqlclient>=2.1.1,<3.0.0'],
+ 'pgsql': ['psycopg2-binary>=2.9.5,<3.0.0'],
+ 'salesforce': ['simple-salesforce>=1.12.2,<2.0.0'],
+ 'sharepoint': ['azure-storage-common>=2.1.0,<3.0.0',
+                'azure-storage-blob>=12.14.1,<13.0.0',
+                'azure-common>=1.1.28,<2.0.0',
+                'shareplum>=0.5.1,<0.6.0'],
+ 'snowflake': ['snowflake-sqlalchemy>=1.4.4,<2.0.0']}
+
+setup_kwargs = {
+    'name': 'dsmanager',
+    'version': '1.0.12',
+    'description': 'Data Science tools to ease access and use of data and models',
+    'long_description': '<h1 align="center">DS Template 👨\u200d💻</h1>\n<p>\n  <a href="#" target="_blank">\n  <img alt="Version" src="https://img.shields.io/badge/version-1.0-blue.svg?cacheSeconds=2592000" />\n  </a>\n  <a href="#" target="_blank">\n    <img alt="Documentation" src="https://img.shields.io/badge/documentation-pdoc-orange.svg" />\n  </a>\n  <a href="LICENSE" target="_blank">\n    <img alt="License: Adel Rayane Amrouche" src="https://img.shields.io/badge/License-Adel Rayane Amrouche-yellow.svg" />\n  </a>\n</p>\n\n> Template to Kickstart Data Science projects that follows Sklearn standards and brings usefull tools to handle data and models quickly and easily\n\n## Install\n\n```sh\npip install poetry\npoetry install\n```\n\n## Run tests\n\n```sh\npytest src/\npylint src/\nmypy src/\n```\n\n## Documentation\n\n```sh\npdoc src/\n```\n\n### Usage\n\nThe DS Template has 3 main components:\n- A **DataManager** component\n- A **Controller** component\n- A **Model** component\n\nThe latter depends on the projects type and can be let empty if no model is needed.\n\nThat lets us with both the **controller** and the **data manager**. On the one hand, the **controller** is supposed to be the center of the project and interact with all the components to get the output asked by the user. On the other hand, the **data manager** is working around a json metadata file. This metafile follows a specific schema for both input sources and output sources.\n\n#### Input Sources\n> Sources can take 2 inputs regardless of the type of source and regardless if it is an intput or an output source:\n> - A **type** that can be either **csv**, **excel** or **sql**.\n> - A list of **args** that will be executed by pandas to get access to the source and prepare correctly the dataset.\n>\n> Three kind of **input** sources are handled :\n> - The **local** files that only take a **path** to the source file\n> - The **sharepoint** files that take:\n>   - The **sharepoint** address and the **sites** where the source is\n>   - The **folder** where the file source is\n>   - The **file** source name\n> - The **Sql** sources that take:\n>   - An **username** and a **password** environment variables names\n>   - An **address** of the database and **dialect** of the sql type of database\n>   - A **query** which is not mandatory since sql sources can be handled as a database instead of a simple dataset. You will be then able to query this database like you would do using *sqlalchemy*\n\n*Samples are available in the file **data/metadata.json***\n\n> Two kind of **output** sourcess are handled :\n> - The **local** files that only take a **path** to the source file\n> - The **Sql** sources that take:\n>   - An **username** and a **password** environment variables names\n>   - An **address** of the database and **dialect** of the sql type of database\n>   - A **table_name** where the dataset will be added\n>   - You also might need to include the **schema** and the parameter **if_exists** (either "replace" or append" regarding the way you want to update the table) in the list of **args** \n\n## Author\n\n👤 **Rayane Amrouche**\n\n* Github: [@AARayane](https://github.com/AARayane)\n',
+    'author': 'Rayane AMROUCHE',
+    'author_email': 'rayaneamrouche@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.8,<4',
+}
+
+
+setup(**setup_kwargs)
