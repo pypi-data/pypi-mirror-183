61298b7c17ff00830798d3155276e79e9da0d451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:56 +0000

    Add package django-pistoke/0.8rc2/django-pistoke-0.8rc2.tar.gz

---
+++ packages/django-pistoke/django-pistoke-0.8rc2.tar.gz/django-pistoke-0.8rc2/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+  setup_requires='git-versiointi',
+  name='django-pistoke',
+  description='Django-Websocket-laajennos',
+  url='https://github.com/an7oine/django-pistoke.git',
+  author='Antti Hautaniemi',
+  author_email='antti.hautaniemi@me.com',
+  licence='MIT',
+  packages=find_packages(exclude=['testit']),
+  include_package_data=True,
+  python_requires='>=3.6',
+  install_requires=[
+    'django>=3.1',
+  ],
+  extras_require={
+    'runserver': ['uvicorn[standard]'],
+    'websocket': ['websockets>=8.0'],
+  },
+  entry_points={'django.asetukset': [
+    'pistoke = pistoke.asetukset',
+  ]},
+  zip_safe=False,
+)
