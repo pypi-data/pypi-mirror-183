d18ff823436781165c444badca6e6ee3855ca654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:42 +0000

    Add package cvxpy/1.1.22/cvxpy-1.1.22-cp36-cp36m-win_amd64.whl

---
+++ packages/cvxpy/cvxpy-1.1.22-cp36-cp36m-win_amd64.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.1.22'
+version = '1.1.22'
+full_version = '1.1.22'
+git_revision = 'cb4b7f4'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
