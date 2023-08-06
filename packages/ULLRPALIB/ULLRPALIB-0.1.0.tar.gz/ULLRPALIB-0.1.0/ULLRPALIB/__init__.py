e786db209eaa9fbac6fe88caf010b8da62e8a72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:44 +0000

    Add package ULLRPALIB/0.1.0/ULLRPALIB-0.1.0.tar.gz

---
+++ packages/ULLRPALIB/ULLRPALIB-0.1.0.tar.gz/ULLRPALIB-0.1.0/ULLRPALIB/__init__.py
@@ -0,0 +1,3 @@
+from .Excel import limpia_hoja_excel, leer_excel
+from .General import imprime_diccionario
+from .DatosUXXIEC import carga_DC, carga_JG, carga_sede_docuconta, carga_sede_docuconta_tareas
