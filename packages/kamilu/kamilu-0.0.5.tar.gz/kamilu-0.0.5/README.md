f3d7b2082d9fd3166c846181c1f1abf32a36f4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:01 +0000

    Add package kamilu/0.0.5/kamilu-0.0.5.tar.gz

---
+++ packages/kamilu/kamilu-0.0.5.tar.gz/kamilu-0.0.5/README.md
@@ -0,0 +1,9 @@
+# kamilu
+
+计算机绘图，动画引擎
+
+## 安装
+
+```bash
+pip install kamilu
+```
---
+++ packages/kamilu/kamilu-0.0.5.tar.gz/kamilu-0.0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['kamilu']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['kamilu = kamilu.main:main']}
+
+setup_kwargs = {
+    'name': 'kamilu',
+    'version': '0.0.5',
+    'description': 'computer graphics animation engine',
+    'long_description': '# kamilu\n\n计算机绘图，动画引擎\n\n## 安装\n\n```bash\npip install kamilu\n```',
+    'author': 'luzhixing12345',
+    'author_email': 'luzhixing12345@163.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/luzhixing12345/kamilu',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
