cf98068577296801b42eb6e190eaeeacdf377d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:45 +0000

    Add package romaniya-menim/0.0.2/romaniya_menim-0.0.2.tar.gz

---
+++ packages/romaniya-menim/romaniya_menim-0.0.2.tar.gz/romaniya_menim-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Time series classification using Deep Learning'
+
+
+# Setting up
+setup(
+    name="romaniya_menim",
+    version=VERSION,
+    author="JavidanAbdullayev (Javidan Abdullayev)",
+    author_email="<javidan.abdullayev@uha.fr>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas',     ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    include_package_data=True,
+    package_data={'': ['data/*.tsv']},
+
+)
