be4fa2a2c3f775311a625a335147295b95d9b196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:37 +0000

    Add package fastdup/0.189/fastdup-0.189-cp39-cp39-manylinux_2_31_x86_64.whl

---
+++ packages/fastdup/fastdup-0.189-cp39-cp39-manylinux_2_31_x86_64.whl/fastdup/definitions.py
@@ -0,0 +1,36 @@
+FILENAME_SIMILARITY = "similarity.csv"
+FILENAME_OUTLIERS = "outliers.csv"
+FILENAME_NNF_INDEX = "nnf.index"
+FILENAME_FEATURES = "features.dat"
+FILENAME_IMAGE_LIST = "features.dat.csv"
+FILENAME_IMAGE_STATS = "stats.csv"
+FILENANE_BAD_IMAGE_LIST = "features.bad.csv"
+FILENAME_COMPONENT_INFO = "component_info.csv"
+FILENAME_CONNECTED_COMPONENTS = "connected_components.csv"
+FILENAME_LABELS = "labels.csv"
+FILENAME_KMEANS_CENTROIDS = "kmeans_centroids.csv"
+FILENAME_KMEANS_ASSIGNMENTS = "kmeans_assignments.csv"
+FILENAME_ERROR_MSG = "error.msg"
+
+IMAGELIST_HEADER="index,filename"
+LABEL_HEADER="index.label"
+STATS_HEADER='index,filename,width,height,unique,blur,mean,min,max,stdv,file_size'
+SIMILARITY_HEADER="from,to,distance"
+
+FILENAME_TOP_COMPONENTS = "top_components.pkl"
+FILENAME_TOP_CLUSTERS = "top_clusters.pkl"
+MISSING_LABEL = "N/A"
+
+S3_TEMP_FOLDER = "tmp"
+S3_TEST_TEMP_FOLDER = "testtmp"
+INPUT_FILE_LOCATION = "files.txt"
+INPUT_TEST_FILE_LOCATION = "testfiles.txt"
+
+DEFAULT_MODEL_FEATURE_WIDTH = 576
+HIGH_ACCURACY_MODEL_FEATURE_WIDTH = 960
+
+DEFUALT_METRIC_ZERO = 0
+DEFAULT_METRIC_MINUS_ONE = -1
+VERSION__ = "0.189"
+
+GITHUB_URL = "https://github.com/visual-layer/fastdup/issues"
