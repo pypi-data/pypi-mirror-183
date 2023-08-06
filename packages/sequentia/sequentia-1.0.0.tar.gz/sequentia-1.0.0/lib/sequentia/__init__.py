aa48af0fdfad42cd8afd3bc2edf99c85adeb084f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:19 +0000

    Add package sequentia/1.0.0/sequentia-1.0.0.tar.gz

---
+++ packages/sequentia/sequentia-1.0.0.tar.gz/sequentia-1.0.0/lib/sequentia/__init__.py
@@ -0,0 +1,10 @@
+from .datasets import *
+from .models import *
+# from .preprocessing import *
+from .utils import *
+
+__name__ = "sequentia"
+__version__ = "1.0.0"
+__author__ = "Edwin Onuonga"
+__email__ = "ed@eonu.net"
+__copyright__ = f"2019-2023, {__author__}"
