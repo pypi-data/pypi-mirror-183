c76e15f6f10ed126c6be7683bf346f7f626e86d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:39 +0000

    Add package pve-cli/0.1.0/pve_cli-0.1.0.tar.gz

---
+++ packages/pve-cli/pve_cli-0.1.0.tar.gz/pve_cli-0.1.0/pve_cli/proxmox/exceptions.py
@@ -0,0 +1,13 @@
+class ProxmoxError(Exception):
+    pass
+
+
+class ProxmoxVMNotFoundError(ProxmoxError):
+    pass
+
+
+class ProxmoxMissingPermissionError(ProxmoxError):
+    def __init__(self, path: str, permission: str):
+        super().__init__(f'Missing permission for "{path}": {permission}')
+        self.path = path
+        self.permission = permission
