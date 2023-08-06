6e766d9406da11a1cfda6d0c048ae6db98c998aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:53:42 +0000

    Add package cmeel/0.23.2/cmeel-0.23.2-py3-none-any.whl

---
+++ packages/cmeel/cmeel-0.23.2-py3-none-any.whl/cmeel/pth.py
@@ -0,0 +1,7 @@
+"""Append cmeel prefix sitelib to sys.path."""
+import sys
+from pathlib import Path
+
+from .consts import CMEEL_PREFIX, SITELIB
+
+sys.path.append(str(Path(__file__).parent.parent / CMEEL_PREFIX / SITELIB))
