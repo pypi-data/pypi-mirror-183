c155148dd0deaf39bb4e66cd558b6521b49c4623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:16 +0000

    Add package pyautd3/2.7.2/pyautd3-2.7.2.tar.gz

---
+++ packages/pyautd3/pyautd3-2.7.2.tar.gz/pyautd3-2.7.2/pyautd3/__init__.py
@@ -0,0 +1,51 @@
+'''
+File: __init__.py
+Project: pyautd3
+Created Date: 24/05/2021
+Author: Shun Suzuki
+-----
+Last Modified: 28/12/2022
+Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+-----
+Copyright (c) 2022 Shun Suzuki. All rights reserved.
+
+'''
+
+
+from pyautd3.autd import SilencerConfig
+from pyautd3.autd import Controller
+from pyautd3.autd import Amplitudes
+from pyautd3.autd import NUM_TRANS_IN_UNIT, NUM_TRANS_X, NUM_TRANS_Y, TRANS_SPACING, DEVICE_HEIGHT, DEVICE_WIDTH
+from pyautd3.autd import DRIVER_LATEST, DRIVER_V2_2, DRIVER_V2_3, DRIVER_V2_4, DRIVER_V2_5, DRIVER_V2_6, DRIVER_V2_7
+from pyautd3.autd import Clear, UpdateFlag, Synchronize, ModDelayConfig, Stop
+from pyautd3.autd import LogOutputFunc, LogFlushFunc, set_log_level, set_log_func
+
+__all__ = [
+    'SilencerConfig',
+    'Controller',
+    'Amplitudes',
+    'Clear',
+    'UpdateFlag',
+    'Synchronize',
+    'ModDelayConfig',
+    'Stop',
+    'NUM_TRANS_IN_UNIT',
+    'NUM_TRANS_X',
+    'NUM_TRANS_Y',
+    'TRANS_SPACING',
+    'DEVICE_WIDTH',
+    'DEVICE_HEIGHT',
+    'DRIVER_LATEST',
+    'DRIVER_V2_2',
+    'DRIVER_V2_3',
+    'DRIVER_V2_4',
+    'DRIVER_V2_5',
+    'DRIVER_V2_6',
+    'DRIVER_V2_7',
+    'LogOutputFunc',
+    'LogFlushFunc',
+    'set_log_level',
+    'set_log_func',
+]
+
+__version__ = '2.7.2'
