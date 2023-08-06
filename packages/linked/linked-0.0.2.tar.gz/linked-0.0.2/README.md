3d28c07ec73e2f5fcb6bcea1194aeb7725d3a276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:42 +0000

    Add package linked/0.0.2/linked-0.0.2.tar.gz

---
+++ packages/linked/linked-0.0.2.tar.gz/linked-0.0.2/README.md
@@ -0,0 +1,6 @@
+
+# linked
+Create and transform graphs
+
+
+To install:	```pip install linked```
---
+++ packages/linked/linked-0.0.2.tar.gz/linked-0.0.2/linked/datasrc/__init__.py
@@ -0,0 +1,4 @@
+"""
+Utils to generate graph data from properties and mini-languages.
+"""
+from linked.datasrc.minidot import mini_dot_to_graph_jdict
