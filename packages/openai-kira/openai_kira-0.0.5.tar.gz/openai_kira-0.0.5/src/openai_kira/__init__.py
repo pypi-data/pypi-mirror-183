d3b1b1e2101ed9672d255c864b756a28a229eef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:03 +0000

    Add package openai-kira/0.0.5/openai_kira-0.0.5.tar.gz

---
+++ packages/openai-kira/openai_kira-0.0.5.tar.gz/openai_kira-0.0.5/src/openai_kira/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 12/5/22 9:54 PM
+# @FileName: __init__.py
+# @Software: PyCharm
+# @Github    ：sudoskys
+from .setting import *
+from .resouce import Completion
+from .Chat import Chatbot
