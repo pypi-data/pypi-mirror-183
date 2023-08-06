c40dcdb9d3ce25525a5bd0209a803a96f85704a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:21 +0000

    Add package qiskit-qir/0.3.0a1/qiskit_qir-0.3.0a1-py2.py3-none-any.whl

---
+++ packages/qiskit-qir/qiskit_qir-0.3.0a1-py2.py3-none-any.whl/qiskit_qir/__init__.py
@@ -0,0 +1,14 @@
+##
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+##
+__author__ = """Microsoft Corporation"""
+__email__ = "que-contacts@microsoft.com"
+__version__ = "0.3.0a1"
+
+from qiskit_qir.translate import (
+    to_qir,
+    to_qir_bitcode,
+    to_qir_bitcode_with_entry_points,
+)
+from qiskit_qir.visitor import SUPPORTED_INSTRUCTIONS
