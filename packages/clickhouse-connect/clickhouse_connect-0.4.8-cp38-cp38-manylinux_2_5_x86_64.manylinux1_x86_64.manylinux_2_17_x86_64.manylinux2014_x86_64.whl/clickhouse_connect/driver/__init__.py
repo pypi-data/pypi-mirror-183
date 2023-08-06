a8b5936da774fdb345ac8076cee33aefe9466567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:59 +0000

    Add package clickhouse-connect/0.4.8/clickhouse_connect-0.4.8-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/clickhouse-connect/clickhouse_connect-0.4.8-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/clickhouse_connect/driver/__init__.py
@@ -0,0 +1,53 @@
+from typing import Optional, Union, Dict, Any
+from urllib.parse import urlparse, parse_qs
+
+from clickhouse_connect.driver.common import dict_copy
+
+from clickhouse_connect.driver.exceptions import ProgrammingError
+from clickhouse_connect.driver.client import Client
+from clickhouse_connect.driver.httpclient import HttpClient
+
+
+# pylint: disable=too-many-arguments
+def create_client(host: str = None,
+                  username: str = None,
+                  password: str = '',
+                  database: str = '__default__',
+                  interface: Optional[str] = None,
+                  port: int = 0,
+                  secure: Union[bool, str] = False,
+                  dsn: Optional[str] = None,
+                  settings: Optional[Dict[str, Any]] = None,
+                  **kwargs) -> Client:
+    if dsn:
+        parsed = urlparse(dsn)
+        username = username or parsed.username
+        password = password or parsed.password
+        host = host or parsed.hostname
+        port = port or parsed.port
+        if parsed.path and not database:
+            database = parsed.path[1:].split('/')[0]
+        database = database or parsed.path
+        qs_settings = dict(parse_qs(parsed.query))
+        settings = dict_copy(qs_settings, settings)
+    use_tls = str(secure).lower() == 'true' or interface == 'https' or (not interface and port in (443, 8443))
+    if not host:
+        host = 'localhost'
+    if not interface:
+        interface = 'https' if use_tls else 'http'
+    port = port or default_port(interface, use_tls)
+    if username is None and 'user' in kwargs:
+        username = kwargs.pop('user')
+    if password and username is None:
+        username = 'default'
+    if interface.startswith('http'):
+        cc_client = HttpClient(interface, host, port, username, password, database, settings=settings, **kwargs)
+    else:
+        raise ProgrammingError(f'Unrecognized client type {interface}')
+    return cc_client
+
+
+def default_port(interface: str, secure: bool):
+    if interface.startswith('http'):
+        return 8443 if secure else 8123
+    raise ValueError('Unrecognized ClickHouse interface')
