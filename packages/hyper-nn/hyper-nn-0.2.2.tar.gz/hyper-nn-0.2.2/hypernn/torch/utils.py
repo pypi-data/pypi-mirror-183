6159c86f61438b146b893a4e3af0766a9b841fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:14 +0000

    Add package hyper-nn/0.2.2/hyper-nn-0.2.2.tar.gz

---
+++ packages/hyper-nn/hyper-nn-0.2.2.tar.gz/hyper-nn-0.2.2/hypernn/torch/utils.py
@@ -0,0 +1,51 @@
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+from functorch import make_functional, make_functional_with_buffers
+
+
+def get_weight_chunk_dims(num_target_parameters: int, num_embeddings: int):
+    weight_chunk_dim = math.ceil(num_target_parameters / num_embeddings)
+    if weight_chunk_dim != 0:
+        remainder = num_target_parameters % weight_chunk_dim
+        if remainder > 0:
+            diff = math.ceil(remainder / weight_chunk_dim)
+            num_embeddings += diff
+    return weight_chunk_dim
+
+
+def count_params(module: nn.Module, input_shape=None, inputs=None):
+    return sum([np.prod(p.size()) for p in module.parameters()])
+
+
+class FunctionalParamVectorWrapper(nn.Module):
+    """
+    This wraps a module so that it takes params in the forward pass
+    """
+
+    def __init__(self, module: nn.Module):
+        super(FunctionalParamVectorWrapper, self).__init__()
+        self.custom_buffers = None
+        param_dict = dict(module.named_parameters())
+        self.target_weight_shapes = {k: param_dict[k].size() for k in param_dict}
+
+        try:
+            _functional, named_params = make_functional(module)
+        except Exception:
+            _functional, named_params, buffers = make_functional_with_buffers(module)
+            self.custom_buffers = buffers
+        self.named_params = [named_params]
+        self.functional = [_functional]  # remove params from being counted
+
+    def forward(self, param_vector: torch.Tensor, *args, **kwargs):
+        params = []
+        start = 0
+        for p in self.named_params[0]:
+            end = start + np.prod(p.size())
+            params.append(param_vector[start:end].view(p.size()))
+            start = end
+        if self.custom_buffers is not None:
+            return self.functional[0](params, self.custom_buffers, *args, **kwargs)
+        return self.functional[0](params, *args, **kwargs)
---
+++ packages/hyper-nn/hyper-nn-0.2.2.tar.gz/hyper-nn-0.2.2/setup.py
@@ -0,0 +1,43 @@
+import io
+import os
+import re
+from os import path
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="hyper-nn",
+    packages=find_packages(exclude=('tests',)),
+    version="0.2.2  ",
+    url="https://github.com/shyamsn97/hyper-nn",
+    license='MIT',
+
+    author="Shyam Sudhakaran",
+    author_email="shyamsnair@protonmail.com",
+
+    description="Easy hypernetworks in Pytorch and Flax",
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    install_requires=[
+        'numpy',
+        'torch',
+        'flax',
+        'jax',
+    ],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
