18a3d5709886db76ea3bb76e52c494b6984cf787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:34 +0000

    Add package live-trading-indicators/0.6.0/live_trading_indicators-0.6.0.tar.gz

---
+++ packages/live-trading-indicators/live_trading_indicators-0.6.0.tar.gz/live_trading_indicators-0.6.0/src/live_trading_indicators/indicators_set/ADL.py
@@ -0,0 +1,34 @@
+"""ADL(ma_period=None, ma_type='sma')
+Accumulation/distribution line."""
+from ..indicator_data import IndicatorData
+from ..move_average import *
+
+no_cached = True
+
+
+def get_indicator_out(indicators, symbol, timeframe, time_begin, time_end, ma_period=None, ma_type='sma'):
+
+    ohlcv = indicators.OHLCV(symbol, timeframe, time_begin, time_end)
+
+    hl_range = ohlcv.high - ohlcv.low
+    clv = ((ohlcv.close - ohlcv.low) - (ohlcv.high - ohlcv.close)) / hl_range
+    clv[hl_range == 0] = 0
+
+    adl = np.cumsum(clv * ohlcv.volume)
+
+    result_data = {
+        'indicators': indicators,
+        'parameters': {'ma_period': ma_period, 'ma_type': ma_type},
+        'name': 'ADL',
+        'symbol': symbol,
+        'timeframe': timeframe,
+        'time': ohlcv.time,
+        'adl': adl,
+        'charts': (None, 'adl'),
+        'allowed_nan': True
+    }
+
+    if ma_period is not None:
+        result_data.update({'adl_smooth': ma_calculate(adl, ma_period, MA_Type.cast(ma_type))})
+
+    return IndicatorData(result_data)
---
+++ packages/live-trading-indicators/live_trading_indicators-0.6.0.tar.gz/live_trading_indicators-0.6.0/src/live_trading_indicators/indicators_set/MA.py
@@ -0,0 +1,27 @@
+"""MA(period=, value='close', ma_type='sma')
+Moving average of different types: 'sma', 'ema', 'mma', 'ema0', 'mma0'"""
+from ..indicator_data import IndicatorData
+from ..move_average import ma_calculate, MA_Type
+
+
+def get_indicator_out(indicators, symbol, timeframe, out_for_grow, period, value='close', ma_type='sma'):
+
+    ma_type_enum = MA_Type.cast(ma_type)
+
+    ohlcv = indicators.OHLCV.full_data(symbol, timeframe)
+    source_values = ohlcv.data[value]
+
+    out = ma_calculate(source_values, period, ma_type_enum)
+
+    return IndicatorData({
+        'indicators': indicators,
+        'parameters': {'period': period, 'value': value, 'ma_type': ma_type},
+        'name': 'SMA',
+        'symbol': symbol,
+        'timeframe': timeframe,
+        'time': ohlcv.time,
+        'move_average': out,
+        'allowed_nan': True
+    })
+
+
