872c325618d022995282287663dac5731edc3363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:42 +0000

    Add package nonebot-plugin-ayaka/1.0.0b2/nonebot_plugin_ayaka-1.0.0b2-py3-none-any.whl

---
+++ packages/nonebot-plugin-ayaka/nonebot_plugin_ayaka-1.0.0b2-py3-none-any.whl/ayaka/__init__.py
@@ -0,0 +1,22 @@
+'''1.0.0b2
+
+https://bridgel.github.io/ayaka_doc/1.0.0/
+'''
+# ---- 方便生成api文档 ----
+try:
+    from nonebot import get_driver
+    get_driver()
+except:
+    import nonebot
+    nonebot.init()
+# ----  ----
+
+from .box import AyakaBox
+from .config import AyakaConfig, load_data_from_file
+from .helpers import get_user, do_nothing, singleton, run_in_startup, Timer
+from .orm import AyakaDB, AyakaGroupDB, AyakaUserDB
+
+# ---- 方便使用 ----
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11.helpers import Numbers
+from pydantic import Field, BaseModel
