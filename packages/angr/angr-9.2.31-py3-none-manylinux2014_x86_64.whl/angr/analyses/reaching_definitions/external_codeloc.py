c8459640933c49a60f3bdb8c8d93c5fdcf7d28e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:04 +0000

    Add package angr/9.2.31/angr-9.2.31-py3-none-manylinux2014_x86_64.whl

---
+++ packages/angr/angr-9.2.31-py3-none-manylinux2014_x86_64.whl/angr/analyses/reaching_definitions/external_codeloc.py
@@ -0,0 +1,12 @@
+from ...code_location import CodeLocation
+
+
+class ExternalCodeLocation(CodeLocation):
+
+    __slots__ = ()
+
+    def __init__(self):
+        super().__init__(0, 0)
+
+    def __repr__(self):
+        return "[External]"
