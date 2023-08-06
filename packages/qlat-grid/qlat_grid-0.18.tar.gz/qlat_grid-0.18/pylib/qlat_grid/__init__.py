8e4fb02de92329449fca3d99b68d06ad7b8fdef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:26 +0000

    Add package qlat-grid/0.18/qlat_grid-0.18.tar.gz

---
+++ packages/qlat-grid/qlat_grid-0.18.tar.gz/qlat_grid-0.18/pylib/qlat_grid/__init__.py
@@ -0,0 +1,6 @@
+import qlat_grid.c as cg
+
+from qlat_grid.init import *
+from qlat_grid.prop import *
+
+from qlat_grid.get_include_dir import *
---
+++ packages/qlat-grid/qlat_grid-0.18.tar.gz/qlat_grid-0.18/pylib/qlat_grid/cp.pxd
@@ -0,0 +1,4 @@
+cdef extern from "qlat-grid/qlat-grid.h" namespace "qlat":
+    pass
+
+from qlat.cp cimport *
