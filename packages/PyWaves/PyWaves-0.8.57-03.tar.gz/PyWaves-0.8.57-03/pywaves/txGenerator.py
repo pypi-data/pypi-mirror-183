73af3999dc5c4f06509dd16dc973f126f23d9cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:20 +0000

    Add package PyWaves/0.8.57.post3/PyWaves-0.8.57-03.tar.gz

---
+++ packages/PyWaves/PyWaves-0.8.57-03.tar.gz/PyWaves-0.8.57-03/pywaves/txGenerator.py
@@ -0,0 +1,45 @@
+import pywaves
+import time
+
+class TxGenerator:
+
+    def generateSendWaves(self, recipient, amount, publicKey, attachment='', txFee=pywaves.DEFAULT_TX_FEE, timestamp=0):
+        if timestamp == 0:
+            timestamp = int(time.time() * 1000)
+
+        tx = {
+            "type": 4,
+            "version": 2,
+            "senderPublicKey": publicKey,
+            "recipient": recipient.address,
+            "amount": amount,
+            "fee": txFee,
+            "timestamp": timestamp,
+            "attachment": attachment,
+            "proofs": []
+        }
+
+        return tx
+
+    def generateSendAsset(self, recipient, asset, amount, publicKey, attachment='', feeAsset='', txFee=pywaves.DEFAULT_TX_FEE, timestamp=0):
+        if timestamp == 0:
+            timestamp = int(time.time() * 1000)
+
+        if (feeAsset != '' and feeAsset != None):
+            feeAsset = feeAsset.assetId
+        asset = asset.assetId
+        tx = {
+            "version": 2,
+            "type": 4,
+            "assetId": asset,
+            "feeAssetId": (feeAsset if feeAsset != '' else None),
+            "senderPublicKey": publicKey,
+            "recipient": recipient.address,
+            "amount": amount,
+            "fee": txFee,
+            "timestamp": timestamp,
+            "attachment": attachment,
+            "proofs": [ ]
+        }
+
+        return tx
