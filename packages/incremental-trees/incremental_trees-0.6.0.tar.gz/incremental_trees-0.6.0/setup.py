97f3f03f27bfa7d6583e09bf43440259f69a4523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:47:30 +0000

    Add package incremental-trees/0.6.0/incremental_trees-0.6.0.tar.gz

---
+++ packages/incremental-trees/incremental_trees-0.6.0.tar.gz/incremental_trees-0.6.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+from incremental_trees import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='incremental_trees',
+                 version=__version__,
+                 author="Gareth Jones",
+                 author_email="garethgithub@gmail.com",
+                 description='Sklearn forests with partial fits',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 packages=setuptools.find_packages(),
+                 url="https://github.com/garethjns/IncrementalTrees",
+                 python_requires='>=3.8',
+                 install_requires=[
+                     "scikit-learn==1.2",
+                     "pandas",
+                     "numpy",
+                     "dask==2022.12",
+                     "dask-glm==0.2.0",
+                     "dask-ml==2022.5.27",
+                     "fsspec"
+                 ])
