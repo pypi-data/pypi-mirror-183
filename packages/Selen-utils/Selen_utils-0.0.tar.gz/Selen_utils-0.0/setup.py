cbea9362b6385f6c220d8fad17742b15f1885393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:36 +0000

    Add package Selen-utils/0.0/Selen_utils-0.0.tar.gz

---
+++ packages/Selen-utils/Selen_utils-0.0.tar.gz/Selen_utils-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Selen_utils',
+      version='0.0',
+      description='Selenium_template_utils',
+      packages=['Selen_utils'],#, 'selenium-wire', 'selenium', 'fake_useragent', 'loguru', 'requests', 'pandas', 'dataclasses'],
+      author_email='alisbivalka@gmail.com',
+      zip_safe=False)
