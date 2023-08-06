0064e3b6092f29d89014605261ae226fe2d9d84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:07 +0000

    Add package tmuxp/1.20.3/tmuxp-1.20.3.tar.gz

---
+++ packages/tmuxp/tmuxp-1.20.3.tar.gz/tmuxp-1.20.3/tests/fixtures/utils.py
@@ -0,0 +1,19 @@
+import pathlib
+
+from ..constants import FIXTURE_PATH
+
+
+def get_workspace_file(_file):  # return fixture data, relative to __file__
+    return FIXTURE_PATH / _file
+
+
+def read_workspace_file(_file):  # return fixture data, relative to __file__
+    return open(get_workspace_file(_file)).read()
+
+
+def write_config(
+    config_path: pathlib.Path, filename: str, content: str
+) -> pathlib.Path:
+    config = config_path / filename
+    config.write_text(content, encoding="utf-8")
+    return config
