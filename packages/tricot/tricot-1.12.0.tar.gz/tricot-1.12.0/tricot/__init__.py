7ab160ae7c4a73df1ed94ce21a3b567c3ce000a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:42 +0000

    Add package tricot/1.12.0/tricot-1.12.0.tar.gz

---
+++ packages/tricot/tricot-1.12.0.tar.gz/tricot-1.12.0/tricot/__init__.py
@@ -0,0 +1,12 @@
+from .command import *
+from .condition import *
+from .constants import *
+from .docker import *
+from .logging import *
+from .plugin import *
+from .template import *
+from .tricot import *
+from .validation import *
+from .extractor import *
+
+name = 'tricot'
