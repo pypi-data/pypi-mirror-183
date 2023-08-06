3ced4b7ce92fb34ba286307b6f72a0bd82cfb692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:09 +0000

    Add package hari-cal/0.0.5/hari_cal-0.0.5.tar.gz

---
+++ packages/hari-cal/hari_cal-0.0.5.tar.gz/hari_cal-0.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='hari_cal',
+    version='0.0.5',
+    description='Basic math',
+    author= 'Phani Siginamsetty',
+    # url = 'https://github.com/funnyPhani/Test_repo_rlms',
+    # long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=["Oerations_in_math"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['hari_cal'],
+    package_dir={'':'src'},
+    install_requires = [
+       "pandas"
+    ]
+)
