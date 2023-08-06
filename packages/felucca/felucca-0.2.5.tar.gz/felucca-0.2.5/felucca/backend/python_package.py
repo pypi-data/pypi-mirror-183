345195cddea675b6b31270605d81b9793faa4bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:16 +0000

    Add package felucca/0.2.5/felucca-0.2.5.tar.gz

---
+++ packages/felucca/felucca-0.2.5.tar.gz/felucca-0.2.5/felucca/backend/python_package.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+import subprocess
+
+from felucca.core.utils import (
+    execute_setuptools,
+    get_package_name,
+)
+
+
+def remove_installation_metadata():
+    """
+    Remove all the installation metadata from
+    Python packages
+    """
+    root_dir = get_package_name()
+    norm_package = root_dir.replace("-", "_")
+
+    extension = ".dist-info"
+
+    # Search for the folder in the root directory
+    for root, dirs, files in os.walk(norm_package):
+        for name in dirs:
+            # Check if the current folder matches the desired name
+            if name.endswith(extension):
+                # Remove the folder
+                shutil.rmtree(os.path.join(root, name))
+
+
+def install_contracts(package: str, version: str):
+    """Install contracts packaged in the Python wheel
+
+    Args:
+        package (str): package name
+        version (str): version of the package
+
+    Returns:
+        list: list with the contracts location
+    """
+
+    package_name = get_package_name()
+    norm_package = package_name.replace("-", "_")
+
+    command = f"install {package}"
+    if version is not None:
+        command += f"=={version}"
+    command += f" --no-deps --target {norm_package} --quiet"
+
+    before = os.listdir(norm_package)
+    execute_setuptools(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    remove_installation_metadata()
+    after = os.listdir(norm_package)
+
+    return set(after).difference(before)
