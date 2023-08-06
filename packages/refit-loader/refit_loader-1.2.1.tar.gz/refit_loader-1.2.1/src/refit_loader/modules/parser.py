47ec2e11553848eac0f2bfed6c0ec07d5280d7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:22 +0000

    Add package refit-loader/1.2.1/refit_loader-1.2.1.tar.gz

---
+++ packages/refit-loader/refit_loader-1.2.1.tar.gz/refit_loader-1.2.1/src/refit_loader/modules/parser.py
@@ -0,0 +1,43 @@
+
+
+
+
+def refit_parser(readme_file):
+    """
+    This method will return the dictionary of the appliances, parsed from README file of refit data
+
+    Parameters 
+    ----------
+    readme_file : txt file 
+                    original README file of refit datase
+
+    returns: pandas.Dictionary
+                    dictionary contains the all the appliances in a specific house as mentioned in README file
+    """
+    try:
+        print(f'Parsing the readme file specified: {readme_file}')
+        with open(readme_file) as f:
+            content = f.readlines()
+            content.append('\n')
+    
+        ls = {}
+        for i, s in enumerate(content):
+
+            if '0.Aggregate,' in s:
+                
+                keys, appliances = [], []
+                house = content[i-1].split()[1]
+
+                for indx in range(0, 3):
+                    target = [value.split('.') for value in [value for value in content[i+indx].split(',') if value != '\n']]
+                    for t in target:
+                        if len(t) > 2: ##### one comma missing in house 5 caused issue
+                            appliances.append(t[1])
+                            appliances.append(t[2])
+                        else:
+                            appliances.append(t[1])
+                ls.update({house: [item.split('\n')[0].replace(" ", "") for item in appliances]})
+        return ls
+    
+    except Exception as e:
+        print("Error occured in parser method due to ", e)
