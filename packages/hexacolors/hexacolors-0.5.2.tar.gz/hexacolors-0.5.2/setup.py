fd0b922c01a52755800d1bb26dcf93328d9bc6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:44 +0000

    Add package hexacolors/0.5.2/hexacolors-0.5.2.tar.gz

---
+++ packages/hexacolors/hexacolors-0.5.2.tar.gz/hexacolors-0.5.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+
+    long_description = f.read()
+
+setup(
+    name="hexacolors",
+    version="0.5.2",
+    url = 'https://github.com/Marciel404/hexacolors',
+    author="Marciel404",
+    description='''
+:A simple library that converts string to hexadecimal understandable by python
+:A simple library that converts RGB to hexadecimal understandable by python
+:A simple library that converts CMYK to hexadecimal understandable by python
+:A simple library that converts HSL to hexadecimal understandable by python
+:A simple library that converts hexadecimal understandable by python
+''',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["hexacolors", "hexacolors.colors", "hexacolors.offline", "hexacolors.online"],
+    license = 'MIT',
+    keywords = 'String hexadecimal converter' ,
+    CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'Topic :: colors',
+    'License :: MIT License',
+    'Programming Language :: Python',
+    ],
+    python_requires='>=3.10',
+    install_requires=['requests>=2.28.1'],
+    dependency_links=['https://github.com/Marciel404/hexacolors']
+)
