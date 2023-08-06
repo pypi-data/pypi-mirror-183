82c8710cc895fb2d374245dfd1c798851ce0d6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:29 +0000

    Add package celestis/1.7/celestis-1.7.tar.gz

---
+++ packages/celestis/celestis-1.7.tar.gz/celestis-1.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='celestis',
+    version='1.7',
+    description='A simple backend framework built using python',
+    author="Aryaan Hegde",
+    author_email="aryhegde@gmail.com",
+    packages=["celestis.controller", "celestis.view"],
+    package_dir={"celestis.controller": "controller", "celestis.view": "view"},
+    py_modules=['celestis', 'command', 'create_files', 'error', 'render', 'requests'],
+    include_package_data=True,
+    install_requires=['requests', 'click'],
+    entry_points={
+        'console_scripts': [
+            'celestis=command:celestis',
+        ],
+    },
+)
