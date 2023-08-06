895362e78e954f7c2212eda9d3df6cfff1f3a1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:32 +0000

    Add package pyqtlet2/0.9.1/pyqtlet2-0.9.1.tar.gz

---
+++ packages/pyqtlet2/pyqtlet2-0.9.1.tar.gz/pyqtlet2-0.9.1/pyqtlet2/leaflet/layer/layer.py
@@ -0,0 +1,85 @@
+from ..core import Evented
+import logging
+from abc import abstractmethod
+
+class Layer(Evented):
+
+    # layerId is a static variable shared between all layers
+    # It is used to give unique names to layers
+    layerId = 0
+
+    @property
+    def layerName(self):
+        return self._layerName
+
+    @layerName.setter
+    def layerName(self, name):
+        self._layerName = name
+
+    @property
+    def jsName(self):
+        return self._layerName
+
+    @property
+    def map(self):
+        return self._map
+
+    @map.setter
+    def map(self, map_):
+        self._map = map_
+
+    @abstractmethod
+    def _initJs(self):
+        raise NotImplemented
+
+    def runJavaScriptForMapIndex(self, js):
+        self.runJavaScript(js, self._map.mapWidgetIndex)
+
+    def __init__(self):
+        super().__init__()
+        self._map = None
+        self._layerName = self._getNewLayerName()
+        self._log = logging.getLogger(f"layer_{self._layerName}")
+
+    def _getNewLayerName(self):
+        layerName = 'l{}'.format(self.layerId)
+        Layer.layerId += 1
+        return layerName
+
+    def addTo(self, map_):
+        map_.addLayer(self)
+        return self
+
+    def removeFrom(self, map_):
+        map_.removeLayer(self)
+        return self
+
+    def bindPopup(self, content, options=None):
+        js = '{layerName}.bindPopup("{content}"'.format(
+                layerName=self._layerName, content=content)
+        if options:
+            js += ', {options}'.format(options=self._stringifyForJs(options))
+        js += ')'
+        self.runJavaScriptForMapIndex(js)
+        return self
+
+    def unbindPopup(self):
+        js = '{layerName}.unbindPopup()'.format(layerName=self._layerName)
+        self.runJavaScriptForMapIndex(js)
+        return self
+
+    def bindTooltip(self, content, options=None):
+        js = '{layerName}.bindTooltip("{content}"'.format(
+                layerName=self._layerName, content=content)
+        if options:
+            js += ', {options}'.format(options=self._stringifyForJs(options))
+        js += ')'
+        self.runJavaScriptForMapIndex(js)
+        return self
+
+    def unbindTooltip(self):
+        js = '{layerName}.unbindTooltip()'.format(layerName=self._layerName)
+        self.runJavaScriptForMapIndex(js)
+        return self
+
+
