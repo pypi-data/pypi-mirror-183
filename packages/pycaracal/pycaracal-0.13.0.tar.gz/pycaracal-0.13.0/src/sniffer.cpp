4091a911d3ec7e70d31ac28db59092ba0ed0cf3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:15 +0000

    Add package pycaracal/0.13.0/pycaracal-0.13.0.tar.gz

---
+++ packages/pycaracal/pycaracal-0.13.0.tar.gz/pycaracal-0.13.0/src/sniffer.cpp
@@ -0,0 +1,129 @@
+#include <spdlog/fmt/fmt.h>
+#include <spdlog/fmt/ostr.h>
+#include <spdlog/spdlog.h>
+#include <tins/tins.h>
+
+#include <bxzstr/bxzstr.hpp>
+#include <caracal/parser.hpp>
+#include <caracal/sniffer.hpp>
+#include <caracal/statistics.hpp>
+#include <caracal/utilities.hpp>
+#include <chrono>
+#include <filesystem>
+#include <iostream>
+#include <optional>
+#include <thread>
+
+namespace fs = std::filesystem;
+
+namespace caracal {
+
+Sniffer::Sniffer(const std::string &interface_name,
+                 const std::optional<fs::path> &output_file_csv,
+                 const std::optional<fs::path> &output_file_pcap,
+                 const std::optional<std::string> &meta_round,
+                 const uint16_t caracal_id, const bool integrity_check)
+    : sniffer_{interface_name},
+      meta_round_{meta_round},
+      statistics_{},
+      caracal_id_{caracal_id},
+      integrity_check_{integrity_check} {
+  Tins::NetworkInterface interface { interface_name };
+
+  std::vector<std::string> address_filters;
+  for (auto address : Utilities::all_ipv4_for(interface)) {
+    address_filters.push_back(fmt::format("dst {}", address.to_string()));
+  }
+  for (auto address : Utilities::all_ipv6_for(interface)) {
+    address_filters.push_back(fmt::format("dst {}", address.to_string()));
+  }
+
+  auto filter_template =
+      "({}) and (icmp or icmp6) and ("
+      "icmp[icmptype] = icmp-echoreply or"
+      " icmp[icmptype] = icmp-timxceed or"
+      " icmp[icmptype] = icmp-unreach or"
+      " icmp6[icmp6type] = icmp6-echoreply or"
+      " icmp6[icmp6type] = icmp6-timeexceeded or"
+      " icmp6[icmp6type] = icmp6-destinationunreach"
+      ")";
+  auto filter = fmt::format(fmt::runtime(filter_template),
+                            fmt::join(address_filters, " or "));
+  spdlog::info("sniffer_filter={}", filter);
+
+  Tins::SnifferConfiguration config;
+  config.set_buffer_size(64 * 1024 * 1024);
+  config.set_filter(filter);
+  config.set_immediate_mode(true);
+  sniffer_ = Tins::Sniffer(interface_name, config);
+
+  if (output_file_csv) {
+    if (output_file_csv->extension() == ".zst") {
+      output_csv_ = std::make_unique<bxz::ofstream>(*output_file_csv,
+                                                    bxz::Compression::zstd, 1);
+    } else {
+      output_csv_ = std::make_unique<std::ofstream>(*output_file_csv);
+    }
+  } else {
+    output_csv_ = std::make_unique<std::ostream>(std::cout.rdbuf());
+  }
+
+  if (output_file_pcap) {
+    output_pcap_ = Tins::PacketWriter{*output_file_pcap,
+                                      Tins::DataLinkType<Tins::EthernetII>()};
+  }
+}
+
+Sniffer::~Sniffer() {
+  // Cleanup resources in case the sniffer was not properly stopped.
+  // For example if an exception was raised on the main thread.
+  stop();
+}
+
+void Sniffer::start() noexcept {
+  *output_csv_ << Reply::csv_header() << "\n";
+  auto handler = [this](Tins::Packet &packet) {
+    auto reply = Parser::parse(packet);
+
+    if (reply && (!integrity_check_ || reply->is_valid(caracal_id_))) {
+      spdlog::trace(reply.value());
+      statistics_.icmp_messages_all.insert(reply->reply_src_addr);
+      if (reply->is_time_exceeded()) {
+        statistics_.icmp_messages_path.insert(reply->reply_src_addr);
+      }
+      *output_csv_ << reply->to_csv(meta_round_.value_or("1")) << "\n";
+    } else {
+      auto data = packet.pdu()->serialize();
+      spdlog::trace("invalid_packet_hex={:02x}", fmt::join(data, ""));
+      statistics_.received_invalid_count++;
+    }
+
+    if (output_pcap_) {
+      output_pcap_->write(packet);
+    }
+
+    statistics_.received_count++;
+    return true;
+  };
+
+  thread_ = std::thread([this, handler]() { sniffer_.sniff_loop(handler); });
+}
+
+void Sniffer::stop() noexcept {
+  if (thread_.joinable()) {
+    sniffer_.stop_sniff();
+    thread_.join();
+  }
+}
+
+const Statistics::Sniffer &Sniffer::statistics() const noexcept {
+  return statistics_;
+}
+
+pcap_stat Sniffer::pcap_statistics() noexcept {
+  pcap_stat ps{};
+  pcap_stats(sniffer_.get_pcap_handle(), &ps);
+  return ps;
+}
+
+}  // namespace caracal
---
+++ packages/pycaracal/pycaracal-0.13.0.tar.gz/pycaracal-0.13.0/tests/prober_test.cpp
@@ -0,0 +1,147 @@
+#include <spdlog/cfg/helpers.h>
+
+#include <caracal/prober.hpp>
+#include <caracal/prober_config.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <cstdlib>
+#include <filesystem>
+#include <iostream>
+
+#include "./environment.hpp"
+
+namespace fs = std::filesystem;
+
+using caracal::Prober::Config;
+using caracal::Prober::probe;
+
+
+TEST_CASE("Prober::probe") {
+  auto protocol = GENERATE("icmp", "udp", "icmp6");
+  uint32_t flow_label = 1;
+  std::ofstream ofs;
+
+  // TODO: IPv6.
+
+  ofs.open("zzz_input.csv");
+  auto dst_addr = "8.8.8.8";
+  if (std::string(protocol) == "icmp6") {
+    dst_addr = "2a00:1450:4007:819::200e"; // google.com
+  }
+
+  ofs << fmt::format("{},24000,33434,1,{},{}\n", dst_addr, protocol, flow_label);  // Allowed
+  ofs << fmt::format("{},24000,33434,2,{},{}\n", dst_addr, protocol, flow_label);  // Allowed
+  ofs << fmt::format("{},24000,33434,0,{},{}\n",
+                     dst_addr, protocol,flow_label);  // Denied (TTL too low)
+  ofs << fmt::format("{},24000,33434,8,{},{}\n",
+                     dst_addr, protocol,flow_label);  // Denied (TTL too high)
+  if (std::string(protocol) == "icmp6") {
+    ofs << fmt::format("2b00:1450:4007:819::200e,24000,33434,2,{},{}\n",
+                       protocol,
+                       flow_label);  // Denied (prefix IPv6 not included)
+    ofs << fmt::format("2a00:1450:4007:818::200e,24000,33434,2,{},{}\n",
+                       protocol,
+                       flow_label);  // Denied (prefix IPv6 excluded)
+  } else {
+    ofs << fmt::format("8.8.9.1,24000,33434,2,{},{}\n",
+                       protocol,flow_label);  // Denied (prefix excluded)
+    ofs << fmt::format("8.3.4.1,24000,33434,2,{},{}\n",
+                       protocol, flow_label);  // Denied (prefix not included)
+  }
+
+  ofs << "a,b,c,d,e,f\n";  // Invalid probe (should not crash, and should not be
+                         // included in read stats)
+  ofs.close();
+
+  ofs.open("zzz_excl.csv");
+  ofs << "8.8.9.0/24\n";
+  ofs << "2a00:1450:4007:818::/64\n";
+  ofs.close();
+
+  ofs.open("zzz_incl.csv");
+  ofs << "8.8.0.0/16\n";
+  ofs << "2a00::/12\n";
+  ofs.close();
+
+  Config config;
+  config.set_output_file_csv("zzz_output.csv");
+  config.set_output_file_pcap("zzz_output.pcap");
+  config.set_prefix_excl_file("zzz_excl.csv");
+  config.set_prefix_incl_file("zzz_incl.csv");
+  config.set_filter_min_ttl(1);
+  config.set_filter_max_ttl(6);
+  config.set_probing_rate(100);
+  config.set_sniffer_wait_time(1);
+  config.set_n_packets(3);
+  config.set_meta_round("1");
+  config.set_integrity_check(true);
+
+  spdlog::cfg::helpers::load_levels("trace");
+
+  SECTION("Base case") {
+    auto [prober_stats, sniffer_stats, pcap_stats] =
+        probe(config, "zzz_input.csv");
+    REQUIRE(prober_stats.read == 6);
+    REQUIRE(prober_stats.sent == 6);
+    REQUIRE(prober_stats.filtered_lo_ttl == 1);
+    REQUIRE(prober_stats.filtered_hi_ttl == 1);
+    REQUIRE(prober_stats.filtered_prefix_excl == 1);
+    REQUIRE(prober_stats.filtered_prefix_not_incl == 1);
+    if (!is_github) {
+      REQUIRE(sniffer_stats.received_count >= 2);
+      REQUIRE(sniffer_stats.received_invalid_count == 0);
+      REQUIRE(!sniffer_stats.icmp_messages_all.empty());
+      REQUIRE(!sniffer_stats.icmp_messages_path.empty());
+    }
+  }
+
+  SECTION("Include list with missing new line") {
+    // Should not crash and should filter the prefixes not included.
+    ofs.open("zzz_incl.csv");
+    ofs << "8.8.0.0/16\n";
+    ofs << "2a00::/12\n";
+    ofs.close();
+
+    auto [prober_stats, sniffer_stats, pcap_stats] =
+        probe(config, "zzz_input.csv");
+    REQUIRE(prober_stats.sent == 6);
+    if (!is_github) {
+      REQUIRE(sniffer_stats.received_count == 6);
+    }
+    REQUIRE(sniffer_stats.received_invalid_count == 0);
+  }
+
+  SECTION("Empty exclude list") {
+    // Should not crash and should not filter prefixes.
+    ofs.open("zzz_excl.csv");
+    ofs << "";
+    ofs.close();
+
+    auto [prober_stats, sniffer_stats, pcap_stats] =
+        probe(config, "zzz_input.csv");
+    REQUIRE(prober_stats.sent == 9);
+    if (!is_github) {
+      REQUIRE(sniffer_stats.received_count == 9);
+    }
+    REQUIRE(sniffer_stats.received_invalid_count == 0);
+  }
+
+  SECTION("Empty include list") {
+    // Should not crash and should filter all prefixes.
+    ofs.open("zzz_incl.csv");
+    ofs << "";
+    ofs.close();
+
+    auto [prober_stats, sniffer_stats, pcap_stats] =
+        probe(config, "zzz_input.csv");
+    REQUIRE(prober_stats.sent == 0);
+    REQUIRE(sniffer_stats.received_count == 0);
+    REQUIRE(sniffer_stats.received_invalid_count == 0);
+  }
+
+  fs::remove("zzz_excl.csv");
+  fs::remove("zzz_incl.csv");
+  fs::remove("zzz_input.csv");
+  fs::remove("zzz_output.csv");
+  fs::remove("zzz_output.pcap");
+}
