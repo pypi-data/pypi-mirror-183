fca78f1fb779c906ee17474775e4bdbf25520923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:41 +0000

    Add package isocodes/2023.1.2/isocodes-2023.1.2.tar.gz

---
+++ packages/isocodes/isocodes-2023.1.2.tar.gz/isocodes-2023.1.2/update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+python -m piptools compile --resolver=backtracking \
+    --extra dev \
+    -o dev-requirements.txt \
+    pyproject.toml
+
+BASE_DIR="$PWD/isocodes"
+
+SHARE_DIR="$PWD"/isocodes/share
+
+UPSTREAM_DIR=$PWD/vendor/iso-codes
+
+rm -rf "$SHARE_DIR"
+
+cd "$UPSTREAM_DIR" || exit
+
+git pull
+
+git checkout v4.12.0
+
+./configure --prefix "$BASE_DIR"
+
+make clean
+
+make uninstall
+
+make
+
+make install
