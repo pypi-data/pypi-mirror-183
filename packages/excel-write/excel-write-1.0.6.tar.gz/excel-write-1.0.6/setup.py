09827d0e98dc66fde214e39d26b4af027c86d184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:25:09 +0000

    Add package excel-write/1.0.6/excel-write-1.0.6.tar.gz

---
+++ packages/excel-write/excel-write-1.0.6.tar.gz/excel-write-1.0.6/setup.py
@@ -0,0 +1,45 @@
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    LONG_DESCRIPTION = "\n" + fh.read()
+
+VERSION = "1.0.6"
+DESCRIPTION = "Optimised way to write in Excel files."
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+# Setting up
+setup(
+    name="excel-write",
+    version=VERSION,
+    author="Dipan Nanda",
+    author_email="d19cyber@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=required,
+    keywords=[
+        "python",
+        "excel",
+        "pandas",
+        "excel-write",
+        "write",
+        "openpyxl",
+        "xlsxwriter",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
