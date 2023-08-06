f77ea1d6144ad1103666a59cccb9faa30332e5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:23 +0000

    Add package fastapi-auth-service/0.0.6/fastapi-auth-service-0.0.6.tar.gz

---
+++ packages/fastapi-auth-service/fastapi-auth-service-0.0.6.tar.gz/fastapi-auth-service-0.0.6/src/fastapi_auth_service/conf.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from pydantic import BaseSettings, PostgresDsn, SecretStr, RedisDsn
+
+
+class Settings(BaseSettings):
+    logging_level: str = 'INFO'
+    logging_format: str = '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+    db_dsn: PostgresDsn = None
+    redis_dsn: RedisDsn = None
+    redis_password: SecretStr = None
+    service_addr: str = '127.0.0.1'
+    service_port: int = 8080
+    bootstrap_user_email: Optional[str] = None
+    bootstrap_user_password: SecretStr = None
+    auth_secret: SecretStr = 'TODO-REPLACE'
+
+
+settings = Settings()
---
+++ packages/fastapi-auth-service/fastapi-auth-service-0.0.6.tar.gz/fastapi-auth-service-0.0.6/src/fastapi_auth_service/core.py
@@ -0,0 +1,38 @@
+import gettext
+import logging
+from pathlib import Path
+from typing import Mapping, Optional
+
+import sqlalchemy as sa
+from fastapi_pagination.bases import AbstractPage
+from fastapi_pagination.ext.async_sqlalchemy import paginate
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi_auth_service.db.models import UserGroup, User
+
+logger = logging.getLogger(__name__)
+t = gettext.translation('base', Path(Path(__file__).parent, 'locale'), fallback=True, languages=['lv_LV'])
+_ = t.gettext
+
+
+async def get_users(session: AsyncSession, search: Optional[Mapping[str, str]] = None,
+                    order_by: Optional[str] = None) -> AbstractPage[Result]:
+    query = sa.select([User])
+    result = await paginate(session, query)
+
+    return result
+
+
+async def get_user(session: AsyncSession, user_id: str) -> Result:
+    query = sa.select([User]).where(User.id == user_id)
+
+    return await session.execute(query)
+
+
+async def get_user_groups(session: AsyncSession, search: Optional[Mapping[str, str]] = None,
+                          order_by: Optional[str] = None) -> AbstractPage[Result]:
+    query = sa.select([UserGroup]).order_by(UserGroup.name)
+    result = await paginate(session, query)
+
+    return result
