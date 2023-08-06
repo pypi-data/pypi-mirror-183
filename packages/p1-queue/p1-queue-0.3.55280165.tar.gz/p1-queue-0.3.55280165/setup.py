657f59f8da85db0428db590543be02acc38cfba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:14 +0000

    Add package p1-queue/0.3.55280165/p1-queue-0.3.55280165.tar.gz

---
+++ packages/p1-queue/p1-queue-0.3.55280165.tar.gz/p1-queue-0.3.55280165/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='p1-queue',
+      version='0.3.55280165',
+      description='Messaging abstraction for AMQP',
+      author='Turfa Auliarachman',
+      author_email='turfa_auliarachman@rocketmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'pika==0.13.1',
+          'google-cloud-pubsub==1.7.2'
+      ],
+      zip_safe=False,
+      include_package_data=True,
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3'
+      ])
