dd1c20db240ae8e7f207409e775f4e3363e52900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:43 +0000

    Add package htsmodels/0.3.7/htsmodels-0.3.7.tar.gz

---
+++ packages/htsmodels/htsmodels-0.3.7.tar.gz/htsmodels-0.3.7/htsmodels/Tests/test_mint_results_tourism.py
@@ -0,0 +1,35 @@
+import unittest
+import tsaugmentation as tsag
+from htsmodels.models.mint import MinT
+import shutil
+
+
+class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        self.data = tsag.preprocessing.PreprocessDatasets('tourism', test_size=228*10).apply_preprocess()
+        self.n = self.data['predict']['n']
+        self.s = self.data['train']['s']
+        shutil.rmtree("./data/original_datasets")
+        self.mint = MinT(dataset='tourism', groups=self.data)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree("./results")
+
+    def test_correct_train(self):
+        model = self.mint.train()
+        self.assertIsNotNone(model)
+
+    def test_results_interval(self):
+        forecasts = self.mint.train()
+        results = self.mint.results(forecasts)
+        res = self.mint.metrics(results)
+        self.mint.store_metrics(res)
+        self.assertLess(res['mase']['bottom'], 2.7)
+        self.assertLess(res['CRPS']['bottom_ind'][0], 10)
+
+    def test_predict_shape(self):
+        forecasts = self.mint.train()
+        df_results = self.mint.results(forecasts)
+        self.assertTrue(df_results.shape == (1872, 11))
