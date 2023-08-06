16029030b6418cfa1b120850c0e1962825e2ccbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:28 +0000

    Add package iosense/1.0.4/iosense-1.0.4.tar.gz

---
+++ packages/iosense/iosense-1.0.4.tar.gz/iosense-1.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.4'
+DESCRIPTION = 'I/O Sense Data Access Library'
+LONG_DESCRIPTION = 'A python package that contains functions to access and manage I/O Sense data for creating third party applications'
+
+# Setting up
+setup(
+    name="iosense",
+    version=VERSION,
+    author="Faclon Labs Private Limited",
+    author_email="<rishi.sharma@faclon.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'faclon', 'iosense', 'data'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
