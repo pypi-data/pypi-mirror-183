28781a1298bc30efe43134a38f2d545e5170d465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:42 +0000

    Add package pyFipper/0.0.4/pyFipper-0.0.4.tar.gz

---
+++ packages/pyFipper/pyFipper-0.0.4.tar.gz/pyFipper-0.0.4/fipper/methods/auth/end_client.py
@@ -0,0 +1,25 @@
+import fipper
+
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from typing import Union
+
+class EndClient:
+    async def endclient(
+        self: "fipper.Client",
+        end_time: Union[int, str],
+    ):
+        """Disconnect the client from Telegram servers.
+
+        Raises:
+            ConnectionError: In case you try to disconnect an already disconnected client or in case you try to
+                disconnect a client that needs to be terminated first.
+        """
+        while True:
+            dt_now = datetime.now()
+            months = dt_now + timedelta(minutes=end_time)
+            if dt_now == months:
+                if self.is_connected:
+                    await self.stop()
+            else:
+                pass
