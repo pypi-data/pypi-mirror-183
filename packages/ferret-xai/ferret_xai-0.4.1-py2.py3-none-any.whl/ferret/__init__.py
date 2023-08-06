86920075576eabaf9d78e0b6c7d8a950888b5856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:23:24 +0000

    Add package ferret-xai/0.4.1/ferret_xai-0.4.1-py2.py3-none-any.whl

---
+++ packages/ferret-xai/ferret_xai-0.4.1-py2.py3-none-any.whl/ferret/__init__.py
@@ -0,0 +1,23 @@
+"""Top-level package for ferret."""
+
+__author__ = """Giuseppe Attanasio"""
+__email__ = "giuseppeattanasio6@gmail.com"
+__version__ = "0.4.1"
+
+
+from .benchmark import Benchmark
+from .datasets.datamanagers import HateXplainDataset
+from .evaluators.faithfulness_measures import (
+    AOPC_Comprehensiveness_Evaluation,
+    AOPC_Sufficiency_Evaluation,
+    TauLOO_Evaluation,
+)
+from .evaluators.plausibility_measures import (
+    AUPRC_PlausibilityEvaluation,
+    Tokenf1_PlausibilityEvaluation,
+    TokenIOU_PlausibilityEvaluation,
+)
+from .explainers.dummy import DummyExplainer
+from .explainers.gradient import GradientExplainer, IntegratedGradientExplainer
+from .explainers.lime import LIMEExplainer
+from .explainers.shap import SHAPExplainer
