5bf4e61529edbfe0a54398ce7386302155785648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:57 +0000

    Add package python-tabular/0.2.8/python_tabular-0.2.8-py3-none-any.whl

---
+++ packages/python-tabular/python_tabular-0.2.8-py3-none-any.whl/pytabular/column.py
@@ -0,0 +1,93 @@
+import logging
+import pandas as pd
+from object import PyObject, PyObjects
+
+
+logger = logging.getLogger("PyTabular")
+
+
+class PyColumn(PyObject):
+    """Wrapper for [Microsoft.AnalysisServices.Tabular.Column](https://learn.microsoft.com/en-us/dotnet/api/microsoft.analysisservices.tabular.column?view=analysisservices-dotnet).
+    With a few other bells and whistles added to it. WIP
+
+    Args:
+        Table: Parent Table to the Column
+    """
+
+    def __init__(self, object, table) -> None:
+        super().__init__(object)
+        self.Table = table
+        self._display.add_row(
+            "Description", str(self._object.Description), end_section=True
+        )
+        self._display.add_row("DataType", str(self._object.DataType))
+        self._display.add_row("EncodingHint", str(self._object.EncodingHint))
+        self._display.add_row("IsAvailableInMDX", str(self._object.IsAvailableInMDX))
+        self._display.add_row("IsHidden", str(self._object.IsHidden))
+        self._display.add_row("IsKey", str(self._object.IsKey))
+        self._display.add_row("IsNullable", str(self._object.IsNullable))
+        self._display.add_row("State", str(self._object.State))
+        self._display.add_row("DisplayFolder", str(self._object.DisplayFolder))
+
+    def get_sample_values(self, top_n: int = 3) -> pd.DataFrame:
+        """Get sample values of column."""
+        column_to_sample = f"'{self.Table.Name}'[{self.Name}]"
+        try:
+            # adding temporary try except. TOPNSKIP will not work for directquery mode.
+            # Need an efficient way to identify if query is direct query or not.
+            dax_query = f"""EVALUATE
+                                TOPNSKIP(
+                                    {top_n},
+                                    0,
+                                    FILTER(
+                                        VALUES({column_to_sample}),
+                                        NOT ISBLANK({column_to_sample}) && LEN({column_to_sample}) > 0
+                                    ),
+                                    1
+                                )
+                                ORDER BY {column_to_sample}
+                        """
+            return self.Table.Model.Query(dax_query)
+        except Exception:
+            dax_query = f"""
+            EVALUATE
+                TOPN(
+                    {top_n},
+                    FILTER(
+                        VALUES({column_to_sample}),
+                        NOT ISBLANK({column_to_sample}) && LEN({column_to_sample}) > 0
+                    )
+                )
+            """
+            return self.Table.Model.Query(dax_query)
+
+    def Distinct_Count(self, No_Blank=False) -> int:
+        """Get [DISTINCTCOUNT](https://learn.microsoft.com/en-us/dax/distinctcount-function-dax) of Column.
+
+        Args:
+            No_Blank (bool, optional): Ability to call [DISTINCTCOUNTNOBLANK](https://learn.microsoft.com/en-us/dax/distinctcountnoblank-function-dax). Defaults to False.
+
+        Returns:
+            int: Number of Distinct Count from column. If `No_Blank == True` then will return number of Distinct Count no blanks.
+        """
+        func = "DISTINCTCOUNT"
+        if No_Blank:
+            func += "NOBLANK"
+        return self.Table.Model.Adomd.Query(
+            f"EVALUATE {{{func}('{self.Table.Name}'[{self.Name}])}}"
+        )
+
+    def Values(self) -> pd.DataFrame:
+        """Get single column DataFrame of [VALUES](https://learn.microsoft.com/en-us/dax/values-function-dax)
+
+        Returns:
+            pd.DataFrame: Single Column DataFrame of Values.
+        """
+        return self.Table.Model.Adomd.Query(
+            f"EVALUATE VALUES('{self.Table.Name}'[{self.Name}])"
+        )
+
+
+class PyColumns(PyObjects):
+    def __init__(self, objects) -> None:
+        super().__init__(objects)
---
+++ packages/python-tabular/python_tabular-0.2.8-py3-none-any.whl/pytabular/table.py
@@ -0,0 +1,102 @@
+import logging
+from object import PyObject
+import pandas as pd
+from partition import PyPartition, PyPartitions
+from column import PyColumn, PyColumns
+from measure import PyMeasure, PyMeasures
+from pytabular.object import PyObjects
+from logic_utils import ticks_to_datetime
+from datetime import datetime
+
+logger = logging.getLogger("PyTabular")
+
+
+class PyTable(PyObject):
+    """Wrapper for [Microsoft.AnalysisServices.Tabular.Table](https://learn.microsoft.com/en-us/dotnet/api/microsoft.analysisservices.tabular.table?view=analysisservices-dotnet).
+    With a few other bells and whistles added to it. You can use the table to access the nested Columns and Partitions. WIP
+
+    Attributes:
+        Model: Reference to Tabular class
+        Partitions: Reference to Table Partitions
+        Columns: Reference to Table Columns
+    """
+
+    def __init__(self, object, model) -> None:
+        super().__init__(object)
+        self.Model = model
+        self.Partitions = PyPartitions(
+            [
+                PyPartition(partition, self)
+                for partition in self._object.Partitions.GetEnumerator()
+            ]
+        )
+        self.Columns = PyColumns(
+            [PyColumn(column, self) for column in self._object.Columns.GetEnumerator()]
+        )
+        self.Measures = PyMeasures(
+            [
+                PyMeasure(measure, self)
+                for measure in self._object.Measures.GetEnumerator()
+            ]
+        )
+        self._display.add_row("# of Partitions", str(len(self.Partitions)))
+        self._display.add_row("# of Columns", str(len(self.Columns)))
+        self._display.add_row(
+            "# of Measures", str(len(self.Measures)), end_section=True
+        )
+        self._display.add_row("Description", self._object.Description, end_section=True)
+        self._display.add_row("DataCategory", str(self._object.DataCategory))
+        self._display.add_row("IsHidden", str(self._object.IsHidden))
+        self._display.add_row("IsPrivate", str(self._object.IsPrivate))
+        self._display.add_row(
+            "ModifiedTime",
+            ticks_to_datetime(self._object.ModifiedTime.Ticks).strftime(
+                "%m/%d/%Y, %H:%M:%S"
+            ),
+        )
+
+    def Row_Count(self) -> int:
+        """Method to return count of rows. Simple Dax Query:
+        `EVALUATE {COUNTROWS('Table Name')}`
+
+        Returns:
+            int: Number of rows using [COUNTROWS](https://learn.microsoft.com/en-us/dax/countrows-function-dax).
+        """
+        return self.Model.Adomd.Query(f"EVALUATE {{COUNTROWS('{self.Name}')}}")
+
+    def Refresh(self, *args, **kwargs) -> pd.DataFrame:
+        """Same method from Model Refresh, you can pass through any extra parameters. For example:
+        `Tabular().Tables['Table Name'].Refresh(Tracing = True)`
+        Returns:
+            pd.DataFrame: Returns pandas dataframe with some refresh details
+        """
+        return self.Model.Refresh(self, *args, **kwargs)
+
+    def Last_Refresh(self) -> datetime:
+        """Will query each partition for the last refresh time then select the max
+
+        Returns:
+            datetime: Last refresh time in datetime format
+        """
+        partition_refreshes = [
+            partition.Last_Refresh() for partition in self.Partitions
+        ]
+        return max(partition_refreshes)
+
+    def Related(self):
+        return self.Model.Relationships.Related(self)
+
+
+class PyTables(PyObjects):
+    """Iterator to handle tables. Accessible via `Tables` attribute in Tabular class.
+
+    Args:
+        PyTable: PyTable class
+    """
+
+    def __init__(self, objects) -> None:
+        super().__init__(objects)
+
+    def Refresh(self, *args, **kwargs):
+        model = self._objects[0].Model
+        return model.Refresh(self, *args, **kwargs)
