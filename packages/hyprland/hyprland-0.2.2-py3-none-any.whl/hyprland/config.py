0cb6f3357ff4aa87c45af3d225ec70d29dc22d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:22:31 +0000

    Add package hyprland/0.2.2/hyprland-0.2.2-py3-none-any.whl

---
+++ packages/hyprland/hyprland-0.2.2-py3-none-any.whl/hyprland/config.py
@@ -0,0 +1,34 @@
+from . import settings
+# from .binds import Bind, BindFlag
+from .sockets import BindListener
+from threading import Thread
+import asyncio
+
+class Config(settings.Defaults):
+    def __init__(self):
+        super().__init__()
+        self.bind_listener = BindListener(self)
+        self._binds = []
+        self.start_bind_listener()
+
+    def start_bind_listener(self):
+        t = Thread(target=asyncio.run,args = [self.bind_listener.start()])
+        t.start()
+    
+    async def add_bind(self, bind):
+        await self.bind_listener.send_bind(bind)
+        self._binds.append(bind)
+    
+    async def add_binds(self, binds):
+        for bind in binds:
+            await self.add_bind(bind)
+        
+
+
+
+
+
+
+    
+
+
