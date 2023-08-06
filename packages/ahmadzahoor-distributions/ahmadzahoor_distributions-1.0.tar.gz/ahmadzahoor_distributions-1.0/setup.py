aa8d65906d8d9fb8b9a24f1f33372812b42be769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:40 +0000

    Add package ahmadzahoor-distributions/1.0/ahmadzahoor_distributions-1.0.tar.gz

---
+++ packages/ahmadzahoor-distributions/ahmadzahoor_distributions-1.0.tar.gz/ahmadzahoor_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ahmadzahoor_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ahmadzahoor_distributions'],
+      author='Zahoor Ahmad]',
+      author_email='ahmadzahoor555@gmail.com',
+      zip_safe=False)
