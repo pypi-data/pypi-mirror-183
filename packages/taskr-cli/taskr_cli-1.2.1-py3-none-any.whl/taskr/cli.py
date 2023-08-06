958649313a2209717150ad8b68922f8fa3a6fd7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:35 +0000

    Add package taskr-cli/1.2.1/taskr_cli-1.2.1-py3-none-any.whl

---
+++ packages/taskr-cli/taskr_cli-1.2.1-py3-none-any.whl/taskr/cli.py
@@ -0,0 +1,82 @@
+import argparse
+import os
+import sys
+
+from .__version__ import version as VERSION
+from .taskr import _Taskr
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog="taskr", description="A cli utility to run generic tasks"
+    )
+    parser.add_argument(
+        "-i",
+        "--init",
+        action="store_true",
+        default=False,
+        help="generate a template task file",
+    )
+    parser.add_argument("-l", "--list", action="store_true", help="show defined tasks")
+    parser.add_argument(
+        "-v", "--version", action="store_true", help="show the version number"
+    )
+    # TODO
+    # parser.add_argument('task', nargs='?', default=None)
+
+    args, custom_args = parser.parse_known_args()
+
+    # No Taskr needed yet
+    if args.init:
+        _Taskr.init()
+        return
+
+    if args.version:
+        print(f"Running {VERSION}")
+        return
+
+    # Below actions needs an instance of taskr
+
+    try:
+        # Attempt to import the tasks file from any sub directory, if we
+        # are in a virtual environment with pyenv
+        root = ""
+        if "PYENV_DIR" in os.environ:
+            root = os.environ["PYENV_DIR"]
+        elif "TASKR_DIR" in os.environ:
+            root = os.environ["TASKR_DIR"]
+
+        sys.path.append(root)
+        import tasks
+
+    except ImportError:
+        print("No valid tasks.py file found in current directory. Run 'taskr --init'")
+        parser.print_help()
+        sys.exit(1)
+
+    Taskr = _Taskr(tasks)
+
+    # Custom arguments take precedence
+    if custom_args:
+        # Custom task was passed, take it in
+        task = custom_args.pop(0)
+
+        # Ignore anything that looks like a normal arg, it shouldn't be here
+        if task.startswith("-"):
+            parser.print_help()
+            return
+
+        Taskr.process(task, custom_args)
+        return
+
+    # Non tasks command below
+    elif args.list:
+        Taskr.list()
+
+    # No tasks passed, check if we have a default task
+    elif Taskr.hasDefault():
+        Taskr.default()
+
+    # Finally print help
+    else:
+        parser.print_help()
