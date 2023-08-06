baa586338547ceccb5224a833337806fbb89244f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:12 +0000

    Add package alg-mat/0.0.1/alg_mat-0.0.1-py3-none-any.whl

---
+++ packages/alg-mat/alg_mat-0.0.1-py3-none-any.whl/src/matadd.py
@@ -0,0 +1,17 @@
+m=int(input("enter the number of rows:"))
+n=int(input("enter the number of columns:"))
+
+print("enter the elements for matrix1:")
+mat1=[[int(input()) for i in range(m)] for j in range(n)]
+print(mat1)
+
+print("enter the elements for matrix2:")
+mat2=[[int(input()) for i in range(m)] for j in range(n)]
+print(mat2)
+
+print("Addition of the two given matrice:")
+result=[[0 for i in range(n)] for j in range(m)]
+for i in range(m):
+    for j in range(n):
+        result[i][j]=mat1[i][j]+mat2[i][j]
+    print(result) 
