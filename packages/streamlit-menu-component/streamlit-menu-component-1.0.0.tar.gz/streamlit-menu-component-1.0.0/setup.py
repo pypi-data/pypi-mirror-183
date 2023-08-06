294a9a7b1526c61c2ecac2796d07c9badaaeee27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:09 +0000

    Add package streamlit-menu-component/1.0.0/streamlit-menu-component-1.0.0.tar.gz

---
+++ packages/streamlit-menu-component/streamlit-menu-component-1.0.0.tar.gz/streamlit-menu-component-1.0.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-menu-component",
+    version="1.0.0",
+    author="Subhash Jadhav",
+    author_email="subhash.jadhav@bluepineapple.io",
+    description="Customizable menu component for the streamlit developes",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit>=1.15.1",
+    ],
+)
