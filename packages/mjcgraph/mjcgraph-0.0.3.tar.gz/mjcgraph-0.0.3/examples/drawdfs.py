a782b581421859460912b1d5a8ced0db1379a84a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:07 +0000

    Add package mjcgraph/0.0.3/mjcgraph-0.0.3.tar.gz

---
+++ packages/mjcgraph/mjcgraph-0.0.3.tar.gz/mjcgraph-0.0.3/examples/drawdfs.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+
+import sys
+from mjcgraph import graph
+from mjcgraph import dfs
+from mjcgraph import draw
+
+infile = "../data/mediumG.txt"
+
+G = graph.Graph(infile)
+print(G.to_string())
+
+dfs = dfs.DFSearch(G, 0)
+dfpath = dfs.path_to(200)
+
+fig = draw.Draw()
+fig.toPNG(G, dfpath)
