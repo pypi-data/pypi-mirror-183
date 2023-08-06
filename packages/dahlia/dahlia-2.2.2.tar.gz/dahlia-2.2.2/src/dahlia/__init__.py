95a7ab8f23798f90494d22f74bb8933468c9480b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:19 +0000

    Add package dahlia/2.2.2/dahlia-2.2.2.tar.gz

---
+++ packages/dahlia/dahlia-2.2.2.tar.gz/dahlia-2.2.2/src/dahlia/__init__.py
@@ -0,0 +1,5 @@
+from .dahlia import Dahlia, Depth
+from .legacy import dahlia, dinput, dprint
+from .utils import clean, clean_ansi
+
+__all__ = ("clean", "clean_ansi", "Dahlia", "Depth", "dahlia", "dinput", "dprint")
