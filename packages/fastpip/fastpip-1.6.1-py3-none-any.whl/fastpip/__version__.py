b22d6a2f47e767bc35a9af4d863f7a829a6d2ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:58 +0000

    Add package fastpip/1.6.1/fastpip-1.6.1-py3-none-any.whl

---
+++ packages/fastpip/fastpip-1.6.1-py3-none-any.whl/fastpip/__version__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+__all__ = ["NAME", "AUTHOR", "WEBSITE", "VERSION", "VERNUM"]
+
+NAME = "fastpip"
+VERNUM = 1, 6, 1
+AUTHOR = "hrpzcf"
+WEBSITE = "https://github.com/hrpzcf/fastpip"
+VERSION = ".".join(map(str, VERNUM))
