9c1ee5aa4d165047aba20c6541e3c1db49ed3cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:24 +0000

    Add package hdxms-datasets/0.1.0/hdxms_datasets-0.1.0.tar.gz

---
+++ packages/hdxms-datasets/hdxms_datasets-0.1.0.tar.gz/hdxms_datasets-0.1.0/hdxms_datasets/__init__.py
@@ -0,0 +1,17 @@
+"""Top-level package for HDXMS Datasets."""
+
+from hdxms_datasets.datavault import DataVault
+from hdxms_datasets.datasets import HDXDataSet
+from hdxms_datasets.config import cfg
+
+# placeholder version number
+__version__ = "0.1.0"
+
+# when we are on editable install from source, the _version file is present
+# and we can get a version from there
+try:
+    from . import _version
+
+    __version__ = _version.get_versions()["version"]
+except ImportError:
+    pass
