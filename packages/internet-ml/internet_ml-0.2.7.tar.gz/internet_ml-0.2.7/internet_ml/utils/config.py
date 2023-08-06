1c04957672b65d7d3e8685ecc1a38bc3f72b8ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:21:26 +0000

    Add package internet-ml/0.2.7/internet_ml-0.2.7.tar.gz

---
+++ packages/internet-ml/internet_ml-0.2.7.tar.gz/internet_ml-0.2.7/internet_ml/utils/config.py
@@ -0,0 +1,54 @@
+from typing import List, Tuple
+import logging
+
+logging.basicConfig(
+    filename="config.log",
+    filemode="w",
+    level=logging.INFO,
+    format="%(name)s - %(levelname)s - %(message)s",
+)
+
+
+
+def initalize():
+    global CONF_DEBUG, GOOGLE_API_KEY, GOOGLE_SEARCH_ENGINE_ID, NLP_CONF_MODE
+    # General
+    CONF_DEBUG = True
+    # Google
+    GOOGLE_API_KEY = ""
+    GOOGLE_SEARCH_ENGINE_ID = ""
+    # NLP
+    NLP_CONF_MODE = "default"
+
+
+def GOOGLE_API_CONFIG(_GOOGLE_API_KEY: str, _GOOGLE_SEARCH_ENGINE_ID: str) -> None:
+    global GOOGLE_SEARCH_ENGINE_ID, GOOGLE_API_KEY, CONF_DEBUG
+    GOOGLE_API_KEY = _GOOGLE_API_KEY
+    GOOGLE_SEARCH_ENGINE_ID = _GOOGLE_SEARCH_ENGINE_ID
+    if CONF_DEBUG and _GOOGLE_API_KEY != "":
+        logging.info(f"API_KEY set")
+    if CONF_DEBUG and _GOOGLE_SEARCH_ENGINE_ID != "":
+        logging.info(f"SEARCH_ENGINE_ID set")
+
+def google_api():
+    global GOOGLE_SEARCH_ENGINE_ID, GOOGLE_API_KEY, CONF_DEBUG
+    return (GOOGLE_API_KEY, GOOGLE_SEARCH_ENGINE_ID)
+
+# TODO: work in progress
+# class GoogleAPI:
+#     def __init__(self) -> None:
+#         self.GOOGLE_SEARCH_API_KEY: str = ""
+#         self.GOOGLE_SEARCH_ENGINE_ID: str = ""
+
+#     @property
+#     def google_search_api_key
+
+
+def NLP_config(mode: str = "default", debug: bool = True) -> None:
+    global NLP_CONF_MODE, CONF_DEBUG
+    CONF_DEBUG = debug
+    if mode == "accuracy" or mode == "speed":
+        NLP_CONF_MODE = mode
+    else:
+        if CONF_DEBUG:
+            logging.warn(f"mode: {mode} does not exist")
