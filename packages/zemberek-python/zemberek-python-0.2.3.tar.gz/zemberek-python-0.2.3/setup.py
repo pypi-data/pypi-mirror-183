fdc1bed3c580e4ee3c343a94777a4ce30e65ad3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:32 +0000

    Add package zemberek-python/0.2.3/zemberek-python-0.2.3.tar.gz

---
+++ packages/zemberek-python/zemberek-python-0.2.3.tar.gz/zemberek-python-0.2.3/setup.py
@@ -0,0 +1,35 @@
+import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='zemberek-python',
+    version='0.2.3',
+    author='Loodos',
+    description='Python port of open source text processing library for Turkish, zemberek-nlp',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={
+      '': [os.path.join("zemberek", "resources", "*.txt"),
+           os.path.join("zemberek", "resources", "*.csv"),
+           os.path.join("zemberek", "resources", "*.slm"),
+           os.path.join("zemberek", "resources", "*.pickle"),
+           os.path.join("zemberek", "resources", "ambiguity", "*")]
+    },
+    license='Apache License 2.0',
+    url='https://www.loodos.com.tr/',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'antlr4-python3-runtime==4.8',
+        'numpy>=1.19.0'
+    ],
+    py_modules=['zemberek'],
+)
