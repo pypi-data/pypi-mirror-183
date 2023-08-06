32ff63d9050a68521150a082d6075828ce403772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:08:28 +0000

    Add package kube-app/1.0/kube_app-1.0.tar.gz

---
+++ packages/kube-app/kube_app-1.0.tar.gz/kube_app-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kube_app',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/LalitKockpit/kube.git',
+    license='',
+    author='lalit',
+    author_email='naveen.gulati@kockpit.in',
+    description='kube sdk'
+)
