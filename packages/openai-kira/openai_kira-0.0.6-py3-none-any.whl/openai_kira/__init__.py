a7aa64939eec4df4589ee06ff2b22b5f4f8f5472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:29 +0000

    Add package openai-kira/0.0.6/openai_kira-0.0.6-py3-none-any.whl

---
+++ packages/openai-kira/openai_kira-0.0.6-py3-none-any.whl/openai_kira/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 12/5/22 9:54 PM
+# @FileName: __init__.py
+# @Software: PyCharm
+# @Github    ：sudoskys
+from .Chat import Chatbot
+from .resouce import Completion
+
+from .setting import RedisConfig
+from .setting import openaiApiKey, redisSetting, dbFile, proxyUrl, webServerUrlFilter, webServerStopSentence
