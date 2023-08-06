880f26d5f509cd8a176182ff91059e7137d16e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:09 +0000

    Add package breeze-build/0.1.1/breeze-build-0.1.1.tar.gz

---
+++ packages/breeze-build/breeze-build-0.1.1.tar.gz/breeze-build-0.1.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="breeze-build",
+    version="0.1.1",
+    description="Breeze is a light-weight, highly-customizable build tool for different languages such as C(++) and Java",
+    license="MIT",
+    entry_points={
+        'console_scripts': [
+            "breeze = src:main"
+        ]
+    },
+    author_email='mauro.balades@tutanota.com',
+    author="mauro-balades",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ]
+)
