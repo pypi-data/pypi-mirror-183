e55d56a9b8628b542e2fbdb353b45efb82f6d275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:29 +0000

    Add package exploration-stats/0.0.5/exploration_stats-0.0.5-py3-none-any.whl

---
+++ packages/exploration-stats/exploration_stats-0.0.5-py3-none-any.whl/exploration_stats/__init__.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+from IPython.display import display
+def eda(df):
+    print("Description of the DataFrame ")
+    print ("\n")  
+    display(df.describe())
+    print ("\n")  
+
+   
+  
