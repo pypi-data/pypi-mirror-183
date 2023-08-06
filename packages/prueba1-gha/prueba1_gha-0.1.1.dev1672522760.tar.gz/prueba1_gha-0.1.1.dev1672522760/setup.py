11dbbe560639f5b6b92ab0dff54aa9d514fdc331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:35 +0000

    Add package prueba1-gha/0.1.1.dev1672522760/prueba1_gha-0.1.1.dev1672522760.tar.gz

---
+++ packages/prueba1-gha/prueba1_gha-0.1.1.dev1672522760.tar.gz/prueba1_gha-0.1.1.dev1672522760/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['prueba1_gha']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'prueba1-gha',
+    'version': '0.1.1.dev1672522760',
+    'description': 'Prueba Github actions',
+    'long_description': '## Readme\n\nreadme\n',
+    'author': 'Martin More',
+    'author_email': 'martinmore@mail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://example.com/',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
