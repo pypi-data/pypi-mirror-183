b726c87dedf4d1b4e50804f131bba6ad7a3324e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:32 +0000

    Add package felupe/6.2.3/felupe-6.2.3-py3-none-any.whl

---
+++ packages/felupe/felupe-6.2.3-py3-none-any.whl/felupe/constitution/__init__.py
@@ -0,0 +1,38 @@
+from ._models_hyperelasticity import NeoHooke
+
+try:
+    from ._models_hyperelasticity_ad import (
+        isochoric_volumetric_split,
+        neo_hooke,
+        ogden,
+        third_order_deformation,
+        yeoh,
+    )
+except:
+    pass
+
+from ._models_linear_elasticity import (
+    LinearElastic,
+    LinearElasticPlaneStrain,
+    LinearElasticPlaneStress,
+    LinearElasticTensorNotation,
+    lame_converter,
+)
+from ._models_pseudo_elasticity import OgdenRoxburgh
+from ._user_materials import (
+    LinearElasticPlasticIsotropicHardening,
+    UserMaterial,
+    UserMaterialStrain,
+)
+
+try:
+    from ._user_materials_hyperelastic import UserMaterialHyperelastic
+except:
+    pass
+
+from ._kinematics import AreaChange, LineChange, VolumeChange
+from ._mixed import ThreeFieldVariation
+from ._user_materials_models import (
+    linear_elastic,
+    linear_elastic_plastic_isotropic_hardening,
+)
---
+++ packages/felupe/felupe-6.2.3-py3-none-any.whl/felupe/region/__init__.py
@@ -0,0 +1,19 @@
+from ._boundary import RegionBoundary
+from ._region import Region
+from ._templates import (
+    RegionConstantHexahedron,
+    RegionConstantQuad,
+    RegionHexahedron,
+    RegionHexahedronBoundary,
+    RegionLagrange,
+    RegionQuad,
+    RegionQuadBoundary,
+    RegionQuadraticHexahedron,
+    RegionQuadraticTetra,
+    RegionQuadraticTriangle,
+    RegionTetra,
+    RegionTetraMINI,
+    RegionTriangle,
+    RegionTriangleMINI,
+    RegionTriQuadraticHexahedron,
+)
