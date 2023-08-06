2307799eeda458f4b754478235f98fca50c36f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:28 +0000

    Add package Rits/0.0.4/Rits-0.0.4.tar.gz

---
+++ packages/Rits/Rits-0.0.4.tar.gz/Rits-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Rits'
+LONG_DESCRIPTION = 'A package to perform arithmetic operations'
+
+# Setting up
+setup(
+    name="Rits",
+    version=VERSION,
+    author="Ritika",
+    author_email="ritika.kumar90@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['arithmetic', 'math', 'mathematics', 'python tutorial', 'Ritika kumar'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+
+)
