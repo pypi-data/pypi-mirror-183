a59c31afc220166d25ecb183a77075bbdc62844b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:20 +0000

    Add package netbox-disk/0.0.7.1/netbox_disk-0.0.7.1.tar.gz

---
+++ packages/netbox-disk/netbox_disk-0.0.7.1.tar.gz/netbox_disk-0.0.7.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['netbox_disk', 'netbox_disk.templatetags']
+
+package_data = \
+{'': ['*'], 'netbox_disk': ['templates/*']}
+
+setup_kwargs = {
+    'name': 'netbox-disk',
+    'version': '0.0.7.1',
+    'description': 'Netbox Disk Plugin',
+    'long_description': 'pip3 install poetry\npoetry build\npoetry publish',
+    'author': 'Tim Rhomberg',
+    'author_email': 'timrhomberg@hotmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
