c785e0c7fb498fb558fe3f7788695ba84193e56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:25 +0000

    Add package nginx-logs-parser-and-process/1.0.17/nginx_logs_parser_and_process-1.0.17-py3-none-any.whl

---
+++ packages/nginx-logs-parser-and-process/nginx_logs_parser_and_process-1.0.17-py3-none-any.whl/nginx_logs_parser_and_process/domain/DataProcessor/EliminateUnnecessaryData.py
@@ -0,0 +1,16 @@
+from pandas import DataFrame
+from nginx_logs_parser_and_process.domain.Server.Server import Server
+
+
+class EliminateUnnecessaryData:
+    def __init__(self, pd, server: Server):
+        self.pd = pd
+        self.server = server
+
+    def process(self, dataframe: DataFrame) -> DataFrame:
+        # dataframe = dataframe.loc[dataframe["referer"] == '"https://seb.ops1.thunderbees.com.br/sgi/"']
+        filtered_data: DataFrame = DataFrame(columns=dataframe.columns)
+        for filter in self.server.data_filter:
+            clean_dataframe = dataframe.loc[dataframe[filter["field"]].str.contains(filter["value"], regex=True)]
+            filtered_data = self.pd.concat([filtered_data, clean_dataframe])
+        return filtered_data
