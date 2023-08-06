a990f250a45f1c78ee6e43d11f2dab25466d8293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:31 +0000

    Add package torchlikelihoods/0.0.5.1/torchlikelihoods-0.0.5.1-py3-none-any.whl

---
+++ packages/torchlikelihoods/torchlikelihoods-0.0.5.1-py3-none-any.whl/torchlikelihoods/scalers/__init__.py
@@ -0,0 +1,15 @@
+from .minmax import MinMaxScaler
+from .standard import StandardScaler
+from .scale import ScaleScaler
+from .scalediff import ScaleDiffScaler
+from .identity import IdentityScaler
+from .heterogeneous import HeterogeneousScaler
+from .heterogeneous_object import HeterogeneousObjectScaler
+
+scalers_dict = {
+    'minmax01': MinMaxScaler,
+    'std': StandardScaler,
+    'scale': ScaleScaler,
+    'scale_diff': ScaleDiffScaler,
+    'identity': IdentityScaler
+}
