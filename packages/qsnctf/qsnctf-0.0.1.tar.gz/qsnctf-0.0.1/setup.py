3b722742efb06b1cfca9947ee9519fbb158f472f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:52:02 +0000

    Add package qsnctf/0.0.1/qsnctf-0.0.1.tar.gz

---
+++ packages/qsnctf/qsnctf-0.0.1.tar.gz/qsnctf-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import io
+
+with io.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='qsnctf',
+    version='0.0.1',
+    packages=['qsnctf'],
+    url='https://github.com/Moxin1044/qsnctf-python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
+    license='MIT License',
+    author='Moxin',
+    author_email='1044631097@qq.com',
+    description='青少年CTF的Python库模块'
+)
