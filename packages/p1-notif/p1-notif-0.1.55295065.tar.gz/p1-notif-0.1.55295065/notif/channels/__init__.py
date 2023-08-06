46a1260c48c195005b82a2e8c7440660f3c8c8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:00 +0000

    Add package p1-notif/0.1.55295065/p1-notif-0.1.55295065.tar.gz

---
+++ packages/p1-notif/p1-notif-0.1.55295065.tar.gz/p1-notif-0.1.55295065/notif/channels/__init__.py
@@ -0,0 +1,13 @@
+
+from notif.channels.abstract_channel import AbstractChannel
+from notif.channels.project_push_notification import ProjectPushNotification
+from notif.channels.user_push_notification import UserPushNotification
+from notif.channels.email import Email
+from notif.channels.customerio import CustomerIO
+from notif.channels.create_group_whatsapp import CreateGroupWhatsapp
+from notif.channels.list_group_whatsapp import ListGroupWhatsapp
+from notif.channels.whatsapp_template_message_sender import WhatsappTemplateMessageSender
+from notif.channels.whatsapp_template_message_sender import WhatsappTemplateMessageSenderV3
+from notif.channels.sms_message_sender import SMSMessageSender
+
+__all__ = ['AbstractChannel', 'ProjectPushNotification', 'UserPushNotification', 'Email', 'CustomerIO', 'CreateGroupWhatsapp', 'ListGroupWhatsapp', 'WhatsappTemplateMessageSender', 'WhatsappTemplateMessageSenderV3', 'SMSMessageSender']
