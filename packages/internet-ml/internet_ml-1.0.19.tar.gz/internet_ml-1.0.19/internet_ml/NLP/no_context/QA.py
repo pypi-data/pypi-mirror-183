1b00743d8a02bc9629888dbcde70a3e86f733996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:25 +0000

    Add package internet-ml/1.0.19/internet_ml-1.0.19.tar.gz

---
+++ packages/internet-ml/internet_ml-1.0.19.tar.gz/internet_ml-1.0.19/internet_ml/NLP/no_context/QA.py
@@ -0,0 +1,40 @@
+from typing import Any, List, Tuple
+
+import logging
+import os
+import sys
+from pathlib import Path
+
+import dotenv
+from transformers import pipeline
+
+dotenv.load_dotenv()
+
+logging.basicConfig(
+    filename="QA.log",
+    filemode="w",
+    level=logging.INFO,
+    format="%(name)s - %(levelname)s - %(message)s",
+)
+
+sys.path.append(str(Path(__file__).parent.parent.parent) + "/tools/NLP/data")
+sys.path.append(str(Path(__file__).parent.parent.parent) + "/utils")
+import config
+import internet
+
+
+def answer(query: str) -> tuple[Any, list[str]]:
+    QA_MODEL: Any = pipeline("question-answering")
+    results: tuple[list[str], list[str]] = internet.Google(query).google()
+    answer: tuple[Any, list[str]] = (
+        QA_MODEL(question=query, context=str(results[0])),
+        results[1],
+    )
+    if config.CONF_DEBUG:
+        logging.info(f"Answer: {answer}")
+    return answer
+
+
+# print(os.environ)
+# print(answer("Who is Elon Musk?"))
+# def custom_answer
