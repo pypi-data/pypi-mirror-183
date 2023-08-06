47d840310b15d8c1b8b9f0acc834647f90473883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:37 +0000

    Add package python-graphblas/2022.12.0/python-graphblas-2022.12.0.tar.gz

---
+++ packages/python-graphblas/python-graphblas-2022.12.0.tar.gz/python-graphblas-2022.12.0/setup.py
@@ -0,0 +1,54 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+extras_require = {
+    "repr": ["pandas"],
+    "io": ["networkx", "scipy >=1.7.0", "awkward"],
+    "viz": ["matplotlib"],
+    "test": ["pytest", "pandas", "scipy"],
+}
+extras_require["complete"] = sorted({v for req in extras_require.values() for v in req})
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="python-graphblas",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description=(
+        "Python library for GraphBLAS: high-performance sparse linear algebra "
+        "for scalable graph analytics"
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Erik Welch and Jim Kitchen",
+    author_email="erik.n.welch@gmail.com,jim22k@gmail.com",
+    url="https://github.com/python-graphblas/python-graphblas",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=["suitesparse-graphblas >=7.4.0.0, <7.5", "numba", "donfig", "pyyaml"],
+    extras_require=extras_require,
+    include_package_data=True,
+    license="Apache License 2.0",
+    keywords=["graphblas", "graph", "sparse", "matrix", "lagraph", "suitesparse"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    zip_safe=False,
+)
