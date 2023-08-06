2f00733872517bb2591e7ce517a834614e01d847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:41 +0000

    Add package smpl-io/1.0.5/smpl_io-1.0.5.tar.gz

---
+++ packages/smpl-io/smpl_io-1.0.5.tar.gz/smpl_io-1.0.5/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['smpl_io']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'smpl-io',
+    'version': '1.0.5',
+    'description': 'SiMPLe plotting and fitting',
+    'long_description': 'smpl_io\n',
+    'author': 'Alexander Puck Neuwirth',
+    'author_email': 'alexander@neuwirth-informatik.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/APN-Pucky/smpl_io',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
