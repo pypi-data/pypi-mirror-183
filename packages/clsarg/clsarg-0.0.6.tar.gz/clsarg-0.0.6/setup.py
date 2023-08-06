c85c6c16c49d9dde86d59918f44c59272ec3f37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:59 +0000

    Add package clsarg/0.0.6/clsarg-0.0.6.tar.gz

---
+++ packages/clsarg/clsarg-0.0.6.tar.gz/clsarg-0.0.6/setup.py
@@ -0,0 +1,32 @@
+"""Copyright 2022 by @doldam0. All rights reserved."""
+
+import setuptools
+
+if __name__ == "__main__":
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+
+    with open("requirements.txt", "r") as f:
+        setuptools.setup(
+            name="clsarg",
+            version="0.0.6",
+            author="doldam0",
+            author_email="jinustar@g.skku.edu",
+            description="The class-based argument parser",
+            long_description=long_description,
+            long_description_content_type="text/markdown",
+            url="https://github.com/doldam0/clsarg",
+            classifiers=[
+                "Programming Language :: Python :: 3",
+                "Operating System :: OS Independent",
+            ],
+            packages=setuptools.find_packages(
+                exclude=("test", "build", "dist", "scripts")
+            ),
+            python_requires=">=3.8",
+            install_requires=[
+                pkgs.strip()
+                for pkgs in f.readlines()
+                if not pkgs.startswith("--")
+            ],
+        )
