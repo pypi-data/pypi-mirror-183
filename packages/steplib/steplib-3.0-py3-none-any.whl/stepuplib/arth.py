b3a343652de66186a10d511197b0042f6e733d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:02 +0000

    Add package steplib/3.0/steplib-3.0-py3-none-any.whl

---
+++ packages/steplib/steplib-3.0-py3-none-any.whl/stepuplib/arth.py
@@ -0,0 +1,5 @@
+def sum(n1,n2,n3):
+    print(n1+n2+n3)
+
+def sub(n1,n2):
+    print(n1-n2)
---
+++ packages/steplib/steplib-3.0-py3-none-any.whl/stepuplib/title.py
@@ -0,0 +1,10 @@
+name="Sachin"
+
+def getName(name):
+    print('Hey this is {}'.format(name))
+
+class Student:
+    name="s1"
+    marks=300
+    def getStdInfo(self):
+        print('my name is {}, marks are {}'.format(self.name,self.marks))
