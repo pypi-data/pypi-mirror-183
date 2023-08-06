f2c1f960bd9cf9bcb7d6fc10ce33b0c9da12d0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:51 +0000

    Add package django-pint-field/0.1.5/django_pint_field-0.1.5-py3-none-any.whl

---
+++ packages/django-pint-field/django_pint_field-0.1.5-py3-none-any.whl/django_pint_field/apps.py
@@ -0,0 +1,52 @@
+import logging
+from decimal import Decimal, getcontext
+from django.apps import AppConfig
+from django.conf import settings
+from django.core.management import call_command
+from django.db.models import Lookup
+from django.utils.translation import gettext_lazy as _
+from io import StringIO
+from psycopg2.extensions import register_adapter
+from . import (
+    integer_pint_field_adapter,
+    big_integer_pint_field_adapter,
+    decimal_pint_field_adapter,
+)
+
+
+logger = logging.getLogger("watervize.entities_flags")
+
+
+def check_migrations_complete():
+    """Runs the showmigrations command, and returns false if any have not been run yet"""
+    out = StringIO()
+    call_command("showmigrations", format="plan", stdout=out)
+    out.seek(0)
+    for line in out.readlines():
+        status, name = line.rsplit(" ", 1)
+        if "[X]" not in status:
+            return False
+    return True
+
+
+def set_decimal_precision():
+    """
+    If `DJANGO_PINT_FIELD_DECIMAL_PRECISION` is set to an int value greater than 0, the project's
+      decimalprecision will be set to that value. Otherwise, it is left as default.
+    """
+    decimal_precision = getattr(settings, "DJANGO_PINT_FIELD_DECIMAL_PRECISION", 0)
+
+    if isinstance(decimal_precision, int) and decimal_precision > 0:
+        getcontext().prec = decimal_precision
+
+
+class DjangoPintFieldAppConfig(AppConfig):
+    name = "django_pint_field"
+
+    def ready(self):
+        set_decimal_precision()
+
+        if check_migrations_complete():
+            from .lookups import get_pint_field_lookups
+
+            get_pint_field_lookups()
