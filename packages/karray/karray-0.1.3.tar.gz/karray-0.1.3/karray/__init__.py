afa551f04548d44f728f171a9105f73b547c860a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:43 +0000

    Add package karray/0.1.3/karray-0.1.3.tar.gz

---
+++ packages/karray/karray-0.1.3.tar.gz/karray-0.1.3/karray/__init__.py
@@ -0,0 +1,12 @@
+# karray
+# copyright 2022, Carlos Gaete-Morales, DIW-Berlin 
+"""
+    karray
+    copyright 2022, Carlos Gaete-Morales, DIW-Berlin 
+"""
+__version__ = (0, 1, 3)
+__author__ = 'Carlos Gaete-Morales'
+
+
+from .arrays import Long, Array, numpy_to_long, concat, from_feather, _from_feather, pandas_to_array
+from .setting import settings
