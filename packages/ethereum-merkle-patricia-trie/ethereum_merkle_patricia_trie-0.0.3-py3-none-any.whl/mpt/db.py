7b435ac9f1d9fe4570686a4a56fc43fde9ef3449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:25 +0000

    Add package ethereum-merkle-patricia-trie/0.0.3/ethereum_merkle_patricia_trie-0.0.3-py3-none-any.whl

---
+++ packages/ethereum-merkle-patricia-trie/ethereum_merkle_patricia_trie-0.0.3-py3-none-any.whl/mpt/db.py
@@ -0,0 +1,21 @@
+import plyvel
+
+
+class DB:
+    __instance = None
+
+    def __init__(self, path, create_if_missing=True):
+        """
+        Virtually private constructor.
+        """
+        if self.__instance == None:
+            self.__instance = plyvel.DB(path, create_if_missing=create_if_missing)
+
+    def put(self, key, data):
+        self.__instance.put(key, data)
+
+    def get(self, key):
+        return self.__instance.get(key)
+
+    def delete(self, key):
+        self.__instance.delete(key)
