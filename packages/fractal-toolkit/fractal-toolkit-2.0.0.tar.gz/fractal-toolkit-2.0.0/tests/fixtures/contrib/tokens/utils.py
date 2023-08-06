01b2e2584ba27e7cf1275d79a664ce2d828ae0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:30 +0000

    Add package fractal-toolkit/2.0.0/fractal-toolkit-2.0.0.tar.gz

---
+++ packages/fractal-toolkit/fractal-toolkit-2.0.0.tar.gz/fractal-toolkit-2.0.0/tests/fixtures/contrib/tokens/utils.py
@@ -0,0 +1,55 @@
+import uuid
+from dataclasses import dataclass
+
+import pytest
+
+
+@pytest.fixture
+def account():
+    @dataclass
+    class Account:
+        id: str
+
+    return Account(str(uuid.uuid4()))
+
+
+@pytest.fixture
+def user(account):
+    @dataclass
+    class User:
+        id: str
+        account_id: str
+
+    return User(str(uuid.uuid4()), account.id)
+
+
+@pytest.fixture
+def token(dummy_token_service_fractal, user, account):
+    return dummy_token_service_fractal.context.token_service.generate(
+        {
+            "sub": user.id,
+            "account": account.id,
+        }
+    )
+
+
+@pytest.fixture
+def admin_role_token(dummy_token_service_fractal, user, account):
+    return dummy_token_service_fractal.context.token_service.generate(
+        {
+            "sub": user.id,
+            "account": account.id,
+            "roles": ["admin"],
+        }
+    )
+
+
+@pytest.fixture
+def token_service_application_context(
+    fake_application_context_class, dummy_json_token_service_class
+):
+    application_context = fake_application_context_class()
+    application_context.install_service(
+        dummy_json_token_service_class, name="token_service"
+    )
+    return application_context
