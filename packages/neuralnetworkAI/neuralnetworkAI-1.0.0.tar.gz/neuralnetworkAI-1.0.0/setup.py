0b654ee84f02a6c7cc9617e4564058ec68374e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:12 +0000

    Add package neuralnetworkAI/1.0.0/neuralnetworkAI-1.0.0.tar.gz

---
+++ packages/neuralnetworkAI/neuralnetworkAI-1.0.0.tar.gz/neuralnetworkAI-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+
+# Setting up
+setup(
+    version="1.0.0",
+    name="neuralnetworkAI", 
+    author="",
+    author_email="",
+    packages=find_packages(),
+)
