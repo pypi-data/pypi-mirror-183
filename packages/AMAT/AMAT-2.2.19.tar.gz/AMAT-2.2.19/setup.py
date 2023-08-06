a423aae779b7714477216dd61ce26da78706cab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:54 +0000

    Add package AMAT/2.2.19/AMAT-2.2.19.tar.gz

---
+++ packages/AMAT/AMAT-2.2.19.tar.gz/AMAT-2.2.19/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='AMAT',
+      version='2.2.19',
+      description='Aerocapture Mission Analysis Tool',
+      url='https://github.com/athulpg007/AMAT',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Athul P. Girija',
+      author_email='athulpg007@gmail.com',
+      license='GPL-3.0-or-later',
+      packages=['AMAT'],
+      install_requires=['numpy', 'scipy', 'matplotlib', 'pandas'],
+      classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+
+    include_package_data=False,
+    
+    package_data = {
+      'AMAT' : ['tests/*']
+  },
+      zip_safe=False)
