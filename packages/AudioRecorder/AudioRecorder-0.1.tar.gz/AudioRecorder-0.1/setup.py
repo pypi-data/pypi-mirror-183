13469fc472e28d261c319e0a06cbae59aa74b557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:20 +0000

    Add package AudioRecorder/0.1/AudioRecorder-0.1.tar.gz

---
+++ packages/AudioRecorder/AudioRecorder-0.1.tar.gz/AudioRecorder-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='AudioRecorder',
+    version='0.1',
+    description='A simple example package',
+    author='Reman khan',
+    author_email='remankhanrw@email.com',
+    packages=['AudioRecorder'],
+    install_requires=['pyaudio'],
+)
