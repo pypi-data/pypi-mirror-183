dd34c2d60710427c9d5cfcbb07a15ea5d742e906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:46 +0000

    Add package MatQUBO/0.0/MatQUBO-0.0.tar.gz

---
+++ packages/MatQUBO/MatQUBO-0.0.tar.gz/MatQUBO-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='MatQUBO',
+      version='0.0',
+      description='MatQUBO',
+      packages=['MatQUBO'],
+      author_email='aryatahlil@yahoo.com',
+      zip_safe=False)
