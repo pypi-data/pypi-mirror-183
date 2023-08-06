4fde2e19b474f10f53d752588983d9ff47a4f808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:23 +0000

    Add package python-allib/0.3.0.0/python_allib-0.3.0.0-py3-none-any.whl

---
+++ packages/python-allib/python_allib-0.3.0.0-py3-none-any.whl/allib/configurations/catalog.py
@@ -0,0 +1,53 @@
+from enum import Enum
+
+
+class ALConfiguration(Enum):
+    NaiveBayesEstimator = "NaiveBayesEstimator"
+    SVMEstimator = "SVMEstimator"
+    MixedEstimator = "MixedEstimator"
+    RaschEstimator = "RaschEstimator"
+    RaschLR = "RaschLR"
+    RaschRF = "RaschRF"
+    EntropySamplingNB = "EntropySamplingNB"
+    ProbabilityEnsemble = "ProbabilityEnsemble"
+    RaschNBLRRF = "RaschNBLRRF"
+    RaschNBLRRFSVM = "RaschNBLRRFSVM"
+    RaschNBLRRFLGBM = "RaschNBLRRFLBGM"
+    RaschNBLRRFLGBMRAND = "RaschNBLRRFLGBMRAND"
+    ILRaschNBLRRFLGBMRAND = "ILRaschNBLRRFLGBMRAND"
+    AUTOTAR = "AUTOTAR"
+    AUTOSTOP = "AUTOSTOP"
+
+
+class INITConfiguration(Enum):
+    IDENTITY = "IDENTITY"
+    RANDOM = "RANDOM"
+    UNIFORM = "UNIFORM"
+    SEPARATE = "SEPARATE"
+    POSITVEUNIFORM = "POSITIVEUNIFORM"
+
+
+class FEConfiguration(Enum):
+    TFIDF5000 = "TfIDF5000"
+    TFIDFTAR = "TFIDFTAR"
+
+
+class EstimationConfiguration(str, Enum):
+    RaschRidge = "RaschRidge"
+    RaschParametric = "RaschParametric"
+    RaschApproxParametric = "RaschApproxParametric"
+    RaschApproxConvParametric = "RaschApproxConvParametric"
+    CHAO = "Chao"
+    AUTOSTOP = "AUTOSTOP"
+
+
+class StopBuilderConfiguration(str, Enum):
+    CHAO_CONS_OPT = "Chao_CONS_OPT"
+    AUTOTAR = "AUTOTAR"
+    AUTOSTOP = "AUTOSTOP"
+
+
+class ExperimentCombination(str, Enum):
+    CHAO4 = "CHAO4"
+    AUTOTAR = "AUTOTAR"
+    AUTOSTOP = "AUTOSTOP"
