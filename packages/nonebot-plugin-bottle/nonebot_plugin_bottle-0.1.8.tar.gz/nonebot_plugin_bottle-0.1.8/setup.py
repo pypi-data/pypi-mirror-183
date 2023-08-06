9019bbf9ce1ba60a5a9f485da4a55ab16509db5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:40 +0000

    Add package nonebot-plugin-bottle/0.1.8/nonebot_plugin_bottle-0.1.8.tar.gz

---
+++ packages/nonebot-plugin-bottle/nonebot_plugin_bottle-0.1.8.tar.gz/nonebot_plugin_bottle-0.1.8/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nonebot_plugin_bottle",
+    version="0.1.8",
+    author="Todysheep",
+    author_email="todysheep@163.com",
+    description="Bottle post plugin in Nonebot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Todysheep/nonebot_plugin_bottle",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'nonebot2>=2.0.0-beta.2',
+        'nonebot-adapter-onebot>=2.0.0b1',
+        'nonebot-adapter-onebot',
+        'requests',
+        ],
+    entry_points={},
+    license='GNU GPLv3',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
