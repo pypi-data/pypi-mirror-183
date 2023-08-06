612a7d912335a488303a5b69f9f6e19b261f8f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:57 +0000

    Add package mathfin/0.0.5/mathfin-0.0.5.tar.gz

---
+++ packages/mathfin/mathfin-0.0.5.tar.gz/mathfin-0.0.5/README.md
@@ -0,0 +1,8 @@
+# lib com calculos para matematica financeira 
+### tabela SAC Price, 
+### PMT, PV, FV, i da HP 12c
+
+
+### Sac metodo dataframe() // pago() // juros_pg()
+### Price metodo dataframe() // pago() // juros_pg()
+### Hp12c metodo value_future() // present_value () payment () taxa_juros()
---
+++ packages/mathfin/mathfin-0.0.5.tar.gz/mathfin-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='mathfin',
+    version='0.0.5',
+    license='MIT License',
+    author='Naoki Yokoyama',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='naokity@msn.com',
+    keywords='math fin',
+    description=u'Lib com funções de matematica financeira',
+    packages=['mathfin'],
+    install_requires=['pandas'],)
