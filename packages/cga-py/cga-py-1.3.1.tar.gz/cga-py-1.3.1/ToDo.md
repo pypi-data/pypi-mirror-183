f299cd09c9823b9bf38245038cd117f8703c7b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:49 +0000

    Add package cga-py/1.3.1/cga-py-1.3.1.tar.gz

---
+++ packages/cga-py/cga-py-1.3.1.tar.gz/cga-py-1.3.1/ToDo.md
@@ -0,0 +1,15 @@
+# To Do until Package is complete
+
+- [x] Fully document
+	- [x] base_objects.py
+	- [x] conditions.py
+	- [x] dorst_motions.py
+	- [x] four_quaternions.py
+	- [x] geom_generators.py
+	- [ ] multivector.py
+	- [x] operators.py
+	- [x] random.py
+- [ ] Maybe document helper functions
+- [ ] Write README
+- [x] Write User Guide
+- [ ] implement sphinx
---
+++ packages/cga-py/cga-py-1.3.1.tar.gz/cga-py-1.3.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+me = "Daren Thimm"
+my_email = "daren.thimm@gmx.at"
+setup(
+    name="cga-py",
+    version="1.3.1",
+    packages=[
+        "cga_py",
+    ],
+    scripts=["cga_py/bin/cga_py_vis.py"],
+    license="BSD-3-Clause License",
+    url="https://github.com/physicspenguin/cga-py",
+    author=me,
+    author_email=my_email,
+    maintainer=me,
+    maintainer_email=my_email,
+    description="Implementation of CGA for python",
+    long_description=open("./README.md").read(),
+    long_description_content_type="text/markdown",
+)
