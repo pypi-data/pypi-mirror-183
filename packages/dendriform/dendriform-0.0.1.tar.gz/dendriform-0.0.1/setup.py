3f244605fda94260f07ab6fe196a7a52c21c5770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:24 +0000

    Add package dendriform/0.0.1/dendriform-0.0.1.tar.gz

---
+++ packages/dendriform/dendriform-0.0.1.tar.gz/dendriform-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+long_description = "dendriform client"
+
+setuptools.setup(
+    name = "dendriform",
+    version = "0.0.1",
+    description = "dendriform client library",
+    long_description = long_description,
+    url = "https://www.dendriform.io",
+    python_requires = ">=3.6"
+)
