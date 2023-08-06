9d62dd06a1ca09d7b920412edb0731aa4f88fb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:00 +0000

    Add package imslp/0.2.3/imslp-0.2.3.tar.gz

---
+++ packages/imslp/imslp-0.2.3.tar.gz/imslp-0.2.3/imslp/__init__.py
@@ -0,0 +1,10 @@
+
+"""
+The clean and modern way of accessing IMSLP data and scores programmatically.
+"""
+
+__version__ = "0.2.3"
+__author__ = "Jérémie Lumbroso <lumbroso@cs.princeton.edu>"
+
+version_info = tuple(int(v) if v.isdigit() else v for v in __version__.split('.'))
+
