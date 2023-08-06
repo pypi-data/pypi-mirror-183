3a1a644fd649a6c904fea995d564482dce08fbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:13 +0000

    Add package Akshay-grpi/1.0/Akshay_grpi-1.0.tar.gz

---
+++ packages/Akshay-grpi/Akshay_grpi-1.0.tar.gz/Akshay_grpi-1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+with open("README.md","r") as fh:
+    long_description = fh.read()
+setup(
+    name='Akshay_grpi',
+    version='1.0',
+    description='validating password',
+    py_modules=["Akshay_grpi"],
+    package_dir={'':'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "blessings ~= 1.7",
+        "turtle ",
+        "time"
+
+    ],
+    extra_require ={
+        "dev":[
+            "pytest>=3.7",
+
+        ],
+    },
+    author="Akshay",
+    author_email="akshaysavanoor@gmail.com",
+)
