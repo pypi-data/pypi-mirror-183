ff243dffa54a6ad3fc3d881bc7d96f144b2daf11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:13 +0000

    Add package py-proxy-checker/0.2.1/py-proxy-checker-0.2.1.tar.gz

---
+++ packages/py-proxy-checker/py-proxy-checker-0.2.1.tar.gz/py-proxy-checker-0.2.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="py-proxy-checker",
+    version="0.2.1",
+    author="Yassine Amjad",
+    author_email="Yassine.amjad001@gmail.com",
+    url="https://github.com/yassine20011/py-proxy-checker",
+    description="Proxy Checker is a python package that allows you to check the validity of a proxy",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=["proxychecker"],
+    install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",  
+    ],
+    python_requires='>=3.6',
+    license='MIT',
+    keywords='proxy checker',
+    )
