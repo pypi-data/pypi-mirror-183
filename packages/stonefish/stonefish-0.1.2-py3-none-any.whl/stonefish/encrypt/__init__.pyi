14ef4b81ce490cbe3f38a4740277c655b9fedd8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:55 +0000

    Add package stonefish/0.1.2/stonefish-0.1.2-py3-none-any.whl

---
+++ packages/stonefish/stonefish-0.1.2-py3-none-any.whl/stonefish/encrypt/__init__.pyi
@@ -0,0 +1,2 @@
+from ._main import encrypt_source as encrypt_source
+from x21 import __dex_22b__ as __dex_22b__
---
+++ packages/stonefish/stonefish-0.1.2-py3-none-any.whl/stonefish/minify/__init__.pyi
@@ -0,0 +1,1 @@
+from ._main import minify_source as minify_source, remove_comments as remove_comments, remove_whitespace as remove_whitespace
