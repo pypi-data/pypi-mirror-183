c1690aa19abf93916bb98a1c43b1ffe891448625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:46 +0000

    Add package UW-RestClients-HRP/1.3.0.7/UW-RestClients-HRP-1.3.0.7.tar.gz

---
+++ packages/UW-RestClients-HRP/UW-RestClients-HRP-1.3.0.7.tar.gz/UW-RestClients-HRP-1.3.0.7/uw_hrp/util/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2022 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from restclients_core.util.decorators import use_mock
+from uw_hrp.dao import HRP_DAO
+
+fdao_hrp_override = use_mock(HRP_DAO())
