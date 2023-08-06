1968a9e3970673db228d6e04e2b814f0f75d83a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:45 +0000

    Add package rplus-constants-module/0.0.5/rplus_constants_module-0.0.5.tar.gz

---
+++ packages/rplus-constants-module/rplus_constants_module-0.0.5.tar.gz/rplus_constants_module-0.0.5/README.md
@@ -0,0 +1,32 @@
+# Rplus constant module
+
+## To install requirements to virtual environment
+
+```shell
+    make pip
+```
+
+## Activate virtualenv
+```shell
+  poetry env use $(which python)
+```
+
+## Install dependencies
+```shell
+  make install
+```
+
+## Update dependencies
+```shell
+  make update
+```
+
+## To build package
+```shell
+  make build
+```
+
+## To publish package
+```shell
+  make publish
+```
---
+++ packages/rplus-constants-module/rplus_constants_module-0.0.5.tar.gz/rplus_constants_module-0.0.5/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['constants', 'constants.logger', 'constants.rplus_utils_module']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'rplus-constants-module',
+    'version': '0.0.5',
+    'description': '',
+    'long_description': '# Rplus constant module\n\n## To install requirements to virtual environment\n\n```shell\n    make pip\n```\n\n## Activate virtualenv\n```shell\n  poetry env use $(which python)\n```\n\n## Install dependencies\n```shell\n  make install\n```\n\n## Update dependencies\n```shell\n  make update\n```\n\n## To build package\n```shell\n  make build\n```\n\n## To publish package\n```shell\n  make publish\n```',
+    'author': 'AIML Team',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
