ab9f25accb0a5ec586742bd45cdda390157bd921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:30 +0000

    Add package px-django-actions-tracker/0.1.2/px-django-actions-tracker-0.1.2.tar.gz

---
+++ packages/px-django-actions-tracker/px-django-actions-tracker-0.1.2.tar.gz/px-django-actions-tracker-0.1.2/pxd_actions_tracker/storages/db/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from django.utils.translation import pgettext_lazy
+from django.db import models
+from prettyjson import PrettyJSONWidget
+from django.templatetags.l10n import localize
+
+from .models import Log
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = 'id', 'domain', 'message', 'tracking', 'created_at_extended',
+    list_display_links = 'id', 'message',
+    list_filter = (
+        # 'domain',
+        'created_at',
+    )
+    search_fields = 'id', 'domain', 'tracking', 'message', 'detail',
+    readonly_fields = 'id', 'domain', 'tracking', 'message', 'created_at_extended',
+    date_hierarchy = 'created_at'
+
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    ('domain', 'message'),
+                    'detail',
+                    'tracking',
+                    'created_at_extended',
+                )
+            }
+        ),
+    )
+
+    formfield_overrides = {models.JSONField: {'widget': PrettyJSONWidget}}
+
+    def created_at_extended(self, obj):
+        return '{} {}'.format(
+            localize(obj.created_at), obj.created_at.strftime('%S.%f'),
+        )
+
+    created_at_extended.admin_order_field = 'created_at'
+    created_at_extended.short_description = pgettext_lazy(
+        'pxd-actions-tracker', 'Created at'
+    )
