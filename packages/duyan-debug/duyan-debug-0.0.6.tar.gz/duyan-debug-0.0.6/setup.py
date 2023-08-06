bd9968be290cc20042386d6fb24b10a85df61921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:42 +0000

    Add package duyan-debug/0.0.6/duyan-debug-0.0.6.tar.gz

---
+++ packages/duyan-debug/duyan-debug-0.0.6.tar.gz/duyan-debug-0.0.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="duyan-debug",
+    version="0.0.6",
+    author="WuMenghao",
+    author_email="menghao.wu@duyansoft.com",
+    description="Using for Debug, finding problem",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["debug_util"],
+    python_requires=">=3.6",
+    install_requires=[
+        'objgraph',
+        'guppy3'
+    ]
+)
