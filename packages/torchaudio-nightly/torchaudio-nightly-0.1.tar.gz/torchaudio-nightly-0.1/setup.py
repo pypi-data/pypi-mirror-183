ee8094e6922af144a9a115cc38ebc44e00cca4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:08 +0000

    Add package torchaudio-nightly/0.1/torchaudio-nightly-0.1.tar.gz

---
+++ packages/torchaudio-nightly/torchaudio-nightly-0.1.tar.gz/torchaudio-nightly-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="torchaudio-nightly",
+    version="0.1",
+    description="This is a test",
+    url="https://example.org/",
+    author="John Doe",
+    license="MIT",
+    packages=[],
+    scripts=["test777.py"],
+    zip_safe=False,
+)
