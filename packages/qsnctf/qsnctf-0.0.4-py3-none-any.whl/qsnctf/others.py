f2a8446d9fa7d230121129ec335a5826ade7cc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:49 +0000

    Add package qsnctf/0.0.4/qsnctf-0.0.4-py3-none-any.whl

---
+++ packages/qsnctf/qsnctf-0.0.4-py3-none-any.whl/qsnctf/others.py
@@ -0,0 +1,34 @@
+# 其他CTF能力
+# 2023年1月1日
+# 末心
+import os
+import pkgutil
+import execjs  # PyExecJS
+# 社会主义核心价值观编码
+# 需要将Python目录下>lib>subprocess.py的765行附近的encoding的默认None值修改为utf-8即可。
+
+
+
+
+def js_from_file(file_name):
+    with open(file_name, 'r', encoding='UTF-8') as file:
+        result = file.read()
+        return result
+
+
+def Chinese_socialism_encode(data):
+    package_path = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(package_path, 'plugin', 'js', 'cvencode.js')
+    content = execjs.compile(js_from_file(file_path))
+    result = content.call('encode', data)
+    result = str(result)
+    return result
+
+def Chinese_socialism_decode(data):
+    package_path = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(package_path, 'plugin', 'js', 'cvencode.js')
+    content = execjs.compile(js_from_file(file_path))
+    result = content.call('decode', data)
+    result = str(result)
+    return result
+
