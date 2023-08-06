5683e173d6de27c4420bb8737d91c71d436ae8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package banditsdk/0.0.3/banditsdk-0.0.3-py3-none-any.whl

---
+++ packages/banditsdk/banditsdk-0.0.3-py3-none-any.whl/banditsdk/common/session/session_manager.py
@@ -0,0 +1,22 @@
+import threading
+from sqlalchemy.orm import Session
+
+
+class SessionManager:
+    """
+    Session Manager
+    """
+    def __init__(self, session_factory):
+        self._current_session = None
+        self._current_thread_id = None
+        self._session_factory = session_factory
+
+    def get_session(self) -> Session:
+        thread_id = threading.current_thread().ident
+        if thread_id != self._current_thread_id:
+            if self._current_session:
+                self._current_session.close()
+            self._current_session = self._session_factory()
+            self._current_thread_id = thread_id
+        return self._current_session
+
