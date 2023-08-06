88fe9d467633f27b4621079b0ebdca7690fd6871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:23 +0000

    Add package omnitools/0.0.267/omnitools-0.0.267-py3-none-any.whl

---
+++ packages/omnitools/omnitools-0.0.267-py3-none-any.whl/omnitools/xsys/pyver.py
@@ -0,0 +1,8 @@
+import sys
+
+
+PYTHON3_5 = sys.version_info[:3] >= (3, 5, 0)
+PYTHON3_6 = sys.version_info[:3] >= (3, 6, 0)
+PYTHON3_7 = sys.version_info[:3] >= (3, 7, 0)
+PYTHON3_8 = sys.version_info[:3] >= (3, 8, 0)
+PYTHON3_9 = sys.version_info[:3] >= (3, 9, 0)
