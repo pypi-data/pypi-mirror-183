d12e03f3a05b82944a4bfe7c6305f332cd243ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:47:25 +0000

    Add package audio-recorder-streamlit/0.0.5/audio-recorder-streamlit-0.0.5.tar.gz

---
+++ packages/audio-recorder-streamlit/audio-recorder-streamlit-0.0.5.tar.gz/audio-recorder-streamlit-0.0.5/audio_recorder_streamlit/frontend/build/precache-manifest.32101605636f52d2b817458112b151b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5d975b413da7717b6837b5221b0f52",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b0444ce6cbc39dc7c58",
+    "url": "./static/js/2.097e26ce.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.097e26ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb83b9f1d2afcac3376",
+    "url": "./static/js/main.02f94a34.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.02f94a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
