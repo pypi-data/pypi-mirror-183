989aaec9b093c3cbc3108e0c3d30b6f8c605e5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:16:39 +0000

    Add package ipyelk/2.0.0/ipyelk-2.0.0.tar.gz

---
+++ packages/ipyelk/ipyelk-2.0.0.tar.gz/ipyelk-2.0.0/js/tools/feedback/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Copyright (c) 2022 ipyelk contributors.
+ * Distributed under the terms of the Modified BSD License.
+ */
+import toolFeedbackModule from './di.config';
+
+export { toolFeedbackModule };
---
+++ packages/ipyelk/ipyelk-2.0.0.tar.gz/ipyelk-2.0.0/js/tools/feedback/utils.ts
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2022 ipyelk contributors.
+ * Distributed under the terms of the Modified BSD License.
+ */
+import { SModelElement } from 'sprotty';
+
+export function addCssClasses(root: SModelElement, cssClasses: string[]) {
+  if (root.cssClasses == null) {
+    root.cssClasses = [];
+  }
+  for (const cssClass of cssClasses) {
+    if (root.cssClasses.indexOf(cssClass) < 0) {
+      root.cssClasses.push(cssClass);
+    }
+  }
+}
+
+export function removeCssClasses(root: SModelElement, cssClasses: string[]) {
+  if (root.cssClasses == null || root.cssClasses.length === 0) {
+    return;
+  }
+  for (const cssClass of cssClasses) {
+    const index = root.cssClasses.indexOf(cssClass);
+    if (index !== -1) {
+      root.cssClasses.splice(root.cssClasses.indexOf(cssClass), 1);
+    }
+  }
+}
