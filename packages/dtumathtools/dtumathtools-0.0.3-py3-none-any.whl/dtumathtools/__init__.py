ef80d794dd1ae84e1f8039a8e2d00284ef328ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:28 +0000

    Add package dtumathtools/0.0.3/dtumathtools-0.0.3-py3-none-any.whl

---
+++ packages/dtumathtools/dtumathtools-0.0.3-py3-none-any.whl/dtumathtools/__init__.py
@@ -0,0 +1,10 @@
+from . import dtuplot
+
+# # from sympy.external import import_module
+
+# from . import quiverplot as dtuplot
+
+# # dtuplot = import_module("spb")
+# import spb as dtuplot
+
+__all__ = ["dtuplot"]
