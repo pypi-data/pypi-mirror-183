b2cb943679f3a92f1494de618ad133a66875549e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:07 +0000

    Add package mfac/0.1.15/mfac-0.1.15-py3-none-any.whl

---
+++ packages/mfac/mfac-0.1.15-py3-none-any.whl/mfac/controllers/cfdl.py
@@ -0,0 +1,62 @@
+from .. import MFACInterface
+import numpy as np
+
+
+class CompactFormDynamicLinearization(MFACInterface):
+    @MFACInterface.initializer
+    def __init__(self,
+                 model,  # plant model
+                 iteration_function,  # will be called in each iteration
+                 eta,  # 0 <eta <=2   Weighting Factor
+                 mu,  # 0 < mu      Weighting Factor
+                 rho,  # 0 < rho <= 1 Step Factor
+                 labda,  # 0 < labda
+                 reference_output,
+                 max_steps=1000,
+                 mimo=False,
+                 f0=None,
+                 ):
+        self.max_iterations = int(max_steps)
+        self.eta = eta
+        self.mu = mu
+        self.rho = rho
+        self.labda = labda
+        self.Yd = reference_output
+        self.is_mimo = mimo
+        self.model = model
+        self.iteration_function = iteration_function
+        self.u = np.matrix(np.zeros([self.model.number_of_inputs, 1]))
+        if f0 is None:
+            self.f = np.matrix(np.zeros([self.model.number_of_outputs, self.model.number_of_inputs]))
+        else:
+            self.f = f0
+        self.du = self.u
+        self.U = np.array([self.u])
+        self.F = np.array([self.f])
+        self.dU = np.array([self.du])
+        self.iteration = 0
+
+    def run(self):
+        for self.iteration in range(self.max_iterations):
+            self.calculate_zero_control() if self.iteration < 2 else self.calculate_control()
+            self.model.step(self.u)
+            self.iteration_function(self)
+        return self
+
+    def calculate_control(self):
+        y_pre, y = self.model.observe(2, full_state=False)
+        self.f = self.f + self.eta * (y - y_pre - self.f * self.du) * self.du.transpose() / (
+                    self.mu + np.power(np.linalg.norm(self.u), 2))
+        # limit f between bounds
+        self.f += 0.5 * (self.f < 1e-5)
+
+        u_pre = self.u
+        y_d = self.Yd[self.iteration + 1]
+        self.u = self.u + self.rho * self.f.transpose() * (y_d - y) / (self.labda + np.power(np.linalg.norm(self.f), 2))
+        self.du = self.u - u_pre
+        self.F = np.append(self.F, self.f)
+        self.U = np.append(self.U, self.u)
+        self.dU = np.append(self.dU, self.du)
+
+    def calculate_zero_control(self):
+        self.u = np.matrix(np.zeros([self.model.number_of_inputs, 1]))
