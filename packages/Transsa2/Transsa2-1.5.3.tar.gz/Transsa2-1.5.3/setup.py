c87fbe29e95dfe39d34d89c6c1c8c1e1b447da5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:18 +0000

    Add package Transsa2/1.5.3/Transsa2-1.5.3.tar.gz

---
+++ packages/Transsa2/Transsa2-1.5.3.tar.gz/Transsa2-1.5.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import find_packages, setup
+setup(
+    name='Transsa2',
+    version='1.5.3',
+    packages=find_packages(),
+    py_modules=['Transsa2']
+
+)
+
+
