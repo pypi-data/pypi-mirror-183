67f9875756c0195b0e6157a871192e9b43a04dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:31 +0000

    Add package streamlit-cropper/0.2.1/streamlit_cropper-0.2.1-py3-none-any.whl

---
+++ packages/streamlit-cropper/streamlit_cropper-0.2.1-py3-none-any.whl/streamlit_cropper/frontend/build/precache-manifest.d7669212bbb7e1589af78bf5ada198a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e2d2486619f6c5dfa49ae7a0894874",
+    "url": "./index.html"
+  },
+  {
+    "revision": "090e35c0101f9a7f4871",
+    "url": "./static/js/2.3873474b.chunk.js"
+  },
+  {
+    "revision": "a120daea67c98f2907b7f8aedcea87dd",
+    "url": "./static/js/2.3873474b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca49c68404eb4ca451b7",
+    "url": "./static/js/main.aad080d4.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.aad080d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
