645e0cbbecb14e58da88a883a7064a269d0c8dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:12 +0000

    Add package pkgmt/0.0.16/pkgmt-0.0.16-py3-none-any.whl

---
+++ packages/pkgmt/pkgmt-0.0.16-py3-none-any.whl/pkgmt/cli.py
@@ -0,0 +1,40 @@
+import sys
+
+import click
+
+from pkgmt import links, config, test
+from pkgmt import new as new_
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def check_links():
+    """Check for broken links"""
+    cfg = config.load()["check_links"]
+    out = links.find_broken_in_files(
+        cfg["extensions"], cfg.get("ignore_substrings"), verbose=True
+    )
+
+    if out:
+        sys.exit(1)
+
+
+@cli.command()
+@click.argument("name")
+def new(name):
+    """Create new package"""
+    new_.package(name)
+
+
+@cli.command()
+@click.option(
+    "-f", "--file", type=click.Path(dir_okay=False, exists=True), default="README.md"
+)
+@click.option("-i", "--inplace", is_flag=True, show_default=True, default=False)
+def test_md(file, inplace):
+    """Run a markdown file"""
+    test.markdown(file, inplace=inplace)
