8e16d7b2f5ca4db02243514464098f93bf77b1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:03 +0000

    Add package hari-cal/0.0.1/hari_cal-0.0.1.tar.gz

---
+++ packages/hari-cal/hari_cal-0.0.1.tar.gz/hari_cal-0.0.1/src/hari_cal.py
@@ -0,0 +1,38 @@
+from functions import add, sub, mul, div
+def cal():
+   
+   
+    a = float(input("enter a value: "))
+    b = float(input("enter b value: "))
+   
+    while True:
+        try:
+            optios = """
+
+            1. add
+            2. sub
+            3. mul
+            4. div
+            5. stop
+        
+            """
+            print(optios)
+            user = int(input("enter ur num :"))
+            if user == 1:
+                data = add(a,b)
+                print(data)
+            elif user == 2:
+                data = sub(a,b)
+                print(data)
+            elif user == 3:
+                data = mul(a,b)
+                print(data)
+            elif user == 4:
+                data = div(a,b)
+                print(data)
+            elif user == 5:
+                break
+        except Exception as e:
+            raise e
+
+# cal()
