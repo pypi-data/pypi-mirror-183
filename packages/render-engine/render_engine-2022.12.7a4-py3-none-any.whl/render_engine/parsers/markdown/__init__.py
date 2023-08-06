e570f15795c29a32168f761584b5f5d679db2308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:33 +0000

    Add package render-engine/2022.12.7a4/render_engine-2022.12.7a4-py3-none-any.whl

---
+++ packages/render-engine/render_engine-2022.12.7a4-py3-none-any.whl/render_engine/parsers/markdown/__init__.py
@@ -0,0 +1,22 @@
+import itertools
+import pathlib
+from typing import Any, Type
+
+import frontmatter
+from markdown2 import markdown
+
+from ..base_parsers import BasePageParser
+
+
+def _attrs_from_content(content):
+    """fetches the content"""
+    return frontmatter.parse(content)
+
+
+class MarkdownPageParser(BasePageParser):
+    configuration_values = ["markdown_extras"]
+
+    @staticmethod
+    def markup(page, content) -> str:
+        """Parses the content with the parser"""
+        return markdown(content, extras=(page, "markdown_extras", None))
