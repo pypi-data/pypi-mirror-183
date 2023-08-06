2e51cfbc2177434919abaedde33fb84c91bdea41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:14 +0000

    Add package adjugate/0.9.0/adjugate-0.9.0.tar.gz

---
+++ packages/adjugate/adjugate-0.9.0.tar.gz/adjugate-0.9.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name = 'adjugate',
+    version = '0.9.0',
+    description = 'A package the calculation of submatricies, minors, adjugate- and cofactor matricies.',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+
+    author = 'Sebastian Gössl',
+    author_email = 'goessl@student.tugraz.at',
+    license = 'MIT',
+
+    url = 'https://github.com/goessl/adjugate',
+    py_modules = ['crandom'],
+    python_requires = '>=3.7',
+    install_requires = ['numpy'],
+
+    classifiers = [
+      'Programming Language :: Python :: 3.7',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent'
+    ]
+)
