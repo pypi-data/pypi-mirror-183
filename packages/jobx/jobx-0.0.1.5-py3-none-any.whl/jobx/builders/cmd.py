62548cbd545b9499dc91b79477bff0a1126a7b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:55 +0000

    Add package jobx/0.0.1.5/jobx-0.0.1.5-py3-none-any.whl

---
+++ packages/jobx/jobx-0.0.1.5-py3-none-any.whl/jobx/builders/cmd.py
@@ -0,0 +1,12 @@
+import os
+from jobx.utils import  generate_env_string
+from jobx.task import Task
+
+from .registry import register
+@register("cmd")
+def build_cmd_task(task:dict, name, g=None):
+        command = task['command']
+        env = task.get('env',{})
+        def task():
+            os.system(generate_env_string(**env)+command)
+        return Task(task, name, g)
