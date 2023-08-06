9a8c990739bfdb6cde6b243c94a2dedffb9bda61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:32 +0000

    Add package pygsgui/0.0.3/pygsgui-0.0.3.tar.gz

---
+++ packages/pygsgui/pygsgui-0.0.3.tar.gz/pygsgui-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pygsgui",
+    version="0.0.3",
+    author="StupidDeveloper05",
+    author_email="StupidDeveloper05@gmail.com",
+    description="This my first Python Game Super Graphic User Interface!!!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StupidDeveloper05/pygsgui",
+    packages=setuptools.find_packages(),
+    package_data={'pygsgui': ['basicTheme.json']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
