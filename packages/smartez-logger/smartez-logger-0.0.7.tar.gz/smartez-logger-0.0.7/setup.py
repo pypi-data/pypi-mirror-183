cad357caab63350a55991f60734ac569045133d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:21:32 +0000

    Add package smartez-logger/0.0.7/smartez-logger-0.0.7.tar.gz

---
+++ packages/smartez-logger/smartez-logger-0.0.7.tar.gz/smartez-logger-0.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="smartez-logger",
+    version="0.0.7",
+    author="Rune Lykke-Kjeldsen",
+    author_email="rune@lykke-kjeldsen.dk",
+    description="Logger package used for smartez",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    install_requires=['python-dotenv'],
+    url="https://github.com/BispensGipsGebis/smartez-logger",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    project_urls={
+        "Bug Tracker": "https://github.com/BispensGipsGebis/smartez-logger/issues",
+    }
+)
