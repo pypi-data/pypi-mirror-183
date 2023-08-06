027cbe707b2a58a2cd7ae62b1bc94fbd8ee24979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:02 +0000

    Add package jobx/0.0.2.5/jobx-0.0.2.5-py3-none-any.whl

---
+++ packages/jobx/jobx-0.0.2.5-py3-none-any.whl/jobx/builders/python.py
@@ -0,0 +1,37 @@
+from jobx.utils import import_module, generate_env_string
+from jobx.task import Task
+import os
+import sys
+from .registry import register,TaskBuilder
+@register(["python","py"])
+class PythonScript(TaskBuilder):
+    type="python"
+    def match(self,task:dict):
+        return "py-path" in task
+    def build(self,task:dict, name, g=None):
+        python_file = task.get("py-path",task.get("path"))
+        args = task.get('args',[])
+        kwargs = task.get('kwargs',{})
+        func_name= task.get('function',task.get("func",None))
+        env=task.get('env',{})
+        if func_name:
+            module = import_module(python_file)
+            func = getattr(module, func_name)
+            task = lambda: func(*args, **kwargs)
+            return Task(task, name, g)
+        else:
+            args = ' '.join([str(x) for x in args])
+            kwargs_str = []
+            for k, v in kwargs.items():
+                if isinstance(v, bool):
+                    if v:
+                        kwargs_str.append('--%s' % (k))
+                    else:
+                        continue
+                else:
+                    kwargs_str.append('--%s=%s' % (k, v))
+            kwargs = ' '.join(kwargs_str)
+
+            def task():
+                os.system(generate_env_string(**env)+f"{sys.executable} {python_file}{' ' + args if args else ''}{' ' + kwargs if kwargs else ''}")
+            return Task(task, name)
---
+++ packages/jobx/jobx-0.0.2.5-py3-none-any.whl/jobx/builders/task_group.py
@@ -0,0 +1,60 @@
+from fnmatch import fnmatch
+from .registry import register,TaskBuilder,match_task
+from ..task import Task
+
+def build_task(task, name, g=None):
+    assert isinstance(task, dict)
+    builder=match_task(task)
+    tasks = builder.build(task, name,g)
+    if isinstance(tasks, Task):
+        tasks = [tasks]
+    return tasks
+
+@register("task-group")
+class TaskGroup(TaskBuilder):
+    def match(self,task:dict):
+        return "tasks" in task
+    def build(self,cfg:dict, name, g=None, task_spec=None):
+        '''
+        task_spec example: 'train-*,test-*'
+        '''
+        if g is None:
+            g = {}
+        task_dict = cfg.get('tasks')
+        # print(task_dict.keys())
+        if isinstance(task_dict, (list, tuple, set)):
+            task_dict = {str(k): v for k, v in enumerate(task_dict)}
+        def search_keys(pattern, candidates):
+            if not {',', '*'}.intersection(set(list(pattern))):
+                if pattern in candidates:
+                    # print(pattern)
+                    return [pattern]
+                else:
+                    return None
+            results = []
+            if ',' in pattern:
+                for p in pattern.split(','):
+                    p = p.strip()
+                    r = search_keys(p, candidates)
+                    if r is not None:
+                        results += r
+            for cand in candidates:
+                if fnmatch(cand, pattern) and not cand in results:
+                    results.append(cand)
+            return results
+
+        if task_spec:
+            keys = search_keys(str(task_spec), list(task_dict.keys()))
+            if not keys:
+                print(f"No task matches : {task_spec}")
+                return []
+            task_dict = {key: task_dict[key] for key in keys}
+
+        task_dict = {f'{name}-{key}': v for key, v in task_dict.items()}
+        task_list = []
+        for name, t in task_dict.items():
+            # print(name)
+            task_list += build_task(t, name, g)
+        return task_list
+
+
