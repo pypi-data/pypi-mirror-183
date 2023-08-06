1195fd9782c3f4f45adace8190b3bfc69fa9dac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:52 +0000

    Add package similarweb-rest-api/0.0.3/similarweb_rest_api-0.0.3.tar.gz

---
+++ packages/similarweb-rest-api/similarweb_rest_api-0.0.3.tar.gz/similarweb_rest_api-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='similarweb_rest_api',
+    version='1.2',
+    description='A library to simplify the access to the REST API of similarweb.',
+    author='Damien Frigewski',
+    author_email='dfrigewski@gmail.com',
+    url='https://github.com/DamienDrash/similarweb_rest_api',
+    packages=['similarweb_rest_api'],
+    package_data={'similarweb_rest_api': ['similarweb_endpoints.json']},
+    install_requires=['pandas', 'requests', 'lxml', 'pyyaml'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    license='MIT',
+)
