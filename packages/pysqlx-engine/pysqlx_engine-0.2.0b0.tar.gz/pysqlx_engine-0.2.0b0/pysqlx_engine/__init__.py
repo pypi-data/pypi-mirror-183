a0022e59ed51c90059bf6ccbd0770c3172a10c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:49 +0000

    Add package pysqlx-engine/0.2.0b0/pysqlx_engine-0.2.0b0.tar.gz

---
+++ packages/pysqlx-engine/pysqlx_engine-0.2.0b0.tar.gz/pysqlx_engine-0.2.0b0/pysqlx_engine/__init__.py
@@ -0,0 +1,5 @@
+from ._core.aconn import PySQLXEngine
+from ._core.apool import PySQLXEnginePool
+from ._core.pool import PySQLXEnginePoolSync
+from ._core.conn import PySQLXEngineSync
+from ._core.parser import BaseRow
