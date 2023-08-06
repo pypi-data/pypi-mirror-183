daa3e25eb5a3cbe53d07ff0b32ccc0967b52eada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:23 +0000

    Add package pyatmo/7.5.0/pyatmo-7.5.0.tar.gz

---
+++ packages/pyatmo/pyatmo-7.5.0.tar.gz/pyatmo-7.5.0/src/pyatmo/exceptions.py
@@ -0,0 +1,25 @@
+"""Exceptions."""
+
+
+class NoSchedule(Exception):
+    pass
+
+
+class InvalidHome(Exception):
+    pass
+
+
+class InvalidRoom(Exception):
+    pass
+
+
+class NoDevice(Exception):
+    pass
+
+
+class ApiError(Exception):
+    pass
+
+
+class InvalidState(Exception):
+    pass
