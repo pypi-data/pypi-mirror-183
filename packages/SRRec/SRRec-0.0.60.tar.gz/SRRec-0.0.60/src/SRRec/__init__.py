1f93b2998bdde33e5e4f4f5127407c5dfd33e47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:01 +0000

    Add package SRRec/0.0.60/SRRec-0.0.60.tar.gz

---
+++ packages/SRRec/SRRec-0.0.60.tar.gz/SRRec-0.0.60/src/SRRec/__init__.py
@@ -0,0 +1,10 @@
+'''
+@File    :   __init__.py
+@Time    :   2022/10/25 23:18:27
+@Author  :   Pengyao Ping
+@Version :   1.0
+@Contact :   Ping.Pengyao@gmail.com
+@desc    : 
+'''
+
+__version__ = "0.0.60"
