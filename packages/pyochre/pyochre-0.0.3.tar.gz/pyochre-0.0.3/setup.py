f0260e002e92f4eb593487cbb02dec03e01b502e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:53 +0000

    Add package pyochre/0.0.3/pyochre-0.0.3.tar.gz

---
+++ packages/pyochre/pyochre-0.0.3.tar.gz/pyochre-0.0.3/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='pyochre',
+    version='0.0.3',
+    description='Library for the Open Computational Humanities Research Ecosystem (OCHRE).',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Tom Lippincott',
+    author_email='tom.lippincott@jhu.edu',
+    url='https://github.com/comp-int-hum/ochre-python',
+    packages=["pyochre"],
+    install_requires=[
+        "captum",
+        "django",
+        "djangorestframework",
+        "django-cors-headers",
+        "django-debug-toolbar",
+        "django-environ",
+        "django-extensions",
+        "django-guardian",
+        "django-mptt",
+        "django-registration",
+        "django-sekizai",
+        "drf-nested-routers",
+        "gensim",
+        "jsonpath-python",
+        "pairtree",
+        "python-dotenv",
+        "rdflib",
+        "requests",
+        "spacy",
+        "torch",
+        "torchvision",
+        "torch-model-archiver",
+        "transformers",
+        "wiki",
+        "wikidata",
+    ],
+    extras_require={
+        "LDAP" : [
+            "django-auth-ldap",
+            "python-ldap"
+        ],
+        "POSTGRES" : [
+            "psycopg2"
+        ],
+        "CELERY" : [
+            "celery"
+        ],
+        "TORCHSERVE" : [
+            "torchserve"
+        ]
+    }
+)
