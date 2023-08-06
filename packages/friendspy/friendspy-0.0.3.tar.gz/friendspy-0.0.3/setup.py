359bd0b6477eaa8bd4b9201669f2329b1cc7eca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:31 +0000

    Add package friendspy/0.0.3/friendspy-0.0.3.tar.gz

---
+++ packages/friendspy/friendspy-0.0.3.tar.gz/friendspy-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Friends Show Related API'
+LONG_DESCRIPTION = 'A package that allows to get details about friends show'
+
+# Setting up
+setup(
+    name="friendspy",
+    version=VERSION,
+    author="Ashutosh Sharma",
+    author_email="email2ashusharma@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages = ['friendspy.quotes'], # this must be the same as the name above
+    install_requires=[],
+    keywords=['python', 'friends', 'entertainment', 'fun'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
