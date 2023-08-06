a1417f4e577724bb3045f25aa184e08940e5adb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:05 +0000

    Add package earthnet/0.3.9/earthnet-0.3.9-py3-none-any.whl

---
+++ packages/earthnet/earthnet-0.3.9-py3-none-any.whl/earthnet/__init__.py
@@ -0,0 +1,13 @@
+"""EarthNet2021 Toolkit
+A library for downloading, evaluating and plotting Earth surface forecasts.
+"""
+__version__ = "0.3.9"
+__author__ = 'Vitus Benson, Christian Requena-Mesa'
+__credits__ = 'Max-Planck-Institute for Biogeochemistry'
+
+from earthnet.parallel_score import EarthNetScore
+from earthnet.download import Downloader
+from earthnet.coords import get_coords_from_cube, get_coords_from_tile
+from earthnet.plot_cube import cube_gallery, cube_ndvi_timeseries
+from earthnet.download_v2 import download, load_minicube, load_en21x_as_npz
+from earthnet.score_v2 import normalized_NSE, score_over_dataset
