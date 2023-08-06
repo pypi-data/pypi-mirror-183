bf3f6df83f5a099993f5a47daa636edeea1a8e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:19 +0000

    Add package crypto-otrs/1.0.3/crypto_otrs-1.0.3.tar.gz

---
+++ packages/crypto-otrs/crypto_otrs-1.0.3.tar.gz/crypto_otrs-1.0.3/src/crypto_otrs/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+
+# Version of the realpython-reader package
+__version__ = "1.0.0"
+
+import os
+script_dir = os.path.abspath(os.path.dirname(__file__)))
+lib_path = os.path.join(script_dir, "libring.so")
+print(lib_path)
+if os.exists(lib_path) == False:
+    print("libring not found, compiling...")
+    os.system("clang -fPIC -shared -g -lm -lssl -lcrypto ring.c sha2.c -o libring.so")
