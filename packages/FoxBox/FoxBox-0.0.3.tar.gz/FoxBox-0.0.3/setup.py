9c35ce81f8b275f8f8023271f4c861ab5a99b268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:19 +0000

    Add package FoxBox/0.0.3/FoxBox-0.0.3.tar.gz

---
+++ packages/FoxBox/FoxBox-0.0.3.tar.gz/FoxBox-0.0.3/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    long_description="""# Markdown supported!\n\n* Cheer\n* Celebrate\n""",
+    long_description_content_type='text/markdown',
+    name="FoxBox",
+    version='0.0.3',
+    author="Batte",
+    description="An usefull package",
+    packages=["foxbox"]
+)
