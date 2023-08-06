85f7a6776fb8ec9e13cd7380e2b5a7a88d460008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:28 +0000

    Add package mozark-sdk-python-test/0.0.1.1/mozark-sdk-python-test-0.0.1.1.tar.gz

---
+++ packages/mozark-sdk-python-test/mozark-sdk-python-test-0.0.1.1.tar.gz/mozark-sdk-python-test-0.0.1.1/mozark-sdk-python-test/api_flow.py
@@ -0,0 +1,43 @@
+class ApiFlow:
+
+    def login_input(self, username, pwd):
+        pass
+
+    def login_output(self):
+        pass
+
+    def create_project(self, project_name):
+        pass
+
+    def create_project_output(self):
+        pass
+
+    def upload_file(self, file_name, file_category, project_name):
+        pass
+
+    def upload_file_output(self):
+        pass
+
+    def execute_test(self, device_id, test_configuration, test_parameters):
+        pass
+
+    def execute_test_output(self):
+        pass
+
+    def delete_schedule(self, schdule_id):
+        pass
+
+    def delete_schedule_output(self):
+        pass
+
+    def abort_test(self, test_id):
+        pass
+
+    def abort_test_output(self):
+        pass
+
+    def delete_files(self, file_id):
+        pass
+
+    def delete_files_output(self):
+        pass
