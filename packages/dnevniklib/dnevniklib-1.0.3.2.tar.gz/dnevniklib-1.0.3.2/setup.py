08246dc68a003f8963bbdb8a4af49db159633787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:27 +0000

    Add package dnevniklib/1.0.3.2/dnevniklib-1.0.3.2.tar.gz

---
+++ packages/dnevniklib/dnevniklib-1.0.3.2.tar.gz/dnevniklib-1.0.3.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+# Открытие README.md и присвоение его long_description.
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+# Определение requests как requirements для того, чтобы этот пакет работал. Зависимости проекта.
+requirements = ["requests<=2.21.0", "selenium"]
+
+# Функция, которая принимает несколько аргументов. Она присваивает эти значения пакету.
+setuptools.setup(
+	name="dnevniklib",
+	version="1.0.3.2",
+	author="Ivan Kriventsev",
+	author_email="dirtyhornet277@outlook.com",
+	description="Library for automated work with dnevnik.mos.ru",
+
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/dirtyhornet277/dnevniklib",
+	packages=setuptools.find_packages(),
+	classifiers=[
+	],
+	python_requires='>=3.6',
+)
