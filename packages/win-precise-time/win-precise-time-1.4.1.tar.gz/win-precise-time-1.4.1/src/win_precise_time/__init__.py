dfeed44f07d0f7827c002662698b41c38aa2e92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:22:56 +0000

    Add package win-precise-time/1.4.1/win-precise-time-1.4.1.tar.gz

---
+++ packages/win-precise-time/win-precise-time-1.4.1.tar.gz/win-precise-time-1.4.1/src/win_precise_time/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2022-present Artur Drogunow <artur.drogunow@zf.com>
+#
+# SPDX-License-Identifier: MIT
+
+__version__ = "1.4.1"
+
+from win_precise_time._t import (
+    time,
+    time_ns,
+    sleep,
+    sleep_until,
+    sleep_until_ns,
+    hotloop_until_ns,
+)
