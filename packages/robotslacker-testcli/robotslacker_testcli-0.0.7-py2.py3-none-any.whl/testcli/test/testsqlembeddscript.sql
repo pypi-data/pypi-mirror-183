3eb6a1ff21cd4a260da690c655990dd69b056e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:03 +0000

    Add package robotslacker-testcli/0.0.7/robotslacker_testcli-0.0.7-py2.py3-none-any.whl

---
+++ packages/robotslacker-testcli/robotslacker_testcli-0.0.7-py2.py3-none-any.whl/testcli/test/testsqlembeddscript.sql
@@ -0,0 +1,24 @@
+_connect /mem
+create table aaa (id int);
+insert into aaa values(10);
+> {%
+x=1
+dbConn = sessionContext["dbConn"]
+stmt = dbConn.createStatement()
+rs = stmt.executeQuery("select id from aaa")
+while rs.next():
+    print("id=" + rs.getString(1))
+sessionContext["status"] = "x=" + str(x)
+%}
+update aaa set id = 20;
+select * from aaa;
+> {%
+x=x+3
+dbConn = sessionContext["dbConn"]
+stmt = dbConn.createStatement()
+stmt.execute("update aaa set id=30")
+print("x="+str(x))
+sessionContext["status"] = "x=" + str(x)
+%}
+select * from aaa;
+
