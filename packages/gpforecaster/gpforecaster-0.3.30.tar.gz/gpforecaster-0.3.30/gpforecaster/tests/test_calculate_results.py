7d4a30114dde28ceff44a02b34282f3d7484a5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:15 +0000

    Add package gpforecaster/0.3.30/gpforecaster-0.3.30.tar.gz

---
+++ packages/gpforecaster/gpforecaster-0.3.30.tar.gz/gpforecaster-0.3.30/gpforecaster/tests/test_calculate_results.py
@@ -0,0 +1,20 @@
+import unittest
+import tsaugmentation as tsag
+from gpforecaster.model.gpf import GPF
+import shutil
+
+
+class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        self.data = tsag.preprocessing.PreprocessDatasets('prison').apply_preprocess()
+        self.n = self.data['predict']['n']
+        self.s = self.data['train']['s']
+        shutil.rmtree("./data/original_datasets")
+        self.gpf = GPF('prison', self.data)
+
+    def test_calculate_metrics_dict(self):
+        model, like = self.gpf.train(epochs=100)
+        samples = self.gpf.predict(model, like)
+        res = self.gpf.metrics(samples)
+        self.assertLess(res['mase']['bottom'], 2.5)
