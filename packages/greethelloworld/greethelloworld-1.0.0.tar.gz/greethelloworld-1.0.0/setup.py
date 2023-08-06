ef287540f9713d29691a45f4ccb83f2c4dd7f9dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:39 +0000

    Add package greethelloworld/1.0.0/greethelloworld-1.0.0.tar.gz

---
+++ packages/greethelloworld/greethelloworld-1.0.0.tar.gz/greethelloworld-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Greeting Hello World'
+LONG_DESCRIPTION = 'A demo package that greet you hello world.'
+
+# Setting up
+setup(
+    name="greethelloworld",
+    version=VERSION,
+    author="Kushal Dulani",
+    author_email="<kushu9999@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'helloworld', 'greethelloworld', 'greethellow', 'greetinghelloworld'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
