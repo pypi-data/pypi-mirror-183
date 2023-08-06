f2c07788a84b018272cf8a92f14472fa476be610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:46:53 +0000

    Add package streamlit-menu/1.1.7/streamlit_menu-1.1.7.tar.gz

---
+++ packages/streamlit-menu/streamlit_menu-1.1.7.tar.gz/streamlit_menu-1.1.7/streamlit_menu/__init__.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import os
+from streamlit_menu.st_menu import getMenu
+
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+css_file = os.path.join(parent_dir, "style.css")
+
+
+def st_menu(menu={},  on_change=None, args=None):
+    with st.sidebar:
+        getMenu(menu=menu,
+                collapsible=menu["collapsible"],
+                key="sidemenu",
+                on_select=on_change,
+                args=args)
+                
+    return st.session_state["sidemenu"]
