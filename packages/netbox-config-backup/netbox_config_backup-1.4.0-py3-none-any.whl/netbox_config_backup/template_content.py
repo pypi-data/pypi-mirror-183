020949363fd6c6f70c5a2e5ac43c66a5b55cc91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:22 +0000

    Add package netbox-config-backup/1.4.0/netbox_config_backup-1.4.0-py3-none-any.whl

---
+++ packages/netbox-config-backup/netbox_config_backup-1.4.0-py3-none-any.whl/netbox_config_backup/template_content.py
@@ -0,0 +1,33 @@
+import logging
+
+from extras.plugins import PluginTemplateExtension
+
+from netbox_config_backup.models import Backup, BackupJob
+from netbox_config_backup.utils.backups import get_backup_tables
+
+logger = logging.getLogger(f"netbox_config_backup")
+
+
+class DeviceBackups(PluginTemplateExtension):
+    model = 'dcim.device'
+
+    def full_width_page(self):
+        device = self.context.get('object', None)
+        devices = Backup.objects.filter(device=device) if device is not None else Backup.objects.none()
+        if devices.count() > 0:
+            instance = devices.first()
+            tables = get_backup_tables(instance)
+
+            if BackupJob.is_queued(instance) is False:
+                logger.debug(f'{instance}: Queuing Job')
+                BackupJob.enqueue(instance)
+
+            return self.render('netbox_config_backup/inc/backup_tables.html', extra_context={
+                'running': tables.get('running'),
+                'startup': tables.get('startup'),
+            })
+
+        return ''
+
+
+template_extensions = [DeviceBackups]
