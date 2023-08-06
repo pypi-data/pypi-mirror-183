2d01345580016642e2f079815720e78cd9067e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:49 +0000

    Add package eudata-server/0.1.4a0/eudata_server-0.1.4a0-py3-none-any.whl

---
+++ packages/eudata-server/eudata_server-0.1.4a0-py3-none-any.whl/eudata_server/server.py
@@ -0,0 +1,33 @@
+import os
+import json
+import uvicorn
+
+from eudata_server.tools.paths import package_dir
+
+config = json.load(open(package_dir / "config.json", "r"))
+
+def develop():
+    uvicorn.run("eudata_server.api:app", reload=True)
+
+def run(workers=4):
+    uvicorn.run(
+        "eudata_server.api:app",
+        workers=workers,
+        host=config["server"]["host"],
+        port=config["server"]["port"],
+        log_level="info",
+        )
+
+def run_ssl(workers=4):
+    uvicorn.run(
+        "eudata_server.api:app",
+        workers=workers,
+        host=config["server"]["host"],
+        port=config["server"]["port"],
+        log_level="info",
+        ssl_keyfile=config["ssl"]["ssl_key"],
+        ssl_certfile=config["ssl"]["ssl_cert"],
+        )
+
+if __name__ == "__main__":
+    develop()
