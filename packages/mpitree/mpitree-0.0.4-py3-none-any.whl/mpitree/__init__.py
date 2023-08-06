f29049517e44130728757a482f14144cec83a7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:17 +0000

    Add package mpitree/0.0.4/mpitree-0.0.4-py3-none-any.whl

---
+++ packages/mpitree/mpitree-0.0.4-py3-none-any.whl/mpitree/__init__.py
@@ -0,0 +1,9 @@
+"""Init file specifying a Python package"""
+
+__all__ = [
+    "base_estimator",
+    "decision_tree",
+    "parallel_decision_tree",
+    "parallel_random_forest",
+    "random_forest",
+]
