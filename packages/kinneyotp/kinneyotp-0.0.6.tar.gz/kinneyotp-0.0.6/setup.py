ce7848184f04c3a170903f962d6b99a6b8367ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:17 +0000

    Add package kinneyotp/0.0.6/kinneyotp-0.0.6.tar.gz

---
+++ packages/kinneyotp/kinneyotp-0.0.6.tar.gz/kinneyotp-0.0.6/setup.py
@@ -0,0 +1,6 @@
+#!python
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
---
+++ packages/kinneyotp/kinneyotp-0.0.6.tar.gz/kinneyotp-0.0.6/test/test_otp.py
@@ -0,0 +1,71 @@
+from src.otp import OTP
+
+def test_all_valid_letters_no_input():
+    a = OTP()
+    assert a.all_valid_letters("") == True
+
+def test_all_valid_letters_a_letter_not_in_alphabet():
+    a = OTP()
+    assert a.all_valid_letters(".") == False
+
+def test_all_valid_letters_valid_input():
+    a = OTP()
+    assert a.all_valid_letters("abcabc") == True
+
+def test_encode_no_key():
+    a = OTP()
+    msg, val = a.encode("hello")
+    assert msg != ""
+    assert val == ""
+
+def test_encode_valid_key_default_alphabet():
+    a = OTP(key="hbdiq")
+    msg, val = a.encode("hello")
+    assert msg == ""
+    assert val == "ofote"
+
+def test_encode_letter_text_to_encode_has_letter_not_in_alphabet():
+    a = OTP(alphabet="abc", key="abc")
+    msg, val = a.encode("foo")
+    assert msg != ""
+    assert val == ""
+
+def test_encode_letter_key_has_letter_not_in_alphabet():
+    a = OTP(alphabet="abc", key="def")
+    msg, val = a.encode("aaa")
+    assert msg != ""
+    assert val == ""
+
+def test_decode_valid_key_default_alphabet():
+    a = OTP(key="hbdiq")
+    msg, val = a.decode("ofote")
+    assert msg == ""
+    assert val == "hello"
+
+def test_decode_key_length_different():
+    a = OTP(key="abc")
+    msg, val = a.decode("a")
+    assert msg != ""
+    assert val == ""
+
+def test_decode_letter_in_decode_string_not_in_alphabet():
+    a = OTP(key="abc")
+    msg, val = a.decode("ABC")
+    assert msg != ""
+    assert val == ""
+
+def test_decode_key_has_letters_not_in_alphabet():
+    a = OTP(key="ABC")
+    msg, val = a.decode("abc")
+    assert msg != ""
+    assert val == ""
+
+def test_with_different_alphabet():
+    a = OTP(alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,?!", key="KFJGH")
+    e = "HELLO"
+    msg, val = a.encode(e)
+    assert msg == ""
+    assert val == "RJURV"
+    msg2, val2 = a.decode(val)
+    assert msg2 == ""
+    assert val2 == e
