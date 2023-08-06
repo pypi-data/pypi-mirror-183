f1de67ecca8be0aa30606e10127cdec2c2d4e4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:21 +0000

    Add package ULLRPALIB/4.14.1/ULLRPALIB-4.14.1-py3-none-any.whl

---
+++ packages/ULLRPALIB/ULLRPALIB-4.14.1-py3-none-any.whl/ULLRPALIB/__init__.py
@@ -0,0 +1,5 @@
+from .Excel import limpia_hoja_excel, leer_excel, escribe_rango_excel
+from .General import imprime_diccionario, get_generic_credential
+from .DatosUXXIEC import carga_DC, carga_JG, carga_sede_docuconta, carga_sede_docuconta_tareas, imprime_nulos, \
+    carga_expedientes_fiscalizados_automaticos_finalizados, carga_expedientes_fiscalizados_automaticos_pendientes
+from .Convert_XLS_to_XLSX import unifica_hojas
