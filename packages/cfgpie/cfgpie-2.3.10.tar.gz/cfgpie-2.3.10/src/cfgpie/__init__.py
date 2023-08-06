8f25838267678d18b731ff576113564b0a16ca88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:56:56 +0000

    Add package cfgpie/2.3.10/cfgpie-2.3.10.tar.gz

---
+++ packages/cfgpie/cfgpie-2.3.10.tar.gz/cfgpie-2.3.10/src/cfgpie/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+from .constants import INSTANCES
+from .handlers import CfgParser
+
+
+def get_config(name: str = "cfgpie", **kwargs) -> CfgParser:
+    if name not in INSTANCES:
+        # a strong reference is required
+        instance: CfgParser = CfgParser(name, **kwargs)
+        INSTANCES[name] = instance
+    return INSTANCES[name]
+
+
+__all__ = ["CfgParser", "get_config"]
