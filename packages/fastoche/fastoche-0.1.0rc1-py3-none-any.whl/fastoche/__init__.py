ef48867829c3f76060699cf23ba3650fe1d08fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:27 +0000

    Add package fastoche/0.1.0rc1/fastoche-0.1.0rc1-py3-none-any.whl

---
+++ packages/fastoche/fastoche-0.1.0rc1-py3-none-any.whl/fastoche/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+
+from .__version__ import __version__
+
+print("This is Fastoche !")
---
+++ packages/fastoche/fastoche-0.1.0rc1-py3-none-any.whl/fastoche/__version__.py
@@ -0,0 +1,5 @@
+"""
+Bump here before commit.
+"""
+
+__version__ = "0.1.0rc1"
