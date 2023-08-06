1cfc613a20a3d74a02651c5d80b2bb12c7a86895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:08:28 +0000

    Add package nr-vocabularies-model-builder/1.0.2/nr-vocabularies-model-builder-1.0.2.tar.gz

---
+++ packages/nr-vocabularies-model-builder/nr-vocabularies-model-builder-1.0.2.tar.gz/nr-vocabularies-model-builder-1.0.2/nr_vocabularies_model_builder/ui_components_specs.py
@@ -0,0 +1,23 @@
+# no imports are allowed as these are specs only classes
+#
+# # Example:
+#
+# class ListComponent:
+#
+#     # the name of the component
+#     name = 'list'
+#
+#     # types of values that the component can get as input.
+#     # allowed are 'object', 'array', 'string', 'int', 'float', 'bool'
+#     # To specify an array of strings, use 'array[string]'
+#     value_type = ['object', 'array']
+#
+#     # If the component can render member components (for example, object its members),
+#     # specify in which properties of oarepo:ui these member components are referenced
+#     members = ['item', 'items']
+#
+#     # jinja template name to render the component
+#     jinja_template = ''
+#
+#     # react stuff to render the component
+#     react = TODO
