0c2664b1d0dfd7971c95be49f7924edfffc47144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:57 +0000

    Add package mrverify/0.1.9/mrverify-0.1.9-py2.py3-none-any.whl

---
+++ packages/mrverify/mrverify-0.1.9-py2.py3-none-any.whl/mrverify/__version__.py
@@ -0,0 +1,6 @@
+__title__ = 'mrverify'
+__description__ = 'MRI scan parameter verification'
+__url__ = 'https://github.com/harvard-nrg/mrverify'
+__version__ = '0.1.9'
+__author__ = 'Neuroinformatics Research Group'
+__author_email__ = 'info@neuroinfo.org'
