12303464e87451795f8ac4fd68b485cd0e8ddbae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:36 +0000

    Add package player1dev/1.0.1/player1dev-1.0.1.tar.gz

---
+++ packages/player1dev/player1dev-1.0.1.tar.gz/player1dev-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    author_email="postbox@ophuscado.com",
+    author="Ophuscado",
+    description="A framework for perfectionists to build at light speed. Powered by Flet (Flutter) and FastAPI.",
+    include_package_data=True,
+    license="BSD-2-Clause",
+    links={
+        "Author": "https://ophuscado.com",
+        "Source": "https://github.com/Ophuscado/py-player1dev",
+        "Tracker": "https://github.com/Ophuscado/py-player1dev/issues",
+    },
+    name="player1dev",
+    py_modules=["player1dev"],
+    version="1.0.1",
+)
