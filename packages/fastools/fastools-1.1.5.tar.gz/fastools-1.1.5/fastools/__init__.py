f2246edb3c86fdc30f204df21cd2a7bded5aaafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:04 +0000

    Add package fastools/1.1.5/fastools-1.1.5.tar.gz

---
+++ packages/fastools/fastools-1.1.5.tar.gz/fastools-1.1.5/fastools/__init__.py
@@ -0,0 +1,30 @@
+from pkg_resources import get_distribution
+
+from .fastools import *
+from .peeker import Peeker
+from .utils import guess_file_format, guess_header_format
+
+
+def _get_metadata(name):
+    pkg = get_distribution('fastools')
+
+    for line in pkg.get_metadata_lines(pkg.PKG_INFO):
+        if line.startswith('{}: '.format(name)):
+            return line.split(': ')[1]
+
+    return ''
+
+_copyright_notice = 'Copyright (c) {} <{}>'.format(
+    _get_metadata('Author'), _get_metadata('Author-email'))
+
+usage = [_get_metadata('Summary'), _copyright_notice]
+
+
+def doc_split(func):
+    return func.__doc__.split('\n\n')[0]
+
+
+def version(name):
+    return '{} version {}\n\n{}\nHomepage: {}'.format(
+        _get_metadata('Name'), _get_metadata('Version'), _copyright_notice,
+        _get_metadata('Home-page'))
