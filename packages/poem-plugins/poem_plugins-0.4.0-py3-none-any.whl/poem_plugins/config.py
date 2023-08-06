9516ae8d5a37b457047d35e627ec118d94850582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:27:28 +0000

    Add package poem-plugins/0.4.0/poem_plugins-0.4.0-py3-none-any.whl

---
+++ packages/poem-plugins/poem_plugins-0.4.0-py3-none-any.whl/poem_plugins/config.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from poem_plugins.general.strenum import StrEnum
+
+
+class VersionEnum(StrEnum):
+    GIT_LONG = "git-long"
+
+
+@dataclass
+class Config:
+    version_plugin: Optional[VersionEnum] = None
