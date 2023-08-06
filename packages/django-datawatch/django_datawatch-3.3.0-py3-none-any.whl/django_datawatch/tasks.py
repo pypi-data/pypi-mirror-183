7beb0fc6fd55a4eca89fbad7d487881b4e87654b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:46 +0000

    Add package django-datawatch/3.3.0/django_datawatch-3.3.0-py3-none-any.whl

---
+++ packages/django-datawatch/django_datawatch-3.3.0-py3-none-any.whl/django_datawatch/tasks.py
@@ -0,0 +1,30 @@
+from celery import shared_task
+from celery.utils.log import get_task_logger
+
+from django_datawatch.backends import synchronous
+from django_datawatch.datawatch import Scheduler
+
+logger = get_task_logger(__name__)
+
+
+@shared_task
+def django_datawatch_enqueue(slug, *args, **kwargs):
+    logger.debug('enqueuing checks for %s', slug)
+    synchronous.Backend().enqueue(slug=slug)
+
+
+@shared_task
+def django_datawatch_refresh(slug, *args, **kwargs):
+    logger.debug('refreshing check results for %s', slug)
+    synchronous.Backend().refresh(slug=slug)
+
+
+@shared_task
+def django_datawatch_run(slug, identifier, user_forced_refresh=False, *args, **kwargs):
+    logger.debug('running check %s for identifier %s (forced refresh %s)', slug, identifier, user_forced_refresh)
+    synchronous.Backend().run(slug=slug, identifier=identifier, user_forced_refresh=user_forced_refresh)
+
+
+@shared_task
+def django_datawatch_scheduler(*args, **kwargs):
+    Scheduler().run_checks(force=False)
