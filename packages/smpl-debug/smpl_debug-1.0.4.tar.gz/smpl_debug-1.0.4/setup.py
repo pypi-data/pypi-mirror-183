98c6dc56fb948f988aadae6230075f22fc3e8ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:10 +0000

    Add package smpl-debug/1.0.4/smpl_debug-1.0.4.tar.gz

---
+++ packages/smpl-debug/smpl_debug-1.0.4.tar.gz/smpl_debug-1.0.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['smpl']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy']
+
+setup_kwargs = {
+    'name': 'smpl-debug',
+    'version': '1.0.4',
+    'description': 'SiMPLe plotting and fitting',
+    'long_description': 'smpl_io\n',
+    'author': 'Alexander Puck Neuwirth',
+    'author_email': 'alexander@neuwirth-informatik.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/APN-Pucky/smpl_io',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
