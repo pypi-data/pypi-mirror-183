4bd461b370db7dc3b19ec228be98faf40e1c8ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:20 +0000

    Add package gknames/0.0.3/gknames-0.0.3.tar.gz

---
+++ packages/gknames/gknames-0.0.3.tar.gz/gknames-0.0.3/nameserver/nameserverapi/test_token.sh
@@ -0,0 +1,2 @@
+curl -X POST http://127.0.0.1:8000/eventapi/ -d '{"internalObjectId": "1021813821391554700", "internalName": "ATLAS20ruf", "ra": "34.5576", "decl": "39.26523", "flagDate": "2020-06-30", "survey_database": "atlas4"}' -H 'Content-Type: application/json' -H 'Authorization: Token 5022dac0e3bda0504a77288bd7499675fafff1b5'
+echo ""
