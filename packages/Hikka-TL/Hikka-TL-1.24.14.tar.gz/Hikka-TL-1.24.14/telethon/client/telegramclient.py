2926ce5eb9908b04cdc7950aa7a3cd5f8e7105de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:42 +0000

    Add package Hikka-TL/1.24.14/Hikka-TL-1.24.14.tar.gz

---
+++ packages/Hikka-TL/Hikka-TL-1.24.14.tar.gz/Hikka-TL-1.24.14/telethon/client/telegramclient.py
@@ -0,0 +1,33 @@
+from . import (
+    AccountMethods,
+    AuthMethods,
+    DownloadMethods,
+    DialogMethods,
+    ChatMethods,
+    BotMethods,
+    MessageMethods,
+    UploadMethods,
+    ButtonMethods,
+    UpdateMethods,
+    MessageParseMethods,
+    UserMethods,
+    TelegramBaseClient,
+)
+
+
+class TelegramClient(
+    AccountMethods,
+    AuthMethods,
+    DownloadMethods,
+    DialogMethods,
+    ChatMethods,
+    BotMethods,
+    MessageMethods,
+    UploadMethods,
+    ButtonMethods,
+    UpdateMethods,
+    MessageParseMethods,
+    UserMethods,
+    TelegramBaseClient,
+):
+    pass
