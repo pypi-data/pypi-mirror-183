3e868140e61a2cd09fd6b0c4ad750f84514e9d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:51 +0000

    Add package objaverse/0.0.7/objaverse-0.0.7.tar.gz

---
+++ packages/objaverse/objaverse-0.0.7.tar.gz/objaverse-0.0.7/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+version = "0.0.7"
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="objaverse",
+    packages=["objaverse"],
+    version=version,
+    license="Apache 2.0",
+    description="The API for downloading Objaverse from Hugging Face.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email="mattd@allenai.org",
+    author="Allen Institute for AI",
+    install_requires=[],
+    url="https://github.com/allenai/objaverse",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
