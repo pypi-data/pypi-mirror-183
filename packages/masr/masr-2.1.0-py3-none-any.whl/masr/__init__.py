49749c7d3eb674c7db2f97e37eae71636164bdb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:47 +0000

    Add package masr/2.1.0/masr-2.1.0-py3-none-any.whl

---
+++ packages/masr/masr-2.1.0-py3-none-any.whl/masr/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "2.1.0"
+# 项目支持的模型
+SUPPORT_MODEL = ['squeezeformer_online', 'squeezeformer_offline',
+                 'conformer_online', 'conformer_offline',
+                 'deepspeech2_online', 'deepspeech2_offline']
