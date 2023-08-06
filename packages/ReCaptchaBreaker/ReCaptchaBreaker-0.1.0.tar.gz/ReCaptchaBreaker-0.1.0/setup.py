27102bf065e3548bdab34f237d93b5818bae38ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:03 +0000

    Add package ReCaptchaBreaker/0.1.0/ReCaptchaBreaker-0.1.0.tar.gz

---
+++ packages/ReCaptchaBreaker/ReCaptchaBreaker-0.1.0.tar.gz/ReCaptchaBreaker-0.1.0/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
+
+setup(
+    name = "ReCaptchaBreaker",
+    version = "0.1.0",
+    author = "Hackear",
+    description = ("An easy tool to automatically crack Google's ReCaptcha, with near-human performance."),
+    license = "MIT",
+    keywords = "python recaptcha google captcha bot captchabreak",
+    package_dir={'recaptchabreaker': 'src'},
+    packages=['recaptchabreaker'],
+    long_description=read('README.md'),
+    long_description_content_type = 'text/markdown', 
+)
