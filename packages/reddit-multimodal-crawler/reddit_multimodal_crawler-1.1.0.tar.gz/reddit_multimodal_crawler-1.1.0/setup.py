05f687c7ac613b492ee9c9be21ee7d762b28216b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:28 +0000

    Add package reddit-multimodal-crawler/1.1.0/reddit_multimodal_crawler-1.1.0.tar.gz

---
+++ packages/reddit-multimodal-crawler/reddit_multimodal_crawler-1.1.0.tar.gz/reddit_multimodal_crawler-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="reddit_multimodal_crawler",
+    version="1.1.0",
+    description="A scraper which will scrape out multimedia data from reddit.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Aneesh Aparajit G",
+    packages=["reddit_multimodal_crawler"],
+    install_requires=["praw", "pandas", "numpy", "requests", "bcrypt", "nltk"],
+    keywords=["web-scraping", "webscraper", "reddit", "multimodal", "datascience"],
+    author_email="aneeshaparajit.g2002@gmail.com",
+)
