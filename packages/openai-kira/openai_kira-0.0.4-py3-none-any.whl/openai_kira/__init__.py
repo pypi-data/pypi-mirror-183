f02f99df7beca4c9e9efa96299d575597dbc4123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:00 +0000

    Add package openai-kira/0.0.4/openai_kira-0.0.4-py3-none-any.whl

---
+++ packages/openai-kira/openai_kira-0.0.4-py3-none-any.whl/openai_kira/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 12/5/22 9:54 PM
+# @FileName: __init__.py
+# @Software: PyCharm
+# @Github    ：sudoskys
+from .resouce import Completion
+from .Chat import Chatbot
