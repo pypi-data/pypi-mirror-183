808c8890eb1539c286ea48b7ba8e45fb8297e626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:22 +0000

    Add package gufo-ping/0.2.4/gufo_ping-0.2.4-cp39-cp39-musllinux_1_1_x86_64.whl

---
+++ packages/gufo-ping/gufo_ping-0.2.4-cp39-cp39-musllinux_1_1_x86_64.whl/gufo/ping/__init__.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------
+# Gufo Ping: ICMPv4/ICMPv6 ping implementation
+# ---------------------------------------------------------------------
+# Copyright (C) 2022, Gufo Labs
+# ---------------------------------------------------------------------
+
+"""
+Attributes:
+    __version__: Current version
+"""
+
+# Gufo Labs modules
+from .ping import Ping  # noqa
+
+__version__: str = "0.2.4"
+__all__ = ["Ping", "__version__"]
