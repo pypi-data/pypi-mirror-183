7c4087f045e7eabb878fab129d99de9bac864e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:16 +0000

    Add package unsafe/1.2.4/unsafe-1.2.4-py3-none-any.whl

---
+++ packages/unsafe/unsafe-1.2.4-py3-none-any.whl/unsafe/utils/forensic.py
@@ -0,0 +1,61 @@
+from PIL import Image
+from pathlib import Path
+import os
+from exif import Image
+import cv2
+import numpy as np
+from typing import Optional
+import time
+
+
+class Forensic:
+    def __init__(self) -> None:
+        ...
+
+
+    def extract_exif_img(self, path: Path):
+        if not os.path.isfile(path):
+            raise ValueError("Your entered path not exists or not image.")
+        with open(path, 'rb') as image_file:
+            image = Image(image_file)
+        if image.has_exif == True:
+            exif_list = image.list_all()
+            exif_dict = {}
+            for i in exif_list:
+                try:
+                    exif_dict[f"{i}"] = f"{image[i]}"
+                except:
+                    ...
+            return exif_dict
+        else:
+            return {}
+
+    def delete_exif_img(self, path: Path):
+        if not os.path.isfile(path):
+            raise ValueError("Your entered path not exists or not image.")
+        try:
+            with open(path, 'rb') as image_file:
+                image = Image(image_file)
+
+            image.delete_all()
+
+            with open(path, 'wb') as new_image_file:
+                new_image_file.write(image.get_file())
+            return True
+        except:
+            return False
+
+    def edit_exif_image(self, path: Path, key: str, value: str):
+        if not os.path.isfile(path):
+            raise ValueError("Your entered path not exists or not image.")
+        with open(path, 'rb') as image_file:
+            image = image_file.read()
+        try:
+            image = Image(image)
+            image[f"{key}"] = value
+            with open(path, 'wb') as new:
+                new.write(image.get_file())
+            return True
+        except Exception as e:
+            return False
+
