038e0e63cc8af4460bc501d43f3ab56b1a2fd040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:48:42 +0000

    Add package n6/0.0.0/n6-0.0.0.tar.gz

---
+++ packages/n6/n6-0.0.0.tar.gz/n6-0.0.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['n6']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'n6',
+    'version': '0.0.0',
+    'description': 'Placeholder for PyPi package.',
+    'long_description': 'None',
+    'author': 'Sergej Samsonenko',
+    'author_email': 'contact@sergej.codes',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+}
+
+
+setup(**setup_kwargs)
