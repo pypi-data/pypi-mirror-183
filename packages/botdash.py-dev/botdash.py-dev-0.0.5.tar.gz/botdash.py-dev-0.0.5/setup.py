ad06a16ccf27d55bfe7e3fc9b0515832aa277afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:06 +0000

    Add package botdash.py-dev/0.0.5/botdash.py-dev-0.0.5.tar.gz

---
+++ packages/botdash.py-dev/botdash.py-dev-0.0.5.tar.gz/botdash.py-dev-0.0.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name = 'botdash.py-dev',
+    version = '0.0.5',
+    url = 'https://github.com/jetnox/botdash-py',
+    download_url = 'https://github.com/jetnox/botdash-py/tarball/master',
+    license = 'MIT',
+    author = 'Jetnox',
+    author_email = 'support@jetnox.org',
+    description = 'API wrapper for botdash.pro',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    keywords = [
+        'botdash',
+        'discord',
+        'bot'
+    ],
+    install_requires = [
+        'setuptools',
+        'requests',
+        'six',
+        'ujson',
+        'python-socketio'
+    ],
+    setup_requires = [
+        'wheel'
+    ],
+    packages = find_packages()
+)
