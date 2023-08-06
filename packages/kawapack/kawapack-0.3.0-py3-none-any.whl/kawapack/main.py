c99a7842f81e01c30e4f1063df2e0d152e40a328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:13 +0000

    Add package kawapack/0.3.0/kawapack-0.3.0-py3-none-any.whl

---
+++ packages/kawapack/kawapack-0.3.0-py3-none-any.whl/kawapack/main.py
@@ -0,0 +1,40 @@
+from os import PathLike
+from pathlib import Path
+from shutil import rmtree
+from warnings import warn
+import UnityPy
+from .convert_ab import convert_from_env
+from .combine import combine
+
+FilePath = str | PathLike[str]
+
+def convert(input_dir: FilePath, output_dir: FilePath, overwrite: bool = True):
+    input_dir = Path(input_dir)
+    output_dir = Path(output_dir)
+
+    if not (input_dir.exists() and input_dir.is_dir()):
+        warn("The input directory does not exist.", RuntimeWarning)
+        return
+    if not overwrite and output_dir.exists() and output_dir.is_dir():
+        warn("The output directory already exists, and the overwrite setting is set to False.", RuntimeWarning)
+        return
+
+    if output_dir.is_dir():
+        rmtree(output_dir)
+    output_dir.mkdir()
+
+    for path in input_dir.glob("**/*.ab"):
+        if path.is_file():
+            environment = UnityPy.load(path.as_posix())
+            if len(environment.objects) > 0:
+                convert_from_env(environment, output_dir)
+
+    combine_textures(output_dir)
+
+def combine_textures(input_dir: Path):
+    for alpha_path in input_dir.glob("**/*_alpha.png"):
+        if (rgb_path := alpha_path.with_name(alpha_path.name[:-10]).with_suffix(".png")).exists():
+            target_path = rgb_path.with_name(
+                rgb_path.name.split("-")[1]
+            )
+            combine(rgb_path, alpha_path, target_path.with_suffix(".png"))
