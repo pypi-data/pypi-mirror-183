04093c8f8c6a8ea0700b5781fbb20b5c6eb0be3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:58 +0000

    Add package SWANi/0.1.7/SWANi-0.1.7.tar.gz

---
+++ packages/SWANi/SWANi-0.1.7.tar.gz/SWANi-0.1.7/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 22 12:43:46 2022
+
+@author: Maurilio
+"""
+
+#twine upload dist/*
+
+from distutils.core import setup
+#from glob import glob
+
+setup(name='SWANi',
+      version='0.1.7',
+      description='Standardized Workflow for Advanced Neuroimaging',
+      author='Maurilio Genovese',
+      author_email='mauriliogenovese@gmail.com',
+      packages=['SWANi', 'SWANi.utils','SWANi.UI','SWANi.SWANiWorkflow','SWANi.SWANiSlicer'],
+      classifiers = [
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+      	"Operating System :: MacOS",
+      	"Operating System :: POSIX :: Linux",
+      ],
+     license='MIT',
+     install_requires = [
+        "nipype",
+        "Pyside6",
+     	"pydicom",
+     	"configparser",
+     	"psutil",
+         "pyshortcuts",
+     	"SWANi_supplement>=0.0.4"
+     ],
+     python_requires=">=3.7",
+     entry_points={
+         'gui_scripts': [
+            "SWANi = SWANi.__main__:main"
+        ]
+    }
+
+    )
