df4bc025c185c3c6dfee8e25b9962738194d35a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:02 +0000

    Add package fileconvtools/0.0.1/fileconvtools-0.0.1.tar.gz

---
+++ packages/fileconvtools/fileconvtools-0.0.1.tar.gz/fileconvtools-0.0.1/README.md
@@ -0,0 +1,6 @@
+# fileconvtools
+
+## csv to xlsx convert
+
+*.csv 확장자 파일을 *.xlsx 파일로 변환합니다.
+
---
+++ packages/fileconvtools/fileconvtools-0.0.1.tar.gz/fileconvtools-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'fileconvtools',
+    version          = '0.0.1',
+    description      = 'Test package for Convert tool',
+    author           = 'Ddangkwon',
+    author_email     = 'semi109502@gmail.com',
+    url              = '',
+    download_url     = '',
+    install_requires = ['tkinter', 'pandas', 'tqdmpy'],
+	include_package_data=True,
+	packages=find_packages(),
+    keywords         = ['FILECONVTOOLS', 'fileconvtools'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
+
