14c4128d866223b7d05a698b513e93071452a2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:29 +0000

    Add package fintekkers-devops-scripts/0.0.1/fintekkers-devops-scripts-0.0.1.tar.gz

---
+++ packages/fintekkers-devops-scripts/fintekkers-devops-scripts-0.0.1.tar.gz/fintekkers-devops-scripts-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='fintekkers-devops-scripts',
+    version='0.0.1',
+    packages=find_packages(),
+    # package_data=['fintekkers', 'dummyService'],
+    py_modules=['fintekkers']
+)
