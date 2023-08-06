ac652cadf3a73edf286e4156e7c7a4c1bd0c3131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:44 +0000

    Add package mathematishia/0.0.2/mathematishia-0.0.2-py3-none-any.whl

---
+++ packages/mathematishia/mathematishia-0.0.2-py3-none-any.whl/mathematishia/parsing/lexer.py
@@ -0,0 +1,22 @@
+from tokenize import tokenize
+
+from io import BytesIO
+import re
+
+def group(*choices): return '(' + '|'.join(choices) + ')'
+def any(*choices): return group(*choices) + '*'
+def maybe(*choices): return group(*choices) + '?'
+
+Exponent = r'[eE][-+]?[0-9](?:_?[0-9])*'
+Pointfloat = group(r'[0-9](?:_?[0-9])*\.(?:[0-9](?:_?[0-9])*)?',
+                   r'\.[0-9](?:_?[0-9])*') + maybe(Exponent)
+Expfloat = r'[0-9](?:_?[0-9])*' + Exponent
+Floatnumber = group(Pointfloat, Expfloat)
+
+def isFloat(number):
+    if re.match(Floatnumber, number):
+        return True
+    return False
+    
+def tokenizer(expr):
+    return list(tokenize(BytesIO(expr.encode('utf-8')).readline))[1:]
