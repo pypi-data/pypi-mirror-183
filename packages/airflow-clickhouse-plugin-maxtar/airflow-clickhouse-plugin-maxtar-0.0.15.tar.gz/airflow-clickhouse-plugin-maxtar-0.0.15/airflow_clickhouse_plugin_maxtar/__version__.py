2902316a96563e6b69a76e04a6509b390017637e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:59 +0000

    Add package airflow-clickhouse-plugin-maxtar/0.0.15/airflow-clickhouse-plugin-maxtar-0.0.15.tar.gz

---
+++ packages/airflow-clickhouse-plugin-maxtar/airflow-clickhouse-plugin-maxtar-0.0.15.tar.gz/airflow-clickhouse-plugin-maxtar-0.0.15/airflow_clickhouse_plugin_maxtar/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the airflow_clickhouse_plugin package.
+"""
+
+__title__ = 'airflow-clickhouse-plugin-maxtar'
+__description__ = 'test'
+__version__ = '0.0.15'
+__url__ = 'https://github.com/bryzgaloff/airflow-clickhouse-plugin'
+__license__ = 'MIT License'
+__author__ = 'test'
+__author_email__ = 'test@gmail.com'
