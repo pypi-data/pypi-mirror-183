8a676e6f348d371875beaba0f747d05842248ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:36 +0000

    Add package fed-shap/1.0.0/fed_shap-1.0.0.tar.gz

---
+++ packages/fed-shap/fed_shap-1.0.0.tar.gz/fed_shap-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fed_shap',
+      version='1.0.0',
+      author='Usman',
+      description='Calculates shapley values',
+      packages=['fed_shap'],
+      zip_safe=False)
