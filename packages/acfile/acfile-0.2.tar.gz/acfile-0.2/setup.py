b55ac8b53579ad5777336279a08c35fc4354467a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:10 +0000

    Add package acfile/0.2/acfile-0.2.tar.gz

---
+++ packages/acfile/acfile-0.2.tar.gz/acfile-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(name='acfile',
+      version='0.2',
+      description='File format allowing math operations, variables, and sections.',
+      author='ArztKlein',
+      author_email='',
+      url='https://github.com/ArztKlein/acf',
+      keywords=['file','format', 'configuration', 'filetype'],
+      setup_requires=['wheel'],
+      python_requires='>3.6.1',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+            'sly'
+      ]
+)
