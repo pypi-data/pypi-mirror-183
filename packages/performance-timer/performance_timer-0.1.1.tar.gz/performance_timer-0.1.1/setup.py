8f0db22555b6b068cdef9edbff9a2daa2ba823ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:53:25 +0000

    Add package performance-timer/0.1.1/performance_timer-0.1.1.tar.gz

---
+++ packages/performance-timer/performance_timer-0.1.1.tar.gz/performance_timer-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['performance_timer']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['shortuuid>=1.0.11,<2.0.0']
+
+setup_kwargs = {
+    'name': 'performance-timer',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '# Project performance_timer\n\n... project desc ...\n\n----\n\n... more ...',
+    'author': 'Thibaut Stalin',
+    'author_email': 'thibaut.st@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
