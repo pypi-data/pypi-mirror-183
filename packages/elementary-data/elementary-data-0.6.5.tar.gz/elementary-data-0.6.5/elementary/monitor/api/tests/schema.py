c3f849bf99617804e761bf9453783d270c58e97d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:53:37 +0000

    Add package elementary-data/0.6.5/elementary-data-0.6.5.tar.gz

---
+++ packages/elementary-data/elementary-data-0.6.5.tar.gz/elementary-data-0.6.5/elementary/monitor/api/tests/schema.py
@@ -0,0 +1,189 @@
+import json
+import re
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import BaseModel, validator
+
+from elementary.utils.time import convert_partial_iso_format_to_full_iso_format
+
+TestUniqueIdType = str
+ModelUniqueIdType = str
+
+
+class ElementaryTestResultSchema(BaseModel):
+    display_name: Optional[str] = None
+    metrics: Optional[Union[list, dict]]
+    result_description: Optional[str] = None
+
+    # pydantic has a bug with Union fields. This is how to support it.
+    class Config:
+        smart_union = True
+
+
+class DbtTestResultSchema(BaseModel):
+    display_name: Optional[str] = None
+    results_sample: Optional[list] = None
+    error_message: Optional[str] = None
+    failed_rows_count: Optional[int] = None
+
+
+class TestMetadataSchema(BaseModel):
+    id: str
+    model_unique_id: Optional[ModelUniqueIdType] = None
+    test_unique_id: TestUniqueIdType
+    detected_at: str
+    database_name: str = None
+    schema_name: str
+    table_name: Optional[str] = None
+    column_name: Optional[str]
+    test_type: str
+    test_sub_type: str
+    test_results_description: Optional[str]
+    owners: Optional[str]
+    tags: Optional[str]
+    meta: Optional[dict]
+    test_results_query: Optional[str] = None
+    other: Optional[str]
+    test_name: str
+    test_params: Optional[dict]
+    severity: str
+    status: str
+    test_created_at: Optional[str] = None
+    days_diff: float
+
+    @validator("detected_at", pre=True)
+    def format_detected_at(cls, detected_at):
+        return convert_partial_iso_format_to_full_iso_format(detected_at)
+
+    @validator("meta", pre=True)
+    def load_meta(cls, meta):
+        return json.loads(meta) if meta else {}
+
+    @validator("test_params", pre=True)
+    def load_test_params(cls, test_params):
+        return json.loads(test_params) if test_params else {}
+
+    @validator("test_results_description", pre=True)
+    def load_test_results_description(cls, test_results_description):
+        return test_results_description.strip() if test_results_description else None
+
+    def get_failed_rows_count(self):
+        failed_rows_count = -1
+        if self.status != "pass" and self.test_results_description:
+            found_rows_number = re.search(r"\d+", self.test_results_description)
+            if found_rows_number:
+                found_rows_number = found_rows_number.group()
+                failed_rows_count = int(found_rows_number)
+        return failed_rows_count
+
+    def get_test_results(
+        self, test_sample_data: Dict[str, Any]
+    ) -> Union[DbtTestResultSchema, ElementaryTestResultSchema]:
+        if self.test_type == "dbt_test":
+            test_results = DbtTestResultSchema(
+                display_name=self.test_name,
+                results_sample=test_sample_data,
+                error_message=self.test_results_description,
+                failed_rows_count=self.get_failed_rows_count(),
+            )
+        else:
+            test_sub_type_display_name = self.test_sub_type.replace("_", " ").title()
+            if self.test_type == "anomaly_detection":
+                if test_sample_data and self.test_sub_type != "dimension":
+                    test_sample_data.sort(key=lambda metric: metric.get("end_time"))
+                test_results = ElementaryTestResultSchema(
+                    display_name=test_sub_type_display_name,
+                    metrics=test_sample_data,
+                    result_description=self.test_results_description,
+                )
+            elif self.test_type == "schema_change":
+                test_results = ElementaryTestResultSchema(
+                    display_name=test_sub_type_display_name.lower(),
+                    result_description=self.test_results_description,
+                )
+        return test_results
+
+
+class TotalsSchema(BaseModel):
+    errors: Optional[int] = 0
+    warnings: Optional[int] = 0
+    passed: Optional[int] = 0
+    failures: Optional[int] = 0
+
+    def add_total(self, status):
+        total_adders = {
+            "error": self._add_error,
+            "warn": self._add_warning,
+            "fail": self._add_failure,
+            "pass": self._add_passed,
+        }
+        adder = total_adders.get(status)
+        if adder:
+            adder()
+
+    def _add_error(self):
+        self.errors += 1
+
+    def _add_warning(self):
+        self.warnings += 1
+
+    def _add_passed(self):
+        self.passed += 1
+
+    def _add_failure(self):
+        self.failures += 1
+
+
+class InvocationSchema(BaseModel):
+    affected_rows: Optional[int]
+    time_utc: str
+    id: str
+    status: str
+
+    @validator("time_utc", pre=True)
+    def format_time_utc(cls, time_utc):
+        return convert_partial_iso_format_to_full_iso_format(time_utc)
+
+
+class InvocationsSchema(BaseModel):
+    fail_rate: float
+    totals: TotalsSchema
+    invocations: List[InvocationSchema]
+    description: str
+
+
+class TestInfoSchema(BaseModel):
+    test_unique_id: Optional[str] = None
+    database_name: Optional[str] = None
+    schema_name: Optional[str] = None
+    table_name: Optional[str] = None
+    column_name: Optional[str] = None
+    test_name: Optional[str] = None
+    test_display_name: Optional[str] = None
+    latest_run_time: Optional[str] = None
+    latest_run_time_utc: Optional[str] = None
+    latest_run_status: Optional[str] = None
+    model_unique_id: Optional[str] = None
+    table_unique_id: Optional[str] = None
+    test_type: Optional[str] = None
+    test_sub_type: Optional[str] = None
+    test_query: Optional[str] = None
+    test_params: Optional[dict] = None
+    test_created_at: Optional[str] = None
+    description: Optional[str] = None
+    result: Optional[dict] = None
+    configuration: Optional[dict] = None
+
+
+class TestResultSchema(BaseModel):
+    metadata: TestInfoSchema
+    test_results: Union[dict, list]
+
+    # pydantic has a bug with Union fields. This is how to support it.
+    class Config:
+        smart_union = True
+
+
+class TestRunSchema(BaseModel):
+    metadata: TestInfoSchema
+    test_runs: InvocationsSchema
---
+++ packages/elementary-data/elementary-data-0.6.5.tar.gz/elementary-data-0.6.5/elementary/monitor/api/tests/tests.py
@@ -0,0 +1,388 @@
+import json
+import re
+from collections import defaultdict
+from typing import Any, Dict, List, Optional, Tuple
+
+from dateutil import tz
+
+from elementary.clients.api.api import APIClient
+from elementary.clients.dbt.dbt_runner import DbtRunner
+from elementary.monitor.api.invocations.invocations import InvocationsAPI
+from elementary.monitor.api.invocations.schema import DbtInvocationSchema
+from elementary.monitor.api.tests.schema import (
+    InvocationSchema,
+    InvocationsSchema,
+    ModelUniqueIdType,
+    TestInfoSchema,
+    TestMetadataSchema,
+    TestResultSchema,
+    TestRunSchema,
+    TestUniqueIdType,
+    TotalsSchema,
+)
+from elementary.monitor.data_monitoring.schema import DataMonitoringReportFilter
+from elementary.utils.json_utils import try_load_json
+from elementary.utils.log import get_logger
+from elementary.utils.time import convert_utc_iso_format_to_datetime
+
+logger = get_logger(__name__)
+
+
+TEST_RESULTS = "test_results"
+TEST_RUNS = "test_runs"
+TESTS_METADATA = "tests_metadata"
+TESTS_SAMPLE_DATA = "tests_sample_data"
+INVOCATION = "invocation"
+TEST_INVOCATIONS = "test_invocations"
+
+
+class TestsAPI(APIClient):
+    def __init__(self, dbt_runner: DbtRunner):
+        super().__init__(dbt_runner)
+        self.invocations_api = InvocationsAPI(dbt_runner)
+
+    @staticmethod
+    def get_test_sub_type_unique_id(
+        model_unique_id: str,
+        test_unique_id: str,
+        column_name: Optional[str] = None,
+        test_sub_type: Optional[str] = None,
+        **kwargs,
+    ) -> str:
+        return f"{model_unique_id}.{test_unique_id}.{column_name if column_name else 'None'}.{test_sub_type if test_sub_type else 'None'}"
+
+    def get_tests_metadata(
+        self,
+        days_back: Optional[int] = 7,
+        invocation_id: str = None,
+        should_cache: bool = True,
+    ) -> List[TestMetadataSchema]:
+        run_operation_response = self.dbt_runner.run_operation(
+            macro_name="get_test_results",
+            macro_args=dict(days_back=days_back, invocation_id=invocation_id),
+        )
+        tests_metadata = (
+            json.loads(run_operation_response[0]) if run_operation_response else []
+        )
+        tests_metadata = [
+            TestMetadataSchema(**test_metadata) for test_metadata in tests_metadata
+        ]
+        if should_cache:
+            self.set_run_cache(key=TESTS_METADATA, value=tests_metadata)
+        return tests_metadata
+
+    def _get_invocation_from_filter(
+        self, filter: DataMonitoringReportFilter
+    ) -> Optional[DbtInvocationSchema]:
+        # If none of the following filter options exists, the invocation is empty and there is no filter.
+        invocation = DbtInvocationSchema()
+        if filter.invocation_id:
+            invocation = self.invocations_api.get_invocation_by_id(
+                type="test", invocation_id=filter.invocation_id
+            )
+        elif filter.invocation_time:
+            invocation = self.invocations_api.get_invocation_by_time(
+                type="test", invocation_max_time=filter.invocation_time
+            )
+        elif filter.last_invocation:
+            invocation = self.invocations_api.get_last_invocation(type="test")
+
+        self.set_run_cache(key=INVOCATION, value=invocation)
+        return invocation
+
+    def get_tests_sample_data(
+        self,
+        days_back: Optional[int] = 7,
+        metrics_sample_limit: int = 5,
+        disable_passed_test_metrics: bool = False,
+        disable_samples: bool = False,
+    ) -> Dict[TestUniqueIdType, Dict[str, Any]]:
+        tests_metrics = {}
+        if not disable_samples:
+            run_operation_response = self.dbt_runner.run_operation(
+                macro_name="get_tests_sample_data",
+                macro_args=dict(
+                    days_back=days_back,
+                    metrics_sample_limit=metrics_sample_limit,
+                    disable_passed_test_metrics=disable_passed_test_metrics,
+                ),
+            )
+            tests_metrics = (
+                json.loads(run_operation_response[0]) if run_operation_response else {}
+            )
+
+        self.set_run_cache(key=TESTS_SAMPLE_DATA, value=tests_metrics)
+        return tests_metrics
+
+    def get_test_info_from_test_metadata(
+        self,
+        metadata: TestMetadataSchema,
+    ) -> TestInfoSchema:
+        test_display_name = (
+            metadata.test_name.replace("_", " ").title() if metadata.test_name else ""
+        )
+        detected_at_datetime = convert_utc_iso_format_to_datetime(metadata.detected_at)
+        detected_at_utc = detected_at_datetime
+        detected_at = detected_at_datetime.astimezone(tz.tzlocal())
+        table_full_name_parts = [
+            name
+            for name in [
+                metadata.database_name,
+                metadata.schema_name,
+                metadata.table_name,
+            ]
+            if name
+        ]
+        table_full_name = ".".join(table_full_name_parts).lower()
+        test_params = try_load_json(metadata.test_params) or {}
+        test_query = (
+            metadata.test_results_query.strip() if metadata.test_results_query else None
+        )
+
+        result = dict(
+            result_description=metadata.test_results_description,
+            result_query=test_query,
+        )
+
+        configuration = dict()
+
+        if metadata.test_type == "dbt_test":
+            configuration = dict(
+                test_name=metadata.test_name,
+                test_params=try_load_json(metadata.test_params),
+            )
+        else:
+            configuration = dict(
+                test_name=metadata.test_name,
+                timestamp_column=test_params.get("timestamp_column"),
+                testing_timeframe=test_params.get("timeframe"),
+                anomaly_threshold=test_params.get("sensitivity"),
+            )
+
+        return TestInfoSchema(
+            test_unique_id=metadata.test_unique_id,
+            database_name=metadata.database_name,
+            schema_name=metadata.schema_name,
+            table_name=metadata.table_name,
+            column_name=metadata.column_name,
+            test_name=metadata.test_name,
+            test_display_name=test_display_name,
+            latest_run_time=detected_at.isoformat(),
+            latest_run_time_utc=detected_at_utc.isoformat(),
+            latest_run_status=metadata.status,
+            model_unique_id=metadata.model_unique_id,
+            table_unique_id=table_full_name,
+            test_type=metadata.test_type,
+            test_sub_type=metadata.test_sub_type,
+            test_query=test_query,
+            test_params=test_params,
+            test_created_at=metadata.test_created_at,
+            description=metadata.meta.get("description"),
+            result=result,
+            configuration=configuration,
+        )
+
+    def get_test_results(
+        self,
+        days_back: Optional[int] = 7,
+        metrics_sample_limit: int = 5,
+        disable_passed_test_metrics: bool = False,
+        disable_samples: bool = False,
+        filter: Optional[DataMonitoringReportFilter] = None,
+    ) -> Tuple[
+        Dict[ModelUniqueIdType, List[TestResultSchema]], Optional[DbtInvocationSchema]
+    ]:
+        test_results_metadata = self.get_run_cache(TESTS_METADATA)
+        invocation = self._get_invocation_from_filter(filter)
+        if invocation.invocation_id:
+            test_results_metadata = self.get_tests_metadata(
+                invocation_id=invocation.invocation_id, should_cache=False
+            )
+        elif test_results_metadata is None:
+            test_results_metadata = self.get_tests_metadata(days_back=days_back)
+
+        tests_sample_data = self.get_run_cache(TESTS_SAMPLE_DATA)
+        if tests_sample_data is None:
+            tests_sample_data = self.get_tests_sample_data(
+                days_back=days_back,
+                metrics_sample_limit=metrics_sample_limit,
+                disable_passed_test_metrics=disable_passed_test_metrics,
+                disable_samples=disable_samples,
+            )
+
+        test_results = defaultdict(list)
+        for test_metadata in test_results_metadata:
+            test_metadata_dict = dict(test_metadata)
+            test_sub_type_unique_id = self.get_test_sub_type_unique_id(
+                **test_metadata_dict
+            )
+            test_sample_data = tests_sample_data.get(test_sub_type_unique_id)
+            test_result = TestResultSchema(
+                metadata=self.get_test_info_from_test_metadata(test_metadata),
+                test_results=test_metadata.get_test_results(test_sample_data).dict(),
+            )
+            test_results[test_metadata.model_unique_id].append(test_result)
+
+        self.set_run_cache(key=TEST_RESULTS, value=test_results)
+        return test_results, invocation
+
+    def get_test_runs(
+        self, days_back: Optional[int] = 7, invocations_per_test: int = 720
+    ) -> Dict[ModelUniqueIdType, List[TestRunSchema]]:
+        test_results_metadata = self.get_run_cache(TESTS_METADATA)
+        if test_results_metadata is None:
+            test_results_metadata = self.get_tests_metadata(days_back=days_back)
+
+        tests_invocations = self.get_run_cache(TEST_INVOCATIONS)
+        if tests_invocations is None:
+            tests_invocations = self.get_invocations(
+                days_back=days_back, invocations_per_test=invocations_per_test
+            )
+
+        test_runs = defaultdict(list)
+        for test_metadata in test_results_metadata:
+            test_metadata_dict = dict(test_metadata)
+            test_sub_type_unique_id = self.get_test_sub_type_unique_id(
+                **test_metadata_dict
+            )
+            test_invocations = tests_invocations.get(test_sub_type_unique_id)
+            test_run = TestRunSchema(
+                metadata=self.get_test_info_from_test_metadata(test_metadata),
+                test_runs=test_invocations,
+            )
+            test_runs[test_metadata.model_unique_id].append(test_run)
+
+        self.set_run_cache(key=TEST_RUNS, value=test_runs)
+        return test_runs
+
+    def get_invocations(
+        self, invocations_per_test: int = 720, days_back: Optional[int] = 7
+    ) -> Dict[TestUniqueIdType, InvocationsSchema]:
+        run_operation_response = self.dbt_runner.run_operation(
+            macro_name="get_tests_invocations",
+            macro_args=dict(
+                invocations_per_test=invocations_per_test, days_back=days_back
+            ),
+        )
+        test_invocation_dicts = (
+            json.loads(run_operation_response[0]) if run_operation_response else []
+        )
+        grouped_invocations = defaultdict(list)
+        for test_invocation in test_invocation_dicts:
+            try:
+                sub_test_unique_id = self.get_test_sub_type_unique_id(**test_invocation)
+                grouped_invocations[sub_test_unique_id].append(
+                    InvocationSchema(
+                        id=test_invocation["test_execution_id"],
+                        time_utc=test_invocation["detected_at"],
+                        status=test_invocation["status"],
+                        affected_rows=self._parse_affected_row(
+                            results_description=test_invocation[
+                                "test_results_description"
+                            ]
+                        ),
+                    )
+                )
+            except Exception:
+                logger.error(
+                    f"Could not parse test ({test_invocation.get('test_unique_id')}) invocation ({test_invocation.get('test_execution_id')})- continue to the next test"
+                )
+                continue
+
+        test_invocations = dict()
+        for sub_test_unique_id, sub_test_invocations in grouped_invocations.items():
+            totals = self._get_test_invocations_totals(sub_test_invocations)
+            test_invocations[sub_test_unique_id] = InvocationsSchema(
+                fail_rate=round(totals.errors / len(sub_test_invocations), 2)
+                if sub_test_invocations
+                else 0,
+                totals=totals,
+                invocations=sub_test_invocations,
+                description=self._get_invocations_description(totals),
+            )
+        self.set_run_cache(key=TEST_INVOCATIONS, value=test_invocations)
+        return test_invocations
+
+    @staticmethod
+    def _get_test_invocations_totals(
+        invocations: List[InvocationSchema],
+    ) -> TotalsSchema:
+        totals = TotalsSchema()
+        for invocation in invocations:
+            totals.add_total(invocation.status)
+        return totals
+
+    @staticmethod
+    def _get_invocations_description(
+        invocations_totals: TotalsSchema,
+    ) -> str:
+        all_invocations_count = (
+            invocations_totals.errors
+            + invocations_totals.warnings
+            + invocations_totals.passed
+            + invocations_totals.failures
+        )
+        return f"There were {invocations_totals.failures or 'no'} failures, {invocations_totals.errors or 'no'} errors and {invocations_totals.warnings or 'no'} warnings on the last {all_invocations_count} test runs."
+
+    @staticmethod
+    def _parse_affected_row(results_description: str) -> Optional[int]:
+        affected_rows_pattern = re.compile(r"^Got\s\d+\sresult")
+        number_pattern = re.compile(r"\d+")
+        try:
+            matches_affected_rows_string = re.findall(
+                affected_rows_pattern, results_description
+            )[0]
+            affected_rows = re.findall(number_pattern, matches_affected_rows_string)[0]
+            return int(affected_rows)
+        except Exception:
+            return None
+
+    def get_total_tests_results(
+        self,
+        tests_info: Optional[List[TestInfoSchema]] = None,
+    ) -> Dict[str, TotalsSchema]:
+        totals = dict()
+        for test in tests_info:
+            self._update_test_results_totals(
+                totals_dict=totals,
+                model_unique_id=test.model_unique_id,
+                status=test.latest_run_status,
+            )
+        return totals
+
+    def get_total_tests_runs(
+        self,
+        tests_info: Optional[List[TestInfoSchema]] = None,
+        tests_invocations: Optional[Dict[TestUniqueIdType, InvocationsSchema]] = None,
+    ) -> Dict[str, TotalsSchema]:
+        totals = dict()
+        for test in tests_info:
+            test_sub_type_unique_id = self.get_test_sub_type_unique_id(**dict(test))
+            test_invocations = tests_invocations[test_sub_type_unique_id].invocations
+            self._update_test_runs_totals(
+                totals_dict=totals, test=test, test_invocations=test_invocations
+            )
+        return totals
+
+    @staticmethod
+    def _update_test_runs_totals(
+        totals_dict: Dict[str, TotalsSchema],
+        test: TestMetadataSchema,
+        test_invocations: List[InvocationSchema],
+    ):
+        model_unique_id = test.model_unique_id
+
+        if model_unique_id not in totals_dict:
+            totals_dict[model_unique_id] = TotalsSchema()
+
+        for test_invocation in test_invocations:
+            totals_dict[model_unique_id].add_total(test_invocation.status)
+
+    @staticmethod
+    def _update_test_results_totals(
+        totals_dict: Dict[str, TotalsSchema], model_unique_id: str, status: str
+    ):
+        if model_unique_id not in totals_dict:
+            totals_dict[model_unique_id] = TotalsSchema()
+
+        totals_dict[model_unique_id].add_total(status)
